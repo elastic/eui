5739de223282bdd2530d7c6d92820a02bfe3b117
Nathan Reese <reese.nathan@gmail.com>
2017-12-04 13:16:03 -0700

    add space to demos

--- src-docs/src/views/tooltip/examples.js
+++ src-docs/src/views/tooltip/examples.js
@@ -12,44 +12,50 @@ then what is the point?`;
 export default () => (
   <div>
     <div>
-      Check out this
-      <TooltipTrigger tooltip="I am the body" title="I am the title">
-        <span className="overlay-trigger" tabIndex="0"> tooltip with title.</span>
-      </TooltipTrigger>
+      Check out this {(
+        <TooltipTrigger tooltip="I am the body" title="I am the title">
+          <span className="overlay-trigger" tabIndex="0"> tooltip with title.</span>
+        </TooltipTrigger>
+      )}
     </div>
     <div>
-      Check out this
-      <TooltipTrigger
-        tooltip={autoPlacementTooltip} placement="top" size="s">
-        <span className="overlay-trigger" tabIndex="0"> tooltip on the top (overflow).</span>
-      </TooltipTrigger>
+      Check out this {(
+        <TooltipTrigger
+          tooltip={autoPlacementTooltip} placement="top" size="s">
+          <span className="overlay-trigger" tabIndex="0"> tooltip on the top (overflow).</span>
+        </TooltipTrigger>
+      )}
     </div>
     <div>
-      Check out this
-      <TooltipTrigger tooltip="I should be on the left" placement="left">
-        <span className="overlay-trigger" tabIndex="0"> tooltip on the left.</span>
-      </TooltipTrigger>
-    </div>
-
-    <div>
-      Check out this
-      <TooltipTrigger tooltip="I should be on the right" placement="right">
-        <span className="overlay-trigger" tabIndex="0"> tooltip on the right.</span>
-      </TooltipTrigger>
+      Check out this {(
+        <TooltipTrigger tooltip="I should be on the left" placement="left">
+          <span className="overlay-trigger" tabIndex="0"> tooltip on the left.</span>
+        </TooltipTrigger>
+      )}
     </div>
 
     <div>
-      Check out this
-      <TooltipTrigger tooltip="I should be on the top" placement="top">
-        <span className="overlay-trigger" tabIndex="0"> tooltip on the top.</span>
-      </TooltipTrigger>
+      Check out this {(
+        <TooltipTrigger tooltip="I should be on the right" placement="right">
+          <span className="overlay-trigger" tabIndex="0"> tooltip on the right.</span>
+        </TooltipTrigger>
+      )}
     </div>
 
     <div>
-      Check out this
-      <TooltipTrigger tooltip="I should be on the bottom" placement="bottom">
-        <span className="overlay-trigger" tabIndex="0"> tooltip on the bottom.</span>
-      </TooltipTrigger>
+      Check out this {(
+        <TooltipTrigger tooltip="I should be on the top" placement="top">
+          <span className="overlay-trigger" tabIndex="0"> tooltip on the top.</span>
+        </TooltipTrigger>
+      )}
+    </div>
+
+    <div>
+      Check out this {(
+        <TooltipTrigger tooltip="I should be on the bottom" placement="bottom">
+          <span className="overlay-trigger" tabIndex="0"> tooltip on the bottom.</span>
+        </TooltipTrigger>
+      )}
     </div>
   </div>
 );
--- src-docs/src/views/tooltip/tooltip_example.js
+++ src-docs/src/views/tooltip/tooltip_example.js
@@ -19,34 +19,36 @@ import TooltipExamples from './examples';
 const examplesSource = require('!!raw-loader!./examples');
 const examplesHtml = renderToHtml(TooltipExamples);
 
-export default props => (
-  <GuidePage title={props.route.name}>
-    <EuiCallOut
-      title="Work in progress"
-      color="warning"
-    >
+export const TooltipExample = {
+  title: 'Tooltip',
+  intro: (
+    <div>
+      <EuiCallOut
+        title="Work in progress"
+        color="warning"
+      >
+        <p>
+          This component is still undergoing active development, and its interface and implementation
+          are both subject to change.
+        </p>
+      </EuiCallOut>
+
+      <EuiSpacer size="l" />
+    </div>
+  ),
+  sections: [{
+    title: 'Tooltip',
+    source: [{
+      type: GuideSectionTypes.JS,
+      code: '',
+    }, {
+      type: GuideSectionTypes.HTML,
+      code: '',
+    }],
+    text: (
       <p>
-        This component is still undergoing active development, and its interface and implementation
-        are both subject to change.
       </p>
-    </EuiCallOut>
-
-    <EuiSpacer size="l" />
-
-    <GuideSection
-      title="Tooltip"
-      source={[{
-        type: GuideSectionTypes.JS,
-        code: examplesSource,
-      }, {
-        type: GuideSectionTypes.HTML,
-        code: examplesHtml,
-      }]}
-      demo={
-        <div>
-          <TooltipExamples />
-        </div>
-      }
-    />
-  </GuidePage>
-);
+    ),
+    demo: <TooltipExamples />,
+  }],
+};
