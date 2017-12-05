2c919b625982139874d880496a344511ba8a0238
CJ Cenizal <cj@cenizal.com>
2017-12-04 20:10:13 -0800

    Add test coverage for EuiButtonIcon.

--- src/components/button/button_icon/button_icon.test.js
+++ src/components/button/button_icon/button_icon.test.js
@@ -2,15 +2,52 @@ import React from 'react';
 import { render } from 'enzyme';
 import { requiredProps } from '../../../test/required_props';
 
-import { EuiButtonIcon } from './button_icon';
+import { EuiButtonIcon, COLORS } from './button_icon';
 
 describe('EuiButtonIcon', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiButtonIcon aria-label="ariaLabel" {...requiredProps} />
+      <EuiButtonIcon {...requiredProps} />
     );
 
     expect(component)
       .toMatchSnapshot();
+  });
+
+  describe('props', () => {
+    describe('isDisabled', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiButtonIcon aria-label="button" isDisabled />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('iconType', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiButtonIcon aria-label="button" iconType="user" />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('color', () => {
+      COLORS.forEach(color => {
+        test(`${color} is rendered`, () => {
+          const component = render(
+            <EuiButtonIcon aria-label="button" color={color} />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
   });
 });
