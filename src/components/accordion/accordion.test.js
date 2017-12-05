d0e6cbd5a0164e8c521b428be6832b7541148c35
CJ Cenizal <cj@cenizal.com>
2017-12-04 17:21:14 -0800

    Add test coverage for EuiAccordion. (#183)

--- src/components/accordion/accordion.test.js
+++ src/components/accordion/accordion.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 import { requiredProps } from '../../test/required_props';
 
 import { EuiAccordion } from './accordion';
@@ -18,5 +18,78 @@ describe('EuiAccordion', () => {
 
     expect(component)
       .toMatchSnapshot();
+  });
+
+  describe('props', () => {
+    describe('buttonContentClassName', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiAccordion
+            id={getId()}
+            buttonContentClassName="button content class name"
+          />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('buttonContent', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiAccordion
+            id={getId()}
+            buttonContent={<div>Button content</div>}
+          />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('extraAction', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiAccordion
+            id={getId()}
+            extraAction={<button>Extra action</button>}
+          />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('behavior', () => {
+    it('opens when clicked once', () => {
+      const component = mount(
+        <EuiAccordion
+          id={getId()}
+        />
+      );
+
+      component.find('button').simulate('click');
+
+      expect(component)
+        .toMatchSnapshot();
+    });
+
+    it('closes when clicked twice', () => {
+      const component = mount(
+        <EuiAccordion
+          id={getId()}
+        />
+      );
+
+      component.find('button').simulate('click');
+      component.find('button').simulate('click');
+
+      expect(component)
+        .toMatchSnapshot();
+    });
   });
 });
