ce690eb9a5a973e96b879d03d8e3d1f627f7d4bf
CJ Cenizal <cj@cenizal.com>
2017-12-05 11:32:37 -0800

    Add test coverage for EuiDescriptionList.

--- src/components/description_list/description_list.test.js
+++ src/components/description_list/description_list.test.js
@@ -2,15 +2,84 @@ import React from 'react';
 import { render } from 'enzyme';
 import { requiredProps } from '../../test/required_props';
 
-import { EuiDescriptionList } from './description_list';
+import { EuiDescriptionList, TYPES, ALIGNMENTS } from './description_list';
 
 describe('EuiDescriptionList', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiDescriptionList {...requiredProps} />
+      <EuiDescriptionList {...requiredProps}>
+        Content
+      </EuiDescriptionList>
     );
 
     expect(component)
       .toMatchSnapshot();
+  });
+
+  describe('props', () => {
+    describe('listItems', () => {
+      test('is rendered', () => {
+        const listItems = [
+          {
+            title: 'Title 1',
+            description: 'Description 1',
+          },
+          {
+            title: 'Title 2',
+            description: 'Description 2',
+          },
+          {
+            title: 'Title 3',
+            description: 'Description 3',
+          },
+        ];
+
+        const component = render(
+          <EuiDescriptionList listItems={listItems}>
+            listItems will render instead of this content
+          </EuiDescriptionList>
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('compressed', () => {
+      test('is rendered', () => {
+        const component = render(
+          <EuiDescriptionList compressed />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('type', () => {
+      TYPES.forEach(type => {
+        test(`${type} is rendered`, () => {
+          const component = render(
+            <EuiDescriptionList type={type} />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('align', () => {
+      ALIGNMENTS.forEach(alignment => {
+        test(`${alignment} is rendered`, () => {
+          const component = render(
+            <EuiDescriptionList align={alignment} />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
   });
 });
