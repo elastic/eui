7077992bda81be2233b1cd04fe2e332022cc4df7
Chris Roberson <chrisronline@gmail.com>
2017-12-07 14:35:38 -0500

    Field search tests

--- src/components/form/field_search/field_search.test.js
+++ src/components/form/field_search/field_search.test.js
@@ -4,13 +4,70 @@ import { requiredProps } from '../../../test/required_props';
 
 import { EuiFieldSearch } from './field_search';
 
+jest.mock('../form_control_layout', () => ({ EuiFormControlLayout: 'eui-form-control-layout' }));
+jest.mock('../validatable_control', () => ({ EuiValidatableControl: 'eui-validatable-control' }));
+
 describe('EuiFieldSearch', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiFieldSearch {...requiredProps} />
+      <EuiFieldSearch
+        name="elastic"
+        id="1"
+        placeholder="Placeholder"
+        value="1"
+        onChange={() => {}}
+        {...requiredProps}
+      />
     );
 
     expect(component)
       .toMatchSnapshot();
+  });
+
+  describe('props', () => {
+    describe('isInvalid', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldSearch
+              isInvalid={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('fullWidth', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldSearch
+              fullWidth={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('isLoading', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldSearch
+              isLoading={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
   });
 });
