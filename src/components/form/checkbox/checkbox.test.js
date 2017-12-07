e147736c6c11a20ba0607bb8cbb4a61022ddbe93
Chris Roberson <chrisronline@gmail.com>
2017-12-07 09:54:42 -0500

    Checkbox tests

--- src/components/form/checkbox/checkbox.test.js
+++ src/components/form/checkbox/checkbox.test.js
@@ -1,16 +1,122 @@
 import React from 'react';
 import { render } from 'enzyme';
-import { requiredProps } from '../../../test/required_props';
+import {
+  requiredProps,
+  startThrowingReactWarnings,
+  stopThrowingReactWarnings,
+} from '../../../test';
 
-import { EuiCheckbox } from './checkbox';
+import {
+  EuiCheckbox,
+  TYPES,
+} from './checkbox';
+
+beforeAll(startThrowingReactWarnings);
+afterAll(stopThrowingReactWarnings);
+
+const checkboxRequiredProps = {
+  id: 'id',
+  onChange: () => {},
+};
 
 describe('EuiCheckbox', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiCheckbox id="id" onChange={() => {}} {...requiredProps} />
+      <EuiCheckbox
+        id="id"
+        onChange={() => {}}
+        {...requiredProps}
+      />
     );
 
     expect(component)
       .toMatchSnapshot();
+  });
+
+  describe('props', () => {
+    test('id is required', () => {
+      expect(() => render(
+        <EuiCheckbox checked={true} onChange={() => {}}/>
+      )).toThrow();
+    });
+
+    test('onChange is required', () => {
+      expect(() => render(
+        <EuiCheckbox id="id" checked={true}/>
+      )).toThrow();
+    });
+
+    describe('checked', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiCheckbox
+              {...checkboxRequiredProps}
+              checked={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('label', () => {
+      [(<span>Label</span>)].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiCheckbox
+              {...checkboxRequiredProps}
+              label={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('type', () => {
+      TYPES.forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiCheckbox
+              {...checkboxRequiredProps}
+              type={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('disabled', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiCheckbox
+              {...checkboxRequiredProps}
+              disabled={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+    /*
+    id: PropTypes.string.isRequired,
+    checked: PropTypes.bool.isRequired,
+    label: PropTypes.node,
+    onChange: PropTypes.func.isRequired,
+    type: PropTypes.oneOf(TYPES),
+    disabled: PropTypes.bool,
+    */
+
   });
 });
