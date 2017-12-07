bf67fa5e81559c39dffc5a1fb83a2eb5d69766d7
Chris Roberson <chrisronline@gmail.com>
2017-12-07 14:28:45 -0500

    Field number tests

--- src/components/form/field_number/field_number.test.js
+++ src/components/form/field_number/field_number.test.js
@@ -4,13 +4,73 @@ import { requiredProps } from '../../../test/required_props';
 
 import { EuiFieldNumber } from './field_number';
 
+jest.mock('../form_control_layout', () => ({ EuiFormControlLayout: 'eui-form-control-layout' }));
+jest.mock('../validatable_control', () => ({ EuiValidatableControl: 'eui-validatable-control' }));
+
 describe('EuiFieldNumber', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiFieldNumber {...requiredProps} />
+      <EuiFieldNumber
+        id="1"
+        name="elastic"
+        min={1}
+        max={8}
+        step={1}
+        value={1}
+        icon="alert"
+        onChange={() => {}}
+        {...requiredProps}
+      />
     );
 
     expect(component)
       .toMatchSnapshot();
+  });
+
+  describe('props', () => {
+    describe('isInvalid', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldNumber
+              isInvalid={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('fullWidth', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldNumber
+              fullWidth={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('isLoading', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldNumber
+              isLoading={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
   });
 });
