4beeacc8e9fef34278e1345f6d271e696e766195
Chris Roberson <chrisronline@gmail.com>
2017-12-07 14:39:30 -0500

    Field test tests

--- src/components/form/field_text/field_text.test.js
+++ src/components/form/field_text/field_text.test.js
@@ -4,13 +4,73 @@ import { requiredProps } from '../../../test/required_props';
 
 import { EuiFieldText } from './field_text';
 
+jest.mock('../form_control_layout', () => ({ EuiFormControlLayout: 'eui-form-control-layout' }));
+jest.mock('../validatable_control', () => ({ EuiValidatableControl: 'eui-validatable-control' }));
+
 describe('EuiFieldText', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiFieldText {...requiredProps} />
+      <EuiFieldText
+        name="elastic"
+        id="1"
+        placeholder="Placeholder"
+        value="1"
+        icon="foo"
+        inputRef={() => {}}
+        onChange={() => {}}
+        {...requiredProps}
+      />
     );
 
     expect(component)
       .toMatchSnapshot();
   });
+
+  describe('props', () => {
+    describe('isInvalid', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldText
+              isInvalid={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('fullWidth', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldText
+              fullWidth={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('isLoading', () => {
+      [true, false].forEach(value => {
+        test(`${value} is rendered`, () => {
+          const component = render(
+            <EuiFieldText
+              isLoading={value}
+            />
+          );
+
+          expect(component)
+            .toMatchSnapshot();
+        });
+      });
+    });
+  });
 });
+
