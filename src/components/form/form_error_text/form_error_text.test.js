722387547f804b94ccd0b4ae8ffabec72041a314
Chris Roberson <chrisronline@gmail.com>
2017-12-14 10:18:12 -0500

    Form error text tests

--- src/components/form/form_error_text/form_error_text.test.js
+++ src/components/form/form_error_text/form_error_text.test.js
@@ -7,7 +7,9 @@ import { EuiFormErrorText } from './form_error_text';
 describe('EuiFormErrorText', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiFormErrorText {...requiredProps} />
+      <EuiFormErrorText {...requiredProps}>
+        <input/>
+      </EuiFormErrorText>
     );
 
     expect(component)
