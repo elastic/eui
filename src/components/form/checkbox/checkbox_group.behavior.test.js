0efd09934c42ce4f5bfd05b8c128d337e36b7856
Chris Roberson <chrisronline@gmail.com>
2017-12-14 13:43:26 -0500

    Form tests - Phase 1 (#196)
    * Checkbox tests
    
    * Checkbox group tests
    
    * Add comments for mock vs unmock
    
    * Use a delimiter
    
    * Field number tests
    
    * Field password tests
    
    * Remove comment
    
    * Field search tests
    
    * Field test tests
    
    * Simplify some tests
    
    * PR feedback

---
+++ src/components/form/checkbox/checkbox_group.behavior.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
+
+import { EuiCheckboxGroup } from './checkbox_group';
+
+// This exists because we need to run the following tests
+// without mocking the Checkbox component, such as testing
+// an interaction that is handled by the Checkbox component.
+describe('EuiCheckboxGroup behavior', () => {
+  test('id is bound to onChange', () => {
+    const onChangeHandler = sinon.stub();
+    const component = mount(
+      <EuiCheckboxGroup
+        options={[
+          { id: '1', label: 'kibana', disabled: false },
+        ]}
+        idToSelectedMap={{
+          '1': true,
+        }}
+        onChange={onChangeHandler}
+      />
+    );
+
+    component.find('input[type="checkbox"]').simulate('change');
+    sinon.assert.calledWith(onChangeHandler, '1');
+  });
+});
