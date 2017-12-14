ab1ed5d4d983acdb6d20c49ec2e996d49e5c38a7
Chris Roberson <chrisronline@gmail.com>
2017-12-14 14:57:37 -0500

    Form tests - Phase 2 (#207)
    * Form control layout tests
    
    * Form error text tests
    
    * Form help text tests
    
    * Update tests to use text children

--- src/components/form/form_help_text/form_help_text.test.js
+++ src/components/form/form_help_text/form_help_text.test.js
@@ -7,7 +7,9 @@ import { EuiFormHelpText } from './form_help_text';
 describe('EuiFormHelpText', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiFormHelpText {...requiredProps} />
+      <EuiFormHelpText {...requiredProps}>
+        This is help text.
+      </EuiFormHelpText>
     );
 
     expect(component)
