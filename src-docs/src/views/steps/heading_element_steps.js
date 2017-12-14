d0f8bb77d4823234353ecd88974b6d032c3bad33
chorn <caroline@curiouschaos.com>
2017-12-14 17:44:25 -0500

    Spelling and component usage fixes

--- src-docs/src/views/steps/heading_element_steps.js
+++ src-docs/src/views/steps/heading_element_steps.js
@@ -2,21 +2,22 @@ import React from 'react';
 
 import {
   EuiSteps,
-  EuiText
+  EuiTitle,
+  EuiSpacer
 } from '../../../../src/components';
 
 const steps = [
   {
     title: 'Inspect me',
-    children: <EuiText><h3>Did you notice the step title is inside a Heading 2 element?</h3></EuiText>
+    children: <EuiTitle size="s"><h3>Did you notice the step title is inside a Heading 2 element?</h3></EuiTitle>
   }
 ];
 
 export default () => (
   <div>
-    <EuiText><h1>Heading 1</h1></EuiText>
+    <EuiTitle size="l"><h1>Heading 1</h1></EuiTitle>
 
-    <br/><br/>
+    <EuiSpacer size="xl" />
 
     <EuiSteps
       steps={steps}
