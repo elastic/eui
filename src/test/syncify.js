db37d288a9444ccd756bbce862cce71cbe9affc4
Chris Roberson <chrisronline@gmail.com>
2018-01-12 14:23:01 -0500

    Add comment

--- src/test/syncify.js
+++ src/test/syncify.js
@@ -1,4 +1,7 @@
 // Helper designed to help test async/await functions that throw exceptions
+// Example usage:
+// const fn = await syncify(() => asyncFn());
+// expect(fn).toThrow();
 // https://github.com/facebook/jest/issues/1377
 export const syncify = async (fn) => {
   try {
