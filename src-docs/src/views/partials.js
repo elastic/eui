502a4d0620feee0c819f49a7d7721c3943635056
Dave Snider <dave.snider@gmail.com>
2018-02-11 18:27:08 -0800

    fix partial reference

--- src-docs/src/views/partials.js
+++ src-docs/src/views/partials.js
@@ -1,7 +1,7 @@
 export { default as KibanaChrome } from './kibana/kibana_chrome';
 export { default as KibanaHeader } from './header/header';
 export { default as ManagementSideNav } from './side_nav/side_nav_complex';
-export { default as Table } from './table/table';
+export { default as Table } from './tables/custom/custom';
 export {
   default as ToastList,
   addToast,
