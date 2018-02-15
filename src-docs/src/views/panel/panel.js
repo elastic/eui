efe2fde3a7446a050cdc34056ea8b3922b5437df
cchaos <caroline.horn@elastic.co>
2018-02-15 14:18:12 -0500

    Fixing panel doc

--- src-docs/src/views/panel/panel.js
+++ src-docs/src/views/panel/panel.js
@@ -9,31 +9,31 @@ import {
 export default () => (
   <div>
     <EuiPanel paddingSize="none">
-      <EuiCode>sizePadding=&quot;none&quot;</EuiCode>
+      <EuiCode>paddingSize=&quot;none&quot;</EuiCode>
     </EuiPanel>
 
     <EuiSpacer size="l"/>
 
     <EuiPanel paddingSize="s">
-      <EuiCode>sizePadding=&quot;s&quot;</EuiCode>
+      <EuiCode>paddingSize=&quot;s&quot;</EuiCode>
     </EuiPanel>
 
     <EuiSpacer size="l"/>
 
     <EuiPanel paddingSize="m">
-      <EuiCode>sizePadding=&quot;m&quot;</EuiCode>
+      <EuiCode>paddingSize=&quot;m&quot;</EuiCode>
     </EuiPanel>
 
     <EuiSpacer size="l"/>
 
     <EuiPanel paddingSize="l">
-      <EuiCode>sizePadding=&quot;l&quot;</EuiCode>
+      <EuiCode>paddingSize=&quot;l&quot;</EuiCode>
     </EuiPanel>
 
     <EuiSpacer size="l"/>
 
     <EuiPanel paddingSize="l" hasShadow>
-      <EuiCode>sizePadding=&quot;l&quot;</EuiCode>, <EuiCode>hasShadow</EuiCode>
+      <EuiCode>paddingSize=&quot;l&quot;</EuiCode>, <EuiCode>hasShadow</EuiCode>
     </EuiPanel>
   </div>
 );
