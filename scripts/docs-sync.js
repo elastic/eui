4fca2f80b215176d9e0b41a93db7d7b7e3f90e99
CJ Cenizal <cj@cenizal.com>
2018-03-09 12:31:06 -0800

    Updated documentation.

--- scripts/docs-sync.js
+++ scripts/docs-sync.js
@@ -1,5 +1,6 @@
 const { execSync } = require('child_process');
 
+execSync('npm run build');
 execSync('npm run build-docs');
 execSync('git add docs');
 execSync('git commit -am "Updated documentation." || echo "No documentation changes."');
