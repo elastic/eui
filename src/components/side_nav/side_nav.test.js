1f98358f1dff7c9ce3de28d7410d95971d2fe12b
arkwright <1445684+arkwright@users.noreply.github.com>
2018-03-14 13:21:20 -0400

    Add ability to force open EuiSideNav items.

--- src/components/side_nav/side_nav.test.js
+++ src/components/side_nav/side_nav.test.js
@@ -14,135 +14,167 @@ describe('EuiSideNav', () => {
       .toMatchSnapshot();
   });
 
-  describe('isOpenOnMobile', () => {
-    test('defaults to false', () => {
-      const component = render(
-        <EuiSideNav />
-      );
+  describe('props', () => {
+    describe('isOpenOnMobile', () => {
+      test('defaults to false', () => {
+        const component = render(
+          <EuiSideNav />
+        );
 
-      expect(component)
-        .toMatchSnapshot();
+        expect(component)
+          .toMatchSnapshot();
+      });
+
+      test('is rendered when specified as true', () => {
+        const component = render(
+          <EuiSideNav isOpenOnMobile />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
     });
 
-    test('is rendered when specified as true', () => {
-      const component = render(
-        <EuiSideNav isOpenOnMobile />
-      );
-
-      expect(component)
-        .toMatchSnapshot();
-    });
-  });
-
-  describe('items', () => {
-    test('is rendered', () => {
-      const sideNav = [{
-        name: 'A',
-        id: 0,
-        items: [{
-          name: 'B',
-          id: 1,
-        }, {
-          name: 'C',
-          id: 2,
+    describe('items', () => {
+      test('is rendered', () => {
+        const sideNav = [{
+          name: 'A',
+          id: 0,
           items: [{
-            name: 'D',
-            id: 3,
+            name: 'B',
+            id: 1,
           }, {
-            name: 'E',
-            id: 4,
+            name: 'C',
+            id: 2,
+            items: [{
+              name: 'D',
+              id: 3,
+            }, {
+              name: 'E',
+              id: 4,
+            }],
           }],
-        }],
-      }];
+        }];
 
-      const component = render(
-        <EuiSideNav items={sideNav} />
-      );
+        const component = render(
+          <EuiSideNav items={sideNav} />
+        );
 
-      expect(component)
-        .toMatchSnapshot();
-    });
+        expect(component)
+          .toMatchSnapshot();
+      });
 
-    test('renders items which are links', () => {
-      const sideNav = [{
-        name: 'A',
-        id: 0,
-        href: 'http://www.elastic.co',
-        items: [{
-          name: 'B',
-          id: 1,
-        }, {
-          name: 'C',
-          id: 2,
+      test('renders items which are links', () => {
+        const sideNav = [{
+          name: 'A',
+          id: 0,
+          href: 'http://www.elastic.co',
           items: [{
-            name: 'D',
-            id: 3,
+            name: 'B',
+            id: 1,
           }, {
-            name: 'E',
-            id: 4,
+            name: 'C',
+            id: 2,
+            items: [{
+              name: 'D',
+              id: 3,
+            }, {
+              name: 'E',
+              id: 4,
+            }],
           }],
-        }],
-      }];
+        }];
 
-      const component = render(
-        <EuiSideNav items={sideNav} />
-      );
+        const component = render(
+          <EuiSideNav items={sideNav} />
+        );
 
-      expect(component)
-        .toMatchSnapshot();
-    });
+        expect(component)
+          .toMatchSnapshot();
+      });
 
-    test('renders items using a specified callback', () => {
-      const sideNav = [{
-        name: 'A',
-        id: 0,
-        href: 'http://www.elastic.co',
-        items: [{
-          name: 'B',
-          id: 1,
-        }],
-      }];
-
-      const renderItem = ({ href, className, children }) => (
-        <a data-test-id="my-custom-element" href={href} className={className}>
-          {children}
-        </a>);
-
-      const component = render(
-        <EuiSideNav items={sideNav} renderItem={renderItem} />
-      );
-
-      expect(component)
-        .toMatchSnapshot();
-    });
-
-    test('renders selected item and automatically opens parent items', () => {
-      const sideNav = [{
-        name: 'A',
-        id: 0,
-        items: [{
-          name: 'B',
-          id: 1,
-        }, {
-          name: 'C',
-          id: 2,
+      test('renders items using a specified callback', () => {
+        const sideNav = [{
+          name: 'A',
+          id: 0,
+          href: 'http://www.elastic.co',
           items: [{
-            name: 'D',
-            id: 3,
-            isSelected: true,
-          }, {
-            name: 'E',
-            id: 4,
+            name: 'B',
+            id: 1,
           }],
-        }],
-      }];
+        }];
 
-      const component = render(
-        <EuiSideNav items={sideNav} />
-      );
+        const renderItem = ({ href, className, children }) => (
+          <a data-test-id="my-custom-element" href={href} className={className}>
+            {children}
+          </a>);
 
-      expect(component)
-        .toMatchSnapshot();
+        const component = render(
+          <EuiSideNav items={sideNav} renderItem={renderItem} />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+
+      test('renders selected item and automatically opens parent items', () => {
+        const sideNav = [{
+          name: 'A',
+          id: 0,
+          items: [{
+            name: 'B',
+            id: 1,
+          }, {
+            name: 'C',
+            id: 2,
+            items: [{
+              name: 'D',
+              id: 3,
+              isSelected: true,
+            }, {
+              name: 'E',
+              id: 4,
+            }],
+          }],
+        }];
+
+        const component = render(
+          <EuiSideNav items={sideNav} />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+
+      test('renders items having { forceOpen: true } in open state, and automatically opens parent items', () => {
+        const sideNav = [{
+          name: 'A',
+          id: 0,
+          items: [{
+            name: 'B',
+            id: 1,
+          }, {
+            name: 'C',
+            id: 2,
+            items: [{
+              name: 'D',
+              id: 3,
+              forceOpen: true,
+              items: [{
+                name: 'E',
+                id: 4,
+              }],
+            }],
+          }],
+        }];
+
+        const component = render(
+          <EuiSideNav items={sideNav} />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
     });
   });
 });
