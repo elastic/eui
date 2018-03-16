4a65779c2fc3e950aa5836861a6b8ae9a9b8df09
Dave Snider <dave.snider@gmail.com>
2018-03-15 22:40:16 -0700

    refactor color picker to be accessibile and match our styles

--- src/components/color_picker/color_picker.js
+++ src/components/color_picker/color_picker.js
@@ -3,17 +3,26 @@ import React, {
 } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { ChromePicker } from 'react-color';
+import { rgbToHex } from '../../../src/services'
+
+import makeId from '../form/form_row/make_id';
+
+import lightColors from '!!sass-vars-to-js-loader!../../../src/global_styling/variables/_colors.scss'
+
+import { SketchPicker } from 'react-color';
 
 import { EuiOutsideClickDetector } from '../outside_click_detector';
 
 import { EuiColorPickerSwatch } from './color_picker_swatch';
+
+import { EuiPopover } from '../popover';
 
 export class EuiColorPicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showColorSelector: false,
+      id: this.props.id || makeId(),
     };
   }
 
@@ -31,44 +40,76 @@ export class EuiColorPicker extends Component {
 
   getColorLabel() {
     const { color } = this.props;
-    const colorValue = color === null ? '(transparent)' : color;
+    const colorValue = color === null ? '(transparent)' : color.toUpperCase();
     return (
-      <div
+      <span
         className="euiColorPicker__label"
         aria-label={`Color selection is ${ colorValue }`}
       >
         { colorValue }
-      </div>
+      </span>
     );
   }
 
   render() {
     const { color, className, showColorLabel } = this.props;
     const classes = classNames('euiColorPicker', className);
+
+    const allowedColors = [
+      'euiColorVis0',
+      'euiColorVis1',
+      'euiColorVis2',
+      'euiColorVis3',
+      'euiColorVis4',
+      'euiColorVis5',
+      'euiColorVis6',
+      'euiColorVis7',
+      'euiColorVis8',
+      'euiColorVis9',
+    ]
+
+    let colorSwatches = [];
+
+    allowedColors.map(function(color, index) {
+      colorSwatches.push(rgbToHex(lightColors[color].rgba))
+    });
+
+    const button = (
+      <button
+        className="euiColorPicker__preview euiFieldText"
+        onClick={this.toggleColorSelector}
+      >
+        <EuiColorPickerSwatch color={color} aria-label={this.props['aria-label']} />
+        { showColorLabel ? this.getColorLabel() : null }
+      </button>
+    );
+
     return (
       <EuiOutsideClickDetector onOutsideClick={this.closeColorSelector}>
         <div
           className={classes}
           data-test-subj={this.props['data-test-subj']}
         >
-          <div
-            className="euiColorPicker__preview"
-            onClick={this.toggleColorSelector}
+          <EuiPopover
+            id={this.state.id}
+            data-test-subj="colorPickerPopup"
+            button={button}
+            isOpen={this.state.showColorSelector}
+            closePopover={this.closeColorSelector}
+            anchorPosition="downLeft"
+            panelPaddingSize="none"
+            ownFocus
           >
-            <EuiColorPickerSwatch color={color} aria-label={this.props['aria-label']} />
-            { showColorLabel ? this.getColorLabel() : null }
-          </div>
-          {
-            this.state.showColorSelector ?
-              <div className="euiColorPickerPopUp" data-test-subj="colorPickerPopup">
-                <ChromePicker
-                  color={color ? color : '#ffffff'}
-                  disableAlpha={true}
-                  onChange={this.handleColorSelection}
-                />
-              </div>
-              : null
-          }
+            <SketchPicker
+              triangle="hide"
+              color={color ? color : '#ffffff'}
+              disableAlpha={true}
+              onChange={this.handleColorSelection}
+              presetColors={colorSwatches}
+              role="dialog"
+              aria-labelledby={this.state.id}
+            />
+          </EuiPopover>
         </div>
       </EuiOutsideClickDetector>
     );
