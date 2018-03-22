e3a1ac8c9daaac79ac476d28e00549e0fc97e86b
Christopher Davies <github@christophilus.com>
2018-03-22 09:26:12 -0400

    Update docs to show an example link with href and onClick

--- src-docs/src/views/link/link.js
+++ src-docs/src/views/link/link.js
@@ -25,7 +25,21 @@ export default () => (
           button
         </EuiLink>
       )} with an onClick handler.
-
+    </p>
+    <p>
+      Here is an example of a {(
+        <EuiLink
+          href="https://github.com/elastic/eui"
+          onClick={(e) => {
+            e.preventDefault();
+            if (confirm('Are you sure you want to see the eui repo?')) {
+              document.location = e.target.href;
+            }
+          }}
+        >
+          link
+        </EuiLink>
+      )} with both an href and an onClick handler.
     </p>
     <p>Links can be colored as well.</p>
     <ul>
