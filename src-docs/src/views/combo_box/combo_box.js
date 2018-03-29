28b6ef73adfae541f55adc5d56feb58aa87d9005
Dave Snider <dave.snider@gmail.com>
2018-03-28 21:33:31 -0700

    some docs

--- src-docs/src/views/combo_box/combo_box.js
+++ src-docs/src/views/combo_box/combo_box.js
@@ -18,7 +18,7 @@ export default class extends Component {
     }, {
       value: 'mimas',
       label: 'Mimas',
-      color: 'danger',
+      color: '#DB1374',
     }, {
       value: 'dione',
       label: 'Dione',
