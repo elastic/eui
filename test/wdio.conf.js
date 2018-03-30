2ab996c6365c3f7122ffea4fc0a36fa9b987c664
John Dorlus <jsdorlus@elastic.co>
2018-03-30 14:59:21 -0400

    Added new baseline images for chrome and firefox.

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -66,7 +66,7 @@ exports.config = {
     // maxInstances can get overwritten per capability. So if you have an in house Selenium
     // grid with only 5 firefox instance available you can make sure that not more than
     // 5 instance gets started at a time.
-    maxInstances: 2,
+    maxInstances: 1,
     browserName: 'firefox',
     'moz:firefoxOptions': {
       // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
