e39a031f59e8cbe2ac2f305ff3816b4db33c7107
Dave Snider <dave.snider@gmail.com>
2018-05-11 11:17:45 -0700

    fixes #621 adds menubar roles to side nav

--- src-docs/src/components/guide_page/guide_page_chrome.js
+++ src-docs/src/components/guide_page/guide_page_chrome.js
@@ -93,11 +93,15 @@ export class GuidePageChrome extends Component {
   }
 
   renderSubSections = (subSections = []) => {
-    if (subSections.length <= 1) {
+    console.log(subSections);
+
+    const subSectionsWithTitles = subSections.filter(item => (item.title));
+
+    if (subSectionsWithTitles.length <= 1) {
       return;
     }
 
-    return subSections.map(({ title, id }) => ({
+    return subSectionsWithTitles.map(({ title, id }) => ({
       id: `subSection-${id}`,
       name: title,
       onClick: this.onClickLink.bind(this, id),
