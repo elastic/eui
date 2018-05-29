6202d7bf54fb295f763ed22e5ab876ce7877de29
cchaos <caroline.horn@elastic.co>
2018-05-29 15:06:23 -0400

    Changing one example to show without an icon

--- src-docs/src/views/card/card_layout.js
+++ src-docs/src/views/card/card_layout.js
@@ -7,25 +7,33 @@ import {
   EuiFlexItem,
 } from '../../../../src/components';
 
-const icons = ['Beats', 'Cloud', 'Xpack'];
-
-const cardNodes = icons.map(function (item, index) {
-  return (
-    <EuiFlexItem key={index}>
-      <EuiCard
-        layout="horizontal"
-        icon={<EuiIcon size="xxl" type={`logo${item}`} />}
-        title={`Elastic ${item}`}
-        description="Example of a card's description. Stick to one or two sentences."
-        onClick={() => window.alert('Card clicked')}
-        textAlign="right"
-      />
-    </EuiFlexItem>
-  );
-});
-
 export default () => (
   <EuiFlexGroup gutterSize="l">
-    {cardNodes}
+    <EuiFlexItem>
+      <EuiCard
+        layout="horizontal"
+        icon={<EuiIcon size="xxl" type={`logoBeats`} />}
+        title={`Elastic Beats`}
+        description="Example of a card's description. Stick to one or two sentences."
+        onClick={() => window.alert('Card clicked')}
+      />
+    </EuiFlexItem>
+    <EuiFlexItem>
+      <EuiCard
+        layout="horizontal"
+        icon={<EuiIcon size="xxl" type={`logoCloud`} />}
+        title={`Elastic Cloud`}
+        description="Example of a card's description. Stick to one or two sentences."
+        onClick={() => window.alert('Card clicked')}
+      />
+    </EuiFlexItem>
+    <EuiFlexItem>
+      <EuiCard
+        layout="horizontal"
+        title={`No icon example`}
+        description="Example of a card's description. Stick to one or two sentences."
+        onClick={() => window.alert('Card clicked')}
+      />
+    </EuiFlexItem>
   </EuiFlexGroup>
 );
