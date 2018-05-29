8f6bb4e1506ffa83904ef49daae7354b43dfba15
Caroline Horn <549577+cchaos@users.noreply.github.com>
2018-05-29 14:38:48 -0400

    Reverse description lists (#882)

---
+++ src-docs/src/views/description_list/description_list_reverse.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  EuiDescriptionList,
+} from '../../../../src/components';
+
+const favoriteVideoGame = [
+  {
+    title: 'Name',
+    description: 'The Elder Scrolls: Morrowind',
+  },
+  {
+    title: 'Game style',
+    description: 'Open-world, fantasy, action role-playing',
+  },
+  {
+    title: 'Release date',
+    description: '2002',
+  },
+];
+
+export default () => (
+  <EuiDescriptionList textStyle="reverse" listItems={favoriteVideoGame} />
+);
