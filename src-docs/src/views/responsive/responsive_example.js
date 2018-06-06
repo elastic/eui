9af775a80a0ae07155004e06995668f69187c573
cchaos <caroline.horn@elastic.co>
2018-06-06 13:15:05 -0400

    Displaying `$breakpoints` in docs

--- src-docs/src/views/responsive/responsive_example.js
+++ src-docs/src/views/responsive/responsive_example.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { renderToHtml } from '../../services';
+import sizes from '!!sass-vars-to-js-loader!../../../../src/global_styling/mixins/_responsive.scss';
 
 import {
   GuideSectionTypes,
@@ -10,11 +11,20 @@ import {
   EuiCode,
   EuiShowFor,
   EuiHideFrom,
+  EuiCodeBlock,
 } from '../../../../src/components';
 
 import Responsive from './responsive';
 const responsiveSource = require('!!raw-loader!./responsive');
 const responsiveHtml = renderToHtml(Responsive);
+
+function renderSizes(size, index) {
+  return (
+    <span key={index}>
+      {size}: {sizes.breakpoints[size]}px &emsp;
+    </span>
+  )
+}
 
 export const ResponsiveExample = {
   title: 'Responsive',
@@ -28,11 +38,21 @@ export const ResponsiveExample = {
       code: responsiveHtml,
     }],
     text: (
-      <p>
-        Pass an array of screen widths <EuiCode>[xs, s, m, l]</EuiCode> to either
-        the <EuiCode>EuiShowFor</EuiCode> or <EuiCode>EuiHideFrom</EuiCode> components
-        to make them responsive.
-      </p>
+      <div>
+        <p>
+          Pass an array of screen widths <EuiCode>[xs, s, m, l]</EuiCode> to either
+          the <EuiCode>EuiShowFor</EuiCode> or <EuiCode>EuiHideFrom</EuiCode> components
+          to make them responsive.
+        </p>
+
+        <p><strong>The sizing correlates with our SASS variables.</strong></p>
+
+        <EuiCodeBlock language="scss" paddingSize="s">
+          {Object.keys(sizes.breakpoints).map(function (size, index) {
+            return renderSizes(size, index);
+          })}
+        </EuiCodeBlock>
+      </div>
     ),
     props: { EuiShowFor, EuiHideFrom },
     demo: <Responsive />,
