97df3cef398c6ad94b49fbe9ab5d32427af70112
Chandler <chandler.prall@gmail.com>
2018-06-15 09:05:59 -0600

    Custom column sort values for EuiInMemoryTable (#929)
    * Added custom column sort values to EuiInMemoryTable
    
    * changelog
    
    * small code style tweak

---
+++ src-docs/src/views/tables/in_memory/in_memory_custom_sorting.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  EuiInMemoryTable
+} from '../../../../../src/components';
+
+const data = [
+  { animal: 'snail', weight: 25, humanFriendlyWeight: '25g' },
+  { animal: 'peregrine falcon', weight: 900, humanFriendlyWeight: '0.9kg' },
+  { animal: 'small dog', weight: 4500, humanFriendlyWeight: '4.5kg' },
+  { animal: 'brown bear', weight: 180000, humanFriendlyWeight: '180kg' },
+  { animal: 'elephant', weight: 5440000, humanFriendlyWeight: '5440kg' },
+  { animal: 'giraffe', weight: 1180000, humanFriendlyWeight: '1180kg' }
+];
+
+export const Table = () => {
+  const columns = [{
+    field: 'animal',
+    name: 'Animal',
+    sortable: true
+  }, {
+    field: 'humanFriendlyWeight',
+    name: 'Weight',
+    sortable: ({ weight }) => weight
+  }];
+
+  const sorting = {
+    sort: {
+      field: 'humanFriendlyWeight',
+      direction: 'asc',
+    }
+  };
+
+  return (
+    <EuiInMemoryTable
+      items={data}
+      columns={columns}
+      pagination={false}
+      sorting={sorting}
+    />
+  );
+};
