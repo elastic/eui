943dbc875ced7108bcd7e9c0aed2d8e9814f261f
cchaos <caroline.horn@elastic.co>
2018-07-10 19:46:13 -0400

    Adding more responsive styles to page layout

--- src/components/page/page_header/page_header.js
+++ src/components/page/page_header/page_header.js
@@ -2,8 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export const EuiPageHeader = ({ children, className, ...rest }) => {
-  const classes = classNames('euiPageHeader', className);
+export const EuiPageHeader = ({ children, className, responsive, ...rest }) => {
+  const classes = classNames(
+    'euiPageHeader',
+    {
+      'euiPageHeader--responsive': responsive,
+    },
+    className,
+  );
 
   return (
     <div
@@ -18,4 +24,13 @@ export const EuiPageHeader = ({ children, className, ...rest }) => {
 EuiPageHeader.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+  /**
+   * Set to false if you don't want the children to stack
+   * at small screen sizes.
+   */
+  responsive: PropTypes.bool,
+};
+
+EuiPageHeader.defaultProps = {
+  responsive: true,
 };
