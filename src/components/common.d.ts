946f3e32db6041065d0e53807a5a2b0d77b2e3bf
Felix Stürmer <stuermer@weltenwort.de>
2018-07-16 14:44:30 +0200

    Make the `Omit` type compatible with TS 2.9+

--- src/components/common.d.ts
+++ src/components/common.d.ts
@@ -15,8 +15,5 @@ declare module '@elastic/eui' {
 
   // utility types:
 
-  type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-    { [P in U]: never } & { [x: string]: never })[T];
-
-  type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+  type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
 }
