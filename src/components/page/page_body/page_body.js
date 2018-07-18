f2312b5df40ff373385813201b80c2abe43fd9be
Caroline Horn <549577+cchaos@users.noreply.github.com>
2018-07-18 11:16:28 -0400

    Fixing EuiPageContent layout (#1024)

--- src/components/page/page_body/page_body.js
+++ src/components/page/page_body/page_body.js
@@ -2,12 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export const EuiPageBody = ({ children, className, ...rest }) => {
-  const classes = classNames('euiPageBody', className);
+export const EuiPageBody = ({ children, restrictWidth, style, className, ...rest }) => {
+
+  let widthClassname;
+  if (restrictWidth === true) {
+    widthClassname = 'euiPage--restrictWidth-default';
+  } else if (restrictWidth === false) {
+    widthClassname = 'euiPage--widthIsNotRestricted';
+  } else {
+    widthClassname = 'euiPage--restrictWidth-custom';
+
+    // if style has been passed as a prop, add to it
+    if (style) {
+      style.maxWidth = `${restrictWidth}px`;
+    }
+    // otherwise create a new object
+    else {
+      style = { maxWidth: `${restrictWidth}px` };
+    }
+  }
+
+  const classes = classNames('euiPageBody', widthClassname, className);
 
   return (
     <div
       className={classes}
+      style={style}
       {...rest}
     >
       {children}
@@ -18,4 +38,19 @@ export const EuiPageBody = ({ children, className, ...rest }) => {
 EuiPageBody.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
+
+  /**
+   * Sets the max-width of the page,
+   * set to `true` to use the default size,
+   * set to `false` to not restrict the width,
+   * set to a number for a custom width.
+   */
+  restrictWidth: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.number
+  ]),
+};
+
+EuiPageBody.defaultProps = {
+  restrictWidth: false,
 };
