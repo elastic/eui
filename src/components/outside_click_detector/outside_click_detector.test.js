c616baf1b2ecc9fc0d69357b76a31d084d862e62
Chandler Prall <chandler.prall@gmail.com>
2018-07-19 14:26:21 -0600

    Fix EuiOutsideClickDetector to work when its nested inside itself

--- src/components/outside_click_detector/outside_click_detector.test.js
+++ src/components/outside_click_detector/outside_click_detector.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { render, mount } from 'enzyme';
 
 import { EuiOutsideClickDetector } from './outside_click_detector';
 
@@ -13,5 +13,46 @@ describe('EuiOutsideClickDetector', () => {
 
     expect(component)
       .toMatchSnapshot();
+  });
+
+  describe('behavior', () => {
+    test('nested detectors', () => {
+      const unrelatedDetector = jest.fn();
+      const parentDetector = jest.fn();
+      const childDetector = jest.fn();
+
+      // enzyme doesn't mount the components into the global jsdom `document`
+      // but that's where the click detector listener is,
+      // pass the top-level mounted component's click event on to document
+      const triggerDocumentClick = e => {
+        const event = new Event('click');
+        event.euiGeneratedBy = e.nativeEvent.euiGeneratedBy;
+        document.dispatchEvent(event);
+      };
+
+      const component = mount(
+        <div onClick={triggerDocumentClick}>
+          <div>
+            <EuiOutsideClickDetector onOutsideClick={parentDetector}>
+              <div>
+                <EuiOutsideClickDetector onOutsideClick={childDetector}>
+                  <div data-test-subj="target"/>
+                </EuiOutsideClickDetector>
+              </div>
+            </EuiOutsideClickDetector>
+          </div>
+
+          <EuiOutsideClickDetector onOutsideClick={unrelatedDetector}>
+            <div/>
+          </EuiOutsideClickDetector>
+        </div>
+      );
+
+      component.find('[data-test-subj="target"]').simulate('click');
+
+      expect(unrelatedDetector).toHaveBeenCalledTimes(1);
+      expect(parentDetector).toHaveBeenCalledTimes(0);
+      expect(childDetector).toHaveBeenCalledTimes(0);
+    });
   });
 });
