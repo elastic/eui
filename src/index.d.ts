f05f20b850d9b04d1da5702e59ec335a73c29e3c
Chandler Prall <chandler.prall@gmail.com>
2018-08-10 12:38:49 -0600

    include test/index.d.ts in src references

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="./services/index.d.ts" />
 /// <reference path="./components/index.d.ts" />
 /// <reference path="./themes/index.d.ts" />
+/// <reference path="./test/index.d.ts" />
