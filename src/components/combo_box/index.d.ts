3aa60f5d7f8296b08962d2f210fb6d96bc70e37b
Chandler Prall <chandler.prall@gmail.com>
2018-08-14 10:58:18 -0600

    more progress

--- src/components/combo_box/index.d.ts
+++ src/components/combo_box/index.d.ts
@@ -1,8 +1,43 @@
-import { ReactElement, ReactNode, SFC } from 'react';
+import { ButtonHTMLAttributes, ReactElement, ReactNode, SFC } from 'react';
+import { EuiComboBoxOption, EuiComboBoxOptionProps } from '@elastic/eui';
 
 declare module '@elastic/eui' {
-  export interface EuiComboBoxOptionsListProps {
+  export type EuiComboBoxOptionProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    label: string,
+    isGroupLabelOption: boolean,
+  }
 
+  export interface EuiComboBoxOption {
+    option: EuiComboBoxOptionProps,
+    children?: ReactNode,
+    className?: string,
+    optionRef?: (ref: HTMLButtonElement) => any,
+    onClick: (option: EuiComboBoxOptionProps) => any,
+    onEnterKey: (option: EuiComboBoxOptionProps) => any,
+    disabled?: boolean,
+  }
+
+  export interface EuiComboBoxOptionsListProps {
+    options?: Array<EuiComboBoxOptionProps>,
+    isLoading?: boolean,
+    selectedOptions?: Array<any>,
+    onCreateOption?: any,
+    searchValue?: string,
+    matchingOptions?: Array<EuiComboBoxOptionProps>,
+    optionRef?: EuiComboBoxOption['optionRef'],
+    onOptionClick?: EuiComboBoxOption['onClick'],
+    onOptionEnterKey?: EuiComboBoxOption['onEnterKey'],
+    areAllOptionsSelected?: boolean,
+    getSelectedOptionForSearchValue?: (searchValue: string, selectedOptions: Array<any>) => EuiComboBoxOptionProps,
+    updatePosition: PropTypes.func.isRequired,
+    position?: PropTypes.oneOf(POSITIONS),
+    listRef: PropTypes.func.isRequired,
+    renderOption?: PropTypes.func,
+    width?: number,
+    scrollToIndex?: number,
+    onScroll?: PropTypes.func,
+    rowHeight?: number,
+    fullWidth?: boolean,
   }
   export const EuiComboBoxOptionsList: SFC<EuiComboBoxOptionsListProps>;
 
