f8b6f3e8698534c704eb8ece7b7b68ea53b8dfc4
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2018-08-21 12:24:43 -0400

    Rewrites examples for WindowEvent docs

---
+++ src-docs/src/views/window_event/mouse_position.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+import {
+  EuiSwitch,
+  EuiDescriptionList,
+  EuiSpacer
+} from '../../../../src/components';
+
+import {
+  EuiWindowEvent,
+} from '../../../../src/services';
+
+export class MousePosition extends Component {
+
+  state = {
+    tracking: false,
+    coordinates: {}
+  };
+
+  onSwitchChange = () => this.setState((state) => ({ tracking: !state.tracking }));
+
+  onMouseMove = ({ clientX, clientY }) => this.setState({ coordinates: { clientX, clientY } });
+
+  render() {
+    const listItems = [
+      { title: 'Position X', description: this.state.coordinates.clientX || '??' },
+      { title: 'Position Y', description: this.state.coordinates.clientY || '??' }
+    ];
+    return (
+      <div>
+        <EuiSwitch
+          label="Track mouse position"
+          checked={this.state.tracking}
+          onChange={this.onSwitchChange}
+        />
+        {this.state.tracking ? <EuiWindowEvent event="mousemove" handler={this.onMouseMove} /> : null}
+        <EuiSpacer size="l" />
+        <EuiDescriptionList listItems={listItems} />
+        <EuiSpacer size="xxl" />
+      </div>
+    );
+  }
+}
---
+++ src-docs/src/views/window_event/window_event_conflict.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import {
+  EuiModal,
+  EuiModalBody,
+  EuiModalHeader,
+  EuiModalHeaderTitle,
+  EuiOverlayMask,
+  EuiFieldText,
+  EuiSpacer
+} from '../../../../src/components';
+
+import { ModalExample } from './modal_example_container';
+
+class ConflictModal extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  updateInputValue = e => this.setState({ inputValue: e.target.value });
+
+  clearInputValueOnEscape = e => {
+    if (e.key === 'Escape') {
+      this.setState({ inputValue: '' });
+      e.stopPropagation();
+    }
+  }
+
+  render() {
+    return (
+      <EuiOverlayMask>
+        <EuiModal
+          onClose={this.props.onClose}
+          style={{ width: '800px' }}
+        >
+          <EuiModalHeader>
+            <EuiModalHeaderTitle >
+              Example modal
+            </EuiModalHeaderTitle>
+          </EuiModalHeader>
+          <EuiModalBody>
+            <EuiFieldText
+              value={this.state.inputValue}
+              onChange={this.updateInputValue}
+              onKeyDown={this.clearInputValueOnEscape}
+            />
+            <EuiSpacer size="s" />
+            <p>While typing in this field, ESC will clear the field.</p>
+            <EuiSpacer size="l" />
+            <p>Otherwise, the event bubbles up to the window and ESC closes the modal.</p>
+          </EuiModalBody>
+        </EuiModal>
+      </EuiOverlayMask>
+    );
+  }
+}
+
+export const WindowEventConflict = () => (
+  <ModalExample modal={ConflictModal} buttonText="Open Modal with Conflicting Listener" />
+);
