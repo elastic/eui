769f5f9fc6db56423c3b469f263509d7542801e9
cchaos <caroline.horn@elastic.co>
2018-08-21 14:43:19 -0400

    Using combobox for adding filters

--- src-docs/src/views/header/global_query.js
+++ src-docs/src/views/header/global_query.js
@@ -14,10 +14,64 @@ import {
   EuiToolTip,
   EuiPopoverTitle,
   EuiFormRow,
-  EuiSelect,
   EuiComboBox,
   EuiButton,
 } from '../../../../src/components';
+
+const fieldOptions = [
+  {
+    label: 'Fields',
+    isGroupLabelOption: true,
+  },
+  {
+    label: 'field_1',
+  },
+  {
+    label: 'field_2',
+  },
+  {
+    label: 'field_3',
+  },
+  {
+    label: 'field_4',
+  },
+];
+const operatorOptions = [
+  {
+    label: 'Operators',
+    isGroupLabelOption: true,
+  },
+  {
+    label: 'IS',
+  },
+  {
+    label: 'IS NOT',
+  },
+  {
+    label: 'IS ONE OF',
+  },
+  {
+    label: 'EXISTS',
+  },
+];
+const valueOptions = [
+  {
+    label: 'Values',
+    isGroupLabelOption: true,
+  },
+  {
+    label: 'Value 1',
+  },
+  {
+    label: 'Value 2',
+  },
+  {
+    label: 'Value 3',
+  },
+  {
+    label: 'Value 4',
+  },
+];
 
 function flattenPanelTree(tree, array = []) {
   array.push(tree);
@@ -78,9 +132,12 @@ export default class extends Component {
       idOfOpenPopover: null,
       addFilterMenuWidth: null,
       addFilterMenuIsOpen: false,
+      isComboBoxLoading: false,
+      isComboBoxOpen: false,
+      selectedComboBoxOptions: [],
+      comboBoxOptions: fieldOptions,
     };
 
-    this.filterBarRef = React.createRef();
   }
 
   toggleFilterVisibility = () => {
@@ -106,19 +163,51 @@ export default class extends Component {
     this.setState(prevState => ({
       addFilterMenuIsOpen: !prevState.addFilterMenuIsOpen,
     }));
+  };
 
-    console.log(this.filterBarRef);
+  onComboBoxChange = selectedComboBoxOptions => {
+    let options = fieldOptions;
+    if (selectedComboBoxOptions.length === 1) {
+      options = operatorOptions;
+    } else if (selectedComboBoxOptions.length > 1) {
+      options = valueOptions;
+    }
 
-    requestAnimationFrame(() => {
-      this.setState({
-        addFilterMenuWidth: this.filterBarRef.current.getBoundingClientRect().width - 24, // account for border not inner shadow
-      });
+    this.setState({
+      selectedComboBoxOptions,
+      comboBoxOptions: options,
     });
+  };
+
+  onSearchChange = () => {
+    // let options = this.state.comboBoxOptions;
+    // this.setState({
+    //   isComboBoxLoading: true,
+    //   comboBoxOptions: [],
+    // });
+    // clearTimeout(this.searchTimeout);
+    // if (this.state.selectedComboBoxOptions.length === 1) {
+    //   options = operatorOptions;
+    // } else if (this.state.selectedComboBoxOptions.length > 1) {
+    //   options = valueOptions;
+    // }
+    // this.searchTimeout = setTimeout(() => {
+    //   // Simulate a remotely-executed search.
+    //   this.setState({
+    //     isComboBoxLoading: false,
+    //     comboBoxOptions: options.filter(option => option.label.toLowerCase().includes(searchValue.toLowerCase())),
+    //   });
+    // }, 200);
   };
 
   closePopover = () => {
     //this.setState({ idOfOpenPopover: null });
   };
+
+  componentDidMount() {
+    // Simulate initial load.
+    this.onSearchChange('');
+  }
 
   render() {
     const filterTriggerButton = (
@@ -147,7 +236,6 @@ export default class extends Component {
         />
         {this.state.isFiltersVisible && (
           <GlobalFilterGroup
-            domRef={this.filterBarRef}
             allFiltersButton={this._createFilterBarContextMenu()}
             addFilterButton={this._createFilterBarAddMenu()}
           >
@@ -354,13 +442,21 @@ export default class extends Component {
   };
 
   _createFilterBarAddMenu = () => {
-    const menuId = 'GlobalFilterBarAddMenu';
+    const label = (
+      <EuiFlexGroup alignItems="baseline">
+        <EuiFlexItem>Field, Operator, Value(s)</EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiButtonEmpty size="xs" flush="right">
+            Edit as Query DSL
+          </EuiButtonEmpty>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
 
     return (
       <EuiPopover
-        id={menuId}
         isOpen={this.state.addFilterMenuIsOpen}
-        closePopover={this.toggleAddFilterMenu}
+        closePopover={() => this.setState({ addFilterMenuIsOpen: false })}
         button={
           <EuiButtonEmpty onClick={this.toggleAddFilterMenu} size="xs">
             + Add filter
@@ -370,35 +466,33 @@ export default class extends Component {
         withTitle
       >
         <EuiPopoverTitle>Add a filter</EuiPopoverTitle>
-        <div style={{ width: this.state.addFilterMenuWidth }}>
-          <EuiFlexGroup>
-            <EuiFlexItem>
-              <EuiFormRow label="Field">
-                <EuiSelect />
-              </EuiFormRow>
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <EuiFormRow label="Operator">
-                <EuiSelect />
-              </EuiFormRow>
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <EuiFormRow label="Value(s)">
-                <EuiComboBox />
-              </EuiFormRow>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-          <EuiFlexGroup direction="rowReverse">
+        <div style={{ width: 400 }}>
+          <EuiFormRow label={label}>
+            <EuiComboBox
+              placeholder="Start by selecting a field"
+              async
+              options={this.state.comboBoxOptions}
+              selectedOptions={this.state.selectedComboBoxOptions}
+              isLoading={this.state.isComboBoxLoading}
+              onChange={this.onComboBoxChange}
+              onSearchChange={this.onSearchChange}
+            />
+          </EuiFormRow>
+          {/* <EuiFormRow>
+            <EuiSwitch label="Customize filter label?" />
+          </EuiFormRow>
+          <EuiFormRow label="Label">
+            <EuiFieldText />
+          </EuiFormRow> */}
+
+          <EuiFlexGroup direction="rowReverse" alignItems="center">
             <EuiFlexItem grow={false}>
-              <EuiButton fill>Save</EuiButton>
+              <EuiButton fill>Add</EuiButton>
             </EuiFlexItem>
             <EuiFlexItem grow={false}>
               <EuiButtonEmpty>Cancel</EuiButtonEmpty>
             </EuiFlexItem>
             <EuiFlexItem />
-            <EuiFlexItem grow={false}>
-              <EuiButtonEmpty>Edit Query DSL</EuiButtonEmpty>
-            </EuiFlexItem>
           </EuiFlexGroup>
         </div>
       </EuiPopover>
@@ -406,7 +500,7 @@ export default class extends Component {
   };
 }
 
-// eslint-disable-next-line react/no-multi-comp
+// eslint-disable-next-line react/no-multi-comp, react/prefer-stateless-function
 class GlobalFilterGroup extends Component {
   static propTypes = {
     children: PropTypes.node,
@@ -416,36 +510,36 @@ class GlobalFilterGroup extends Component {
 
   render() {
     return (
-      <div ref={this.props.domRef}>
-        <EuiFlexGroup gutterSize="none" alignItems="flexStart">
-          <EuiFlexItem grow={false}>
-            <svg width="33" height="48" viewBox="0 0 33 48" xmlns="http://www.w3.org/2000/svg">
-              <rect fillOpacity="0" width="33" height="48" />
-              <path
-                d="M17,28 L17,-9.18485099e-17 L18,9.18485099e-17 L18,28 L33,28 L33,29 L17,29 L17,28 Z"
-                fill="#D8D8D8"
-              />
-            </svg>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiFlexGroup
-              className="euiGenericFormStyle euiGenericFormStyle--fullWidth"
-              wrap={true}
-              gutterSize="xs"
-              style={{ marginTop: 8 }}
-            >
-              {this.props.children}
 
-              <EuiFlexItem>
-                <EuiFlexGroup justifyContent="spaceBetween" gutterSize="xs">
-                  <EuiFlexItem grow={false}>{this.props.addFilterButton}</EuiFlexItem>
-                  <EuiFlexItem grow={false}>{this.props.allFiltersButton}</EuiFlexItem>
-                </EuiFlexGroup>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </div>
+      <EuiFlexGroup gutterSize="none" alignItems="flexStart">
+        <EuiFlexItem grow={false}>
+          <svg width="33" height="48" viewBox="0 0 33 48" xmlns="http://www.w3.org/2000/svg">
+            <rect fillOpacity="0" width="33" height="48" />
+            <path
+              d="M17,28 L17,-9.18485099e-17 L18,9.18485099e-17 L18,28 L33,28 L33,29 L17,29 L17,28 Z"
+              fill="#D8D8D8"
+            />
+          </svg>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiFlexGroup
+            className="euiGenericFormStyle euiGenericFormStyle--fullWidth"
+            wrap={true}
+            gutterSize="xs"
+            style={{ marginTop: 8 }}
+          >
+            {this.props.children}
+
+            <EuiFlexItem>
+              <EuiFlexGroup justifyContent="spaceBetween" gutterSize="xs">
+                <EuiFlexItem grow={false}>{this.props.addFilterButton}</EuiFlexItem>
+                <EuiFlexItem grow={false}>{this.props.allFiltersButton}</EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+
     );
   }
 }
