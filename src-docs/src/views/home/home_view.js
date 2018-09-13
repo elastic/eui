f081cb6d38dfa0e06fd1208a5e0b90500eed9939
dave.snider@gmail.com <dave.snider@gmail.com>
2018-09-13 14:13:32 -0700

    Update sketch library link to a 4.0 set (#1193)

--- src-docs/src/views/home/home_view.js
+++ src-docs/src/views/home/home_view.js
@@ -59,7 +59,7 @@ export const HomeView = () => (
                 This will provide symbols that match against their EUI component
                 counterparts."
             >
-              <EuiLink href="https://github.com/elastic/eui/releases/download/v0.0.51/eui_sketch_0.0.51.zip">
+              <EuiLink href="https://github.com/elastic/eui/releases/download/v4.0.0/eui_sketch_4.0.0.zip">
                 <strong>Sketch libraries</strong>
               </EuiLink>
             </EuiToolTip>
