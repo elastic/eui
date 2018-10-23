16e7ca4e6fa37c812809bb601852649ae21dd6ca
Dave Snider <dave.snider@gmail.com>
2018-10-22 23:14:14 -0700

    framer components

--- src-framer/code/button/button.tsx
+++ src-framer/code/button/button.tsx
@@ -1,89 +1,98 @@
-import * as React from "react";
-import { PropertyControls, ControlType } from "framer";
+import { COLORS, EuiButton, ICON_SIDES, SIZES } from '@elastic/eui/lib/components/button/button.js';
 import { ICON_TYPES } from '@elastic/eui/lib/components/icon/index.js';
-import { EuiButton, COLORS, ICON_SIDES, SIZES } from '@elastic/eui/lib/components/button/button.js';
-
+import { ControlType, PropertyControls } from 'framer';
+import * as React from 'react';
+import { FrameSize } from '../_framer_helpers/frame_size';
 
 // Define type of property
 interface Props {
-    text: string;
-    iconType: ICON_TYPES;
-    color: COLORS;
-    iconSide: string;
-    size: string,
-    showIconProps: boolean,
-    fullWidth: boolean,
-    fill: boolean,
+  text: string;
+  iconType: ICON_TYPES;
+  color: COLORS;
+  iconSide: string;
+  size: string;
+  showIconProps: boolean;
+  fullWidth: boolean;
+  fill: boolean;
+  width: number;
+  height: number;
 }
 
-export class button extends React.Component<Props> {
+export class Button extends React.Component<Props> {
 
-    // Set default properties
-    static defaultProps = {
-        text: "Hello World!",
-        color: "primary",
-        iconType: null,
-        iconSide: "left",
-        size: "large",
-        showIconProps: false,
-        fullWidth: false,
-    }
+  // Set default properties
+  public static defaultProps = {
+    text: 'Hello World!',
+    color: 'primary',
+    iconType: null,
+    iconSide: 'left',
+    size: 'large',
+    showIconProps: false,
+    fullWidth: false,
+  };
 
-    // Items shown in property panel
-    static propertyControls: PropertyControls = {
-        text: { type: ControlType.String, title: "text" },
-        fill: {
-            type: ControlType.Boolean,
-            title: "fill",
-        },
-        size: {
-            type: ControlType.SegmentedEnum,
-            options: SIZES,
-            title: "size",
-        },
-        fullWidth: {
-            type: ControlType.Boolean,
-            title: "fullWidth",
-        },
-        color: {
-            type: ControlType.Enum,
-            options: COLORS,
-            title: "color",
-        },
-        showIconProps: {
-            type: ControlType.Boolean,
-            title: "Need icon?",
-        },
-        iconType: {
-            type: ControlType.Enum,
-            options: ICON_TYPES,
-            title: "iconType",
-            hidden(props) {
-                return props.showIconProps === false
-            },
-        },
-        iconSide: {
-            type: ControlType.SegmentedEnum,
-            options: ICON_SIDES,
-            title: "iconSide",
-            hidden(props) {
-                return props.showIconProps === false
-            },
-        },
-    }
+  // Items shown in property panel
+  public static propertyControls: PropertyControls = {
+    text: {
+      type: ControlType.String,
+      title: 'text',
+    },
+    fill: {
+      type: ControlType.Boolean,
+      title: 'fill',
+    },
+    size: {
+      type: ControlType.SegmentedEnum,
+      options: SIZES,
+      title: 'size',
+    },
+    fullWidth: {
+      type: ControlType.Boolean,
+      title: 'fullWidth',
+    },
+    color: {
+      type: ControlType.Enum,
+      options: COLORS,
+      title: 'color',
+    },
+    showIconProps: {
+      type: ControlType.Boolean,
+      title: 'Need icon?',
+    },
+    iconType: {
+      type: ControlType.Enum,
+      options: ICON_TYPES,
+      title: 'iconType',
+      hidden(props) {
+      return props.showIconProps === false;
+      },
+    },
+    iconSide: {
+      type: ControlType.SegmentedEnum,
+      options: ICON_SIDES,
+      title: 'iconSide',
+      hidden(props) {
+      return props.showIconProps === false;
+      },
+    },
+  };
 
-    render() {
+  public render() {
     return (
-        <EuiButton 
-            size={this.props.size} 
-            fill={this.props.fill} 
-            color={this.props.color} 
-            iconType={this.props.iconType}
-            iconSide={this.props.iconSide}
-            style={{ width: this.props.fullWidth ? '100%' : 'auto' }}
+      <FrameSize>
+        <EuiButton
+          size={this.props.size}
+          fill={this.props.fill}
+          color={this.props.color}
+          iconType={this.props.iconType}
+          iconSide={this.props.iconSide}
+          style={{ width: this.props.fullWidth ? '100%' : 'auto' }}
+          width={this.props.width}
+          height={this.props.height}
         >
-            {this.props.text}
+          {this.props.text}
         </EuiButton>
+      </FrameSize>
     );
-    }
+  }
 }
--- src-framer/code/panel/panel.tsx
+++ src-framer/code/panel/panel.tsx
@@ -1,44 +1,44 @@
-import * as React from "react";
-import { PropertyControls, ControlType } from "framer";
 import { EuiPanel, SIZES } from '@elastic/eui/lib/components/panel/panel.js';
+import { ControlType, PropertyControls } from 'framer';
+import * as React from 'react';
 
 // Define type of property
 interface Props {
-    hasShadow: boolean;
-    paddingSize: SIZES;
-    betaBadgeLabel: string;
+  hasShadow: boolean;
+  paddingSize: SIZES;
+  betaBadgeLabel: string;
 }
 
-export class panel extends React.Component<Props> {
+export class Panel extends React.Component<Props> {
 
-    // Set default properties
-    static defaultProps = {
-        paddingSize: 'm',
-        hasShadow: false,
-    }
+  // Set default properties
+  public static defaultProps = {
+    paddingSize: 'm',
+    hasShadow: false,
+  };
 
-    // Items shown in property panel
-    static propertyControls: PropertyControls = {
-        hasShadow: { type: ControlType.Boolean, title: "hasShadow" },
-        betaBadgeLabel: { type: ControlType.String, title: "betaBadgeLabel" },
-        paddingSize: {
-            type: ControlType.SegmentedEnum,
-            options: SIZES,
-            title: "paddingSize",
-        },
-    }
+  // Items shown in property panel
+  public static propertyControls: PropertyControls = {
+    hasShadow: { type: ControlType.Boolean, title: 'hasShadow' },
+    betaBadgeLabel: { type: ControlType.String, title: 'betaBadgeLabel' },
+    paddingSize: {
+      type: ControlType.SegmentedEnum,
+      options: SIZES,
+      title: 'paddingSize',
+    },
+  };
 
-    render() {
-    return (
-        <div style={{ display: 'flex', position: 'absolute', height: '100%', width: '100%' }}>
-            <EuiPanel
-                hasShadow={this.props.hasShadow}
-                paddingSize={this.props.paddingSize}
-                betaBadgeLabel={this.props.betaBadgeLabel}
-            >
-                {this.props.children}
-            </EuiPanel>
-        </div>
-    );
-    }
+  public render() {
+  return (
+    <div style={{ display: 'flex', position: 'absolute', height: '100%', width: '100%' }}>
+      <EuiPanel
+        hasShadow={this.props.hasShadow}
+        paddingSize={this.props.paddingSize}
+        betaBadgeLabel={this.props.betaBadgeLabel}
+      >
+        {this.props.children}
+      </EuiPanel>
+    </div>
+  );
+  }
 }
