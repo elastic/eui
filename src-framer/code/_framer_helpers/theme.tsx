b8a0cdbf408ca3434bd379ca5889049433cbe40a
Ryan Keairns <contactryank@gmail.com>
2018-11-01 17:06:31 -0500

    add offwhite bg color for light theme

--- src-framer/code/_framer_helpers/theme.tsx
+++ src-framer/code/_framer_helpers/theme.tsx
@@ -1,18 +1,22 @@
-import DarkCSS from '!!raw-loader!@elastic/eui/dist/eui_theme_dark.css';
-import LightCSS from '!!raw-loader!@elastic/eui/dist/eui_theme_light.css';
+
 import { ControlType, PropertyControls } from 'framer';
 import * as React from 'react';
 import { FrameSize } from './frame_size.tsx';
 
+const DarkCSS = require('!!raw-loader!@elastic/eui/dist/eui_theme_dark.css');
+const LightCSS = require('!!raw-loader!@elastic/eui/dist/eui_theme_light.css');
+
 // Define type of property
 interface Props {
   theme: string;
+  bgColor: string;
 }
 
 export class Theme extends React.Component<Props> {
   // Set default properties
   public static defaultProps = {
     theme: 'light',
+    bgColor: 'white',
   };
 
   // Items shown in property panel
@@ -22,13 +26,22 @@ export class Theme extends React.Component<Props> {
       options: ['light', 'dark'],
       title: 'Theme',
     },
+    bgColor: {
+      type: ControlType.SegmentedEnum,
+      options: ['white', 'offWhite'],
+      title: '↳ 🧙‍♂️ bgColor',
+      hidden(props) {
+      return props.theme === 'dark';
+      },
+    },
   };
 
   public render() {
 
-    const lightBgColor = '#FFF';
+    const whiteBgColor = '#FFF';
+    const offWhiteBgColor = '#F5F5F5';
     const darkBgColor = '#222';
-    const bgColor = (this.props.theme === 'light' ? lightBgColor : darkBgColor);
+    const bgColor = (this.props.theme === 'light' && this.props.bgColor === 'white') ? whiteBgColor : (this.props.theme === 'light' && this.props.bgColor === 'offWhite') ? offWhiteBgColor : darkBgColor);
     return (
       <FrameSize>
         <div style={{ background: bgColor, flexGrow: 1, display: 'flex' }}>
