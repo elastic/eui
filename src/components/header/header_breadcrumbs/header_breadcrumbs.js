ea8149f04c0d5fbc03505ad9119984073f0ab80e
cchaos <caroline.horn@elastic.co>
2018-12-04 18:19:58 -0500

    Better breadcrumb truncation

--- src/components/header/header_breadcrumbs/header_breadcrumbs.js
+++ src/components/header/header_breadcrumbs/header_breadcrumbs.js
@@ -7,12 +7,6 @@ export const EuiHeaderBreadcrumbs = ({ className, breadcrumbs, ...rest }) => {
   const classes = classNames('euiHeaderBreadcrumbs', className);
 
   return (
-
-    <EuiBreadcrumbs
-      max={4}
-      breadcrumbs={breadcrumbs}
-      className={classes}
-      {...rest}
-    />
+    <EuiBreadcrumbs max={4} truncate breadcrumbs={breadcrumbs} className={classes} {...rest} />
   );
 };
