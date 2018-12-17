7c6bfa33849045b4a0259456465fca1ef6deee9f
Chandler Prall <chandler.prall@gmail.com>
2018-12-17 16:07:55 -0700

    Re-export some service TS defs

--- src/services/color/index.ts
+++ src/services/color/index.ts
@@ -5,3 +5,4 @@ export { calculateContrast, calculateLuminance } from './luminance_and_contrast'
 export { VISUALIZATION_COLORS, DEFAULT_VISUALIZATION_COLOR } from './visualization_colors';
 export { colorPalette } from './color_palette';
 export { palettes } from './eui_palettes';
+export { rgbDef } from './color_types';
