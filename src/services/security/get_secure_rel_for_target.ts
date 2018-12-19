4bf7a8d9ae07acd34f230d4e5030ced9814d09a7
Rory Hunter <roryhunter2@gmail.com>
2018-12-19 11:23:15 +0000

    Migrate security service to TS

--- src/services/security/get_secure_rel_for_target.ts
+++ src/services/security/get_secure_rel_for_target.ts
@@ -1,8 +1,8 @@
 /**
- * Secures outbound links. See https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/
- * for more info.
+ * Secures outbound links. For more info:
+ * https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/
  */
-export const getSecureRelForTarget = (target, rel) => {
+export const getSecureRelForTarget = (target?: '_blank' | '_self' | '_parent' | '_top' | string, rel?: string) => {
   if (!target) {
     return rel;
   }
@@ -20,7 +20,6 @@ export const getSecureRelForTarget = (target, rel) => {
   if (!secureRel.includes('noopener')) {
     secureRel = `${secureRel} noopener`;
   }
-
 
   if (!secureRel.includes('noreferrer')) {
     secureRel = `${secureRel} noreferrer`;
