4f598bde59ac55eda2100ed95c341a94e1d91566
Chandler Prall <chandler.prall@gmail.com>
2019-01-02 11:11:21 -0700

    Update eui.d.ts generator to properly handle nested index files

--- scripts/dtsgenerator.js
+++ scripts/dtsgenerator.js
@@ -1,8 +1,27 @@
+const findup = require('findup');
+const resolve = require('resolve');
 const fs = require('fs');
 const path = require('path');
 const dtsGenerator = require('dts-generator').default;
 
 const baseDir = path.resolve(__dirname, '..');
+const srcDir = path.resolve(baseDir, 'src');
+
+function hasParentIndex(pathToFile) {
+  const isIndexFile = path.basename(pathToFile, path.extname(pathToFile)) === 'index';
+  try {
+    const fileDirectory = path.dirname(pathToFile);
+    const parentIndex = findup.sync(
+      // if this is an index file start looking in its parent directory
+      isIndexFile ? path.resolve(fileDirectory, '..') : fileDirectory,
+      'index.ts'
+    );
+    // ensure the found file is in the project
+    return parentIndex.startsWith(baseDir);
+  } catch (e) {
+    return false;
+  }
+}
 
 const generator = dtsGenerator({
   name: '@elastic/eui',
@@ -10,33 +29,37 @@ const generator = dtsGenerator({
   out: 'eui.d.ts',
   exclude: ['node_modules/**/*.d.ts', 'src/custom_typings/**/*.d.ts'],
   resolveModuleId(params) {
-    if (path.basename(params.currentModuleId) === 'index') {
+    if (path.basename(params.currentModuleId) === 'index' && !hasParentIndex(path.resolve(baseDir, params.currentModuleId))) {
       // this module is exporting from an `index(.d)?.ts` file, declare its exports straight to @elastic/eui module
       return '@elastic/eui';
     } else {
       // otherwise export as the module's path relative to the @elastic/eui namespace
-      return path.join('@elastic/eui', params.currentModuleId);
+      if (params.currentModuleId.endsWith('/index')) {
+        return path.join('@elastic/eui', path.dirname(params.currentModuleId));
+      } else {
+        return path.join('@elastic/eui', params.currentModuleId);
+      }
     }
   },
   resolveModuleImport(params) {
     // only intercept relative imports (don't modify node-modules references)
-    const isRelativeImport = params.importedModuleId[0] === '.';
+    const importFromBaseDir = path.resolve(baseDir, path.dirname(params.currentModuleId));
+    const isFromEuiSrc = importFromBaseDir.startsWith(srcDir);
+    const isRelativeImport = isFromEuiSrc && params.importedModuleId[0] === '.';
 
     if (isRelativeImport) {
-      // if importing an `index` file
-      let isModuleIndex = false;
-      if (path.basename(params.importedModuleId) === 'index') {
-        isModuleIndex = true;
-      } else {
-        const basePath = path.resolve(baseDir, path.dirname(params.currentModuleId));
-        // check if the imported module resolves to ${importedModuleId}/index.ts
-        if (!fs.existsSync(path.resolve(basePath, `${params.importedModuleId}.ts`))) {
-          // not pointing at ${importedModuleId}.ts, check if it's a directory with `index.ts`
-          if (fs.existsSync(path.resolve(basePath, `${params.importedModuleId}/index.ts`))) {
-            isModuleIndex = true;
-          }
+      // if importing from an `index` file (directly or targeting a directory with an index),
+      // then if there is no parent index file this should import from @elastic/eui
+      const importPathTarget = resolve.sync(
+        params.importedModuleId,
+        {
+          basedir: importFromBaseDir,
+          extensions: ['.ts', '.tsx'],
         }
-      }
+      );
+
+      const isIndexFile = importPathTarget.endsWith('/index.ts');
+      const isModuleIndex = isIndexFile && !hasParentIndex(importPathTarget);
 
       if (isModuleIndex) {
         // importing an `index` file, in `resolveModuleId` above we change those modules to '@elastic/eui'
@@ -55,11 +78,14 @@ const generator = dtsGenerator({
   },
 });
 
-// strip any `/// <reference` lines from the generated eui.d.ts
+// 1. strip any `/// <reference` lines from the generated eui.d.ts
+// 2. replace any import("src/...") declarations to import("@elastic/src/...")
 generator.then(() => {
   const defsFilePath = path.resolve(baseDir, 'eui.d.ts');
   fs.writeFileSync(
     defsFilePath,
-    fs.readFileSync(defsFilePath).toString().replace(/\/\/\/\W+<reference.*/g, '')
+    fs.readFileSync(defsFilePath).toString()
+      .replace(/\/\/\/\W+<reference.*/g, '') // 1.
+      .replace(/import\(\"src\/(.*?)\"\)/g, 'import("@elastic/eui/src/$1")') // 2.
   );
 });
