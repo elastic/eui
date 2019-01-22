ebce6a61816d87e51015c1d9458180ab3a408f79
Chandler Prall <chandler.prall@gmail.com>
2019-01-22 11:36:28 -0700

    Refactor EuiI18n place holder parsing, add tests for I18n

--- src/components/i18n/i18n.tsx
+++ src/components/i18n/i18n.tsx
@@ -1,79 +1,8 @@
-import React, { cloneElement, Fragment, ReactChild, ReactElement, SFC } from 'react';
+import React, { Fragment, ReactChild, SFC } from 'react';
 import { EuiI18nConsumer } from '../context';
 import { ExclusiveUnion } from '../common';
 import { I18nShape, Renderable, RenderableValues } from '../context/context';
-import { isBoolean, isString, isNumber } from '../../services/predicate/lodash_predicates';
-
-// <I18n token="foo"/>
-// <I18n token="foo">{(foo) => <p>foo</p>}</I18n>
-// <I18n tokens=['foo', 'bar']>{([foo, bar]) => <p>{foo}, {bar}</p></I18n>
-
-const hasSymbol = typeof Symbol === 'function' && Symbol.for;
-const REACT_ELEMENT_TYPE = hasSymbol
-  ? Symbol.for('react.element')
-  : 0xeac7;
-const isElement = (value: any): value is ReactElement<any> => {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    value.$$typeof === REACT_ELEMENT_TYPE
-  );
-};
-
-function isPrimitive(value: ReactChild) {
-  return isBoolean(value) || isString(value) || isNumber(value);
-}
-
-function processStringToChildren(input: string, values: RenderableValues): string | ReactChild[] {
-  const children: ReactChild[] = [];
-
-  let child;
-  let encounteredNonPrimitive = false;
-  // tslint:disable-next-line:prefer-for-of
-  for (let i = 0; i < input.length; i++) {
-    const char = input[i];
-
-    if (char === '{') {
-      if (child !== undefined) {
-        children.push(child);
-      }
-      // @ts-ignore
-      child = {propName: ''};
-    } else if (char === '}') {
-      // @ts-ignore
-      const propName = child.propName;
-      if (!values.hasOwnProperty(propName)) {
-        throw new Error(`Key "${propName}" not found in ${JSON.stringify(values, null, 2)}`);
-      }
-      const propValue = values[propName];
-      encounteredNonPrimitive = encounteredNonPrimitive || !(isPrimitive(propValue));
-      // tslint:disable-next-line
-      if (isElement(propValue)) {
-        children.push(cloneElement(
-          propValue,
-          { key: children.length }
-        ));
-      } else {
-        children.push(propValue);
-      }
-      child = undefined;
-    } else {
-      if (child === undefined) {
-        child = char;
-      } else if (typeof child === 'string') {
-        child = child + char;
-      } else {
-        child.propName = child.propName + char;
-      }
-    }
-  }
-
-  if (child !== undefined) {
-    children.push(child);
-  }
-
-  return encounteredNonPrimitive ? children : children.join('');
-}
+import { processStringToChildren } from './i18n_util';
 
 function throwError(): never {
   throw new Error('asdf');
---
+++ src/components/i18n/i18n_util.tsx
@@ -0,0 +1,93 @@
+import { cloneElement, ReactChild } from 'react';
+import { isBoolean, isString, isNumber } from '../../services/predicate/lodash_predicates';
+import {isElement} from 'react-is';
+import { RenderableValues } from '../context/context';
+
+function isPrimitive(value: ReactChild) {
+  return isBoolean(value) || isString(value) || isNumber(value);
+}
+
+type Child = string | {propName: string} | ReactChild | undefined;
+
+function hasPropName(child: Child): child is ({propName: string}) {
+  return typeof child === 'object' && child.hasOwnProperty('propName');
+}
+
+/**
+ * Replaces placeholder values in `input` with their matching value in `values`
+ * e.g. input:'Hello, {name}' will replace `{name}` with `values[name]`
+ * @param {string} input
+ * @param {RenderableValues} values
+ * @returns {string | React.ReactChild[]}
+ */
+export function processStringToChildren(input: string, values: RenderableValues): string | ReactChild[] {
+  const children: ReactChild[] = [];
+
+  let child: Child;
+
+  function appendCharToChild(char: string) {
+    if (child === undefined) {
+      child = char;
+    } else if (typeof child === 'string') {
+      child = child + char;
+    } else if (hasPropName(child)) {
+      child.propName = child.propName + char;
+    }
+  }
+
+  function appendValueToChildren(value: Child) {
+    if (value === undefined) {
+      return;
+    } else if (typeof value === 'string') {
+      children.push(value);
+    } else if (isElement(value)) {
+      children.push(cloneElement(
+        value,
+        { key: children.length }
+      ));
+    } else if (hasPropName(value)) {
+      // this won't be called, propName children are converted to a ReactChild before calling this
+    } else {
+      children.push(value);
+    }
+  }
+
+  let encounteredNonPrimitive = false;
+  // tslint:disable-next-line:prefer-for-of
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+
+    if (char === '\\') {
+      // peek at the next character to know if this is an escape
+      const nextChar = input[i + 1];
+      let charToAdd = char; // if this isn't an escape sequence then we will add the backslash
+
+      if (nextChar === '{' || nextChar === '}') {
+        // escaping a brace
+        i += 1; // advance passed the brace
+        charToAdd = input[i];
+      }
+      appendCharToChild(charToAdd);
+    } else if (char === '{') {
+      appendValueToChildren(child);
+      child = {propName: ''};
+    } else if (char === '}') {
+      // @ts-ignore
+      const propName = child.propName;
+      if (!values.hasOwnProperty(propName)) {
+        throw new Error(`Key "${propName}" not found in ${JSON.stringify(values, null, 2)}`);
+      }
+      const propValue = values[propName];
+      encounteredNonPrimitive = encounteredNonPrimitive || !(isPrimitive(propValue));
+      appendValueToChildren(propValue);
+      child = undefined;
+    } else {
+      appendCharToChild(char);
+    }
+  }
+
+  // include any remaining child value
+  appendValueToChildren(child);
+
+  return encounteredNonPrimitive ? children : children.join('');
+}
