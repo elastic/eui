4e1080637331eb9c484b7b1d01c1aa06bde26648
Dave Snider <dave.snider@gmail.com>
2019-01-23 12:09:04 -0800

    feedback

--- src-docs/src/routes.js
+++ src-docs/src/routes.js
@@ -398,7 +398,7 @@ const navigation = [{
   ].map(example => createExample(example)),
 },
 {
-  name: 'Series charts (beta)',
+  name: 'Charts (deprecated)',
   items: [
     XYChartExample,
     XYChartAxisExample,
