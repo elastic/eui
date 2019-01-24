1c63c25ccc531af3948df0ac5fff19370bdf4617
Chandler Prall <chandler.prall@gmail.com>
2019-01-24 11:21:04 -0700

    Added a CSP to docs

--- src-docs/webpack.config.js
+++ src-docs/webpack.config.js
@@ -30,20 +30,41 @@ module.exports = {
       exclude: /node_modules/
     }, {
       test: /\.scss$/,
-      loaders: ['style-loader/useable', 'css-loader', 'postcss-loader', 'sass-loader'],
+      loaders: [
+        {
+          loader: 'style-loader/useable',
+          options: {
+            attrs: {
+              nonce: 'bmV0c3BhcmtlciBydWxlcyA7KQ=='
+            }
+          },
+        },
+        'css-loader',
+        'postcss-loader',
+        'sass-loader'],
+
       exclude: /node_modules/
     }, {
       test: /\.css$/,
-      loaders: ['style-loader/useable', 'css-loader'],
+      loaders: [
+        {
+          loader: 'style-loader/useable',
+          options: {
+            attrs: {
+              nonce: 'bmV0c3BhcmtlciBydWxlcyA7KQ=='
+            }
+          },
+        },
+        'css-loader'
+      ],
       exclude: /node_modules/
     }, {
       test: /\.(woff|woff2|ttf|eot|ico)(\?|$)/,
       loader: 'file-loader',
     }, {
       test: /\.(png|jp(e*)g|svg)$/,
-      loader: 'url-loader',
+      loader: 'file-loader',
       options: {
-        limit: 8000, // Convert images < 8kb to base64 strings
         name: 'images/[hash]-[name].[ext]'
       },
     }],
