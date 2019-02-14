b5423482df6cf8d2d19609d8395da99abb640f7b
Dave Snider <dave.snider@gmail.com>
2019-02-13 19:29:36 -0800

    token table

--- src-docs/src/views/package/i18n_tokens.js
+++ src-docs/src/views/package/i18n_tokens.js
@@ -24,7 +24,7 @@ const columns = [
       return (
         <EuiCodeBlock
           language={highlighting === 'code' ? 'javascript' : 'text'}
-          paddingSize="s"
+          paddingSize="none"
           transparentBackground
           fontSize="s"
         >
