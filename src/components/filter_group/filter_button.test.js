4ca754e3f827f6b842cf42a42d76b30ac55d1841
cchaos <caroline.horn@elastic.co>
2019-02-22 15:27:01 -0500

    More jests for filter button

--- src/components/filter_group/filter_button.test.js
+++ src/components/filter_group/filter_button.test.js
@@ -21,4 +21,94 @@ describe('EuiFilterButton', () => {
 
     expect(component).toMatchSnapshot();
   });
+
+  describe('props', () => {
+    describe('iconType and iconSide', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiFilterButton iconType="user" iconSide="right" />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('numFilters', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiFilterButton numFilters={5} />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('numActiveFilters and hasActiveFilters', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiFilterButton numActiveFilters={5} hasActiveFilters />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('isSelected', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiFilterButton isSelected />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('isDisabled', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiFilterButton isDisabled />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('type', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiFilterButton type="button" />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('grow', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiFilterButton grow />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+
+    describe('noDivider', () => {
+      it('is rendered', () => {
+        const component = render(
+          <EuiFilterButton noDivider />
+        );
+
+        expect(component)
+          .toMatchSnapshot();
+      });
+    });
+  });
 });
