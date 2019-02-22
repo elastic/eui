5c532e563844ee554608f0e3fb222dd5f9238ca6
cchaos <caroline.horn@elastic.co>
2019-02-22 15:19:50 -0500

    More jests for notification badge

--- src/components/badge/notification_badge/badge_notification.test.tsx
+++ src/components/badge/notification_badge/badge_notification.test.tsx
@@ -2,12 +2,40 @@ import React from 'react';
 import { render } from 'enzyme';
 import { requiredProps } from '../../../test/required_props';
 
-import { EuiNotificationBadge } from './badge_notification';
+import { EuiNotificationBadge, COLORS, SIZES } from './badge_notification';
 
 describe('EuiNotificationBadge', () => {
   test('is rendered', () => {
-    const component = render(<EuiNotificationBadge {...requiredProps} />);
+    const component = render(
+      <EuiNotificationBadge {...requiredProps}>5</EuiNotificationBadge>
+    );
 
     expect(component).toMatchSnapshot();
+  });
+
+  describe('props', () => {
+    describe('color', () => {
+      COLORS.forEach(color => {
+        test(`${color} is rendered`, () => {
+          const component = render(
+            <EuiNotificationBadge color={color}>5</EuiNotificationBadge>
+          );
+
+          expect(component).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('size', () => {
+      SIZES.forEach(size => {
+        test(`${size} is rendered`, () => {
+          const component = render(
+            <EuiNotificationBadge size={size}>5</EuiNotificationBadge>
+          );
+
+          expect(component).toMatchSnapshot();
+        });
+      });
+    });
   });
 });
