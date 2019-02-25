1cc501d01978a1ddf31fb27ea2b28051d8c9aa0d
Søren Louv-Jansen <sorenlouv@gmail.com>
2019-02-25 16:35:47 +0100

    Add MIT license and derived note

--- scripts/sass-extract-js-plugin.js
+++ scripts/sass-extract-js-plugin.js
@@ -1,3 +1,10 @@
+// The contents of this file is derived from https://github.com/adamgruber/sass-extract-js by Adam Gruber
+// MIT License
+// Copyright (c) 2017 Adam Gruber
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 /*
  * Add escaped quotes around font names other than the generic CSS font families
  * While quotes are not required, they are recommended by the spec
@@ -83,8 +90,6 @@ function transformVars(varsObj) {
     return acc;
   }, {});
 }
-
-
 
 module.exports = {
   run: () => ({
