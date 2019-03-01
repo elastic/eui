cd7bb9143959c19b6cebd71d0a248fecbe268227
CJ Cenizal <cj@cenizal.com>
2019-02-28 16:14:52 -0800

    Add matcher argument to findTestSubject.

--- src/test/find_test_subject.js
+++ src/test/find_test_subject.js
@@ -2,11 +2,12 @@
  * Find node which matches a specific test subject selector. Returns ReactWrappers around DOM element,
  * https://github.com/airbnb/enzyme/tree/master/docs/api/ReactWrapper.
  * Common use cases include calling simulate or getDOMNode on the returned ReactWrapper.
+ *
+ * The ~= matcher looks for the value in space-separated list, allowing support for multiple data-test-subj
+ * values on a single element.
  */
-export const findTestSubject = (mountedComponent, testSubjectSelector) => {
-  // The ~ looks for the value in space-separated list, allowing support for multiple data-test-subj
-  // values on a single element.
-  const testSubject = mountedComponent.find(`[data-test-subj~="${testSubjectSelector}"]`);
+export const findTestSubject = (mountedComponent, testSubjectSelector, matcher = '~=') => {
+  const testSubject = mountedComponent.find(`[data-test-subj${matcher}"${testSubjectSelector}"]`);
 
   // Restores Enzyme 2's find behavior, which was to only return ReactWrappers around DOM elements.
   // Enzyme 3 returns ReactWrappers around both DOM elements and React components.
