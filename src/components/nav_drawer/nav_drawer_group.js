81e43332e4b124b62714da6b912e05235024b50d
Greg Thompson <thompson.glowe@gmail.com>
2019-03-11 17:36:10 -0500

    use object destructuring rather than delete

--- src/components/nav_drawer/nav_drawer_group.js
+++ src/components/nav_drawer/nav_drawer_group.js
@@ -13,17 +13,15 @@ export const EuiNavDrawerGroup = ({ className, listItems, flyoutMenuButtonClick,
   // Create handlers if flyoutMenu exists
   const newListItems = listItems.map((item) => {
     // If the flyout menu exists, pass back the list of times and the title with the onClick handler of the item
-    if (item.flyoutMenu && flyoutMenuButtonClick) {
-      const items = [...item.flyoutMenu.listItems];
-      const title = `${item.flyoutMenu.title}`;
-      item.onClick = () => flyoutMenuButtonClick(items, title);
+    const { flyoutMenu, ...itemProps } = item;
+    if (flyoutMenu && flyoutMenuButtonClick) {
+      const items = [...flyoutMenu.listItems];
+      const title = `${flyoutMenu.title}`;
+      itemProps.onClick = () => flyoutMenuButtonClick(items, title);
     }
 
-    // Then remove the flyoutMenu key
-    delete item.flyoutMenu;
-
-    // And return the item
-    return item;
+    // And return the item with conditional `onClick` and without `flyoutMenu`
+    return { ...itemProps };
   });
 
 
