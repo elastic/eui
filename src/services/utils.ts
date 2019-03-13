705b88949c821cf2ff38daee9000f850a53372d3
Chandler Prall <chandler.prall@gmail.com>
2019-03-13 10:33:42 -0600

    Wrap lodash functions to avoid exporting lodash type defs

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,7 +1,24 @@
-import times from 'lodash/times';
-import memoize from 'lodash/memoize';
+import _times from 'lodash/times';
+import _memoize from 'lodash/memoize';
 
-export { times, memoize };
+// wrap the lodash functions to avoid having lodash's TS type definition from being
+// exported, which can conflict with the lodash namespace if other versions are used
+
+export function times<T>(count: number): number[];
+export function times<T>(count: number, iteratee: (index: number) => T): T[];
+export function times<T>(count: number, iteratee?: (index: number) => T) {
+  if (iteratee === undefined) {
+    return _times(count);
+  }
+  return _times(count, iteratee);
+}
+
+export function memoize<T extends (...args: any[]) => any>(
+  func: T,
+  resolver?: (...args: any[]) => any
+): (...args: Parameters<T>) => ReturnType<T> {
+  return _memoize(func, resolver);
+}
 
 export const browserTick = (callback: FrameRequestCallback) => {
   requestAnimationFrame(callback);
