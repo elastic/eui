0a61f9f79c64e9af149fca9022e35b5a5c46b88b
Walter Rafelsberger <walter@elastic.co>
2019-03-18 11:34:42 +0100

    add EuiListGroup and EuiListGroupItem type definitions.

--- src/components/list_group/index.d.ts
+++ src/components/list_group/index.d.ts
@@ -1,5 +1,6 @@
+import { IconType } from '../icon'
 import { CommonProps } from '../common';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, ReactNode, ReactPropTypes } from 'react';
 
 declare module '@elastic/eui' {
   /**
@@ -9,8 +10,38 @@ declare module '@elastic/eui' {
    */
 
   type EuiListGroupProps = CommonProps & {
-
+    bordered?: boolean;
+    flush?: boolean;
+    listItems?: FunctionComponent<EuiListGroupItemProps>[];
+    maxWidth?: boolean | number | string;
+    showToolTips?: boolean;
+    wrapText?: boolean;
   };
 
   export const EuiListGroup: FunctionComponent<EuiListGroupProps>;
+
+  /**
+   * list group item type defs
+   *
+   * @see './list_group_item.js'
+   */
+
+  type EuiListGroupItemProps = CommonProps & {
+    size?: 'xs' | 's' | 'm' | 'l';
+    label: ReactNode;
+    isActive?: boolean;
+    isDisabled?: boolean;
+    href?: string;
+    iconType?: IconType;
+    icon?: ReactPropTypes['element'];
+    showToolTip?: boolean;
+    extraAction?: {
+      iconType: IconType;
+      alwaysShow?: boolean;
+    };
+    onClick?(): void;
+    wrapText?: boolean;
+  };
+
+  export const EuiListGroupItem: FunctionComponent<EuiListGroupItemProps>;
 }
