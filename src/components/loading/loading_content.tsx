0bfdce5cd41a1de827515b11ff3fca16807a5aec
Davey Holler <daveyholler@gmail.com>
2019-03-21 16:04:36 -0700

    Transforming the x position rather than animating the background position.

--- src/components/loading/loading_content.tsx
+++ src/components/loading/loading_content.tsx
@@ -15,7 +15,9 @@ export const EuiLoadingContent: FunctionComponent<
 
   for (let i = 0; i < lines; i++) {
     lineElements.push(
-      <div key={i} className="euiLoadingContent__singleLine" />
+      <div key={i} className="euiLoadingContent__singleLine">
+        <div className="euiLoadingContent__singleLine--background" />
+      </div>
     );
   }
 
