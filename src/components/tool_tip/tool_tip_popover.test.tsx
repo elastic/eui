68594fb06385a08c9f8c4f5a67c227d13c1afada
Theofanis Despoudis <thdespou@hotmail.com>
2019-04-08 16:39:05 +0100

    Converted `EuiToolTipPopover` to TS (#1800)
    * Converted EuiToolTipPopover to TS
    
    * Updated Changelog
    
    * `EuiToolTipPopover` Code review fixes

--- src/components/tool_tip/tool_tip_popover.test.tsx
+++ src/components/tool_tip/tool_tip_popover.test.tsx
@@ -7,10 +7,9 @@ import { EuiToolTipPopover } from './tool_tip_popover';
 describe('EuiToolTipPopover', () => {
   test('is rendered', () => {
     const component = render(
+      // tslint:disable-next-line:no-empty
       <EuiToolTipPopover positionToolTip={() => {}} {...requiredProps} />
     );
-
-    expect(component)
-      .toMatchSnapshot();
+    expect(component).toMatchSnapshot();
   });
 });
