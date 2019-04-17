6579cb4bd7fa013d7f9ede4bd62d7e2f03183e4d
Theofanis Despoudis <thdespou@hotmail.com>
2019-04-17 19:00:14 +0100

    `matching_options` converted to TS+tests (#1828)
    * `matching_options` converted to TS+tests
    
    * `matching_options` deleted matching_options.js
    
    * `matching_options` code review fixes for `EuiComboBoxOptionProps`
    
    * `matching_options` updated changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## [`master`](https://github.com/elastic/eui/tree/master)
 
 - Use `cache-loader` to speed up development docs site build ([#1841](https://github.com/elastic/eui/pull/1841)
+- Converted `matching_options` to TS ([#1828](https://github.com/elastic/eui/pull/1828))
 
 **Bug fixes**
 
@@ -8,7 +9,7 @@
 
 ## [`10.0.1`](https://github.com/elastic/eui/tree/v10.0.1)
 
-- Convert `EuiText`, `EuiTextColor` and `EuiTextAlign` to TS ([#1791](https://github.com/elastic/eui/pull/1791))
+- Converted `EuiText`, `EuiTextColor` and `EuiTextAlign` to TS ([#1791](https://github.com/elastic/eui/pull/1791))
 - Updated `IconColor` type to better distinguish between accepted types ([#1842](https://github.com/elastic/eui/pull/1842))
 
 ## [`10.0.0`](https://github.com/elastic/eui/tree/v10.0.0)
---
+++ src/components/combo_box/matching_options.test.ts
@@ -0,0 +1,171 @@
+import { EuiComboBoxOptionProps } from '@elastic/eui';
+import {
+  flattenOptionGroups,
+  getSelectedOptionForSearchValue,
+  getMatchingOptions,
+} from './matching_options';
+
+const options: EuiComboBoxOptionProps[] = [
+  {
+    label: 'Titan',
+    'data-test-subj': 'titanOption',
+  },
+  {
+    label: 'Saturn',
+    'data-test-subj': 'saturnOption',
+  },
+  {
+    label: 'Mimas',
+  },
+];
+
+describe('flattenOptionGroups', () => {
+  test('it flattens one level of options', () => {
+    // Assemble
+    const input = [
+      {
+        label: 'Titan',
+        'data-test-subj': 'titanOption',
+      },
+      {
+        label: 'Enceladus',
+        options: [
+          {
+            label: 'Saturn',
+            'data-test-subj': 'saturnOption',
+          },
+        ],
+      },
+      {
+        label: 'Mimas',
+      },
+    ];
+    const expected = options;
+    // Act
+    const got = flattenOptionGroups(input);
+    // Assert
+    expect(got).toMatchObject(expected);
+  });
+});
+
+describe('getSelectedOptionForSearchValue', () => {
+  test('gets the first matching selected option for search value', () => {
+    // Assemble
+    const expected: EuiComboBoxOptionProps = {
+      label: 'Saturn',
+      'data-test-subj': 'saturnOption',
+    };
+    // Act
+    const got = getSelectedOptionForSearchValue('saturn', options);
+    // Assert
+    expect(got).toMatchObject(expected);
+  });
+});
+
+describe('getSelectedOptionForSearchValue', () => {
+  test('returns undefined when no matching option found for search value', () => {
+    // Act
+    const got = getSelectedOptionForSearchValue('Pluto', options);
+    // Assert
+    expect(got).toBeUndefined();
+  });
+  test('gets the first matching selected option for search value', () => {
+    // Assemble
+    const expected: EuiComboBoxOptionProps = {
+      label: 'Saturn',
+      'data-test-subj': 'saturnOption',
+    };
+    // Act
+    const got = getSelectedOptionForSearchValue('saturn', options);
+    // Assert
+    expect(got).toMatchObject(expected);
+  });
+});
+
+interface GetMatchingOptionsTestCase {
+  options: EuiComboBoxOptionProps[];
+  selectedOptions: EuiComboBoxOptionProps[];
+  searchValue: string;
+  isPreFiltered: boolean;
+  showPrevSelected: boolean;
+  expected: EuiComboBoxOptionProps[];
+}
+
+const testCases: GetMatchingOptionsTestCase[] = [
+  {
+    options,
+    selectedOptions: [
+      {
+        label: 'Saturn',
+        'data-test-subj': 'saturnOption',
+      },
+    ],
+    searchValue: 'saturn',
+    isPreFiltered: false,
+    showPrevSelected: false,
+    expected: [],
+  },
+  {
+    options,
+    selectedOptions: [
+      {
+        label: 'Saturn',
+        'data-test-subj': 'saturnOption',
+      },
+    ],
+    searchValue: 'saturn',
+    isPreFiltered: true,
+    showPrevSelected: false,
+    expected: [
+      { 'data-test-subj': 'titanOption', label: 'Titan' },
+      { label: 'Mimas' },
+    ],
+  },
+  {
+    options,
+    selectedOptions: [
+      {
+        label: 'Saturn',
+        'data-test-subj': 'saturnOption',
+      },
+    ],
+    searchValue: 'saturn',
+    isPreFiltered: false,
+    showPrevSelected: true,
+    expected: [{ 'data-test-subj': 'saturnOption', label: 'Saturn' }],
+  },
+  {
+    options,
+    selectedOptions: [
+      {
+        label: 'Saturn',
+        'data-test-subj': 'saturnOption',
+      },
+    ],
+    searchValue: 'saturn',
+    isPreFiltered: true,
+    showPrevSelected: true,
+    expected: [
+      { 'data-test-subj': 'titanOption', label: 'Titan' },
+      { 'data-test-subj': 'saturnOption', label: 'Saturn' },
+      { label: 'Mimas' },
+    ],
+  },
+];
+
+describe('getMatchingOptions', () => {
+  test.each(testCases)(
+    '.getMatchingOptions(%o)',
+    (testCase: GetMatchingOptionsTestCase) => {
+      expect(
+        getMatchingOptions(
+          testCase.options,
+          testCase.selectedOptions,
+          testCase.searchValue,
+          testCase.isPreFiltered,
+          testCase.showPrevSelected
+        )
+      ).toMatchObject(testCase.expected);
+    }
+  );
+});
