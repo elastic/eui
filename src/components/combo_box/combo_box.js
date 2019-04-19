09af7bb6a5f25fb2d5c0f58f1c4a39b0f043acd0
Chandler Prall <chandler.prall@gmail.com>
2019-04-19 10:57:39 -0600

    Also trigger the combobox invalid visual state when the dropdown array is clicked

--- src/components/combo_box/combo_box.js
+++ src/components/combo_box/combo_box.js
@@ -585,7 +585,7 @@ export class EuiComboBox extends Component {
     // Visually indicate the combobox is in an invalid state if it has lost focus but there is text entered in the input.
     // When custom options are disabled and the user leaves the combo box after entering text that does not match any
     // options, this tells the user that they've entered invalid input.
-    const markAsInvalid = isInvalid || (hasFocus === false && searchValue);
+    const markAsInvalid = isInvalid || ((hasFocus === false || isListOpen === false) && searchValue);
 
     const classes = classNames('euiComboBox', className, {
       'euiComboBox-isOpen': isListOpen,
