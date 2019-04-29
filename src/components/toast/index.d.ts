86b5e64984d60a40682d72ebd01eb903d7683f0e
Theofanis Despoudis <theo.despoudis@teckro.com>
2019-04-29 17:39:06 +0100

    `EuiGlobalToastListItem` convert to TS (#1880)
    * `EuiGlobalToastListItem` convert to TS
    
    * `EuiGlobalToastListItem` code review fixes
    
    * `EuiGlobalToastListItem` updated Changelog
    
    * `EuiGlobalToastListItem` code review fix

--- src/components/toast/index.d.ts
+++ src/components/toast/index.d.ts
@@ -1,7 +1,16 @@
+import {
+  EuiGlobalToastListItemProps as ToastListItemProps,
+  EuiGlobalToastListItem as ToastListItem,
+} from './global_toast_list_item';
 import { CommonProps } from '../common';
 import { IconType } from '../icon';
 
-import { Component, FunctionComponent, HTMLAttributes, ReactChild } from 'react';
+import {
+  Component,
+  FunctionComponent,
+  HTMLAttributes,
+  ReactChild,
+} from 'react';
 
 declare module '@elastic/eui' {
   /**
@@ -9,7 +18,9 @@ declare module '@elastic/eui' {
    *
    * @see './toast.js'
    */
-  export interface EuiToastProps extends CommonProps, HTMLAttributes<HTMLDivElement> {
+  export interface EuiToastProps
+    extends CommonProps,
+      HTMLAttributes<HTMLDivElement> {
     title?: string,
     color?: 'primary' | 'success' | 'warning' | 'danger',
     iconType?: IconType,
@@ -17,20 +28,8 @@ declare module '@elastic/eui' {
   }
 
   export const EuiToast: FunctionComponent<EuiToastProps>;
-
-
-  /**
-   * EuiGlobalToastListItem type def
-   *
-   * @see './global_toast_list_item.js'
-   */
-  export interface EuiGlobalToastListItemProps {
-    isDismissed?: boolean;
-  }
-
-  export const EuiGlobalToastListItem: FunctionComponent<
-    EuiGlobalToastListItemProps
-  >
+  export interface EuiGlobalToastListItemProps extends ToastListItemProps {}
+  export const EuiGlobalToastListItem: typeof ToastListItem;
 
   /**
    * EuiGlobalToastList type def
