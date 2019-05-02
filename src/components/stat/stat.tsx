c2372a21ca877dc532a5cdda7485529253de7ec1
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2019-05-02 12:56:46 -0300

    Fix TS

--- src/components/stat/stat.tsx
+++ src/components/stat/stat.tsx
@@ -11,6 +11,7 @@ import { EuiText } from '../text';
 import { EuiTitle, EuiTitleSize } from '../title/title';
 import { EuiI18n } from '../i18n';
 import makeId from '../form/form_row/make_id';
+import { Omit } from '../common';
 
 const colorToClassNameMap = {
   default: null,
@@ -60,7 +61,7 @@ export interface EuiStatProps {
 }
 
 export const EuiStat: FunctionComponent<
-  CommonProps & HTMLAttributes<HTMLDivElement> & EuiStatProps
+  CommonProps & Omit<HTMLAttributes<HTMLDivElement>, 'title'> & EuiStatProps
 > = ({
   children,
   className,
