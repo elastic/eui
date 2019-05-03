27fd99828520524168d429feee3a88cb3ffe4153
Theofanis Despoudis <theo.despoudis@teckro.com>
2019-05-03 16:59:43 +0100

    `EuiHeaderLogo` converted to TS (#1892)
    * `EuiHeaderLogo` converted to TS
    
    * `EuiHeaderLogo` exported types
    
    * `EuiHeaderLogo` code review
    
    * `EuiHeaderLogo` snapshots

--- src/components/header/header_logo.test.tsx
+++ src/components/header/header_logo.test.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import { render } from 'enzyme';
-import { requiredProps } from '../../test/required_props';
+import { requiredProps } from '../../test';
 
 import { EuiHeaderLogo } from './header_logo';
 
 describe('EuiHeaderLogo', () => {
   test('is rendered', () => {
-    const component = render(
-      <EuiHeaderLogo {...requiredProps} />
-    );
+    const component = render(<EuiHeaderLogo {...requiredProps} />);
 
-    expect(component)
-      .toMatchSnapshot();
+    expect(component).toMatchSnapshot();
   });
 
   test('renders href', () => {
-    const component = render(
-      <EuiHeaderLogo href="#" />
-    );
+    const component = render(<EuiHeaderLogo href="#" />);
 
-    expect(component)
-      .toMatchSnapshot();
+    expect(component).toMatchSnapshot();
   });
 
   test('renders optional props', () => {
@@ -32,7 +26,6 @@ describe('EuiHeaderLogo', () => {
       />
     );
 
-    expect(component)
-      .toMatchSnapshot();
+    expect(component).toMatchSnapshot();
   });
 });
