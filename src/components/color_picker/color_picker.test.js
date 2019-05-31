69fd64064d6fd663badcc09870846309ff795ce1
Greg Thompson <thompson.glowe@gmail.com>
2019-05-31 12:33:05 -0500

    sinon -> jest

--- src/components/color_picker/color_picker.test.js
+++ src/components/color_picker/color_picker.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, mount } from 'enzyme';
-import sinon from 'sinon';
 
 import { EuiColorPicker } from './color_picker';
 import { VISUALIZATION_COLORS, keyCodes } from '../../services';
@@ -13,7 +12,7 @@ jest.mock('../portal', () => ({
 let onChange;
 
 beforeEach(() => {
-  onChange = sinon.spy();
+  onChange = jest.fn();
 });
 
 test('renders EuiColorPicker', () => {
@@ -102,7 +101,7 @@ test('popover color selector is not shown by default', () => {
 });
 
 test('popover color selector is shown when the input is clicked', () => {
-  const onFocusHandler = sinon.stub();
+  const onFocusHandler = jest.fn();
   const colorPicker = mount(
     <EuiColorPicker
       onChange={onChange}
@@ -113,13 +112,13 @@ test('popover color selector is shown when the input is clicked', () => {
   );
 
   findTestSubject(colorPicker, 'colorPickerAnchor').simulate('click');
-  sinon.assert.calledOnce(onFocusHandler);
+  expect(onFocusHandler).toBeCalled();
   const colorSelector = findTestSubject(colorPicker, 'colorPickerPopover');
   expect(colorSelector.length).toBe(1);
 });
 
 test('popover color selector is hidden when the ESC key pressed', () => {
-  const onBlurHandler = sinon.stub();
+  const onBlurHandler = jest.fn();
   const colorPicker = mount(
     <EuiColorPicker
       onChange={onChange}
@@ -134,11 +133,11 @@ test('popover color selector is hidden when the ESC key pressed', () => {
     keyCode: keyCodes.ESCAPE,
   });
   // Portal removal not working with Jest. The blur handler is called just before the portal would be removed.
-  sinon.assert.calledOnce(onBlurHandler);
+  expect(onBlurHandler).toBeCalled();
 });
 
 test('popover color selector is hidden and input regains focus when the ENTER key pressed', () => {
-  const onBlurHandler = sinon.stub();
+  const onBlurHandler = jest.fn();
   const colorPicker = mount(
     <EuiColorPicker
       onChange={onChange}
@@ -157,7 +156,7 @@ test('popover color selector is hidden and input regains focus when the ENTER ke
     findTestSubject(colorPicker, 'colorPickerAnchor').getDOMNode()
   ).toEqual(document.activeElement);
   // Portal removal not working with Jest. The blur handler is called just before the portal would be removed.
-  sinon.assert.calledOnce(onBlurHandler);
+  expect(onBlurHandler).toBeCalled();
 });
 
 test('Setting a new color calls onChange', () => {
@@ -170,8 +169,8 @@ test('Setting a new color calls onChange', () => {
   const inputs = colorPicker.find('input[type="text"]');
   expect(inputs.length).toBe(1);
   inputs.simulate('change', event);
-  sinon.assert.calledOnce(onChange);
-  sinon.assert.calledWith(onChange, '#000000');
+  expect(onChange).toBeCalled();
+  expect(onChange).toBeCalledWith('#000000');
 });
 
 test('Clicking a swatch calls onChange', () => {
@@ -183,8 +182,8 @@ test('Clicking a swatch calls onChange', () => {
   const swatches = colorPicker.find('button.euiColorPicker__swatchSelect');
   expect(swatches.length).toBe(VISUALIZATION_COLORS.length);
   swatches.first().simulate('click');
-  sinon.assert.calledOnce(onChange);
-  sinon.assert.calledWith(onChange, VISUALIZATION_COLORS[0]);
+  expect(onChange).toBeCalled();
+  expect(onChange).toBeCalledWith(VISUALIZATION_COLORS[0]);
 });
 
 test('default mode does redners child components', () => {
