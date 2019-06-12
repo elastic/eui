4c4aa5018b6561a46d4a65edf09abae6bdef10a4
dave.snider@gmail.com <dave.snider@gmail.com>
2019-06-12 12:06:59 -0700

    Change callout docs to using alert icon, aws logo updated for dark mode (#2036)
    * Change callout docs to using alert icon
    * Also changed the aws logo to work better in dark mode

--- src-docs/src/views/call_out/danger.js
+++ src-docs/src/views/call_out/danger.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { EuiCallOut, EuiLink } from '../../../../src/components';
 
 export default () => (
-  <EuiCallOut title="Sorry, there was an error" color="danger" iconType="cross">
+  <EuiCallOut title="Sorry, there was an error" color="danger" iconType="alert">
     <p>
       Now you have to fix it, but maybe{' '}
       <EuiLink href="#">this link can help</EuiLink>.
