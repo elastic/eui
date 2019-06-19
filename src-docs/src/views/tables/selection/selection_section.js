faa9c72ea5ca4f17068241e137e622582dc89e04
rockfield <philipp.b@ya.ru>
2019-06-19 13:01:14 +0300

    Reverting docs back

--- src-docs/src/views/tables/selection/selection_section.js
+++ src-docs/src/views/tables/selection/selection_section.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { EuiBasicTable, EuiCode } from '../../../../../src/components';
 import { GuideSectionTypes } from '../../../components';
 import { renderToHtml } from '../../../services';
