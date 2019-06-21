a11b00b71bfb332744ef0bee3833b94aba5c0da6
Ryan Keairns <contactryank@gmail.com>
2019-06-21 08:47:05 -0500

    adds editor align and dist glyphs (#2070)

---
+++ src/components/icon/assets/editorItemAlignRight.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const EuiIconEditorItemAlignRight = props => (
+  <svg
+    width={16}
+    height={16}
+    viewBox="0 0 16 16"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}>
+    <path d="M3 5h8a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2zm11.5-4a.5.5 0 0 1 .5.5v13a.5.5 0 1 1-1 0v-13a.5.5 0 0 1 .5-.5z" />
+  </svg>
+);
+
+export const icon = EuiIconEditorItemAlignRight;
