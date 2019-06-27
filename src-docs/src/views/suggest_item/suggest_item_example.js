c21843b23e92ce612fb38f4ddb351404713a5dd4
Andrea Del Rio <delrio.andre@gmail.com>
2019-06-27 14:12:00 -0700

    added expandLongLabel

--- src-docs/src/views/suggest_item/suggest_item_example.js
+++ src-docs/src/views/suggest_item/suggest_item_example.js
@@ -10,8 +10,12 @@ import SuggestItem from './suggest_item';
 const suggestItemSource = require('!!raw-loader!./suggest_item');
 const suggestItemHtml = renderToHtml(SuggestItem);
 
+import SuggestItemLongLabel from './suggest_item_long_label';
+const suggestItemLongLabelSource = require('!!raw-loader!./suggest_item_long_label');
+const suggestItemLongLabelHtml = renderToHtml(SuggestItemLongLabel);
+
 export const SuggestItemExample = {
-  title: 'SuggestItem',
+  title: 'Suggest Item',
   sections: [
     {
       title: 'SuggestItem',
@@ -26,13 +30,55 @@ export const SuggestItemExample = {
         },
       ],
       text: (
-        <p>
-          Description needed: how to use the <EuiCode>EuiSuggestItem</EuiCode>{' '}
-          component.
-        </p>
+        <div>
+          <p>
+            <EuiCode>EuiSuggestItem</EuiCode> is a list item component useful to
+            suggestions when typing queries in input fields.
+          </p>
+          <ul>
+            <li>
+              <EuiCode>type</EuiCode> is an object that takes{' '}
+              <EuiCode>icon</EuiCode> and <EuiCode>color</EuiCode>.{' '}
+              <EuiCode>icon</EuiCode> supports any of the icons available in
+              <EuiCode>EuiIcon</EuiCode>. <EuiCode>color</EuiCode> is the color
+              for the icon and its background. The background color is
+              automatically calculated and it is a ligther shade of
+              <EuiCode>color</EuiCode>.
+            </li>
+            <li>
+              <EuiCode>label</EuiCode> is the primary text for the suggestion.
+            </li>
+            <li>
+              <EuiCode>description</EuiCode> is the secondary text for the
+              suggestion and it is optional.
+            </li>
+          </ul>
+        </div>
       ),
       props: { EuiSuggestItem },
       demo: <SuggestItem />,
+    },
+    {
+      title: 'Expand long label',
+      source: [
+        {
+          type: GuideSectionTypes.JS,
+          code: suggestItemLongLabelSource,
+        },
+        {
+          type: GuideSectionTypes.HTML,
+          code: suggestItemLongLabelHtml,
+        },
+      ],
+      text: (
+        <p>
+          By default <EuiCode>EuiSuggestItem</EuiCode>'s{' '}
+          <EuiCode>label</EuiCode> will use ellipsis. It's possible to show the
+          full text by using <EuiCode>expandLongLabel</EuiCode>.
+        </p>
+      ),
+      props: { EuiSuggestItem },
+      demo: <SuggestItemLongLabel />,
     },
   ],
 };
---
+++ src-docs/src/views/suggest_item/suggest_item_long_label.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { EuiSuggestItem } from '../../../../src/components/suggest_item';
+
+const shortDescription = 'This is the description';
+
+const sampleItem = {
+  type: { icon: 'kqlValue', color: 'secondary' },
+  label: '"Charles de Gaulle International Airport" ',
+  description: shortDescription,
+};
+
+export default () => (
+  <div>
+    <EuiSuggestItem
+      type={sampleItem.type}
+      label={sampleItem.label}
+      description={sampleItem.description}
+    />
+    <EuiSuggestItem
+      type={sampleItem.type}
+      expandLongLabel={true}
+      label={sampleItem.label}
+      description={sampleItem.description}
+    />
+  </div>
+);
