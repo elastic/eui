b5820112b20da9b1eb8e32f696fc955325381c92
Greg Thompson <thompson.glowe@gmail.com>
2019-07-03 12:31:11 -0500

    tests passing

--- src/components/suggest_item/suggest_item.test.js
+++ src/components/suggest_item/suggest_item.test.js
@@ -4,9 +4,14 @@ import { requiredProps } from '../../test/required_props';
 
 import { EuiSuggestItem } from './suggest_item';
 
+const TYPE = {
+  icon: 'search',
+  color: 'primary',
+};
+
 describe('EuiSuggestItem', () => {
   test('is rendered', () => {
-    const component = render(<EuiSuggestItem {...requiredProps} />);
+    const component = render(<EuiSuggestItem {...requiredProps} type={TYPE} />);
 
     expect(component).toMatchSnapshot();
   });
