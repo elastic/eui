aabbcfd6e339226a26241dec083f60a8996b97ee
Andrea Del Rio <delrio.andre@gmail.com>
2019-07-05 09:10:22 -0700

    fixed typo and renamed variables

--- src-docs/src/views/suggest_input/suggest_input_example.js
+++ src-docs/src/views/suggest_input/suggest_input_example.js
@@ -32,23 +32,15 @@ export const SuggestInputExample = {
             use when performing queries that will display suggestions. When
             inputting a query, <EuiCode>EuiSuggestInput</EuiCode> will show the
             status of that query (no new changes, changes saved, etc). Based on
-            that status the user can take action (e.g. add query as Saved
+            that status the user can take action (e.g. add query as a Saved
             Query).
           </p>
           <ul>
             <li>
-              <EuiCode>status</EuiCode> is an object that takes{' '}
-              <EuiCode>icon</EuiCode>, <EuiCode>color</EuiCode>,{' '}
-              <EuiCode>label</EuiCode> and <EuiCode>tooltip</EuiCode>.{' '}
-              <EuiCode>icon</EuiCode> supports any of the icons available in
-              <EuiCode>EuiIcon</EuiCode>. <EuiCode>color</EuiCode> is the color
-              for the icon. <EuiCode>label</EuiCode> is the text to display next
-              to the icon. <EuiCode>tooltip</EuiCode> is optional and can be
-              added to the status to guide the user on what action to perform
-              next.
+              <EuiCode>status</EuiCode> can take any of the following values: &apos;notYetSaved&apos;, &apos;saved&apos;, &apos;noNewChanges&apos; and &apos;isLoading&apos;.
             </li>
             <li>
-              <EuiCode>value</EuiCode> is the current value on the input field.
+              <EuiCode>value</EuiCode> is the current value of the input field.
             </li>
           </ul>
         </div>
