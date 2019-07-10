aebfc8d1e3abb228a10752c96a32174c36fb2aa0
Chandler Prall <chandler.prall@gmail.com>
2019-07-10 13:58:10 -0600

    Close EuiComboBox's options list on scroll

--- src-docs/src/views/combo_box/combo_box.js
+++ src-docs/src/views/combo_box/combo_box.js
@@ -82,15 +82,24 @@ export default class extends Component {
   render() {
     const { selectedOptions } = this.state;
     return (
-      <EuiComboBox
-        placeholder="Select or create options"
-        options={this.options}
-        selectedOptions={selectedOptions}
-        onChange={this.onChange}
-        onCreateOption={this.onCreateOption}
-        isClearable={true}
-        data-test-subj="demoComboBox"
-      />
+      <div>
+        <div style={{
+          height: '200px',
+          overflowY: 'scroll'
+        }}>
+          <div style={{height: '50px'}}/>
+          <EuiComboBox
+            placeholder="Select or create options"
+            options={this.options}
+            selectedOptions={selectedOptions}
+            onChange={this.onChange}
+            onCreateOption={this.onCreateOption}
+            isClearable={true}
+            data-test-subj="demoComboBox"
+          />
+          <div style={{height: '500px'}}/>
+        </div>
+      </div>
     );
   }
 }
