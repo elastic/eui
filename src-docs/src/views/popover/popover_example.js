9a28ca4a1343d49963cb199c3d62ec7cd8878501
cchaos <caroline.horn@elastic.co>
2019-07-12 11:52:27 -0400

    prettier

--- src-docs/src/views/popover/popover_example.js
+++ src-docs/src/views/popover/popover_example.js
@@ -376,10 +376,10 @@ export const PopoverExample = {
       text: (
         <div>
           <p>
-            Popover anchors default to{' '}
-            <EuiCode>display: inline-block</EuiCode> so they do not force block
-            display on inline triggers. If you do need to change this to block
-            display, just add <EuiCode>display=&quot;block&quot;</EuiCode>
+            Popover anchors default to <EuiCode>display: inline-block</EuiCode>{' '}
+            so they do not force block display on inline triggers. If you do
+            need to change this to block display, just add{' '}
+            <EuiCode>display=&quot;block&quot;</EuiCode>
           </p>
         </div>
       ),
