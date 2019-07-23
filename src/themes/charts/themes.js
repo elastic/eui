0c118783da86d8fa65f50b79d4367de8d9cbe97f
cchaos <caroline.horn@elastic.co>
2019-07-23 13:31:21 -0400

    Update elastic-charts to 8.1.0 and fix theme

--- src/themes/charts/themes.js
+++ src/themes/charts/themes.js
@@ -4,8 +4,6 @@ import { DEFAULT_VISUALIZATION_COLOR } from '../../services/color/visualization_
 import lightColors from '!!sass-vars-to-js-loader!../../global_styling/variables/_colors.scss';
 import darkColors from '!!sass-vars-to-js-loader!../../themes/eui/eui_colors_dark.scss';
 
-import { mergeWithDefaultTheme } from '@elastic/charts';
-
 const fontFamily = `'Inter UI', -apple-system, BlinkMacSystemFont,
   'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'`;
 
@@ -23,7 +21,7 @@ function createTheme(colors) {
       opacity: 1,
       dash: [4, 4],
     },
-    theme: mergeWithDefaultTheme({
+    theme: {
       chartMargins: {
         left: 0,
         right: 0,
@@ -32,74 +30,52 @@ function createTheme(colors) {
       },
       lineSeriesStyle: {
         line: {
-          visible: true,
-          stroke: DEFAULT_VISUALIZATION_COLOR,
           strokeWidth: 2,
         },
         point: {
-          visible: true,
-          stroke: DEFAULT_VISUALIZATION_COLOR,
+          fill: colors.euiColorEmptyShade.rgba,
           strokeWidth: 2,
           radius: 3,
-          opacity: 1,
         },
       },
       areaSeriesStyle: {
         area: {
-          visible: true,
-          fill: DEFAULT_VISUALIZATION_COLOR,
           opacity: 0.3,
         },
         line: {
-          visible: true,
-          stroke: DEFAULT_VISUALIZATION_COLOR,
           strokeWidth: 2,
         },
         point: {
           visible: false,
-          stroke: DEFAULT_VISUALIZATION_COLOR,
+          fill: colors.euiColorEmptyShade.rgba,
           strokeWidth: 2,
           radius: 3,
-          opacity: 1,
         },
       },
       barSeriesStyle: {
-        border: {
-          visible: false,
-          stroke: colors.euiColorEmptyShade.rgba,
-          strokeWidth: 1,
-        },
         displayValue: {
           fontSize: 8,
           fontFamily: fontFamily,
-          fontStyle: 'normal',
           fill: colors.euiColorDarkShade.rgba,
-          padding: 0,
-          offsetX: 0,
-          offsetY: 0,
         },
       },
       axes: {
         axisTitleStyle: {
           fontSize: 10,
-          fontStyle: 'bold',
           fontFamily: fontFamily,
-          padding: 5,
           fill: colors.euiColorDarkShade.rgba,
         },
         axisLineStyle: {
           stroke: colors.euiColorChartLines.rgba,
-          strokeWidth: 1,
         },
         tickLabelStyle: {
           fontSize: 8,
           fontFamily: fontFamily,
-          fontStyle: 'normal',
           fill: colors.euiColorDarkShade.rgba,
           padding: 2,
         },
         tickLineStyle: {
-          stroke: '#00000000', // transparent
+          stroke: 'rgba(0,0,0,0)', // transparent
           strokeWidth: 0,
         },
       },
@@ -109,17 +85,15 @@ function createTheme(colors) {
       },
       crosshair: {
         band: {
-          visible: true,
           fill: colors.euiColorChartBand.rgba,
         },
         line: {
-          visible: true,
           stroke: colors.euiColorDarkShade.rgba,
           strokeWidth: 1,
           dash: [4, 4],
         },
       },
-    }),
+    },
   };
 }
 
