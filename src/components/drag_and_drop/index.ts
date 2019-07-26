31cb63f150445e349bca111fa70a1b631598acc7
Chandler Prall <chandler.prall@gmail.com>
2019-07-26 10:17:43 -0600

    Export used react-beautiful-dnd interfaces through EUI

--- src/components/drag_and_drop/index.ts
+++ src/components/drag_and_drop/index.ts
@@ -2,3 +2,15 @@ export { EuiDragDropContext } from './drag_drop_context';
 export { EuiDraggable } from './draggable';
 export { EuiDroppable } from './droppable';
 export { copy, move, reorder } from './services';
+
+// Interfaces in react-beautiful-dnd that EUI abstracts over
+// allows consumers to pull these from EUI instead of react-beautiful-dnd
+export {
+  DraggableLocation,
+  DraggableProps,
+  DragDropContextProps,
+  DragStart,
+  DroppableProps,
+  DropResult,
+  ResponderProvided,
+} from 'react-beautiful-dnd';
