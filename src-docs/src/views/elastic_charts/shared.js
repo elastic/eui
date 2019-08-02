d7557cc68d20d6df86f4566bcbb3f4e725d3959b
cchaos <caroline.horn@elastic.co>
2019-08-02 15:35:34 -0400

    Upgrade charts

--- src-docs/src/views/elastic_charts/shared.js
+++ src-docs/src/views/elastic_charts/shared.js
@@ -22,9 +22,9 @@ export const ExternalBadge = () => {
       iconType="popout"
       iconSide="right"
       onClick={() =>
-        window.open('https://github.com/elastic/elastic-charts/tree/v8.1.0')
+        window.open('https://github.com/elastic/elastic-charts/tree/v8.1.5')
       }>
-      External library: elastic-charts v.8.1.0
+      External library: elastic-charts v.8.1.5
     </EuiBadge>
   );
 };
