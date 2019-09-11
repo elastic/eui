e0446e37f59504b0412ec0d6560e22daebe2fb22
Rory Hunter <roryhunter2@gmail.com>
2019-09-11 16:48:40 +0100

    Migrate EuiImage to TS

--- src/components/image/image.tsx
+++ src/components/image/image.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import { CommonProps } from '../common';
 import { EuiOverlayMask } from '../overlay_mask';
 
 import { EuiIcon } from '../icon';
@@ -10,7 +10,9 @@ import { EuiFocusTrap } from '../focus_trap';
 
 import { keyCodes } from '../../services';
 
-const sizeToClassNameMap = {
+type ImageSize = 's' | 'm' | 'l' | 'xl' | 'fullWidth' | 'original';
+
+const sizeToClassNameMap: { [size in ImageSize]: string } = {
   s: 'euiImage--small',
   m: 'euiImage--medium',
   l: 'euiImage--large',
@@ -21,21 +23,33 @@ const sizeToClassNameMap = {
 
 export const SIZES = Object.keys(sizeToClassNameMap);
 
-const fullScreenIconColorMap = {
+type FullScreenIconColor = 'light' | 'dark';
+
+const fullScreenIconColorMap: { [color in FullScreenIconColor]: string } = {
   light: 'ghost',
   dark: 'default',
 };
 
-export class EuiImage extends Component {
-  constructor(props) {
-    super(props);
+interface EuiImageProps extends CommonProps {
+  alt: string;
+  size: ImageSize;
+  fullScreenIconColor?: FullScreenIconColor;
+  url: string;
+  caption?: string;
+  hasShadow?: boolean;
+  allowFullScreen?: boolean;
+}
 
-    this.state = {
-      isFullScreen: false,
-    };
-  }
+interface State {
+  isFullScreen: boolean;
+}
 
-  onKeyDown = event => {
+export class EuiImage extends Component<EuiImageProps, State> {
+  state: State = {
+    isFullScreen: false,
+  };
+
+  onKeyDown = (event: React.KeyboardEvent) => {
     if (event.keyCode === keyCodes.ESCAPE) {
       event.preventDefault();
       event.stopPropagation();
@@ -91,7 +105,11 @@ export class EuiImage extends Component {
       optionalIcon = (
         <EuiIcon
           type="fullScreen"
-          color={fullScreenIconColorMap[fullScreenIconColor]}
+          color={
+            fullScreenIconColor
+              ? fullScreenIconColorMap[fullScreenIconColor]
+              : undefined
+          }
           className="euiImage__icon"
         />
       );
@@ -107,11 +125,7 @@ export class EuiImage extends Component {
               type="button"
               onClick={this.closeFullScreen}
               onKeyDown={this.onKeyDown}>
-              <figure
-                ref={node => {
-                  this.figure = node;
-                }}
-                className="euiImageFullScreen">
+              <figure className="euiImageFullScreen">
                 <img src={url} className="euiImageFullScreen__img" alt={alt} />
                 {optionalCaption}
               </figure>
@@ -140,14 +154,3 @@ export class EuiImage extends Component {
     );
   }
 }
-
-EuiImage.propTypes = {
-  alt: PropTypes.string.isRequired,
-  size: PropTypes.string.isRequired,
-  fullScreenIconColor: PropTypes.string,
-};
-
-EuiImage.defaultProps = {
-  size: 'original',
-  fullScreenIconColor: 'light',
-};
