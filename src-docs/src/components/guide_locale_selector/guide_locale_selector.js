eab1da26483459edb23677871b69e1e23de75e7a
Chandler Prall <chandler.prall@gmail.com>
2019-09-27 13:16:40 -0600

    Allow LANG="en_US.UTF-8" LC_COLLATE="en_US.UTF-8" LC_CTYPE="en_US.UTF-8" LC_MESSAGES="en_US.UTF-8" LC_MONETARY="en_US.UTF-8" LC_NUMERIC="en_US.UTF-8" LC_TIME="en_US.UTF-8" LC_ALL= to be set in i18n context, configures EuiDatePicker to read from that context

--- src-docs/src/components/guide_locale_selector/guide_locale_selector.js
+++ src-docs/src/components/guide_locale_selector/guide_locale_selector.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import moment from 'moment';
+
+// For EuiDatePicker, use the French configuration when using en-xa locale
+moment.defineLocale('en-xa', moment.localeData('fr')._config);
+
 import { EuiSwitch, EuiFormRow } from '../../../../src/components';
 
 export const GuideLocaleSelector = ({ selectedLocale, onToggleLocale }) => {
