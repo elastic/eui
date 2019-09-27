7cecc83b3f5af2f9ce08f1e21a6f6ec21af63f38
Greg Thompson <thompson.glowe@gmail.com>
2019-09-27 16:35:54 -0500

    docs

--- src-docs/src/views/selectable/selectable_search.tsx
+++ src-docs/src/views/selectable/selectable_search.tsx
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react';
 
+// @ts-ignore
+import { EuiLink } from '../../../../src/components/link';
+
 import { EuiSelectable } from '../../../../src/components/selectable';
 import { Option } from '../../../../src/components/selectable/types';
 import { Options } from './data';
@@ -23,20 +26,33 @@ export default class extends Component<{}, { options: Option[] }> {
     const { options } = this.state;
 
     return (
-      <EuiSelectable
-        searchable
-        searchProps={{
-          'data-test-subj': 'selectableSearchHere',
-        }}
-        options={options}
-        onChange={this.onChange}>
-        {(list, search) => (
-          <Fragment>
-            {search}
-            {list}
-          </Fragment>
-        )}
-      </EuiSelectable>
+      <Fragment>
+        <EuiLink
+          onClick={() => {
+            this.setState({
+              options: Options.map(option => ({
+                ...option,
+                checked: undefined,
+              })),
+            });
+          }}>
+          De-select all
+        </EuiLink>
+        <EuiSelectable
+          searchable
+          searchProps={{
+            'data-test-subj': 'selectableSearchHere',
+          }}
+          options={options}
+          onChange={this.onChange}>
+          {(list, search) => (
+            <Fragment>
+              {search}
+              {list}
+            </Fragment>
+          )}
+        </EuiSelectable>
+      </Fragment>
     );
   }
 }
