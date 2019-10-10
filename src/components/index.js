77b4ea03fe7b00af623eefc841dcb21ae06603d3
Chandler Prall <chandler.prall@gmail.com>
2019-10-10 10:02:12 -0600

    Export useRenderToText to top-level package

--- src/components/index.js
+++ src/components/index.js
@@ -150,7 +150,7 @@ export { ICON_TYPES, EuiIcon } from './icon';
 
 export { EuiImage } from './image';
 
-export { useInnerText, EuiInnerText } from './inner_text';
+export { useInnerText, EuiInnerText, useRenderToText } from './inner_text';
 
 export { EuiI18n, EuiI18nNumber } from './i18n';
 
