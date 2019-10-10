7fc21db9066ba306f3bd9f96393048e171790e45
Chandler Prall <chandler.prall@gmail.com>
2019-10-10 15:42:37 -0600

    Internationalize some text

--- src/components/datagrid/column_selector.tsx
+++ src/components/datagrid/column_selector.tsx
@@ -99,15 +99,24 @@ export const useColumnSelector = (
       }>
       <div>
         <EuiPopoverTitle>
-          <EuiFieldText
-            compressed
-            placeholder="Search"
-            aria-label="Search columns"
-            value={columnSearchText}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setColumnSearchText(e.currentTarget.value)
-            }
-          />
+          <EuiI18n
+            tokens={[
+              'euiColumnSelector.search',
+              'euiColumnSelector.searchcolumns',
+            ]}
+            defaults={['Search', 'Search columns']}>
+            {([search, searchcolumns]: string[]) => (
+              <EuiFieldText
+                compressed
+                placeholder={search}
+                aria-label={searchcolumns}
+                value={columnSearchText}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setColumnSearchText(e.currentTarget.value)
+                }
+              />
+            )}
+          </EuiI18n>
         </EuiPopoverTitle>
         <EuiDragDropContext onDragEnd={onDragEnd}>
           <EuiDroppable
