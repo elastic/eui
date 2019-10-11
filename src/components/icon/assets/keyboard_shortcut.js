a14f5ef7eba8a32d4fab519f49892b3f64b82dbc
defazio <michael.defazio@elastic.co>
2019-10-11 10:56:31 -0400

    Fix svg code

--- src/components/icon/assets/keyboard_shortcut.js
+++ src/components/icon/assets/keyboard_shortcut.js
@@ -7,11 +7,7 @@ const EuiIconKeyboardShortcut = props => (
     viewBox="0 0 16 16"
     xmlns="http://www.w3.org/2000/svg"
     {...props}>
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M1 1h14v5H1V1zM0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm2 3h6v1H2V4zm1 6H2v2H0v1h2v2h1v-2h2v-1H3v-2zm11 3H8v1h6v-1zm1-3H7v5h8v-5zM7 9a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1H7z"
-    />
+    <path d="M15 9a1 1 0 0 1 .993.883L16 10v5a1 1 0 0 1-.883.993L15 16H7a1 1 0 0 1-.993-.883L6 15v-5a1 1 0 0 1 .883-.993L7 9h8zM2.5 10a.5.5 0 0 1 .492.41L3 10.5V12h1.5a.5.5 0 0 1 .09.992L4.5 13H3v1.5a.5.5 0 0 1-.992.09L2 14.5V13H.5a.5.5 0 0 1-.09-.992L.5 12H2v-1.5a.5.5 0 0 1 .5-.5zM15 10H7v5h8v-5zm-1 3v1H8v-1h6zm1-13a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1a1 1 0 0 1 1-1h14zm0 1H1v5h14V1zM8 4v1H2V4h6z" />
   </svg>
 );
 
