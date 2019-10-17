a40a0d4d880f01fb8871decaf9ce875373fea95d
Andrea Del Rio <delrio.andre@gmail.com>
2019-10-17 12:33:31 -0500

    added test and fix doc example

--- src-docs/src/views/badge/badge.js
+++ src-docs/src/views/badge/badge.js
@@ -16,14 +16,17 @@ const badges = [
   'danger',
   '#000',
   '#fea27f',
+  'disabled',
 ];
 
 export default () => (
   <EuiFlexGroup wrap responsive={false} gutterSize="xs" style={{ width: 300 }}>
     {badges.map(badge => (
       <EuiFlexItem grow={false} key={badge}>
-        <EuiBadge isDisabled={badge === '#fea27f' ? true : false} color={badge}>
-          {badge === '#fea27f' ? 'disabled' : badge}
+        <EuiBadge
+          isDisabled={badge === 'disabled' ? true : false}
+          color={badge}>
+          {badge}
         </EuiBadge>
       </EuiFlexItem>
     ))}
--- src-docs/src/views/badge/notification_badge.tsx
+++ src-docs/src/views/badge/notification_badge.tsx
@@ -2,8 +2,4 @@ import React from 'react';
 
 import { EuiNotificationBadge } from '../../../../src/components/badge/notification_badge';
 
-export default () => (
-  <div>
-    <EuiNotificationBadge>3</EuiNotificationBadge>
-  </div>
-);
+export default () => <EuiNotificationBadge>3</EuiNotificationBadge>;
