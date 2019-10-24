aa064f791538f6b8e384b5e8467c170ebd691935
Greg Thompson <thompson.glowe@gmail.com>
2019-10-24 18:30:22 -0500

    fix misc docs proptype warnings

--- src-docs/src/views/tables/paginated/paginated.js
+++ src-docs/src/views/tables/paginated/paginated.js
@@ -154,6 +154,7 @@ export class Table extends Component {
     return (
       <div>
         <EuiSwitch
+          checked={this.state.showPerPageOptions}
           label={
             <span>
               Hide per page options with{' '}
