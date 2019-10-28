c967c4ec462ece4a99b8a3daed24a49adb852a23
cchaos <caroline.horn@elastic.co>
2019-10-28 17:16:15 -0400

    [incomplete] Starting on tests

--- src/components/control_bar/control_bar.test.tsx
+++ src/components/control_bar/control_bar.test.tsx
@@ -4,35 +4,76 @@ import { requiredProps, takeMountedSnapshot } from '../../test';
 
 import { EuiControlBar, Control } from './control_bar';
 
-const handleClick = () => {
-  console.log('You clicked');
-};
+// const handleClick = () => {
+//   console.log('You clicked');
+// };
 
 const controls: Control[] = [
   {
-    id: 'sound_the_alarm',
-    label: 'Sound the Alarm',
-    controlType: 'button',
-    onClick: handleClick,
+    id: 'current_file_path',
+    controlType: 'breadcrumbs',
+    responsive: true,
+    breadcrumbs: [
+      {
+        text: 'src',
+      },
+      {
+        text: 'components',
+      },
+    ],
   },
+  // {
+  //   id: 'sound_the_alarm',
+  //   label: 'Sound the Alarm',
+  //   controlType: 'button',
+  //   onClick: handleClick,
+  // },
+  // {
+  //   id: 'close_the_hatch',
+  //   label: 'Close the Hatch',
+  //   controlType: 'button',
+  //   onClick: handleClick,
+  // },
   {
+    controlType: 'text',
     id: 'close_the_hatch',
-    label: 'Close the Hatch',
-    controlType: 'button',
-    onClick: handleClick,
+    text: 'Close the Hatch',
+    'data-test-subj': 'dts',
   },
+  // {
+  //   controlType: 'icon',
+  //   id: 'sample_icon',
+  //   iconType: 'alert',
+  //   `aria-label`: 'Sample Icon',
+  // },
   {
-    id: 'sample_icon',
-    label: 'Sample Icon',
-    controlType: 'icon',
-    iconType: 'alert',
+    controlType: 'spacer',
   },
+  // {
+  //   id: 'status_icon',
+  //   label: 'Repo Status',
+  //   controlType: 'icon',
+  //   iconType: 'alert',
+  //   color: 'warning',
+  // },
 ];
 
 describe('EuiControlBar', () => {
   test('is rendered', () => {
     const component = takeMountedSnapshot(
       mount(<EuiControlBar controls={controls} {...requiredProps} />)
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe('mobile control bar', () => {
+  test('is rendered', () => {
+    const component = takeMountedSnapshot(
+      mount(
+        <EuiControlBar controls={controls} showOnMobile {...requiredProps} />
+      )
     );
 
     expect(component).toMatchSnapshot();
