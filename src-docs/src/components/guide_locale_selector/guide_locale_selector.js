a0091e69adc42faed525452df0f444d5b3c79df1
Chandler Prall <chandler.prall@gmail.com>
2019-11-13 10:26:53 -0700

    Make docs' babelfish mode use pseudo-locale text in EuiDatePicker

--- src-docs/src/components/guide_locale_selector/guide_locale_selector.js
+++ src-docs/src/components/guide_locale_selector/guide_locale_selector.js
@@ -2,9 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import moment from 'moment';
+import { translateUsingPseudoLocale } from '../../../src/services/string/pseudo_locale_translator';
 
-// For EuiDatePicker, use the French configuration when using en-xa locale
-moment.defineLocale('en-xa', moment.localeData('fr')._config);
+// For testing/demoing EuiDatePicker, process moment's `en` locale config into a babelfished version
+const enConfig = moment.localeData('en')._config;
+moment.defineLocale('en-xa', {
+  ...enConfig,
+  months: enConfig.months.map(translateUsingPseudoLocale),
+  monthsShort: enConfig.monthsShort.map(translateUsingPseudoLocale),
+  weekdays: enConfig.weekdays.map(translateUsingPseudoLocale),
+  weekdaysMin: enConfig.weekdaysMin.map(translateUsingPseudoLocale),
+  weekdaysShort: enConfig.weekdaysShort.map(translateUsingPseudoLocale),
+});
 
 import { EuiSwitch, EuiFormRow } from '../../../../src/components';
 
