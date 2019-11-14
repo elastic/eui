8c5688cc18ef75bb910ee9197331ce385277b063
Greg Thompson <thompson.glowe@gmail.com>
2019-11-14 11:34:11 -0600

    less logic for disabled badge

--- src-docs/src/views/badge/badge.js
+++ src-docs/src/views/badge/badge.js
@@ -16,22 +16,17 @@ const badges = [
   'danger',
   '#000',
   '#fea27f',
-  '0000FF',
 ];
 
 export default () => (
   <EuiFlexGroup wrap responsive={false} gutterSize="xs" style={{ width: 300 }}>
-    {badges.map(badge => {
-      const isDisabled = badge === '0000FF';
-      return (
-        <EuiFlexItem grow={false} key={badge}>
-          <EuiBadge
-            isDisabled={isDisabled ? true : false}
-            color={isDisabled ? undefined : badge}>
-            {isDisabled ? 'disabled' : badge}
-          </EuiBadge>
-        </EuiFlexItem>
-      );
-    })}
+    {badges.map(badge => (
+      <EuiFlexItem grow={false} key={badge}>
+        <EuiBadge color={badge}>{badge}</EuiBadge>
+      </EuiFlexItem>
+    ))}
+    <EuiFlexItem grow={false}>
+      <EuiBadge isDisabled={true}>disabled</EuiBadge>
+    </EuiFlexItem>
   </EuiFlexGroup>
 );
