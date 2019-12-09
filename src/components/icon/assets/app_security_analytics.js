a9a21cfc4ec8238d312bdcf3cc34704d332337d1
Patryk Kopyciński <contact@patrykkopycinski.com>
2019-12-09 19:07:23 +0100

    fix dark mode

--- src/components/icon/assets/app_security_analytics.js
+++ src/components/icon/assets/app_security_analytics.js
@@ -8,17 +8,21 @@ const EuiIconAppSecurityAnalytics = ({ title, ...props }) => (
     xmlns="http://www.w3.org/2000/svg"
     {...props}>
     <title>{title}</title>
-    <g fill="none" fillRule="nonzero">
+    <g fill="none" fillRule="evenodd">
       <path
+        className="euiIcon__fillNegative"
         fill="#535966"
+        fillRule="nonzero"
         d="M10 12c5.43 0 9.848 4.327 9.996 9.72L20 22v1h-9v9h-1C4.477 32 0 27.523 0 22s4.477-10 10-10zm-1 9v-6.938a8.001 8.001 0 00-.25 15.84l.25.036V14.062 21zm2-6.938V21h6.938a8.005 8.005 0 00-6.68-6.902l-.257-.036z"
       />
       <path
         fill="#00BFB3"
+        fillRule="nonzero"
         d="M26 22c0-8.837-7.163-16-16-16v2c7.732 0 14 6.268 14 14h2z"
       />
       <path
         fill="#00BFB3"
+        fillRule="nonzero"
         d="M32 22C32 9.85 22.15 0 10 0v2c11.046 0 20 8.954 20 20h2z"
       />
     </g>
--- src/components/icon/assets/logo_security.js
+++ src/components/icon/assets/logo_security.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 const EuiIconLogoSecurity = ({ title, ...props }) => (
-  <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg
+    viewBox="0 0 32 32"
+    xmlns="http://www.w3.org/2000/svg"
+    height="32"
+    width="32"
+    {...props}>
     <title>{title}</title>
     <g fill="none" fillRule="evenodd">
       <path
         d="M0 22c0 5.522 4.478 10 10 10V12C4.478 12 0 16.477 0 22"
         fill="#00BFB3"
       />
-      <path d="M10 12v10h10c0-5.523-4.478-10-10-10" fill="#343741" />
+      <path
+        className="euiIcon__fillNegative"
+        d="M10 12v10h10c0-5.523-4.478-10-10-10"
+        fill="#343741"
+      />
       <path
         d="M10 0v9c7.168 0 13 5.831 13 13h9C32 9.85 22.15 0 10 0"
         fill="#F04E98"
