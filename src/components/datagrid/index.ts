276b085fcafdc007290acae7594d1fa039badb18
Greg Thompson <thompson.glowe@gmail.com>
2019-12-12 11:37:57 -0600

    type exports

--- src/components/datagrid/index.ts
+++ src/components/datagrid/index.ts
@@ -1,1 +1,15 @@
-export { EuiDataGrid } from './data_grid';
+export {
+  EuiDataGridColumnSortingDraggableProps,
+} from './column_sorting_draggable';
+export { EuiDataGrid, EuiDataGridProps } from './data_grid';
+export {
+  EuiDataGridCellProps,
+  EuiDataGridCellValueElementProps,
+} from './data_grid_cell';
+export { EuiDataGridDataRowProps } from './data_grid_data_row';
+export {
+  EuiDataGridSchema,
+  EuiDataGridSchemaDetector,
+} from './data_grid_schema';
+
+export * from './data_grid_types';
