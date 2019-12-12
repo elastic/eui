018afd84ef6b3b7244dcbce76ecbc9c957e14a45
cchaos <caroline.horn@elastic.co>
2019-12-12 15:06:42 -0500

    Use the color palette service for visualization_colors service

--- src/services/color/visualization_colors.ts
+++ src/services/color/visualization_colors.ts
@@ -1,17 +1,7 @@
 // Array of color-blind safe colors to use in visualizations or other
 // spots that need a large range of varied, qualitative colors.
+import { palettes } from '../../services/color/eui_palettes';
 
-export const VISUALIZATION_COLORS = [
-  '#4DAC93',
-  '#3594D6',
-  '#D15D75',
-  '#9170B8',
-  '#EEAFCF',
-  '#ADB6DD',
-  '#BAA066',
-  '#E59145',
-  '#B46F5F',
-  '#47688A',
-];
+export const VISUALIZATION_COLORS = palettes.euiPaletteColorBlind.colors;
 
 export const DEFAULT_VISUALIZATION_COLOR = VISUALIZATION_COLORS[1];
