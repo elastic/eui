01cb2d6c2272ee693b5ceebf101035282aebdd30
supergrecko <me@supergrecko.com>
2019-12-26 19:42:06 +0100

    EuiFormRow: make use of keysOf for object type

--- src/components/form/form_row/form_row.tsx
+++ src/components/form/form_row/form_row.tsx
@@ -1,27 +1,16 @@
-import React, { cloneElement, Component, Children, HTMLAttributes, ReactNode } from 'react'
-import PropTypes from 'prop-types'
-import classNames from 'classnames'
-import { CommonProps } from '../../common'
+import React, {cloneElement, Component, Children, HTMLAttributes, ReactNode} from 'react';
+import classNames from 'classnames';
+import {CommonProps, keysOf} from '../../common';
 
-import { get } from '../../../services/objects'
-import { withRequiredProp } from '../../../utils/prop_types/with_required_prop'
+import {get} from '../../../services/objects';
 
-import { EuiFormHelpText } from '../form_help_text'
-import { EuiFormErrorText } from '../form_error_text'
-import { EuiFormLabel } from '../form_label'
+import {EuiFormHelpText} from '../form_help_text';
+import {EuiFormErrorText} from '../form_error_text';
+import {EuiFormLabel} from '../form_label';
 
-import makeId from './make_id'
+import makeId from './make_id';
 
-export type FormRowDisplayKeys = 'row'
-  | 'rowCompressed'
-  | 'columnCompressed'
-  | 'center'
-  | 'centerCompressed'
-  | 'columnCompressedSwitch';
-type ClassNameMap = {
-  [key in FormRowDisplayKeys]: string | null
-}
-const displayToClassNameMap: ClassNameMap = {
+const displayToClassNameMap = {
   row: null,
   rowCompressed: 'euiFormRow--compressed',
   columnCompressed: 'euiFormRow--compressed euiFormRow--horizontal',
@@ -29,99 +18,38 @@ const displayToClassNameMap: ClassNameMap = {
   centerCompressed: 'euiFormRow--compressed',
   columnCompressedSwitch:
     'euiFormRow--compressed euiFormRow--horizontal euiFormRow--hasSwitch',
-}
+};
 
-// @ts-ignore
-export const DISPLAYS: Array<FormRowDisplayKeys> = Object.keys(displayToClassNameMap)
+export const DISPLAYS = keysOf(displayToClassNameMap);
+
+export type EuiFormRowDisplayKeys = keyof typeof displayToClassNameMap;
 
 interface EuiFormRowState {
   isFocused: boolean;
   id: string;
-}
+};
 
 export type EuiFormRowProps = CommonProps
   & HTMLAttributes<HTMLDivElement & HTMLFieldSetElement>
   & {
-  display?: FormRowDisplayKeys;
+  display?: EuiFormRowDisplayKeys;
   hasEmptyLabelSpace?: boolean;
   fullWidth?: boolean;
   describedByIds?: string[];
   labelType?: 'label' | 'legend';
   hasChildLabel?: boolean;
   children: ReactNode;
-  className?: string;
   label?: ReactNode;
   labelAppend?: any;
   id?: string;
   isInvalid?: boolean;
   error?: ReactNode | Array<ReactNode>;
   helpText?: ReactNode;
-  // Deprecated
   compressed?: boolean;
   displayOnly?: boolean;
 };
 
 export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
-  static propTypes = {
-    children: PropTypes.element.isRequired,
-    className: PropTypes.string,
-    /**
-     * Escape hatch to not render duplicate labels if the child also renders a label
-     */
-    hasChildLabel: PropTypes.bool,
-    label: PropTypes.node,
-    /**
-     * Sets the type of html element the label should be based
-     * on the form row contents. For instance checkbox groups
-     * should use 'legend' instead of the default 'label'
-     */
-    labelType: PropTypes.oneOf(['label', 'legend']),
-    /**
-     * Adds an extra node to the right of the form label without
-     * being contained inside the form label. Good for things
-     * like documentation links.
-     */
-    labelAppend: withRequiredProp(
-      PropTypes.node,
-      'label',
-      'appending to the label requires that the label also exists'
-    ),
-    id: PropTypes.string,
-    isInvalid: PropTypes.bool,
-    error: PropTypes.oneOfType([
-      PropTypes.node,
-      PropTypes.arrayOf(PropTypes.node),
-    ]),
-    helpText: PropTypes.node,
-    hasEmptyLabelSpace: PropTypes.bool,
-    fullWidth: PropTypes.bool,
-    /**
-     * IDs of additional elements that should be part of children's `aria-describedby`
-     */
-    describedByIds: PropTypes.array,
-    /**
-     * **DEPRECATED: use `display: rowCompressed` instead.**
-     * When `true`, tightens up the spacing.
-     */
-    compressed: PropTypes.bool,
-    /**
-     * When `rowCompressed`, just tightens up the spacing;
-     * Set to `columnCompressed` if compressed
-     * and horizontal layout is needed.
-     * Set to `center` or `centerCompressed` to align non-input
-     * content better with inline rows.
-     * Set to `columnCompressedSwitch` if the form control being passed
-     * as the child is a switch.
-     */
-    display: PropTypes.oneOf(DISPLAYS),
-    /**
-     * **DEPRECATED: use `display: center` instead.**
-     * Vertically centers non-input style content so it aligns
-     * better with input style content.
-     */
-    displayOnly: PropTypes.bool,
-  }
-
   static defaultProps = {
     display: 'row',
     hasEmptyLabelSpace: false,
@@ -129,42 +57,42 @@ export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
     describedByIds: [],
     labelType: 'label',
     hasChildLabel: true,
-  }
+  };
 
   constructor(props: EuiFormRowProps) {
-    super(props)
+    super(props);
 
     this.state = {
       isFocused: false,
       id: props.id || makeId(),
-    }
+    };
 
-    this.onFocus = this.onFocus.bind(this)
-    this.onBlur = this.onBlur.bind(this)
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
   }
 
   onFocus(...args: any[]) {
     // Doing this to allow onFocus to be called correctly from the child input element as this component overrides it
-    const onChildFocus = get(this.props, 'children.props.onFocus')
+    const onChildFocus = get(this.props, 'children.props.onFocus');
     if (onChildFocus) {
-      onChildFocus(...args)
+      onChildFocus(...args);
     }
 
     this.setState({
       isFocused: true,
-    })
+    });
   }
 
   onBlur(...args: any[]) {
     // Doing this to allow onBlur to be called correctly from the child input element as this component overrides it
-    const onChildBlur = get(this.props, 'children.props.onBlur')
+    const onChildBlur = get(this.props, 'children.props.onBlur');
     if (onChildBlur) {
-      onChildBlur(...args)
+      onChildBlur(...args);
     }
 
     this.setState({
       isFocused: false,
-    })
+    });
   }
 
   render() {
@@ -186,30 +114,30 @@ export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
       hasChildLabel,
       id: propsId,
       ...rest
-    } = this.props
+    } = this.props;
 
-    const { id } = this.state
+    const {id} = this.state;
 
     /**
      * Remove when `compressed` is deprecated
      */
-    let shimDisplay: keyof ClassNameMap
+    let shimDisplay: EuiFormRowDisplayKeys;
     if (compressed && display === 'row') {
-      shimDisplay = 'rowCompressed'
+      shimDisplay = 'rowCompressed';
     } else {
       /**
        * Safe use of ! as prop default is 'row'
        */
-      shimDisplay = display!
+      shimDisplay = display!;
     }
 
     /**
      * Remove when `displayOnly` is deprecated
      */
     if (compressed && displayOnly) {
-      shimDisplay = 'centerCompressed'
+      shimDisplay = 'centerCompressed';
     } else if (displayOnly && display === 'row') {
-      shimDisplay = 'center'
+      shimDisplay = 'center';
     }
 
     const classes = classNames(
@@ -220,24 +148,24 @@ export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
       },
       displayToClassNameMap[shimDisplay],
       className
-    )
+    );
 
-    let optionalHelpText
+    let optionalHelpText;
 
     if (helpText) {
       optionalHelpText = (
         <EuiFormHelpText id={`${id}-help`} className="euiFormRow__text">
           {helpText}
         </EuiFormHelpText>
-      )
+      );
     }
 
-    let optionalErrors
+    let optionalErrors;
 
     if (error && isInvalid) {
-      const errorTexts = Array.isArray(error) ? error : [error]
+      const errorTexts = Array.isArray(error) ? error : [error];
       optionalErrors = errorTexts.map((error, i) => {
-        const key = typeof error === 'string' ? error : i
+        const key = typeof error === 'string' ? error : i;
         return (
           <EuiFormErrorText
             key={key}
@@ -245,12 +173,12 @@ export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
             className="euiFormRow__text">
             {error}
           </EuiFormErrorText>
-        )
-      })
+        );
+      });
     }
 
-    let optionalLabel
-    const isLegend = label && labelType === 'legend' ? true : false
+    let optionalLabel;
+    const isLegend = label && labelType === 'legend' ? true : false;
 
     if (label || labelAppend) {
       optionalLabel = (
@@ -267,25 +195,25 @@ export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
           {labelAppend && ' '}
           {labelAppend}
         </div>
-      )
+      );
     }
 
-    const optionalProps: React.AriaAttributes = {}
+    const optionalProps: React.AriaAttributes = {};
     /**
      * Safe use of ! as default prop is []
      */
-    const describingIds = [...describedByIds!]
+    const describingIds = [...describedByIds!];
 
     if (optionalHelpText) {
-      describingIds.push(optionalHelpText.props.id)
+      describingIds.push(optionalHelpText.props.id);
     }
 
     if (optionalErrors) {
-      optionalErrors.forEach(error => describingIds.push(error.props.id))
+      optionalErrors.forEach(error => describingIds.push(error.props.id));
     }
 
     if (describingIds.length > 0) {
-      optionalProps['aria-describedby'] = describingIds.join(' ')
+      optionalProps['aria-describedby'] = describingIds.join(' ');
     }
 
     // @ts-ignore
@@ -294,7 +222,7 @@ export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
       onFocus: this.onFocus,
       onBlur: this.onBlur,
       ...optionalProps,
-    })
+    });
 
     const fieldWrapperClasses = classNames('euiFormRow__fieldWrapper', {
       euiFormRow__fieldWrapperDisplayOnly:
@@ -302,9 +230,9 @@ export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
        * Safe use of ! as default prop is 'row'
        */
         displayOnly || display!.startsWith('center'),
-    })
+    });
 
-    const Element = labelType === 'legend' ? 'fieldset' : 'div'
+    const Element = labelType === 'legend' ? 'fieldset' : 'div';
 
     return (
       <Element className={classes} id={`${id}-row`} {...rest}>
@@ -315,6 +243,6 @@ export class EuiFormRow extends Component<EuiFormRowProps, EuiFormRowState> {
           {optionalHelpText}
         </div>
       </Element>
-    )
+    );
   }
 }
