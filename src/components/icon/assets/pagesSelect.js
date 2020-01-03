cc9163e19574b3efd02f96bb3b108f10d5045783
Michael Marcialis <michael.marcialis@elastic.co>
2020-01-03 11:12:55 -0500

    remove clip-rule attributes

--- src/components/icon/assets/pagesSelect.js
+++ src/components/icon/assets/pagesSelect.js
@@ -12,7 +12,6 @@ const EuiIconPagesSelect = ({ title, ...props }) => (
     <path d="M8.758 15H2V2a1 1 0 00-1 1v12a1 1 0 001 1h7.671a4.526 4.526 0 01-.913-1z" />
     <path
       fillRule="evenodd"
-      clipRule="evenodd"
       d="M16 12.5a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0zm-1.146-1.354a.5.5 0 010 .707l-2 2a.5.5 0 01-.708 0l-1-1a.5.5 0 01.708-.707l.646.647 1.646-1.647a.5.5 0 01.708 0z"
     />
   </svg>
