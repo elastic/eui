c5d9e56686adbe76377cda9b2016b1943ec4ad09
Greg Thompson <thompson.glowe@gmail.com>
2020-01-10 14:58:26 -0600

    dir name; remove index

--- scripts/compile-clean.js
+++ scripts/compile-clean.js
@@ -3,4 +3,4 @@ const rimraf = require('rimraf');
 rimraf.sync('dist');
 rimraf.sync('lib');
 rimraf.sync('es');
-rimraf.sync('kbn-test');
+rimraf.sync('test-env');
--- src/index.testenv.js
+++
@@ -1,8 +0,0 @@
-import * as Packages from '../packages';
-
-export * from './components';
-export * from './services';
-export * from './test';
-export * from './utils';
-
-export { Packages };
