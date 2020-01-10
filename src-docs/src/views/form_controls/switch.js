6bc4b0ab41f74796fcabf302ea45b2cd4a7eca83
Andrea Del Rio <delrio.andre@gmail.com>
2020-01-10 15:48:20 -0800

    cleanup

--- src-docs/src/views/form_controls/switch.js
+++ src-docs/src/views/form_controls/switch.js
@@ -72,6 +72,16 @@ export default class extends Component {
           onChange={this.onChange}
           compressed
         />
+
+        <EuiSpacer size="m" />
+
+        <EuiSwitch
+          label="I am a mini switch"
+          checked={this.state.checked}
+          onChange={this.onChange}
+          compressed
+          className="euiSwitch--mini"
+        />
       </Fragment>
     );
   }
