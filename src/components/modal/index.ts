38366bfa46225ed732fb171d165c045bb8a650da
Greg Thompson <thompson.glowe@gmail.com>
2020-01-22 13:15:28 -0600

    TS clean up

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -5,7 +5,10 @@ export {
   CANCEL_BUTTON as EUI_MODAL_CANCEL_BUTTON,
 } from './confirm_modal';
 export { EuiModal, EuiModalProps } from './modal';
-export { EuiModalFooter } from './modal_footer';
-export { EuiModalHeader } from './modal_header';
-export { EuiModalBody } from './modal_body';
-export { EuiModalHeaderTitle } from './modal_header_title';
+export { EuiModalFooter, EuiModalFooterProps } from './modal_footer';
+export { EuiModalHeader, EuiModalHeaderProps } from './modal_header';
+export { EuiModalBody, EuiModalBodyProps } from './modal_body';
+export {
+  EuiModalHeaderTitle,
+  EuiModalHeaderTitleProps,
+} from './modal_header_title';
