0d09aa9c7fba2d022f900e7d1474f63095131242
Greg Thompson <thompson.glowe@gmail.com>
2020-01-23 13:36:47 -0600

    testenv icon rest

--- src/components/icon/icon.testenv.tsx
+++ src/components/icon/icon.testenv.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
-export const EuiIcon = ({
-  type,
-  className,
-  'data-test-subj': dataTestSubj,
-  id,
-  ...rest
-}: any) => (
-  <div
-    data-euiicon-type={type}
-    id={id}
-    className={className}
-    data-test-subj={dataTestSubj}
-    {...rest}
-  />
+export const EuiIcon = ({ type, ...rest }: any) => (
+  <div data-euiicon-type={type} {...rest} />
 );
 
 export const TYPES = [];
