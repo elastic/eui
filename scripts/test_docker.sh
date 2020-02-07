34c786ec4233ccf17651f707dd58a7e0507611b3
Greg Thompson <thompson.glowe@gmail.com>
2020-02-07 10:11:40 -0600

    Revert "trial run"
    This reverts commit f0c8f9f2e0ebf5faf50dc09939470a712572b9ef.

--- scripts/test_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker pull zenato/puppeteer
-docker run -i --rm --cap-add=SYS_ADMIN --volume $PWD:/app --workdir /app \
-    -e GIT_COMMITTER_NAME=test -e GIT_COMMITTER_EMAIL=test -e HOME=/tmp \
-    zenato/puppeteer \
-    bash -c 'npm config set spin false && /opt/yarn*/bin/yarn && npm run test && npm run start-test-server-and-a11y-test && npm run build'
