dc74fdf64fe6c40fe1ae5e6fd8ee2e98107993e5
Matt Kime <matt@mattki.me>
2020-02-10 09:01:23 -0600

    don't export type via js, add to changelog

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -17,7 +17,7 @@ export { EuiFormLabel } from './form_label';
 export { EuiFormRow } from './form_row';
 export { EuiRadio, EuiRadioGroup } from './radio';
 export { EuiDualRange, EuiRange } from './range';
-export { EuiSelect, EuiSelectOptionProps } from './select';
+export { EuiSelect } from './select';
 export { EuiSuperSelect, EuiSuperSelectControl } from './super_select';
 export { EuiSwitch } from './switch';
 export { EuiTextArea } from './text_area';
