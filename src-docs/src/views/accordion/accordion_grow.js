3550df449401e1fddc97f0ff781bd1589f08b93a
Anish Aggarwal <43617894+anishagg17@users.noreply.github.com>
2020-02-28 22:17:52 +0530

    fixes #721 : Added context change to accordion in docs-view (#2939)
    Updates docs to add more context to the accordion examples.

--- src-docs/src/views/accordion/accordion_grow.js
+++ src-docs/src/views/accordion/accordion_grow.js
@@ -8,7 +8,9 @@ import {
   EuiButton,
   EuiSpacer,
   EuiText,
+  EuiScreenReaderOnly,
 } from '../../../../src/components';
+import { htmlIdGenerator } from '../../../../src/services/accessibility/html_id_generator';
 
 class Rows extends Component {
   state = {
@@ -29,21 +31,36 @@ class Rows extends Component {
 
   render() {
     const rows = [];
-    for (let i = 1; i <= this.state.counter; i++) {
-      rows.push(<p key={i}>Row {i}</p>);
+    const { counter } = this.state;
+    for (let i = 1; i <= counter; i++) {
+      rows.push(<li key={i}>Row {i}</li>);
     }
+    const growingAccordianDescriptionId = htmlIdGenerator()();
+    const listId = htmlIdGenerator()();
     return (
       <EuiText>
+        <EuiScreenReaderOnly>
+          <p id={growingAccordianDescriptionId}>
+            Currently height is set to {counter} items
+          </p>
+        </EuiScreenReaderOnly>
         <EuiSpacer size="s" />
         <p>
-          <EuiButton onClick={() => this.onIncrease()}>
-            Increase height
+          <EuiButton
+            onClick={() => this.onIncrease()}
+            aria-controls={listId}
+            aria-describedby={growingAccordianDescriptionId}>
+            Increase height to {counter + 1} items
           </EuiButton>{' '}
-          <EuiButton onClick={() => this.onDecrease()}>
-            Decrease height
+          <EuiButton
+            aria-controls={listId}
+            aria-describedby={growingAccordianDescriptionId}
+            onClick={() => this.onDecrease()}
+            isDisabled={counter === 1}>
+            Decrease height to {counter - 1} item{counter > 2 && 's'}
           </EuiButton>
         </p>
-        {rows}
+        <ul id={listId}>{rows}</ul>
       </EuiText>
     );
   }
