979f207eaf5c5ec5222fe63cbd46e7d9d8225826
LeonY1 <leonyyuan@gmail.com>
2020-03-04 16:29:57 -0600

    Sequential Upgrade for react-beautiful-dnd and @types/react-beautiful-dnd to 11.0.0; Changed the tests for: draggable.tsx, droppable.tsx, drag_drop_context.tsx

--- src/components/drag_and_drop/draggable.test.tsx
+++ src/components/drag_and_drop/draggable.test.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import { render } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 
 // import { findTestSubject } from '../../test';
 import { requiredProps } from '../../test/required_props';
 
 import { EuiDragDropContext, EuiDraggable, EuiDroppable } from './';
 
+function snapshotDragDropContext(component: ReactWrapper) {
+  // Get the Portal's sibling and return its html
+  const renderedHtml = component.html();
+  const container = document.createElement('div');
+  container.innerHTML = renderedHtml;
+  return container.firstChild;
+}
+
 describe('EuiDraggable', () => {
   test('is rendered', () => {
     const handler = jest.fn();
-    const component = render(
+    jest.mock('react', () => {
+      const react = jest.requireActual('react');
+      return {
+        ...react,
+        useLayoutEffect: react.useEffect,
+      };
+    });
+
+    const component = mount(
       <EuiDragDropContext onDragEnd={handler} {...requiredProps}>
         <EuiDroppable droppableId="testDroppable">
           <EuiDraggable draggableId="testDraggable" index={0}>
@@ -19,12 +35,19 @@ describe('EuiDraggable', () => {
       </EuiDragDropContext>
     );
 
-    expect(component).toMatchSnapshot();
+    expect(snapshotDragDropContext(component)).toMatchSnapshot();
   });
 
   test('can be given ReactElement children', () => {
     const handler = jest.fn();
-    const component = render(
+    jest.mock('react', () => {
+      const react = jest.requireActual('react');
+      return {
+        ...react,
+        useLayoutEffect: react.useEffect,
+      };
+    });
+    const component = mount(
       <EuiDragDropContext onDragEnd={handler} {...requiredProps}>
         <EuiDroppable droppableId="testDroppable">
           <EuiDraggable draggableId="testDraggable" index={0}>
@@ -34,6 +57,6 @@ describe('EuiDraggable', () => {
       </EuiDragDropContext>
     );
 
-    expect(component).toMatchSnapshot();
+    expect(snapshotDragDropContext(component)).toMatchSnapshot();
   });
 });
