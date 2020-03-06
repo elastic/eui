63739a6f4562b2a5a57ef8a112dcaddc67681326
Rory Hunter <pugnascotia@users.noreply.github.com>
2020-03-06 15:37:44 +0000

    Convert EuiSearchBar to TypeScript (#2909)
    Convert `EuiSearchBar` to TypeScript.
    
    Also:
    
       * Fix `type:whatever` searches in the docs
       * Fix grammatical errors and an NPE in the docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -37,6 +37,7 @@
 - Added `notification` and `notificationColor` props to `EuiHeaderSectionItemButton` ([#2914](https://github.com/elastic/eui/pull/2914))
 - Added `folderCheck`, `folderExclamation`, `push`, `quote`, `reporter` and `users` icons ([#2935](https://github.com/elastic/eui/pull/2935))
 - Updated `folderClosed` and `folderOpen` to match new additions and sit better on the pixel grid ([#2935](https://github.com/elastic/eui/pull/2935))
+- Converted `EuiSearchBar` to Typescript ([#2909](https://github.com/elastic/eui/pull/2909))
 
 **Bug fixes**
 
---
+++ src/components/search_bar/filters/field_value_selection_filter.tsx
@@ -0,0 +1,491 @@
+import React, { Component, ReactElement, ReactNode } from 'react';
+import { isArray, isNil } from '../../../services/predicate';
+import { keyCodes } from '../../../services';
+import { EuiPopover, EuiPopoverTitle } from '../../popover';
+import { EuiFieldSearch } from '../../form/field_search';
+import { EuiFilterButton, EuiFilterSelectItem } from '../../filter_group';
+import { EuiLoadingChart } from '../../loading';
+import { EuiSpacer } from '../../spacer';
+import { EuiIcon } from '../../icon';
+import { Query } from '../query';
+import { Clause, Value } from '../query/ast';
+
+interface FieldValueOptionType {
+  field?: string;
+  value: Value;
+  name?: string;
+  view?: ReactNode;
+}
+
+type OptionsLoader = () => Promise<FieldValueOptionType[]>;
+
+type OptionsFilter = (
+  name: string,
+  query: string,
+  options?: FieldValueOptionType[]
+) => boolean;
+
+type MultiSelect = boolean | 'and' | 'or';
+
+export interface FieldValueSelectionFilterConfigType {
+  type: 'field_value_selection';
+  field?: string;
+  name: string;
+  options: FieldValueOptionType[] | OptionsLoader;
+  filterWith?: 'prefix' | 'includes' | OptionsFilter;
+  cache?: number;
+  multiSelect?: MultiSelect;
+  loadingMessage?: string;
+  noOptionsMessage?: string;
+  searchThreshold?: number;
+  available?: () => boolean;
+}
+
+export interface FieldValueSelectionFilterProps {
+  index: number;
+  config: FieldValueSelectionFilterConfigType;
+  query: Query;
+  onChange: (query: Query) => void;
+  autoClose?: boolean;
+}
+
+const defaults = {
+  config: {
+    multiSelect: true,
+    filterWith: 'prefix',
+    loadingMessage: 'Loading...',
+    noOptionsMessage: 'No options found',
+    searchThreshold: 10,
+  },
+};
+
+interface State {
+  popoverOpen: boolean;
+  error: string | null;
+  options: {
+    all: FieldValueOptionType[];
+    shown: FieldValueOptionType[];
+  } | null;
+  cachedOptions?: FieldValueOptionType[] | null;
+}
+
+type DefaultProps = Pick<FieldValueSelectionFilterProps, 'autoClose'>;
+
+export class FieldValueSelectionFilter extends Component<
+  FieldValueSelectionFilterProps,
+  State
+> {
+  static defaultProps: DefaultProps = {
+    autoClose: true,
+  };
+
+  private readonly selectItems: EuiFilterSelectItem[];
+  private searchInput: HTMLInputElement | null = null;
+
+  constructor(props: FieldValueSelectionFilterProps) {
+    super(props);
+
+    const { options } = props.config;
+
+    const preloadedOptions = isArray(options)
+      ? {
+          all: options,
+          shown: options,
+        }
+      : null;
+
+    this.selectItems = [];
+    this.state = {
+      popoverOpen: false,
+      error: null,
+      options: preloadedOptions,
+    };
+  }
+
+  closePopover() {
+    this.setState({ popoverOpen: false });
+  }
+
+  onButtonClick() {
+    this.setState(prevState => {
+      if (!prevState.popoverOpen) {
+        // loading options updates the state, so we'll do that in the animation frame
+        window.requestAnimationFrame(() => {
+          this.loadOptions();
+        });
+      }
+      return {
+        options: null,
+        error: null,
+        popoverOpen: !prevState.popoverOpen,
+      };
+    });
+  }
+
+  loadOptions() {
+    const loader = this.resolveOptionsLoader();
+    this.setState({ options: null, error: null });
+    loader()
+      .then(options => {
+        this.setState({
+          error: null,
+          options: {
+            all: options,
+            shown: options,
+          },
+        });
+      })
+      .catch(() => {
+        this.setState({ options: null, error: 'Could not load options' });
+      });
+  }
+
+  filterOptions(q = '') {
+    this.setState(prevState => {
+      if (isNil(prevState.options)) {
+        return {};
+      }
+
+      const predicate = this.getOptionFilter();
+
+      return {
+        ...prevState,
+        options: {
+          ...prevState.options,
+          shown: prevState.options.all.filter((option, i, options) => {
+            const name = this.resolveOptionName(option).toLowerCase();
+            const query = q.toLowerCase();
+            return predicate(name, query, options);
+          }),
+        },
+      };
+    });
+  }
+
+  getOptionFilter(): OptionsFilter {
+    const filterWith =
+      this.props.config.filterWith || defaults.config.filterWith;
+
+    if (typeof filterWith === 'function') {
+      return filterWith;
+    }
+
+    if (filterWith === 'includes') {
+      return (name, query) => name.includes(query);
+    }
+
+    return (name, query) => name.startsWith(query);
+  }
+
+  resolveOptionsLoader: () => OptionsLoader = () => {
+    const options = this.props.config.options;
+    if (isArray(options)) {
+      return () => Promise.resolve(options);
+    }
+
+    return () => {
+      const cachedOptions = this.state.cachedOptions;
+      if (cachedOptions) {
+        return Promise.resolve(cachedOptions);
+      }
+
+      return (options as OptionsLoader)().then(opts => {
+        // If a cache time is set, populate the cache and also schedule a
+        // cache reset.
+        if (this.props.config.cache != null && this.props.config.cache > 0) {
+          this.setState({ cachedOptions: opts });
+          setTimeout(() => {
+            this.setState({ cachedOptions: null });
+          }, this.props.config.cache);
+        }
+
+        return opts;
+      });
+    };
+  };
+
+  resolveOptionName(option: FieldValueOptionType) {
+    return option.name || option.value.toString();
+  }
+
+  onOptionClick(
+    field: string,
+    value: Value,
+    checked: 'on' | 'off' | undefined
+  ) {
+    const multiSelect = this.resolveMultiSelect();
+    const { autoClose } = this.props;
+
+    // we're closing popover only if the user can only select one item... if the
+    // user can select more, we'll leave it open so she can continue selecting
+
+    if (!multiSelect && autoClose) {
+      this.closePopover();
+      const query = checked
+        ? this.props.query.removeSimpleFieldClauses(field)
+        : this.props.query
+            .removeSimpleFieldClauses(field)
+            .addSimpleFieldValue(field, value);
+
+      this.props.onChange(query);
+    } else {
+      if (multiSelect === 'or') {
+        const query = checked
+          ? this.props.query.removeOrFieldValue(field, value)
+          : this.props.query.addOrFieldValue(field, value);
+
+        this.props.onChange(query);
+      } else {
+        const query = checked
+          ? this.props.query.removeSimpleFieldValue(field, value)
+          : this.props.query.addSimpleFieldValue(field, value);
+
+        this.props.onChange(query);
+      }
+    }
+  }
+
+  onKeyDown(
+    index: number,
+    event:
+      | React.KeyboardEvent<HTMLInputElement>
+      | React.KeyboardEvent<HTMLButtonElement>
+  ) {
+    switch (event.keyCode) {
+      case keyCodes.DOWN:
+        if (index < this.selectItems.length - 1) {
+          event.preventDefault();
+          this.selectItems[index + 1].focus();
+        }
+        break;
+
+      case keyCodes.UP:
+        if (index < 0) {
+          return; // it's coming from the search box... nothing to do... nowhere to go
+        }
+        if (index === 0 && this.searchInput) {
+          event.preventDefault();
+          this.searchInput.focus();
+        } else if (index > 0) {
+          event.preventDefault();
+          this.selectItems[index - 1].focus();
+        }
+    }
+  }
+
+  resolveMultiSelect(): MultiSelect {
+    const { config } = this.props;
+    return !isNil(config.multiSelect)
+      ? config.multiSelect
+      : defaults.config.multiSelect;
+  }
+
+  render() {
+    const { index, query, config } = this.props;
+    const multiSelect = this.resolveMultiSelect();
+
+    const activeTop = this.isActiveField(config.field);
+    const activeItem = this.state.options
+      ? this.state.options.all.some(item => this.isActiveField(item.field))
+      : false;
+
+    const active = activeTop || activeItem;
+
+    const button = (
+      <EuiFilterButton
+        iconType="arrowDown"
+        iconSide="right"
+        onClick={this.onButtonClick.bind(this)}
+        hasActiveFilters={active}
+        grow>
+        {config.name}
+      </EuiFilterButton>
+    );
+
+    const searchBox = this.renderSearchBox();
+    const content = this.renderContent(
+      config.field,
+      query,
+      config,
+      multiSelect
+    );
+    const threshold =
+      this.props.config.searchThreshold || defaults.config.searchThreshold;
+    const withTitle =
+      this.state.options != null && this.state.options.all.length >= threshold;
+
+    return (
+      <EuiPopover
+        id={`${config.type}_${index}`}
+        ownFocus
+        button={button}
+        isOpen={this.state.popoverOpen}
+        closePopover={this.closePopover.bind(this)}
+        panelPaddingSize="none"
+        withTitle={withTitle}
+        anchorPosition="downCenter"
+        panelClassName="euiFilterGroup__popoverPanel">
+        {searchBox}
+        {content}
+      </EuiPopover>
+    );
+  }
+
+  renderSearchBox() {
+    const threshold =
+      this.props.config.searchThreshold || defaults.config.searchThreshold;
+    if (this.state.options && this.state.options.all.length >= threshold) {
+      const disabled = this.state.error != null;
+      return (
+        <EuiPopoverTitle>
+          <EuiFieldSearch
+            inputRef={ref => (this.searchInput = ref)}
+            disabled={disabled}
+            incremental={true}
+            onSearch={query => this.filterOptions(query)}
+            onKeyDown={this.onKeyDown.bind(this, -1)}
+          />
+        </EuiPopoverTitle>
+      );
+    }
+  }
+
+  renderContent(
+    field: string | undefined,
+    query: Query,
+    config: FieldValueSelectionFilterConfigType,
+    multiSelect: MultiSelect
+  ) {
+    if (this.state.error) {
+      return this.renderError(this.state.error);
+    }
+    if (isNil(this.state.options)) {
+      return this.renderLoader();
+    }
+    if (this.state.options.shown.length === 0) {
+      return this.renderNoOptions();
+    }
+
+    if (this.state.options == null) {
+      return;
+    }
+
+    const items: {
+      on: ReactElement[];
+      off: ReactElement[];
+      rest: ReactElement[];
+    } = {
+      on: [],
+      off: [],
+      rest: [],
+    };
+
+    this.state.options.shown.forEach((option, index) => {
+      const optionField = option.field || field;
+
+      if (optionField == null) {
+        throw new Error(
+          'option.field or field should be provided in <FieldValueSelectionFilter/>'
+        );
+      }
+
+      const clause =
+        multiSelect === 'or'
+          ? query.getOrFieldClause(optionField, option.value)
+          : query.getSimpleFieldClause(optionField, option.value);
+
+      const checked = this.resolveChecked(clause);
+      const onClick = () => {
+        // clicking a checked item will uncheck it and effective remove the filter (value = undefined)
+        this.onOptionClick(optionField, option.value, checked);
+      };
+
+      const item = (
+        <EuiFilterSelectItem
+          key={index}
+          checked={checked}
+          onClick={onClick}
+          ref={ref => (this.selectItems[index] = ref!)}
+          onKeyDown={this.onKeyDown.bind(this, index)}>
+          {option.view ? option.view : this.resolveOptionName(option)}
+        </EuiFilterSelectItem>
+      );
+
+      if (!checked) {
+        items.rest.push(item);
+      } else if (checked === 'on') {
+        items.on.push(item);
+      } else {
+        items.off.push(item);
+      }
+      return items;
+    });
+
+    return (
+      <div className="euiFilterSelect__items">
+        {[...items.on, ...items.off, ...items.rest]}
+      </div>
+    );
+  }
+
+  resolveChecked(clause: Clause | undefined): 'on' | 'off' | undefined {
+    if (clause) {
+      return Query.isMust(clause) ? 'on' : 'off';
+    }
+  }
+
+  renderLoader() {
+    const message =
+      this.props.config.loadingMessage || defaults.config.loadingMessage;
+    return (
+      <div className="euiFilterSelect__note">
+        <div className="euiFilterSelect__noteContent">
+          <EuiLoadingChart size="m" />
+          <EuiSpacer size="xs" />
+          <p>{message}</p>
+        </div>
+      </div>
+    );
+  }
+
+  renderError(message: string) {
+    return (
+      <div className="euiFilterSelect__note">
+        <div className="euiFilterSelect__noteContent">
+          <EuiIcon size="m" type="faceSad" color="danger" />
+          <EuiSpacer size="xs" />
+          <p>{message}</p>
+        </div>
+      </div>
+    );
+  }
+
+  renderNoOptions() {
+    const message =
+      this.props.config.noOptionsMessage || defaults.config.noOptionsMessage;
+    return (
+      <div className="euiFilterSelect__note">
+        <div className="euiFilterSelect__noteContent">
+          <EuiIcon type="minusInCircle" />
+          <EuiSpacer size="xs" />
+          <p>{message}</p>
+        </div>
+      </div>
+    );
+  }
+
+  isActiveField(field: string | undefined): boolean {
+    if (typeof field !== 'string') {
+      return false;
+    }
+
+    const { query } = this.props;
+    const multiSelect = this.resolveMultiSelect();
+
+    if (multiSelect === 'or') {
+      return query.hasOrFieldClause(field);
+    }
+
+    return query.hasSimpleFieldClause(field);
+  }
+}
