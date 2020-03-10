2f1d37bf829d053049ccdb2ea988fbab8cf7ac38
cchaos <caroline.horn@elastic.co>
2020-03-10 13:14:44 -0400

    doc cleanoup

--- src-docs/src/views/collapsible_nav/collapsible_nav_example.js
+++ src-docs/src/views/collapsible_nav/collapsible_nav_example.js
@@ -94,10 +94,9 @@ export const CollapsibleNavExample = {
           <p>
             An <strong>EuiCollapsibleNavGroup</strong> adds some basic borders
             and <EuiCode>background</EuiCode> color of <EuiCode>none</EuiCode>,{' '}
-            <EuiCode>light</EuiCode>, or <EuiCode>dark</EuiCode>. You can
-            provide an optional <EuiCode>title</EuiCode> and{' '}
-            <EuiCode>iconType</EuiCode> to give each seaction a heading. Make
-            the section collapsible (
+            <EuiCode>light</EuiCode>, or <EuiCode>dark</EuiCode>. Give each
+            seaction a heading by providing an optional <EuiCode>title</EuiCode>{' '}
+            and <EuiCode>iconType</EuiCode>. Make the section collapsible (
             <Link to="/layout/accordion">accordion style</Link>) with{' '}
             <EuiCode language="js">collapsible=true</EuiCode>.
           </p>
