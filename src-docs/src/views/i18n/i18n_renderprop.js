9846db665027e68646dd8572b542b43e762b4665
Anish Aggarwal <43617894+anishagg17@users.noreply.github.com>
2020-03-11 03:41:01 +0530

    Update src-docs/src/views/i18n/i18n_renderprop.js
    Co-Authored-By: Michail Yasonik <michail@yasonik.com>

--- src-docs/src/views/i18n/i18n_renderprop.js
+++ src-docs/src/views/i18n/i18n_renderprop.js
@@ -12,10 +12,9 @@ export default () => {
       <div>
         <EuiI18n token="euiI18nRenderprop.placeholderName" default="John Doe">
           {placeholderName => (
-            <EuiFieldText
-              placeholder={placeholderName}
-              aria-label="Passing EuiI18n as a render prop child "
-            />
+		<EuiFormRow label={<>This text field&apos;s placeholder reads from <EuiCode>i18n.renderpropexample</EuiCode></>}>
+            <EuiFieldText placeholder={placeholderName}/>
+        </EuiFormRow>
           )}
         </EuiI18n>
       </div>
