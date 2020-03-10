3c1c60d9a919edc001d2fbf3be4a4c21defda8d3
cchaos <caroline.horn@elastic.co>
2020-03-10 11:51:07 -0400

    Added `collapsible` prop

--- src/components/collapsible_nav/collapsible_nav_group/collapsible_nav_group.test.tsx
+++ src/components/collapsible_nav/collapsible_nav_group/collapsible_nav_group.test.tsx
@@ -7,23 +7,34 @@ import { EuiCollapsibleNavGroup, BACKGROUNDS } from './collapsible_nav_group';
 describe('EuiCollapsibleNavGroup', () => {
   test('is rendered', () => {
     const component = render(
-      <EuiCollapsibleNavGroup
-        title="Title"
-        initialIsOpen={false}
-        id="id"
-        {...requiredProps}
-      />
+      <EuiCollapsibleNavGroup id="id" {...requiredProps} />
     );
 
     expect(component).toMatchSnapshot();
   });
 
   describe('props', () => {
+    test('title is rendered', () => {
+      const component = render(
+        <EuiCollapsibleNavGroup title="Title" id="id" />
+      );
+
+      expect(component).toMatchSnapshot();
+    });
+
     test('iconType is rendered', () => {
+      const component = render(
+        <EuiCollapsibleNavGroup title="Title" iconType="bolt" id="id" />
+      );
+
+      expect(component).toMatchSnapshot();
+    });
+
+    test('iconSize is rendered', () => {
       const component = render(
         <EuiCollapsibleNavGroup
           title="Title"
-          initialIsOpen={false}
+          iconSize="s"
           iconType="bolt"
           id="id"
         />
@@ -36,17 +47,55 @@ describe('EuiCollapsibleNavGroup', () => {
       BACKGROUNDS.forEach(color => {
         test(`${color} is rendered`, () => {
           const component = render(
-            <EuiCollapsibleNavGroup
-              title="Title"
-              initialIsOpen={false}
-              id="id"
-              background={color}
-            />
+            <EuiCollapsibleNavGroup id="id" background={color} />
           );
 
           expect(component).toMatchSnapshot();
         });
       });
+    });
+  });
+
+  describe('when collapsible is true', () => {
+    test('will render an accordion', () => {
+      const component = render(
+        <EuiCollapsibleNavGroup
+          collapsible={true}
+          initialIsOpen={false}
+          title="Title"
+          id="id"
+        />
+      );
+
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('throws a warning', () => {
+    const oldConsoleError = console.warn;
+    let consoleStub: jest.Mock;
+
+    beforeEach(() => {
+      // We don't use jest.spyOn() here, because EUI's tests apply a global
+      // console.error() override that throws an exception. For these
+      // tests, we just want to know if console.error() was called.
+      console.warn = consoleStub = jest.fn();
+    });
+
+    afterEach(() => {
+      console.warn = oldConsoleError;
+    });
+
+    test('if iconType is passed without a title', () => {
+      const component = render(
+        <EuiCollapsibleNavGroup iconType="bolt" id="id" />
+      );
+
+      expect(consoleStub).toBeCalled();
+      expect(consoleStub.mock.calls[0][0]).toMatch(
+        'icon without also passing a `title`'
+      );
+      expect(component).toMatchSnapshot();
     });
   });
 });
--- src/components/collapsible_nav/collapsible_nav_group/collapsible_nav_group.tsx
+++ src/components/collapsible_nav/collapsible_nav_group/collapsible_nav_group.tsx
@@ -1,10 +1,10 @@
 import React, { FunctionComponent, ReactNode } from 'react';
 import classNames from 'classnames';
-import { CommonProps } from '../../common';
+import { CommonProps, ExclusiveUnion } from '../../common';
 import { htmlIdGenerator, slugify } from '../../../services';
 
 import { EuiAccordion, EuiAccordionProps } from '../../accordion';
-import { EuiIcon, IconType } from '../../icon';
+import { EuiIcon, IconType, IconSize } from '../../icon';
 import { EuiFlexGroup, EuiFlexItem } from '../../flex';
 
 type Background = 'none' | 'light' | 'dark';
@@ -17,19 +17,17 @@ export const BACKGROUNDS = Object.keys(
   backgroundToClassNameMap
 ) as Background[];
 
-export interface EuiCollapsibleNavGroupProps  // TODO: paddingSize should be optional on EuiAccordion
-  extends CommonProps,
-    Omit<EuiAccordionProps, 'id'> {
+export interface EuiCollapsibleNavGroupProps extends CommonProps {
+  // TODO: paddingSize should be optional on EuiAccordion
   children?: ReactNode;
-  /**
-   * The title gets wrapped in the appropriate heading level
-   * with the option to add an iconType
-   */
-  title: string;
   /**
    * Sits left of the `title` and only when `title` is present
    */
   iconType?: IconType;
+  /**
+   * Change the size of the icon in the `title`
+   */
+  iconSize?: IconSize;
   /**
    * Optionally provide an id, othewise one will be created
    */
@@ -41,29 +39,72 @@ export interface EuiCollapsibleNavGroupProps  // TODO: paddingSize should be opt
   background?: Background;
 }
 
+type GroupAsAccordion = EuiCollapsibleNavGroupProps &
+  Omit<EuiAccordionProps, 'id'> & {
+    /**
+     * If `true`, wraps children in the body of an accordion,
+     * using the `title` (required for use with `collapsible`) as the button
+     */
+    collapsible: true;
+    /**
+     * The title gets wrapped in the appropriate heading level
+     * with the option to add an iconType
+     */
+    title: ReactNode;
+  };
+
+type GroupAsDiv = EuiCollapsibleNavGroupProps & {
+  /**
+   * When `false`, simply renders a div without any accordion functionality
+   */
+  collapsible?: false;
+  /**
+   * The title gets wrapped in the appropriate heading level
+   * with the option to add an iconType
+   */
+  title?: ReactNode;
+};
+
 export const EuiCollapsibleNavGroup: FunctionComponent<
-  EuiCollapsibleNavGroupProps
+  ExclusiveUnion<GroupAsAccordion, GroupAsDiv>
 > = ({
   className,
   children,
   title,
   iconType,
+  iconSize = 'l',
   id,
   background = 'none',
+  collapsible = false,
   ...rest
 }) => {
   const classes = classNames(
     'euiCollapsibleNavGroup',
     backgroundToClassNameMap[background],
+    {
+      'euiCollapsibleNavGroup--withTitle': title,
+    },
     className
   );
-  const buttonClasses = classNames('euiCollapsibleNavGroup__button');
 
-  const buttonContent = (
+  // Warn if consumer passes an iconType without a title
+  if (iconType && !title) {
+    console.warn(
+      'EuiCollapsibleNavGroup will not render an icon without also passing a `title`.'
+    );
+  }
+
+  const content = (
+    <div className="euiCollapsibleNavGroup__children">{children}</div>
+  );
+
+  const titleClasses = classNames('euiCollapsibleNavGroup__button');
+
+  const titleContent = title ? (
     <EuiFlexGroup gutterSize="s" alignItems="center" responsive={false}>
       {iconType && (
         <EuiFlexItem grow={false}>
-          <EuiIcon type={iconType} size="l" aria-hidden="true" />
+          <EuiIcon type={iconType} size={iconSize} aria-hidden="true" />
         </EuiFlexItem>
       )}
 
@@ -71,20 +112,33 @@ export const EuiCollapsibleNavGroup: FunctionComponent<
         <h3>{title}</h3>
       </EuiFlexItem>
     </EuiFlexGroup>
+  ) : (
+    undefined
   );
 
-  const generateID = htmlIdGenerator(slugify(title));
+  if (collapsible && title) {
+    const generateID = htmlIdGenerator(
+      title && typeof title === 'string' ? slugify(title) : ''
+    );
 
-  return (
-    <EuiAccordion
-      id={id || generateID()}
-      className={classes}
-      buttonClassName={buttonClasses}
-      buttonContent={buttonContent}
-      initialIsOpen={true}
-      arrowDisplay="right"
-      {...rest}>
-      <div className="euiCollapsibleNavGroup__children">{children}</div>
-    </EuiAccordion>
-  );
+    return (
+      <EuiAccordion
+        id={id || generateID()}
+        className={classes}
+        buttonClassName={titleClasses}
+        buttonContent={titleContent}
+        initialIsOpen={true}
+        arrowDisplay="right"
+        {...rest}>
+        {content}
+      </EuiAccordion>
+    );
+  } else {
+    return (
+      <div id={id} className={classes} {...rest}>
+        {titleContent && <div className={titleClasses}>{titleContent}</div>}
+        {content}
+      </div>
+    );
+  }
 };
