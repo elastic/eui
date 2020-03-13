1aae3599eae0ba2300b11b4c9aade85586ae2ba6
miukimiu <elizabet.oliveira@elastic.co>
2020-03-13 18:11:18 +0000

    Addding fill negative CSS class

--- src/components/icon/assets/logo_enterprise_search.js
+++ src/components/icon/assets/logo_enterprise_search.js
@@ -22,7 +22,7 @@ const EuiIconLogoEnterpriseSearch = ({ title, titleId, ...props }) => (
       clipRule="evenodd"
     />
     <path
-      fill="#343741"
+      className="euiIcon__fillNegative"
       fillRule="evenodd"
       d="M16 8h-2.152A15.877 15.877 0 0116 16c0 2.918-.786 5.647-2.152 8H16a8 8 0 000-16z"
       clipRule="evenodd"
