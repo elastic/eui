5e569a0b9cbf35c242883f9b83f6523ddb8a2c07
Anish Aggarwal <anish17122000@gmail.com>
2020-03-16 17:36:47 +0530

    test for text-align

--- src/components/text/text_align.test.tsx
+++ src/components/text/text_align.test.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { render } from 'enzyme';
 import { requiredProps } from '../../test';
 
-import { EuiTextAlign } from './text_align';
+import { EuiTextAlign, TextAlignment } from './text_align';
 
 describe('EuiTextAlign', () => {
   test('is rendered', () => {
     const component = render(<EuiTextAlign {...requiredProps} />);
 
     expect(component).toMatchSnapshot();
+  });
+
+  ['left', 'right', 'center'].forEach(direction => {
+    test(`in ${direction} direction`, () => {
+      const component = render(
+        <EuiTextAlign
+          {...requiredProps}
+          textAlign={direction as TextAlignment}
+        />
+      );
+
+      expect(component).toMatchSnapshot();
+    });
   });
 });
