7d08971cd4ad9f0c7a971b122e54a649610eab42
Anish Aggarwal <anish17122000@gmail.com>
2020-03-17 11:38:39 +0530

    fixed control columns

--- src-docs/src/views/datagrid/control_columns.js
+++ src-docs/src/views/datagrid/control_columns.js
@@ -141,6 +141,7 @@ const SelectionHeaderCell = () => {
   return (
     <EuiCheckbox
       id="selection-toggle"
+      aria-label="Select all rows"
       indeterminate={isIndeterminate}
       checked={selectedRows.size > 0}
       onChange={e => {
@@ -168,7 +169,8 @@ const SelectionRowCell = ({ rowIndex }) => {
   return (
     <div>
       <EuiCheckbox
-        id={`selection-checkbox-${rowIndex}`}
+        id={`${rowIndex}`}
+        aria-label={`Select row ${rowIndex}`}
         checked={isChecked}
         onChange={e => {
           if (e.target.checked) {
