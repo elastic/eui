4e793059c0f04573a653985e8ba3b7f300b9d796
Ashik Meerankutty <ashik9591@gmail.com>
2020-03-17 20:17:01 +0530

    Update quick select values on change of input

---
+++ src/components/date_picker/super_date_picker/quick_select_popover/quick_select_utlis.js
@@ -0,0 +1,40 @@
+import { isString } from '../../../../services/predicate';
+
+const LAST = 'last';
+const NEXT = 'next';
+
+export const parseTimeParts = value => {
+  console.log(value);
+  const matches =
+    isString(value) &&
+    value.match(/now(([\-\+])([0-9]+)([smhdwMy])(\/[smhdwMy])?)?/);
+
+  if (!matches) {
+    return {
+      timeValueDefault: 15,
+      timeUnitsDefault: 'm',
+      timeTenseDefault: LAST,
+    };
+  }
+
+  const operator = matches && matches[2];
+  const timeValue = matches && matches[3];
+  const timeUnitsDefault = matches && matches[4];
+
+  if (timeValue && timeUnitsDefault && operator) {
+    const timeValueDefault = parseInt(timeValue);
+    const timeTenseDefault = operator === '+' ? NEXT : LAST;
+
+    return {
+      timeValueDefault,
+      timeUnitsDefault,
+      timeTenseDefault,
+    };
+  }
+
+  return {
+    timeValueDefault: 15,
+    timeUnitsDefault: 'm',
+    timeTenseDefault: LAST,
+  };
+};
