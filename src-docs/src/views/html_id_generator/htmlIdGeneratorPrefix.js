34633f191e0c08da0bc9f9cbe61a19267dbf08cf
Anish Aggarwal <43617894+anishagg17@users.noreply.github.com>
2020-03-19 19:38:23 +0530

    Update src-docs/src/views/html_id_generator/htmlIdGeneratorPrefix.js
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>

--- src-docs/src/views/html_id_generator/htmlIdGeneratorPrefix.js
+++ src-docs/src/views/html_id_generator/htmlIdGeneratorPrefix.js
@@ -46,8 +46,7 @@ export class HtmlIdGeneratorPrefix extends Component {
             </EuiFormRow>
           </EuiFlexItem>
         </EuiFlexGroup>
-        <EuiSpacer size="m" />
-        <EuiSpacer size="m" />
+        <EuiSpacer size="xl" />
         <EuiCode>{id1} </EuiCode>
       </Fragment>
     );
