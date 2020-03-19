c987fb831eea60061a7482e86edc48ec3d6a0e90
cchaos <caroline.horn@elastic.co>
2020-03-19 15:40:59 -0400

    Started page for Pie

---
+++ src-docs/src/views/elastic_charts/pie_example.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { renderToHtml } from '../../services';
+
+import { GuideSectionTypes } from '../../components';
+
+import { EuiCode } from '../../../../src/components';
+
+import PieChart from './pie';
+const pieChartsSource = require('!!raw-loader!./pie');
+const pieChartsHtml = renderToHtml(PieChart);
+
+export const ElasticChartsPieExample = {
+  title: 'Pie charts',
+  sections: [
+    {
+      // title: 'ElasticCharts',
+      source: [
+        {
+          type: GuideSectionTypes.JS,
+          code: pieChartsSource,
+        },
+        {
+          type: GuideSectionTypes.HTML,
+          code: pieChartsHtml,
+        },
+      ],
+      text: (
+        <p>
+          Description needed: how to use the <EuiCode>EuiElasticCharts</EuiCode>{' '}
+          component.
+        </p>
+      ),
+      demo: <PieChart />,
+    },
+  ],
+};
