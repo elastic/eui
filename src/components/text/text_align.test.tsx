622d994afb93aa2e7250db2edaf432b36c4ff6f0
Anish Aggarwal <43617894+anishagg17@users.noreply.github.com>
2020-03-20 23:07:03 +0530

    added various unit tests (#3071)
    * added tests for date_modes
    
    * added test for EuiCopy
    
    * added tests for EuiTreeView
    
    * added tests for EuiToggle
    
    * test for text-align
    
    * added for EuiTab
    
    * Update src/components/copy/copy.test.tsx
    
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * Update src/components/toggle/toggle.test.tsx
    
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * Update src/components/toggle/toggle.test.tsx
    
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * Update src/components/toggle/toggle.test.tsx
    
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * Update src/components/toggle/toggle.test.tsx
    
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * Update src/components/text/text_align.test.tsx
    
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * Update src/components/toggle/toggle.test.tsx
    
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * updated tests
    
    * updated toggle to single
    
    * changed casting
    
    * dateMode tests
    
    * dateMode tests (#6)
    
    * date_modes test
    
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>
    Co-authored-by: Greg Thompson <thompson.glowe@gmail.com>
    Co-authored-by: Greg Thompson <thompsongl@users.noreply.github.com>

--- src/components/text/text_align.test.tsx
+++ src/components/text/text_align.test.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { render } from 'enzyme';
 import { requiredProps } from '../../test';
 
-import { EuiTextAlign } from './text_align';
+import { EuiTextAlign, ALIGNMENTS } from './text_align';
 
 describe('EuiTextAlign', () => {
   test('is rendered', () => {
     const component = render(<EuiTextAlign {...requiredProps} />);
 
     expect(component).toMatchSnapshot();
+  });
+
+  describe('direction prop', () => {
+    ALIGNMENTS.forEach(direction => {
+      test(`${direction} is rendered`, () => {
+        const component = render(<EuiTextAlign textAlign={direction} />);
+
+        expect(component).toMatchSnapshot();
+      });
+    });
   });
 });
