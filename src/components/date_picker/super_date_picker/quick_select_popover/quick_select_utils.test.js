78072f694c1e63f4a865fffff774f29823d7a09a
Ashik Meerankutty <ashik9591@gmail.com>
2020-03-21 12:02:34 +0530

    updated test

--- src/components/date_picker/super_date_picker/quick_select_popover/quick_select_utils.test.js
+++ src/components/date_picker/super_date_picker/quick_select_popover/quick_select_utils.test.js
@@ -1,6 +1,15 @@
 import { parseTimeParts } from './quick_select_utils';
 
 describe('parseTimeParts', () => {
+  it('should parse now', () => {
+    const out = parseTimeParts('now');
+    expect(out).toEqual({
+      timeValueDefault: 0,
+      timeUnitsDefault: 's',
+      timeTenseDefault: 'last',
+    });
+  });
+
   it('should parse now-2h', () => {
     const out = parseTimeParts('now-2h');
     expect(out).toEqual({
