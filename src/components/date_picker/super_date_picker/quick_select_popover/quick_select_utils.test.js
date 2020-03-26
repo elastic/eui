2b5165bbb3b74b711f532c645214d377a993797d
Ashik Meerankutty <ashik9591@gmail.com>
2020-03-26 07:58:11 +0530

    Added test for duration parsing

--- src/components/date_picker/super_date_picker/quick_select_popover/quick_select_utils.test.js
+++ src/components/date_picker/super_date_picker/quick_select_popover/quick_select_utils.test.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { parseTimeParts } from './quick_select_utils';
 
 describe('parseTimeParts', () => {
@@ -25,6 +26,26 @@ describe('parseTimeParts', () => {
       timeValueDefault: 2,
       timeUnitsDefault: 'h',
       timeTenseDefault: 'next',
+    });
+  });
+
+  describe('duration parsing', () => {
+    const duration = moment.duration;
+    beforeEach(() => {
+      moment.duration = () => duration(6 * 60 * 60 * 1000);
+    });
+
+    afterEach(() => {
+      moment.duration = duration;
+    });
+
+    it('should parse now/d', () => {
+      const out = parseTimeParts('now/d', 'now+5m');
+      expect(out).toEqual({
+        timeValueDefault: 6,
+        timeUnitsDefault: 'h',
+        timeTenseDefault: 'last',
+      });
     });
   });
 });
