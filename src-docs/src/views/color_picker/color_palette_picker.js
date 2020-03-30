c13ec9c8fd7a0b59e3e05faa6612f8075435d9e1
miukimiu <elizabet.oliveira@elastic.co>
2020-03-30 03:32:31 +0100

    Adding initial code

---
+++ src-docs/src/views/color_picker/color_palette_picker.js
@@ -0,0 +1,206 @@
+import React, { useState } from 'react';
+
+import {
+  EuiColorPalettePicker,
+  EuiFormRow,
+  EuiButtonIcon,
+  EuiToolTip,
+  EuiModal,
+  EuiModalBody,
+  EuiModalFooter,
+  EuiModalHeader,
+  EuiModalHeaderTitle,
+  EuiOverlayMask,
+  EuiButton,
+  EuiButtonEmpty,
+  EuiColorStops,
+} from '../../../../src/components';
+
+import {
+  euiPaletteColorBlind,
+  euiPaletteComplimentary,
+  euiPaletteForStatus,
+  euiPaletteForTemperature,
+  euiPaletteCool,
+  euiPaletteGray,
+  useColorStopsState,
+} from '../../../../src/services';
+
+const basicExample = [
+  {
+    value: 'basicExample1',
+    title: 'EUI basicPalette Cool',
+    palette: euiPaletteCool(),
+    type: 'stops',
+  },
+  {
+    value: 'basicExample2',
+    title: 'Paul Tor 14',
+    palette: euiPaletteColorBlind(2),
+    type: 'stops',
+  },
+  {
+    value: 'basicExample3',
+    title: 'EUI basicPalette for Status',
+    palette: euiPaletteForStatus(),
+    type: 'stops',
+  },
+  {
+    value: 'basicExample4',
+    title: 'Liner Gradient',
+    palette:
+      'linear-gradient(to right, rgb(0, 104, 55) 0%, rgb(45, 161, 84) 14%, rgb(134, 203, 102) 28%, rgb(205, 233, 131) 42%, rgb(255, 254, 189) 57%, rgb(253, 210, 127) 71%, rgb(248, 139, 81) 85%, rgb(164, 0, 37) 100%)',
+    type: 'gradient',
+  },
+  {
+    title: 'Custom Option',
+    type: 'button',
+  },
+];
+
+// palette type: 'gradient' should use this format,
+// [
+//   {
+//     stop: 100,
+//     color: '#54B399',
+//   },
+//   {
+//     stop: 250,
+//     color: '#D36086',
+//   },
+//   {
+//     stop: 350,
+//     color: '#9170B8',
+//   },
+// ]
+
+const customExample = [
+  {
+    value: 'customExample1',
+    title: 'EUI basicPalette Color Blind',
+    palette: euiPaletteColorBlind(),
+    type: 'stops',
+  },
+  {
+    value: 'customExample2',
+    title: 'EUI basicPalette Complimentary',
+    palette: euiPaletteComplimentary(2),
+    type: 'stops',
+  },
+  {
+    value: 'customExample3',
+    title: 'EUI basicPalette for Temperature',
+    palette: euiPaletteForTemperature(),
+    type: 'stops',
+  },
+  {
+    value: 'customExample4',
+    title: 'EUI basicPalette Gray',
+    palette: euiPaletteGray(),
+    type: 'stops',
+  },
+];
+
+export const ColorPalettePicker = () => {
+  const [basicPalette, setBasicPalette] = useState('basicExample1');
+  const [customPalette, setCustomPalette] = useState('customExample1');
+  const [colorStops, setColorStops] = useColorStopsState(true);
+  const [isShowing, setIsShowing] = useState(false);
+
+  console.log('basicExample', basicExample);
+
+  const onBasicPaletteChange = value => {
+    setBasicPalette(value);
+  };
+
+  const onCustomPaletteChange = value => {
+    setCustomPalette(value);
+  };
+
+  const toggleModal = () => {
+    setIsShowing(!isShowing);
+  };
+
+  const customPaletteAppend = (
+    <EuiToolTip position="top" content="Custom palette">
+      <EuiButtonIcon
+        iconType="gear"
+        color="text"
+        aria-label="Custom palette"
+        onClick={toggleModal}
+      />
+    </EuiToolTip>
+  );
+
+  const newCustomPaletteArray = colorStops.map(item => item.color);
+
+  const extendedCustomExample = customExample.concat({
+    value: 'customExample5',
+    title: 'Custom basicPalette',
+    palette: newCustomPaletteArray,
+    type: 'stops',
+  });
+
+  const saveAndCloseModal = () => {
+    setCustomPalette(
+      extendedCustomExample[extendedCustomExample.length - 1].value
+    );
+    toggleModal();
+  };
+
+  let modal;
+
+  if (isShowing) {
+    modal = (
+      <EuiOverlayMask>
+        <EuiModal onClose={toggleModal} initialFocus="[name=popswitch]">
+          <EuiModalHeader>
+            <EuiModalHeaderTitle>Custom Palette</EuiModalHeaderTitle>
+          </EuiModalHeader>
+
+          <EuiModalBody>
+            <EuiFormRow label="Fixed color segments">
+              <EuiColorStops
+                label="Fixed color segments"
+                onChange={setColorStops}
+                colorStops={colorStops}
+                min={0}
+                max={100}
+                stopType="fixed"
+              />
+            </EuiFormRow>
+          </EuiModalBody>
+
+          <EuiModalFooter>
+            <EuiButtonEmpty onClick={toggleModal}>Cancel</EuiButtonEmpty>
+
+            <EuiButton onClick={saveAndCloseModal} fill>
+              Save and Apply
+            </EuiButton>
+          </EuiModalFooter>
+        </EuiModal>
+      </EuiOverlayMask>
+    );
+  }
+
+  return (
+    <>
+      <EuiFormRow label="Basic palette picker">
+        <EuiColorPalettePicker
+          palettes={basicExample}
+          onChange={onBasicPaletteChange}
+          valueOfSelected={basicPalette}
+        />
+      </EuiFormRow>
+      <EuiFormRow label="Palette picker with one custom palette">
+        <EuiColorPalettePicker
+          palettes={extendedCustomExample}
+          onChange={onCustomPaletteChange}
+          valueOfSelected={customPalette}
+          append={customPaletteAppend}
+        />
+      </EuiFormRow>
+      {modal}
+    </>
+  );
+};
---
+++ src/components/color_palette_picker/color_palette_picker.tsx
@@ -0,0 +1,151 @@
+import React, { FunctionComponent } from 'react';
+
+import {
+  EuiSuperSelect,
+  EuiSuperSelectProps,
+  EuiFormControlLayoutProps,
+} from '../form';
+
+import { EuiText } from '../text';
+
+import { EuiFlexGroup, EuiFlexItem } from '../flex';
+import { CommonProps } from '../common';
+
+import { EuiIcon } from '../icon';
+
+export type EuiColorPalettePickerProps = CommonProps & {
+  /**
+   *  Use the compressed style for EuiColorPalette
+   */
+  compressed?: boolean;
+  disabled?: boolean;
+  fullWidth?: boolean;
+  id?: string;
+  readOnly?: boolean;
+  isInvalid?: boolean;
+  /**
+   * Creates an input group with element(s) coming before input. It only shows when the `display` is set to `default`.
+   * `string` | `ReactElement` or an array of these
+   */
+  prepend?: EuiFormControlLayoutProps['prepend'];
+
+  /**
+   * Creates an input group with element(s) coming after input. It only shows when the `display` is set to `default`.
+   * `string` | `ReactElement` or an array of these
+   */
+  append?: EuiFormControlLayoutProps['append'];
+  /**
+   * Whether to render the alpha channel (opacity) value range slider.
+   */
+  /**
+   * Will format the text input in the provided format when possible (hue and saturation selection)
+   * Exceptions: Manual text input and swatches will display as-authored
+   * Default is to display the last format entered by the user
+   */
+  valueOfSelected?: any;
+
+  /**
+   * You must pass an `onChange` function to handle the update of the value
+   */
+  onChange?: (options: any) => void;
+  /**
+   * An array of objects. `value`: a unique value | `title`: the name of your palette (not required) | `type`: specify if your palette is a `gradient` or `stops` | `palette`: if your palette is a gradient pass a CSS linear gradient or if it's stops pass an array of hexadecimals
+   */
+  palettes: EuiSuperSelectProps<any>['options'];
+};
+
+export const EuiColorPalettePicker: FunctionComponent<
+  EuiColorPalettePickerProps
+> = ({
+  className,
+  compressed = false,
+  disabled,
+  fullWidth = false,
+  isInvalid = false,
+  onChange,
+  readOnly = false,
+  valueOfSelected,
+  palettes,
+  append,
+  prepend,
+  ...rest
+}) => {
+  const getPalette = (palettes: any, type: string) =>
+    type === 'stops' ? (
+      <div className="euiColorPalettePicker__colorContainer">
+        {palettes.map((hexCode: string) => (
+          <span
+            title={hexCode}
+            style={{ backgroundColor: hexCode }}
+            key={hexCode}
+            className={'euiColorPalettePicker__colorStop'}
+          />
+        ))}
+      </div>
+    ) : (
+      <div
+        className="euiColorPalettePicker__colorContainer"
+        style={{ background: palettes }}
+      />
+    );
+
+  const getButton = (title: any) => (
+    <EuiFlexGroup gutterSize="xs">
+      <EuiFlexItem>
+        <EuiText size="xs">
+          <span>{title}</span>
+        </EuiText>
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiIcon type="gear" />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+
+  const paletteOptions = palettes.map((item: any) => {
+    return {
+      value: String(item.value),
+      inputDisplay:
+        item.type !== 'button'
+          ? getPalette(item.palette, item.type)
+          : getButton(item.title),
+      dropdownDisplay: (
+        <EuiFlexGroup gutterSize="xs" direction="column">
+          {item.title && item.type !== 'button' && (
+            <EuiFlexItem>
+              <EuiText
+                size="xs"
+                className="euiColorPalettePicker__paletteTitle">
+                <span>{item.title}</span>
+              </EuiText>
+            </EuiFlexItem>
+          )}
+
+          <EuiFlexItem>
+            {item.type !== 'button'
+              ? getPalette(item.palette, item.type)
+              : getButton(item.title)}
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      ),
+    };
+  });
+
+  return (
+    <EuiSuperSelect
+      className={className}
+      options={paletteOptions}
+      valueOfSelected={valueOfSelected}
+      onChange={onChange}
+      hasDividers
+      isInvalid={isInvalid}
+      compressed={compressed}
+      disabled={disabled}
+      readOnly={readOnly}
+      fullWidth={fullWidth}
+      append={append}
+      prepend={prepend}
+      {...rest}
+    />
+  );
+};
