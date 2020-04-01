293bd6a608f7787e0860f44d2b8505ded61c409c
Greg Thompson <thompson.glowe@gmail.com>
2020-04-01 15:18:03 -0500

    Update src-docs/src/views/tour/tour_example.js
    Co-Authored-By: Caroline Horn <549577+cchaos@users.noreply.github.com>

--- src-docs/src/views/tour/tour_example.js
+++ src-docs/src/views/tour/tour_example.js
@@ -114,9 +114,8 @@ export const TourExample = {
         <p>
           Each <strong>EuiTourStep</strong> can be configured independently via
           props. In this case, each component is stateless and needs to be
-          paired with some form of state management for navigation. See two
-          options in later examples (<strong>useEuiTour</strong> and{' '}
-          <strong>EuiTour</strong>).
+          paired with some form of state management for navigation.
+          The later examples showcase other ways to handle state management via <strong>useEuiTour</strong> and <strong>EuiTour</strong>.
         </p>
       ),
       demo: <Tour />,
