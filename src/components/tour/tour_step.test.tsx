86fb015c08cffe448726bb0d6c75037fb7514c71
Greg Thompson <thompson.glowe@gmail.com>
2020-04-01 15:56:25 -0500

    comments and tests

--- src/components/tour/tour_step.test.tsx
+++ src/components/tour/tour_step.test.tsx
@@ -44,4 +44,56 @@ describe('EuiTourStep', () => {
 
     expect(component).toMatchSnapshot();
   });
+
+  test('can be closed', () => {
+    const component = render(
+      <EuiTourStep
+        {...config}
+        {...steps[0]}
+        isStepOpen={false}
+        {...requiredProps}>
+        <span>Test</span>
+      </EuiTourStep>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  test('can set a minWidth', () => {
+    const component = render(
+      <EuiTourStep {...config} {...steps[0]} minWidth={240} {...requiredProps}>
+        <span>Test</span>
+      </EuiTourStep>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  test('can override the footer action', () => {
+    const component = render(
+      <EuiTourStep
+        {...config}
+        {...steps[0]}
+        footerAction={<button onClick={() => {}}>Test</button>}
+        {...requiredProps}>
+        <span>Test</span>
+      </EuiTourStep>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+  test('can turn off the beacon', () => {
+    const component = render(
+      <EuiTourStep
+        {...config}
+        {...steps[0]}
+        decoration="none"
+        {...requiredProps}>
+        <span>Test</span>
+      </EuiTourStep>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
 });
