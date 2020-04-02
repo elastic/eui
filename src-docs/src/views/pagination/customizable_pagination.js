6588c71fe08af62ef96424c87c4ade5e13f34b5e
Mridul Gogia <gogiamridul@gmail.com>
2020-04-03 02:58:04 +0530

    Converted facet and pagination examplee class component  into function component

--- src-docs/src/views/pagination/customizable_pagination.js
+++ src-docs/src/views/pagination/customizable_pagination.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import {
   EuiButtonEmpty,
@@ -10,107 +10,86 @@ import {
   EuiPopover,
 } from '../../../../src/components';
 
-export default class extends Component {
-  constructor(props) {
-    super(props);
+export default () => {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [activePage, setActivePage] = useState(0);
 
-    this.state = {
-      isPopoverOpen: false,
-      activePage: 0,
-    };
+  const PAGE_COUNT = 10;
 
-    this.PAGE_COUNT = 10;
-  }
+  const onButtonClick = () => setIsPopoverOpen(isPopoverOpen => !isPopoverOpen);
+  const closePopover = () => setIsPopoverOpen(false);
 
-  onButtonClick() {
-    this.setState({
-      isPopoverOpen: !this.state.isPopoverOpen,
-    });
-  }
+  const goToPage = pageNumber => setActivePage(pageNumber);
 
-  closePopover() {
-    this.setState({
-      isPopoverOpen: false,
-    });
-  }
+  const button = (
+    <EuiButtonEmpty
+      size="s"
+      color="text"
+      iconType="arrowDown"
+      iconSide="right"
+      onClick={onButtonClick}>
+      Rows per page: 50
+    </EuiButtonEmpty>
+  );
 
-  goToPage = pageNumber => {
-    this.setState({
-      activePage: pageNumber,
-    });
-  };
+  const items = [
+    <EuiContextMenuItem
+      key="10 rows"
+      icon="empty"
+      onClick={() => {
+        closePopover();
+        window.alert('10 rows');
+      }}>
+      10 rows
+    </EuiContextMenuItem>,
+    <EuiContextMenuItem
+      key="20 rows"
+      icon="empty"
+      onClick={() => {
+        closePopover();
+        window.alert('20 rows');
+      }}>
+      20 rows
+    </EuiContextMenuItem>,
+    <EuiContextMenuItem
+      key="50 rows"
+      icon="check"
+      onClick={() => {
+        closePopover();
+        window.alert('50 rows');
+      }}>
+      50 rows
+    </EuiContextMenuItem>,
+    <EuiContextMenuItem
+      key="100 rows"
+      icon="empty"
+      onClick={() => {
+        closePopover();
+        window.alert('100 rows');
+      }}>
+      100 rows
+    </EuiContextMenuItem>,
+  ];
 
-  render() {
-    const button = (
-      <EuiButtonEmpty
-        size="s"
-        color="text"
-        iconType="arrowDown"
-        iconSide="right"
-        onClick={this.onButtonClick.bind(this)}>
-        Rows per page: 50
-      </EuiButtonEmpty>
-    );
+  return (
+    <EuiFlexGroup justifyContent="spaceBetween" alignItems="center">
+      <EuiFlexItem grow={false}>
+        <EuiPopover
+          button={button}
+          isOpen={isPopoverOpen}
+          closePopover={closePopover}
+          panelPaddingSize="none">
+          <EuiContextMenuPanel items={items} />
+        </EuiPopover>
+      </EuiFlexItem>
 
-    const items = [
-      <EuiContextMenuItem
-        key="10 rows"
-        icon="empty"
-        onClick={() => {
-          this.closePopover();
-          window.alert('10 rows');
-        }}>
-        10 rows
-      </EuiContextMenuItem>,
-      <EuiContextMenuItem
-        key="20 rows"
-        icon="empty"
-        onClick={() => {
-          this.closePopover();
-          window.alert('20 rows');
-        }}>
-        20 rows
-      </EuiContextMenuItem>,
-      <EuiContextMenuItem
-        key="50 rows"
-        icon="check"
-        onClick={() => {
-          this.closePopover();
-          window.alert('50 rows');
-        }}>
-        50 rows
-      </EuiContextMenuItem>,
-      <EuiContextMenuItem
-        key="100 rows"
-        icon="empty"
-        onClick={() => {
-          this.closePopover();
-          window.alert('100 rows');
-        }}>
-        100 rows
-      </EuiContextMenuItem>,
-    ];
-
-    return (
-      <EuiFlexGroup justifyContent="spaceBetween" alignItems="center">
-        <EuiFlexItem grow={false}>
-          <EuiPopover
-            button={button}
-            isOpen={this.state.isPopoverOpen}
-            closePopover={this.closePopover.bind(this)}
-            panelPaddingSize="none">
-            <EuiContextMenuPanel items={items} />
-          </EuiPopover>
-        </EuiFlexItem>
-
-        <EuiFlexItem grow={false}>
-          <EuiPagination
-            pageCount={this.PAGE_COUNT}
-            activePage={this.state.activePage}
-            onPageClick={this.goToPage}
-          />
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    );
-  }
-}
+      <EuiFlexItem grow={false}>
+        <EuiPagination
+          pageCount={PAGE_COUNT}
+          activePage={activePage}
+          onPageClick={goToPage}
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
