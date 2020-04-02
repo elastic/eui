5fb0728c860cee0a51d12f2bc8dff6a9833ac7ae
miukimiu <elizabet.oliveira@elastic.co>
2020-04-02 16:08:28 +0100

    Fixing prettier errors

--- src-docs/src/views/side_nav/side_nav.js
+++ src-docs/src/views/side_nav/side_nav.js
@@ -3,57 +3,55 @@ import React, { useState } from 'react';
 import { EuiSideNav } from '../../../../src/components';
 
 export default function() {
-
-  const [ isSideNavOpenOnMobile, setisSideNavOpenOnMobile ] = useState(false);
+  const [isSideNavOpenOnMobile, setisSideNavOpenOnMobile] = useState(false);
 
   const toggleOpenOnMobile = () => {
     setisSideNavOpenOnMobile(!this.state.isSideNavOpenOnMobile);
-    };
+  };
 
-    const sideNav = [
-      {
-        name: 'Kibana',
-        id: 0,
-        items: [
-          {
-            name: 'Advanced settings',
-            id: 1,
-            onClick: () => {
-              window.alert('Advanced settings');
-            },
+  const sideNav = [
+    {
+      name: 'Kibana',
+      id: 0,
+      items: [
+        {
+          name: 'Advanced settings',
+          id: 1,
+          onClick: () => {
+            window.alert('Advanced settings');
           },
-          {
-            name: 'Index Patterns (link)',
-            id: 2,
-            href: 'http://www.elastic.co',
+        },
+        {
+          name: 'Index Patterns (link)',
+          id: 2,
+          href: 'http://www.elastic.co',
+        },
+        {
+          name: 'Saved Objects',
+          id: 3,
+          onClick: () => {
+            window.alert('Saved Objects');
           },
-          {
-            name: 'Saved Objects',
-            id: 3,
-            onClick: () => {
-              window.alert('Saved Objects');
-            },
-            isSelected: true,
+          isSelected: true,
+        },
+        {
+          name: 'Reporting',
+          id: 4,
+          onClick: () => {
+            window.alert('Reporting');
           },
-          {
-            name: 'Reporting',
-            id: 4,
-            onClick: () => {
-              window.alert('Reporting');
-            },
-          },
-        ],
-      },
-    ];
+        },
+      ],
+    },
+  ];
 
-    return (
-      <EuiSideNav
-        mobileTitle="Navigate within $APP_NAME"
-        toggleOpenOnMobile={() => toggleOpenOnMobile()}
-        isOpenOnMobile={isSideNavOpenOnMobile}
-        style={{ width: 192 }}
-        items={sideNav}
-      />
-    );
-  }
-
+  return (
+    <EuiSideNav
+      mobileTitle="Navigate within $APP_NAME"
+      toggleOpenOnMobile={() => toggleOpenOnMobile()}
+      isOpenOnMobile={isSideNavOpenOnMobile}
+      style={{ width: 192 }}
+      items={sideNav}
+    />
+  );
+}
