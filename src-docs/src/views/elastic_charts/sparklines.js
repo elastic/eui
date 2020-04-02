d1c3ba680d32fbd7df88daecfb471aef3053dd9a
Ashik Meerankutty <ashik9591@gmail.com>
2020-04-02 22:49:32 +0530

    [DOCS] Converted Charts examples to use Functional Components (#3226)
    * converted CategoryChart to functional component
    
    * converted sparklines to functional component
    
    * converted theming category to functional component
    
    * converted time_chart to functional component
    
    * fixes in context
    
    * converted shared to functional component
    
    * fixes in shared

--- src-docs/src/views/elastic_charts/sparklines.js
+++ src-docs/src/views/elastic_charts/sparklines.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useContext, Fragment } from 'react';
 import { cloneDeep } from 'lodash';
 import { ThemeContext } from '../../components';
 import {
@@ -31,144 +31,133 @@ import {
   euiPaletteForLightBackground,
 } from '../../../../src/services';
 
-export class Sparklines extends Component {
-  constructor(props) {
-    super(props);
+export const Sparklines = () => {
+  const themeContext = useContext(ThemeContext);
+  const isDarkTheme = themeContext.theme.includes('dark');
 
-    this.state = {};
-  }
+  const theme = [
+    EUI_SPARKLINE_THEME_PARTIAL,
+    isDarkTheme ? EUI_CHARTS_THEME_DARK.theme : EUI_CHARTS_THEME_LIGHT.theme,
+  ];
 
-  render() {
-    const isDarkTheme = this.context.theme.includes('dark');
-    const theme = [
-      EUI_SPARKLINE_THEME_PARTIAL,
-      isDarkTheme ? EUI_CHARTS_THEME_DARK.theme : EUI_CHARTS_THEME_LIGHT.theme,
-    ];
+  const TIME_DATA_SMALL_REVERSE = cloneDeep(TIME_DATA_SMALL).reverse();
+  const TIME_DATA_SMALL_REVERSE_MAJOR = cloneDeep(TIME_DATA_SMALL_REVERSE);
+  TIME_DATA_SMALL_REVERSE_MAJOR[
+    TIME_DATA_SMALL_REVERSE_MAJOR.length - 1
+  ][1] = -100;
 
-    const TIME_DATA_SMALL_REVERSE = cloneDeep(TIME_DATA_SMALL).reverse();
-    const TIME_DATA_SMALL_REVERSE_MAJOR = cloneDeep(TIME_DATA_SMALL_REVERSE);
-    TIME_DATA_SMALL_REVERSE_MAJOR[
-      TIME_DATA_SMALL_REVERSE_MAJOR.length - 1
-    ][1] = -100;
-
-    return (
-      <Fragment>
-        <EuiFlexGrid columns={4} responsive={false}>
-          <EuiFlexItem>
-            <EuiPanel>
-              <EuiStat
-                title=""
-                description="Number of things"
-                textAlign="right">
-                <EuiSpacer size="s" />
-                <Chart size={{ height: 64 }}>
-                  <Settings theme={theme} showLegend={false} tooltip="none" />
-                  <BarSeries
-                    id="numbers"
-                    data={TIME_DATA_SMALL}
-                    xAccessor={0}
-                    yAccessors={[1]}
-                    customSeriesColors={[
-                      isDarkTheme
-                        ? euiPaletteForDarkBackground()[1]
-                        : euiPaletteForLightBackground()[1],
-                    ]}
-                  />
-                </Chart>
-              </EuiStat>
-            </EuiPanel>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiPanel>
-              <EuiStat
-                title=""
-                description="Increase over time"
-                titleColor="secondary"
-                textAlign="right">
-                <EuiSpacer size="s" />
-                <Chart size={{ height: 48 }}>
-                  <Settings theme={theme} showLegend={false} tooltip="none" />
-                  <LineSeries
-                    id="increase"
-                    data={TIME_DATA_SMALL}
-                    xAccessor={0}
-                    yAccessors={[1]}
-                    customSeriesColors={[
-                      isDarkTheme
-                        ? euiPaletteForDarkBackground()[1]
-                        : euiPaletteForLightBackground()[1],
-                    ]}
-                  />
-                </Chart>
-                <EuiSpacer size="s" />
-                <EuiText size="xs" color="secondary">
-                  <EuiIcon type="sortUp" /> <strong>15%</strong>
-                </EuiText>
-              </EuiStat>
-            </EuiPanel>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiPanel>
-              <EuiStat
-                title={
-                  <span>
-                    <EuiIcon size="xl" type="sortDown" /> 15%
-                  </span>
-                }
-                description="Major decrease over time"
-                titleColor="danger"
-                textAlign="right">
-                <EuiSpacer size="s" />
-                <Chart size={{ height: 16 }}>
-                  <Settings theme={theme} showLegend={false} tooltip="none" />
-                  <LineSeries
-                    id="major"
-                    data={TIME_DATA_SMALL_REVERSE_MAJOR}
-                    xAccessor={0}
-                    yAccessors={[1]}
-                    customSeriesColors={[
-                      isDarkTheme
-                        ? euiPaletteForDarkBackground()[3]
-                        : euiPaletteForLightBackground()[3],
-                    ]}
-                  />
-                </Chart>
-              </EuiStat>
-            </EuiPanel>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiPanel>
-              <EuiStat
-                title=""
-                description="Subtle decrease"
-                titleColor="danger"
-                textAlign="right">
-                <EuiSpacer size="s" />
-                <Chart size={{ height: 48 }}>
-                  <Settings theme={theme} showLegend={false} tooltip="none" />
-                  <AreaSeries
-                    id="subtle"
-                    data={TIME_DATA_SMALL_REVERSE}
-                    xAccessor={0}
-                    yAccessors={[1]}
-                    customSeriesColors={[
-                      isDarkTheme
-                        ? euiPaletteForDarkBackground()[3]
-                        : euiPaletteForLightBackground()[3],
-                    ]}
-                  />
-                </Chart>
-                <EuiSpacer size="s" />
-                <EuiText size="xs" color="danger">
-                  - 15 points since last Tuesday
-                </EuiText>
-              </EuiStat>
-            </EuiPanel>
-          </EuiFlexItem>
-        </EuiFlexGrid>
-      </Fragment>
-    );
-  }
-}
-
-Sparklines.contextType = ThemeContext;
+  return (
+    <Fragment>
+      <EuiFlexGrid columns={4} responsive={false}>
+        <EuiFlexItem>
+          <EuiPanel>
+            <EuiStat title="" description="Number of things" textAlign="right">
+              <EuiSpacer size="s" />
+              <Chart size={{ height: 64 }}>
+                <Settings theme={theme} showLegend={false} tooltip="none" />
+                <BarSeries
+                  id="numbers"
+                  data={TIME_DATA_SMALL}
+                  xAccessor={0}
+                  yAccessors={[1]}
+                  customSeriesColors={[
+                    isDarkTheme
+                      ? euiPaletteForDarkBackground()[1]
+                      : euiPaletteForLightBackground()[1],
+                  ]}
+                />
+              </Chart>
+            </EuiStat>
+          </EuiPanel>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiPanel>
+            <EuiStat
+              title=""
+              description="Increase over time"
+              titleColor="secondary"
+              textAlign="right">
+              <EuiSpacer size="s" />
+              <Chart size={{ height: 48 }}>
+                <Settings theme={theme} showLegend={false} tooltip="none" />
+                <LineSeries
+                  id="increase"
+                  data={TIME_DATA_SMALL}
+                  xAccessor={0}
+                  yAccessors={[1]}
+                  customSeriesColors={[
+                    isDarkTheme
+                      ? euiPaletteForDarkBackground()[1]
+                      : euiPaletteForLightBackground()[1],
+                  ]}
+                />
+              </Chart>
+              <EuiSpacer size="s" />
+              <EuiText size="xs" color="secondary">
+                <EuiIcon type="sortUp" /> <strong>15%</strong>
+              </EuiText>
+            </EuiStat>
+          </EuiPanel>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiPanel>
+            <EuiStat
+              title={
+                <span>
+                  <EuiIcon size="xl" type="sortDown" /> 15%
+                </span>
+              }
+              description="Major decrease over time"
+              titleColor="danger"
+              textAlign="right">
+              <EuiSpacer size="s" />
+              <Chart size={{ height: 16 }}>
+                <Settings theme={theme} showLegend={false} tooltip="none" />
+                <LineSeries
+                  id="major"
+                  data={TIME_DATA_SMALL_REVERSE_MAJOR}
+                  xAccessor={0}
+                  yAccessors={[1]}
+                  customSeriesColors={[
+                    isDarkTheme
+                      ? euiPaletteForDarkBackground()[3]
+                      : euiPaletteForLightBackground()[3],
+                  ]}
+                />
+              </Chart>
+            </EuiStat>
+          </EuiPanel>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiPanel>
+            <EuiStat
+              title=""
+              description="Subtle decrease"
+              titleColor="danger"
+              textAlign="right">
+              <EuiSpacer size="s" />
+              <Chart size={{ height: 48 }}>
+                <Settings theme={theme} showLegend={false} tooltip="none" />
+                <AreaSeries
+                  id="subtle"
+                  data={TIME_DATA_SMALL_REVERSE}
+                  xAccessor={0}
+                  yAccessors={[1]}
+                  customSeriesColors={[
+                    isDarkTheme
+                      ? euiPaletteForDarkBackground()[3]
+                      : euiPaletteForLightBackground()[3],
+                  ]}
+                />
+              </Chart>
+              <EuiSpacer size="s" />
+              <EuiText size="xs" color="danger">
+                - 15 points since last Tuesday
+              </EuiText>
+            </EuiStat>
+          </EuiPanel>
+        </EuiFlexItem>
+      </EuiFlexGrid>
+    </Fragment>
+  );
+};
