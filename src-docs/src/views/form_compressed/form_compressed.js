43ba119c9c4ffe3a70e5631eeaed5af6df9cbc7d
Ashik Meerankutty <ashik9591@gmail.com>
2020-04-04 02:21:39 +0530

    [Docs] Converted compressed forms examples to functional component  (#3237)

--- src-docs/src/views/form_compressed/form_compressed.js
+++ src-docs/src/views/form_compressed/form_compressed.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import {
   EuiCheckboxGroup,
@@ -15,139 +15,122 @@ import {
 
 import { htmlIdGenerator } from '../../../../src/services';
 
-export default class extends Component {
-  constructor(props) {
-    super(props);
+export default () => {
+  const idPrefix = htmlIdGenerator()();
 
-    const idPrefix = htmlIdGenerator()();
+  const [checkboxes] = useState([
+    {
+      id: `${idPrefix}0`,
+      label: 'Option one',
+    },
+    {
+      id: `${idPrefix}1`,
+      label: 'Option two is checked by default',
+    },
+    {
+      id: `${idPrefix}2`,
+      label: 'Option three',
+    },
+  ]);
+  const [isSwitchChecked, setIsSwitchChecked] = useState(false);
+  const [checkboxIdToSelectedMap, setCheckboxIdToSelectedMap] = useState({
+    [`${idPrefix}1`]: true,
+  });
 
-    this.state = {
-      isSwitchChecked: false,
-      checkboxes: [
-        {
-          id: `${idPrefix}0`,
-          label: 'Option one',
-        },
-        {
-          id: `${idPrefix}1`,
-          label: 'Option two is checked by default',
-        },
-        {
-          id: `${idPrefix}2`,
-          label: 'Option three',
-        },
-      ],
-      checkboxIdToSelectedMap: {
-        [`${idPrefix}1`]: true,
-      },
-      comboBoxSelectionOptions: [],
-      value: '20',
-    };
-  }
+  const [comboBoxSelectionOptions, setComboBoxSelectionOptions] = useState([]);
 
-  onRangeChange = e => {
-    this.setState({
-      value: e.target.value,
-    });
+  const [value, setValue] = useState(20);
+
+  const onRangeChange = e => {
+    setValue(e.target.value);
   };
 
-  onSwitchChange = () => {
-    this.setState({
-      isSwitchChecked: !this.state.isSwitchChecked,
-    });
+  const onSwitchChange = () => {
+    setIsSwitchChecked(!isSwitchChecked);
   };
 
-  onCheckboxChange = optionId => {
+  const onCheckboxChange = optionId => {
     const newCheckboxIdToSelectedMap = {
-      ...this.state.checkboxIdToSelectedMap,
+      ...checkboxIdToSelectedMap,
       ...{
-        [optionId]: !this.state.checkboxIdToSelectedMap[optionId],
+        [optionId]: !checkboxIdToSelectedMap[optionId],
       },
     };
-
-    this.setState({
-      checkboxIdToSelectedMap: newCheckboxIdToSelectedMap,
-    });
+    setCheckboxIdToSelectedMap(newCheckboxIdToSelectedMap);
   };
+  return (
+    <EuiPanel style={{ maxWidth: 300 }}>
+      <EuiFormRow
+        label="Text field"
+        helpText="I am some friendly help text."
+        display="rowCompressed">
+        <EuiFieldText name="first" isLoading compressed />
+      </EuiFormRow>
 
-  render() {
-    return (
-      <EuiPanel style={{ maxWidth: 300 }}>
-        <EuiFormRow
-          label="Text field"
-          helpText="I am some friendly help text."
-          display="rowCompressed">
-          <EuiFieldText name="first" isLoading compressed />
-        </EuiFormRow>
-
-        <EuiFormRow label="Select" display="rowCompressed">
-          <EuiSelect
-            options={[
-              { value: 'option_one', text: 'Option one' },
-              { value: 'option_two', text: 'Option two' },
-              { value: 'option_three', text: 'Option three' },
-            ]}
-            compressed
-          />
-        </EuiFormRow>
-
-        <EuiFormRow label="File picker" display="rowCompressed">
-          <EuiFilePicker compressed display="default" />
-        </EuiFormRow>
-
-        <EuiFormRow label="Combobox" display="rowCompressed">
-          <EuiComboBox
-            options={[
-              { label: 'Option one' },
-              { label: 'Option two' },
-              { label: 'Option three' },
-            ]}
-            compressed
-            selectedOptions={this.state.comboBoxSelectionOptions}
-            onChange={comboBoxSelectionOptions =>
-              this.setState({ comboBoxSelectionOptions })
-            }
-          />
-        </EuiFormRow>
-
-        <EuiFormRow label="Range" display="rowCompressed">
-          <EuiRange
-            min={0}
-            max={100}
-            name="range"
-            id="range"
-            showInput
-            compressed
-            value={this.state.value}
-            onChange={this.onRangeChange}
-          />
-        </EuiFormRow>
-
-        <EuiFormRow
-          label="Switch"
-          display="rowCompressed"
-          hasChildLabel={false}>
-          <EuiSwitch
-            label="Should we do this?"
-            name="switch"
-            checked={this.state.isSwitchChecked}
-            onChange={this.onSwitchChange}
-            compressed
-          />
-        </EuiFormRow>
-
-        <EuiSpacer size="m" />
-
-        <EuiCheckboxGroup
-          options={this.state.checkboxes}
-          idToSelectedMap={this.state.checkboxIdToSelectedMap}
-          onChange={this.onCheckboxChange}
-          legend={{
-            children: 'Checkboxes',
-          }}
+      <EuiFormRow label="Select" display="rowCompressed">
+        <EuiSelect
+          options={[
+            { value: 'option_one', text: 'Option one' },
+            { value: 'option_two', text: 'Option two' },
+            { value: 'option_three', text: 'Option three' },
+          ]}
           compressed
         />
-      </EuiPanel>
-    );
-  }
-}
+      </EuiFormRow>
+
+      <EuiFormRow label="File picker" display="rowCompressed">
+        <EuiFilePicker compressed display="default" />
+      </EuiFormRow>
+
+      <EuiFormRow label="Combobox" display="rowCompressed">
+        <EuiComboBox
+          options={[
+            { label: 'Option one' },
+            { label: 'Option two' },
+            { label: 'Option three' },
+          ]}
+          compressed
+          selectedOptions={comboBoxSelectionOptions}
+          onChange={comboBoxSelectionOptions =>
+            setComboBoxSelectionOptions(comboBoxSelectionOptions)
+          }
+        />
+      </EuiFormRow>
+
+      <EuiFormRow label="Range" display="rowCompressed">
+        <EuiRange
+          min={0}
+          max={100}
+          name="range"
+          id="range"
+          showInput
+          compressed
+          value={value}
+          onChange={onRangeChange}
+        />
+      </EuiFormRow>
+
+      <EuiFormRow label="Switch" display="rowCompressed" hasChildLabel={false}>
+        <EuiSwitch
+          label="Should we do this?"
+          name="switch"
+          checked={isSwitchChecked}
+          onChange={onSwitchChange}
+          compressed
+        />
+      </EuiFormRow>
+
+      <EuiSpacer size="m" />
+
+      <EuiCheckboxGroup
+        options={checkboxes}
+        idToSelectedMap={checkboxIdToSelectedMap}
+        onChange={onCheckboxChange}
+        legend={{
+          children: 'Checkboxes',
+        }}
+        compressed
+      />
+    </EuiPanel>
+  );
+};
