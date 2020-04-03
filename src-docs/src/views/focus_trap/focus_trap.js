2341cde2f8d001e25df05d155c220cebd8e54a39
Ashik Meerankutty <ashik9591@gmail.com>
2020-04-03 09:07:52 +0530

    converted focus trap example to functional component

--- src-docs/src/views/focus_trap/focus_trap.js
+++ src-docs/src/views/focus_trap/focus_trap.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import {
   EuiBadge,
@@ -11,51 +11,39 @@ import {
 
 import FormExample from '../form_compressed/form_compressed';
 
-export default class extends Component {
-  constructor(props) {
-    super(props);
+export default () => {
+  const [isDisabled, setIsDisabled] = useState(false);
 
-    this.state = {
-      isDisabled: false,
-    };
-  }
-
-  toggleDisabled = () => {
-    this.setState(prevState => ({
-      isDisabled: !prevState.isDisabled,
-    }));
+  const toggleDisabled = () => {
+    setIsDisabled(!isDisabled);
   };
 
-  render() {
-    const { isDisabled } = this.state;
+  return (
+    <div>
+      <EuiBadge>Trap is {isDisabled ? 'disabled' : 'enabled'}</EuiBadge>
+      <EuiSpacer size="s" />
+      <EuiFocusTrap disabled={isDisabled}>
+        <EuiPanel>
+          <FormExample />
 
-    return (
-      <div>
-        <EuiBadge>Trap is {isDisabled ? 'disabled' : 'enabled'}</EuiBadge>
-        <EuiSpacer size="s" />
-        <EuiFocusTrap disabled={isDisabled}>
-          <EuiPanel>
-            <FormExample />
+          <EuiSpacer size="m" />
 
-            <EuiSpacer size="m" />
+          <EuiButton onClick={toggleDisabled}>
+            {`${!isDisabled ? 'Disable' : 'Enable'} Focus Trap`}
+          </EuiButton>
+        </EuiPanel>
+      </EuiFocusTrap>
 
-            <EuiButton onClick={this.toggleDisabled}>
-              {`${!isDisabled ? 'Disable' : 'Enable'} Focus Trap`}
-            </EuiButton>
-          </EuiPanel>
-        </EuiFocusTrap>
+      <EuiSpacer size="l" />
 
-        <EuiSpacer size="l" />
+      <EuiText>
+        The button below is not focusable by keyboard as long as the focus trap
+        is enabled.
+      </EuiText>
 
-        <EuiText>
-          The button below is not focusable by keyboard as long as the focus
-          trap is enabled.
-        </EuiText>
-
-        <EuiButton onClick={() => alert('External event triggered')}>
-          External Focusable Element
-        </EuiButton>
-      </div>
-    );
-  }
-}
+      <EuiButton onClick={() => alert('External event triggered')}>
+        External Focusable Element
+      </EuiButton>
+    </div>
+  );
+};
