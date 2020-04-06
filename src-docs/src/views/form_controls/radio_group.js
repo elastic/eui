46ecf353fce2e54c69bf5145977c66b74cae8680
walter-ind <52423075+walter-ind@users.noreply.github.com>
2020-04-06 20:20:15 +0530

    Update radio_group.js

--- src-docs/src/views/form_controls/radio_group.js
+++ src-docs/src/views/form_controls/radio_group.js
@@ -4,38 +4,28 @@ import { EuiRadioGroup } from '../../../../src/components';
 
 import { htmlIdGenerator } from '../../../../src/services';
 import { DisplayToggles } from './display_toggles';
+const idPrefix = htmlIdGenerator()();
+export default () => {
+  const radios = [
+    {
+      id: `${idPrefix}0`,
+      label: 'Option one',
+    },
+    {
+      id: `${idPrefix}1`,
+      label: 'Option two is checked by default',
+    },
+    {
+      id: `${idPrefix}2`,
+      label: 'Option three is disabled',
+      disabled: true,
+    },
+  ];
 
-export default class extends Component {
-  constructor(props) {
-    super(props);
+  const [radioIdSelected, setRadioIdSelected] = useState(`${idPrefix}1`);
 
-    const idPrefix = htmlIdGenerator()();
-
-    this.radios = [
-      {
-        id: `${idPrefix}0`,
-        label: 'Option one',
-      },
-      {
-        id: `${idPrefix}1`,
-        label: 'Option two is checked by default',
-      },
-      {
-        id: `${idPrefix}2`,
-        label: 'Option three is disabled',
-        disabled: true,
-      },
-    ];
-
-    this.state = {
-      radioIdSelected: `${idPrefix}1`,
-    };
-  }
-
-  onChange = optionId => {
-    this.setState({
-      radioIdSelected: optionId,
-    });
+  const onChange = optionId => {
+    setRadioIdSelected(optionId);
   };
 
   return (
