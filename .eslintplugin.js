5d649e58ebd949558f6f989e5fd417d8d52c17ea
Chandler Prall <chandler.prall@gmail.com>
2020-04-06 12:45:29 -0600

    add license headers rule, start eslint configuration

--- .eslintplugin.js
+++ .eslintplugin.js
@@ -1,4 +1,5 @@
 exports.rules = {
   i18n: require('./scripts/eslint-plugin-i18n/i18n'),
   'href-with-rel': require('./scripts/eslint-plugin-rel/rel')
+  'require-license-header': require('./scripts/eslint-plugin/require_license_header'),
 };
