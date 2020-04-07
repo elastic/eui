fc26869d305c99ad3b334f3c2cb4b6fb5a7cb134
Michail Yasonik <michail.yasonik@elastic.co>
2020-04-06 21:36:38 -0500

    removing selectable from a11y-testing

--- scripts/a11y-testing.js
+++ scripts/a11y-testing.js
@@ -56,7 +56,6 @@ const docsPages = async (root, page) => {
     `${root}#/forms/filter-group`,
     `${root}#/forms/range-sliders`,
     `${root}#/forms/search-bar`,
-    // `${root}#/forms/selectable`,
     `${root}#/elastic-charts/sizing`,
     `${root}#/elastic-charts/time-series`,
     `${root}#/elastic-charts/categorical`,
@@ -134,8 +133,7 @@ const printResult = result =>
           { id: 'color-contrast', enabled: false },
           {
             id: 'scrollable-region-focusable',
-            matches:
-              '[role="grid"], [data-skip-axe="scrollable-region-focusable"]',
+            matches: '[role="grid"]',
           },
         ],
       })
