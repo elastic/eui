becd65da65d7bfd36525dc4dbbf95fe5a8558afc
walter-ind <52423075+walter-ind@users.noreply.github.com>
2020-04-08 07:11:57 +0530

    Update health_example.js

--- src-docs/src/views/health/health_example.js
+++ src-docs/src/views/health/health_example.js
@@ -29,8 +29,8 @@ export const HealthExample = {
           The <strong>EuiHealth</strong> component should be used when showing
           comparitive health of listed objects (like servers, HTTP response
           status codes(as per convenience), nodes, indexes..etc). Because icons
-          are vague and bulky and color alone does not work, color plus
-          text provides a recognizable, lightweight combo that works in most
+          are vague and bulky and color alone does not work, color plus text
+          provides a recognizable, lightweight combo that works in most
           situations.
         </p>
       ),
