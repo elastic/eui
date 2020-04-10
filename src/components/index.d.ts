ca59dd35ad6310f2a570a65d74e01058afe4b812
Chandler Prall <chandler.prall@gmail.com>
2020-04-10 14:15:44 -0600

    export ReactDatePicker and ReactDatePickerProps from @elastic/eui

--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -1,4 +1,6 @@
 declare module '@elastic/eui' {
   // @ts-ignore
   export * from '@elastic/eui/src/components/common'; // eslint-disable-line import/no-unresolved
+  // @ts-ignore
+  export * from '@elastic/eui/src/components/date_picker/react-datepicker'; // eslint-disable-line import/no-unresolved
 }
