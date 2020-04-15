a939869d6fb6ae68992ad23c2cf894f7c22e5dbf
Ashik Meerankutty <ashik9591@gmail.com>
2020-04-15 21:35:00 +0530

    [Docs] Converted EuiBasicTable, EuiInMemoryTable examples to functional components (#3320)
    * converted table actions examples to functional
    
    * Converted table auto mode example to functional component
    
    * Converted expanding rows expample to Functional component
    
    * Converted footer example to functional component
    
    * Converted in memort table examples to functional component
    
    * Removed console.log
    
    * Converted mobile, paginated, selection, sorting examples to functional components

--- src-docs/src/views/tables/actions/actions.js
+++ src-docs/src/views/tables/actions/actions.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import { formatDate } from '../../../../../src/services/format';
 import { createDataStore } from '../data_store';
 
@@ -37,322 +37,296 @@ Example country object:
 
 const store = createDataStore();
 
-export class Table extends Component {
-  constructor(props) {
-    super(props);
+export const Table = () => {
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(5);
+  const [sortField, setSortField] = useState('firstName');
+  const [sortDirection, setSortDirection] = useState('asc');
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [multiAction, setMultiAction] = useState(false);
+  const [customAction, setCustomAction] = useState(false);
 
-    this.state = {
-      pageIndex: 0,
-      pageSize: 5,
-      sortField: 'firstName',
-      sortDirection: 'asc',
-      selectedItems: [],
-      multiAction: false,
-      customAction: false,
-    };
-  }
-
-  onTableChange = ({ page = {}, sort = {} }) => {
+  const onTableChange = ({ page = {}, sort = {} }) => {
     const { index: pageIndex, size: pageSize } = page;
 
     const { field: sortField, direction: sortDirection } = sort;
 
-    this.setState({
-      pageIndex,
-      pageSize,
-      sortField,
-      sortDirection,
-    });
+    setPageIndex(pageIndex);
+    setPageSize(pageSize);
+    setSortField(sortField);
+    setSortDirection(sortDirection);
   };
 
-  onClickDelete = () => {
-    const { selectedItems } = this.state;
+  const onClickDelete = () => {
     store.deleteUsers(...selectedItems.map(user => user.id));
 
-    this.setState({
-      selectedItems: [],
-    });
+    setSelectedItems([]);
   };
 
-  onSelectionChange = selectedItems => {
-    this.setState({ selectedItems });
+  const onSelectionChange = selectedItems => {
+    setSelectedItems(selectedItems);
   };
 
-  renderDeleteButton() {
-    const { selectedItems } = this.state;
-
+  const renderDeleteButton = () => {
     if (selectedItems.length === 0) {
       return;
     }
 
     return (
       <EuiFlexItem grow={false}>
-        <EuiButton color="danger" iconType="trash" onClick={this.onClickDelete}>
+        <EuiButton color="danger" iconType="trash" onClick={onClickDelete}>
           Delete {selectedItems.length} Users
         </EuiButton>
       </EuiFlexItem>
     );
-  }
-
-  toggleMultiAction = () => {
-    this.setState(prevState => ({ multiAction: !prevState.multiAction }));
   };
 
-  toggleCustomAction = () => {
-    this.setState(prevState => ({ customAction: !prevState.customAction }));
+  const toggleMultiAction = () => {
+    setMultiAction(!multiAction);
   };
 
-  deleteUser = user => {
+  const toggleCustomAction = () => {
+    setCustomAction(!customAction);
+  };
+
+  const deleteUser = user => {
     store.deleteUsers(user.id);
-    this.setState({ selectedItems: [] });
+    setSelectedItems([]);
   };
 
-  cloneUser = user => {
+  const cloneUser = user => {
     store.cloneUser(user.id);
-    this.setState({ selectedItems: [] });
+    setSelectedItems([]);
   };
 
-  render() {
-    const {
-      pageIndex,
-      pageSize,
-      sortField,
-      sortDirection,
-      multiAction,
-      customAction,
-    } = this.state;
+  const { pageOfItems, totalItemCount } = store.findUsers(
+    pageIndex,
+    pageSize,
+    sortField,
+    sortDirection
+  );
 
-    const { pageOfItems, totalItemCount } = store.findUsers(
-      pageIndex,
-      pageSize,
-      sortField,
-      sortDirection
-    );
+  const deleteButton = renderDeleteButton();
 
-    const deleteButton = this.renderDeleteButton();
+  let actions = null;
 
-    let actions = null;
-
-    if (multiAction) {
-      actions = customAction
-        ? [
-            {
-              render: item => {
-                return (
-                  <EuiLink
-                    color="secondary"
-                    onClick={() => this.cloneUser(item)}>
-                    Clone
-                  </EuiLink>
-                );
-              },
+  if (multiAction) {
+    actions = customAction
+      ? [
+          {
+            render: item => {
+              return (
+                <EuiLink color="secondary" onClick={() => cloneUser(item)}>
+                  Clone
+                </EuiLink>
+              );
             },
-            {
-              render: item => {
-                return (
-                  <EuiLink color="danger" onClick={() => this.deleteUser(item)}>
-                    Delete
-                  </EuiLink>
-                );
-              },
+          },
+          {
+            render: item => {
+              return (
+                <EuiLink color="danger" onClick={() => deleteUser(item)}>
+                  Delete
+                </EuiLink>
+              );
             },
-          ]
-        : [
-            {
-              name: 'Clone',
-              description: 'Clone this user',
-              icon: 'copy',
-              onClick: this.cloneUser,
-              'data-test-subj': 'action-clone',
+          },
+        ]
+      : [
+          {
+            name: 'Clone',
+            description: 'Clone this user',
+            icon: 'copy',
+            onClick: cloneUser,
+            'data-test-subj': 'action-clone',
+          },
+          {
+            name: 'Delete',
+            description: 'Delete this user',
+            icon: 'trash',
+            color: 'danger',
+            type: 'icon',
+            onClick: deleteUser,
+            isPrimary: true,
+            'data-test-subj': 'action-delete',
+          },
+          {
+            name: 'Edit',
+            isPrimary: true,
+            available: ({ online }) => !online,
+            description: 'Edit this user',
+            icon: 'pencil',
+            type: 'icon',
+            onClick: () => {},
+            'data-test-subj': 'action-edit',
+          },
+          {
+            name: 'Share',
+            isPrimary: true,
+            description: 'Share this user',
+            icon: 'share',
+            type: 'icon',
+            onClick: () => {},
+            'data-test-subj': 'action-share',
+          },
+          {
+            name: 'Elastic.co',
+            description: 'Go to elastic.co',
+            icon: 'logoElastic',
+            type: 'icon',
+            href: 'https://elastic.co',
+            target: '_blank',
+            'data-test-subj': 'action-outboundlink',
+          },
+        ];
+  } else {
+    actions = customAction
+      ? [
+          {
+            render: item => {
+              return (
+                <EuiLink onClick={() => deleteUser(item)} color="danger">
+                  Delete
+                </EuiLink>
+              );
             },
-            {
-              name: 'Delete',
-              description: 'Delete this user',
-              icon: 'trash',
-              color: 'danger',
-              type: 'icon',
-              onClick: this.deleteUser,
-              isPrimary: true,
-              'data-test-subj': 'action-delete',
-            },
-            {
-              name: 'Edit',
-              isPrimary: true,
-              available: ({ online }) => !online,
-              description: 'Edit this user',
-              icon: 'pencil',
-              type: 'icon',
-              onClick: () => {},
-              'data-test-subj': 'action-edit',
-            },
-            {
-              name: 'Share',
-              isPrimary: true,
-              description: 'Share this user',
-              icon: 'share',
-              type: 'icon',
-              onClick: () => {},
-              'data-test-subj': 'action-share',
-            },
-            {
-              name: 'Elastic.co',
-              description: 'Go to elastic.co',
-              icon: 'logoElastic',
-              type: 'icon',
-              href: 'https://elastic.co',
-              target: '_blank',
-              'data-test-subj': 'action-outboundlink',
-            },
-          ];
-    } else {
-      actions = customAction
-        ? [
-            {
-              render: item => {
-                return (
-                  <EuiLink onClick={() => this.deleteUser(item)} color="danger">
-                    Delete
-                  </EuiLink>
-                );
-              },
-            },
-          ]
-        : [
-            {
-              name: 'Elastic.co',
-              description: 'Go to elastic.co',
-              icon: 'editorLink',
-              color: 'primary',
-              type: 'icon',
-              href: 'https://elastic.co',
-              target: '_blank',
-              'data-test-subj': 'action-outboundlink',
-            },
-          ];
-    }
-
-    const columns = [
-      {
-        field: 'firstName',
-        name: 'First Name',
-        truncateText: true,
-        sortable: true,
-        mobileOptions: {
-          render: item => (
-            <span>
-              {item.firstName} {item.lastName}
-            </span>
-          ),
-          header: false,
-          truncateText: false,
-          enlarge: true,
-          fullWidth: true,
-        },
-      },
-      {
-        field: 'lastName',
-        name: 'Last Name',
-        truncateText: true,
-        mobileOptions: {
-          show: false,
-        },
-      },
-      {
-        field: 'github',
-        name: 'Github',
-        render: username => (
-          <EuiLink href={`https://github.com/${username}`} target="_blank">
-            {username}
-          </EuiLink>
-        ),
-      },
-      {
-        field: 'dateOfBirth',
-        name: 'Date of Birth',
-        dataType: 'date',
-        render: date => formatDate(date, 'dobLong'),
-        sortable: true,
-      },
-      {
-        field: 'nationality',
-        name: 'Nationality',
-        render: countryCode => {
-          const country = store.getCountry(countryCode);
-          return `${country.flag} ${country.name}`;
-        },
-      },
-      {
-        field: 'online',
-        name: 'Online',
-        dataType: 'boolean',
-        render: online => {
-          const color = online ? 'success' : 'danger';
-          const label = online ? 'Online' : 'Offline';
-          return <EuiHealth color={color}>{label}</EuiHealth>;
-        },
-        sortable: true,
-      },
-      {
-        name: 'Actions',
-        actions,
-      },
-    ];
-
-    const pagination = {
-      pageIndex: pageIndex,
-      pageSize: pageSize,
-      totalItemCount: totalItemCount,
-      pageSizeOptions: [3, 5, 8],
-    };
-
-    const sorting = {
-      sort: {
-        field: sortField,
-        direction: sortDirection,
-      },
-    };
-
-    const selection = {
-      selectable: user => user.online,
-      selectableMessage: selectable =>
-        !selectable ? 'User is currently offline' : undefined,
-      onSelectionChange: this.onSelectionChange,
-    };
-
-    return (
-      <Fragment>
-        <EuiFlexGroup alignItems="center">
-          <EuiFlexItem grow={false}>
-            <EuiSwitch
-              label="Multiple Actions"
-              checked={this.state.multiAction}
-              onChange={this.toggleMultiAction}
-            />
-          </EuiFlexItem>
-          <EuiFlexItem grow={false}>
-            <EuiSwitch
-              label="Custom Actions"
-              checked={this.state.customAction}
-              onChange={this.toggleCustomAction}
-            />
-          </EuiFlexItem>
-          <EuiFlexItem />
-          {deleteButton}
-        </EuiFlexGroup>
-
-        <EuiSpacer size="l" />
-
-        <EuiBasicTable
-          items={pageOfItems}
-          itemId="id"
-          columns={columns}
-          pagination={pagination}
-          sorting={sorting}
-          selection={selection}
-          hasActions={customAction ? false : true}
-          onChange={this.onTableChange}
-        />
-      </Fragment>
-    );
+          },
+        ]
+      : [
+          {
+            name: 'Elastic.co',
+            description: 'Go to elastic.co',
+            icon: 'editorLink',
+            color: 'primary',
+            type: 'icon',
+            href: 'https://elastic.co',
+            target: '_blank',
+            'data-test-subj': 'action-outboundlink',
+          },
+        ];
   }
-}
+
+  const columns = [
+    {
+      field: 'firstName',
+      name: 'First Name',
+      truncateText: true,
+      sortable: true,
+      mobileOptions: {
+        render: item => (
+          <span>
+            {item.firstName} {item.lastName}
+          </span>
+        ),
+        header: false,
+        truncateText: false,
+        enlarge: true,
+        fullWidth: true,
+      },
+    },
+    {
+      field: 'lastName',
+      name: 'Last Name',
+      truncateText: true,
+      mobileOptions: {
+        show: false,
+      },
+    },
+    {
+      field: 'github',
+      name: 'Github',
+      render: username => (
+        <EuiLink href={`https://github.com/${username}`} target="_blank">
+          {username}
+        </EuiLink>
+      ),
+    },
+    {
+      field: 'dateOfBirth',
+      name: 'Date of Birth',
+      dataType: 'date',
+      render: date => formatDate(date, 'dobLong'),
+      sortable: true,
+    },
+    {
+      field: 'nationality',
+      name: 'Nationality',
+      render: countryCode => {
+        const country = store.getCountry(countryCode);
+        return `${country.flag} ${country.name}`;
+      },
+    },
+    {
+      field: 'online',
+      name: 'Online',
+      dataType: 'boolean',
+      render: online => {
+        const color = online ? 'success' : 'danger';
+        const label = online ? 'Online' : 'Offline';
+        return <EuiHealth color={color}>{label}</EuiHealth>;
+      },
+      sortable: true,
+    },
+    {
+      name: 'Actions',
+      actions,
+    },
+  ];
+
+  const pagination = {
+    pageIndex: pageIndex,
+    pageSize: pageSize,
+    totalItemCount: totalItemCount,
+    pageSizeOptions: [3, 5, 8],
+  };
+
+  const sorting = {
+    sort: {
+      field: sortField,
+      direction: sortDirection,
+    },
+  };
+
+  const selection = {
+    selectable: user => user.online,
+    selectableMessage: selectable =>
+      !selectable ? 'User is currently offline' : undefined,
+    onSelectionChange: onSelectionChange,
+  };
+
+  return (
+    <Fragment>
+      <EuiFlexGroup alignItems="center">
+        <EuiFlexItem grow={false}>
+          <EuiSwitch
+            label="Multiple Actions"
+            checked={multiAction}
+            onChange={toggleMultiAction}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiSwitch
+            label="Custom Actions"
+            checked={customAction}
+            onChange={toggleCustomAction}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem />
+        {deleteButton}
+      </EuiFlexGroup>
+
+      <EuiSpacer size="l" />
+
+      <EuiBasicTable
+        items={pageOfItems}
+        itemId="id"
+        columns={columns}
+        pagination={pagination}
+        sorting={sorting}
+        selection={selection}
+        hasActions={customAction ? false : true}
+        onChange={onTableChange}
+      />
+    </Fragment>
+  );
+};
--- src-docs/src/views/tables/sorting/sorting.js
+++ src-docs/src/views/tables/sorting/sorting.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { formatDate } from '../../../../../src/services/format';
 import { createDataStore } from '../data_store';
 
@@ -34,179 +34,167 @@ Example country object:
 
 const store = createDataStore();
 
-export class Table extends Component {
-  constructor(props) {
-    super(props);
+export const Table = () => {
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(5);
+  const [sortField, setSortField] = useState('firstName');
+  const [sortDirection, setSortDirection] = useState('asc');
 
-    this.state = {
-      pageIndex: 0,
-      pageSize: 5,
-      sortField: 'firstName',
-      sortDirection: 'asc',
-    };
-  }
-
-  onTableChange = ({ page = {}, sort = {} }) => {
+  const onTableChange = ({ page = {}, sort = {} }) => {
     const { index: pageIndex, size: pageSize } = page;
 
     const { field: sortField, direction: sortDirection } = sort;
 
-    this.setState({
-      pageIndex,
-      pageSize,
-      sortField,
-      sortDirection,
-    });
+    setPageIndex(pageIndex);
+    setPageSize(pageSize);
+    setSortField(sortField);
+    setSortDirection(sortDirection);
   };
 
-  render() {
-    const { pageIndex, pageSize, sortField, sortDirection } = this.state;
+  const { pageOfItems, totalItemCount } = store.findUsers(
+    pageIndex,
+    pageSize,
+    sortField,
+    sortDirection
+  );
 
-    const { pageOfItems, totalItemCount } = store.findUsers(
-      pageIndex,
-      pageSize,
-      sortField,
-      sortDirection
-    );
+  const columns = [
+    {
+      field: 'firstName',
+      name: 'First Name',
+      sortable: true,
+      truncateText: true,
+      mobileOptions: {
+        render: item => (
+          <span>
+            {item.firstName} {item.lastName}
+          </span>
+        ),
+        header: false,
+        truncateText: false,
+        enlarge: true,
+        fullWidth: true,
+      },
+    },
+    {
+      field: 'lastName',
+      name: 'Last Name',
+      sortable: true,
+      truncateText: true,
+      mobileOptions: {
+        show: false,
+      },
+    },
+    {
+      field: 'github',
+      name: (
+        <EuiToolTip content="Their mascot is the Octokitty">
+          <span>
+            Github{' '}
+            <EuiIcon
+              size="s"
+              color="subdued"
+              type="questionInCircle"
+              className="eui-alignTop"
+            />
+          </span>
+        </EuiToolTip>
+      ),
+      sortable: true,
+      render: username => (
+        <EuiLink href={`https://github.com/${username}`} target="_blank">
+          {username}
+        </EuiLink>
+      ),
+    },
+    {
+      field: 'dateOfBirth',
+      name: (
+        <EuiToolTip content="Colloquially known as a 'birthday'">
+          <span>
+            Date of Birth{' '}
+            <EuiIcon
+              size="s"
+              color="subdued"
+              type="questionInCircle"
+              className="eui-alignTop"
+            />
+          </span>
+        </EuiToolTip>
+      ),
+      schema: 'date',
+      render: date => formatDate(date, 'dobLong'),
+      sortable: true,
+    },
+    {
+      field: 'nationality',
+      name: (
+        <EuiToolTip content="The nation in which this person resides">
+          <span>
+            Nationality{' '}
+            <EuiIcon
+              size="s"
+              color="subdued"
+              type="questionInCircle"
+              className="eui-alignTop"
+            />
+          </span>
+        </EuiToolTip>
+      ),
+      sortable: true,
+      render: countryCode => {
+        const country = store.getCountry(countryCode);
+        return `${country.flag} ${country.name}`;
+      },
+    },
+    {
+      field: 'online',
+      name: (
+        <EuiToolTip content="Free to talk or busy with business">
+          <span>
+            Online{' '}
+            <EuiIcon
+              size="s"
+              color="subdued"
+              type="questionInCircle"
+              className="eui-alignTop"
+            />
+          </span>
+        </EuiToolTip>
+      ),
+      schema: 'boolean',
+      sortable: true,
+      render: online => {
+        const color = online ? 'success' : 'danger';
+        const label = online ? 'Online' : 'Offline';
+        return <EuiHealth color={color}>{label}</EuiHealth>;
+      },
+    },
+  ];
 
-    const columns = [
-      {
-        field: 'firstName',
-        name: 'First Name',
-        sortable: true,
-        truncateText: true,
-        mobileOptions: {
-          render: item => (
-            <span>
-              {item.firstName} {item.lastName}
-            </span>
-          ),
-          header: false,
-          truncateText: false,
-          enlarge: true,
-          fullWidth: true,
-        },
-      },
-      {
-        field: 'lastName',
-        name: 'Last Name',
-        sortable: true,
-        truncateText: true,
-        mobileOptions: {
-          show: false,
-        },
-      },
-      {
-        field: 'github',
-        name: (
-          <EuiToolTip content="Their mascot is the Octokitty">
-            <span>
-              Github{' '}
-              <EuiIcon
-                size="s"
-                color="subdued"
-                type="questionInCircle"
-                className="eui-alignTop"
-              />
-            </span>
-          </EuiToolTip>
-        ),
-        sortable: true,
-        render: username => (
-          <EuiLink href={`https://github.com/${username}`} target="_blank">
-            {username}
-          </EuiLink>
-        ),
-      },
-      {
-        field: 'dateOfBirth',
-        name: (
-          <EuiToolTip content="Colloquially known as a 'birthday'">
-            <span>
-              Date of Birth{' '}
-              <EuiIcon
-                size="s"
-                color="subdued"
-                type="questionInCircle"
-                className="eui-alignTop"
-              />
-            </span>
-          </EuiToolTip>
-        ),
-        schema: 'date',
-        render: date => formatDate(date, 'dobLong'),
-        sortable: true,
-      },
-      {
-        field: 'nationality',
-        name: (
-          <EuiToolTip content="The nation in which this person resides">
-            <span>
-              Nationality{' '}
-              <EuiIcon
-                size="s"
-                color="subdued"
-                type="questionInCircle"
-                className="eui-alignTop"
-              />
-            </span>
-          </EuiToolTip>
-        ),
-        sortable: true,
-        render: countryCode => {
-          const country = store.getCountry(countryCode);
-          return `${country.flag} ${country.name}`;
-        },
-      },
-      {
-        field: 'online',
-        name: (
-          <EuiToolTip content="Free to talk or busy with business">
-            <span>
-              Online{' '}
-              <EuiIcon
-                size="s"
-                color="subdued"
-                type="questionInCircle"
-                className="eui-alignTop"
-              />
-            </span>
-          </EuiToolTip>
-        ),
-        schema: 'boolean',
-        sortable: true,
-        render: online => {
-          const color = online ? 'success' : 'danger';
-          const label = online ? 'Online' : 'Offline';
-          return <EuiHealth color={color}>{label}</EuiHealth>;
-        },
-      },
-    ];
+  const pagination = {
+    pageIndex: pageIndex,
+    pageSize: pageSize,
+    totalItemCount: totalItemCount,
+    pageSizeOptions: [3, 5, 8],
+  };
 
-    const pagination = {
-      pageIndex: pageIndex,
-      pageSize: pageSize,
-      totalItemCount: totalItemCount,
-      pageSizeOptions: [3, 5, 8],
-    };
+  const sorting = {
+    sort: {
+      field: sortField,
+      direction: sortDirection,
+    },
+  };
 
-    const sorting = {
-      sort: {
-        field: sortField,
-        direction: sortDirection,
-      },
-    };
-
-    return (
-      <div>
-        <EuiBasicTable
-          items={pageOfItems}
-          columns={columns}
-          pagination={pagination}
-          sorting={sorting}
-          onChange={this.onTableChange}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <EuiBasicTable
+        items={pageOfItems}
+        columns={columns}
+        pagination={pagination}
+        sorting={sorting}
+        onChange={onTableChange}
+      />
+    </div>
+  );
+};
