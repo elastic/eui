77924400a838cda37445fbb5a3e74fd539c824fe
Ashik Meerankutty <ashik9591@gmail.com>
2020-05-04 22:28:09 +0530

    Fixed if condition does not catch for a search prop

--- src/components/basic_table/in_memory_table.tsx
+++ src/components/basic_table/in_memory_table.tsx
@@ -269,23 +269,23 @@ export class EuiInMemoryTable<T> extends Component<
       };
     }
 
-    if (
-      (nextProps.search as EuiSearchBarProps) &&
-      (prevState.prevProps.search as EuiSearchBarProps)
-    ) {
-      const nextQuery = (nextProps.search as EuiSearchBarProps).query;
-      const prevQuery = (prevState.prevProps.search as EuiSearchBarProps).query;
+    const nextQuery = nextProps.search
+      ? (nextProps.search as EuiSearchBarProps).query
+      : '';
+    const prevQuery = prevState.prevProps.search
+      ? (prevState.prevProps.search as EuiSearchBarProps).query
+      : '';
 
-      if (nextQuery !== prevQuery) {
-        return {
-          prevProps: {
-            ...prevState.prevProps,
-            search: nextProps.search,
-          },
-          query: getQueryFromSearch(nextProps.search, false),
-        };
-      }
+    if (nextQuery !== prevQuery) {
+      return {
+        prevProps: {
+          ...prevState.prevProps,
+          search: nextProps.search,
+        },
+        query: getQueryFromSearch(nextProps.search, false),
+      };
     }
+
     return null;
   }
 
