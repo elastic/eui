a8b6d72ebee88f1db6c5a2d2b33b044b04e8de13
Justin Kambic <justin.kambic@elastic.co>
2020-05-04 22:00:48 -0400

    Simplify docs instructions for basic `EuiContextMenu` example. (#3413)

--- src-docs/src/views/context_menu/context_menu.js
+++ src-docs/src/views/context_menu/context_menu.js
@@ -10,21 +10,6 @@ import {
   EuiSpacer,
 } from '../../../../src/components';
 
-function flattenPanelTree(tree, array = []) {
-  array.push(tree);
-
-  if (tree.items) {
-    tree.items.forEach(item => {
-      if (item.panel) {
-        flattenPanelTree(item.panel, array);
-        item.panel = item.panel.id;
-      }
-    });
-  }
-
-  return array;
-}
-
 export default () => {
   const [isPopoverOpen, setPopover] = useState(false);
 
@@ -36,112 +21,113 @@ export default () => {
     setPopover(false);
   };
 
-  const panelTree = {
-    id: 0,
-    title: 'This is a context menu',
-    items: [
-      {
-        name: 'Handle an onClick',
-        icon: <EuiIcon type="search" size="m" />,
-        onClick: () => {
-          closePopover();
-          window.alert('Show fullscreen');
+  const panels = [
+    {
+      id: 0,
+      title: 'This is a context menu',
+      items: [
+        {
+          name: 'Handle an onClick',
+          icon: <EuiIcon type="search" size="m" />,
+          onClick: () => {
+            closePopover();
+            window.alert('Show fullscreen');
+          },
         },
-      },
-      {
-        name: 'Go to a link',
-        icon: 'user',
-        href: 'http://elastic.co',
-        target: '_blank',
-      },
-      {
-        name: 'Nest panels',
-        icon: 'user',
-        panel: {
-          id: 1,
-          title: 'Nest panels',
-          items: [
-            {
-              name: 'PDF reports',
-              icon: 'user',
-              onClick: () => {
-                closePopover();
-                window.alert('PDF reports');
-              },
-            },
-            {
-              name: 'Embed code',
-              icon: 'user',
-              panel: {
-                id: 2,
-                title: 'Embed code',
-                content: (
-                  <div style={{ padding: 16 }}>
-                    <EuiFormRow
-                      label="Generate a public snapshot?"
-                      hasChildLabel={false}>
-                      <EuiSwitch
-                        name="switch"
-                        id="asdf"
-                        label="Snapshot data"
-                        checked={true}
-                        onChange={() => {}}
-                      />
-                    </EuiFormRow>
-                    <EuiFormRow
-                      label="Include the following in the embed"
-                      hasChildLabel={false}>
-                      <EuiSwitch
-                        name="switch"
-                        id="asdf2"
-                        label="Current time range"
-                        checked={true}
-                        onChange={() => {}}
-                      />
-                    </EuiFormRow>
-                    <EuiSpacer />
-                    <EuiButton fill>Copy iFrame code</EuiButton>
-                  </div>
-                ),
-              },
-            },
-            {
-              name: 'Permalinks',
-              icon: 'user',
-              onClick: () => {
-                closePopover();
-                window.alert('Permalinks');
-              },
-            },
-          ],
+        {
+          name: 'Go to a link',
+          icon: 'user',
+          href: 'http://elastic.co',
+          target: '_blank',
         },
-      },
-      {
-        name: 'You can add a tooltip',
-        icon: 'user',
-        toolTipTitle: 'Optional tooltip',
-        toolTipContent: 'Optional content for a tooltip',
-        toolTipPosition: 'right',
-        onClick: () => {
-          closePopover();
-          window.alert('Display options');
+        {
+          name: 'Nest panels',
+          icon: 'user',
+          panel: 1,
         },
-      },
-      {
-        name: 'Disabled option',
-        icon: 'user',
-        toolTipContent: 'For reasons, this item is disabled',
-        toolTipPosition: 'right',
-        disabled: true,
-        onClick: () => {
-          closePopover();
-          window.alert('Disabled option');
+        {
+          name: 'You can add a tooltip',
+          icon: 'user',
+          toolTipTitle: 'Optional tooltip',
+          toolTipContent: 'Optional content for a tooltip',
+          toolTipPosition: 'right',
+          onClick: () => {
+            closePopover();
+            window.alert('Display options');
+          },
         },
-      },
-    ],
-  };
-
-  const panels = flattenPanelTree(panelTree);
+        {
+          name: 'Disabled option',
+          icon: 'user',
+          toolTipContent: 'For reasons, this item is disabled',
+          toolTipPosition: 'right',
+          disabled: true,
+          onClick: () => {
+            closePopover();
+            window.alert('Disabled option');
+          },
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: 'Nest panels',
+      items: [
+        {
+          name: 'PDF reports',
+          icon: 'user',
+          onClick: () => {
+            closePopover();
+            window.alert('PDF reports');
+          },
+        },
+        {
+          name: 'Embed code',
+          icon: 'user',
+          panel: 2,
+        },
+        {
+          name: 'Permalinks',
+          icon: 'user',
+          onClick: () => {
+            closePopover();
+            window.alert('Permalinks');
+          },
+        },
+        ,
+      ],
+    },
+    {
+      id: 2,
+      title: 'Embed code',
+      content: (
+        <div style={{ padding: 16 }}>
+          <EuiFormRow label="Generate a public snapshot?" hasChildLabel={false}>
+            <EuiSwitch
+              name="switch"
+              id="asdf"
+              label="Snapshot data"
+              checked={true}
+              onChange={() => {}}
+            />
+          </EuiFormRow>
+          <EuiFormRow
+            label="Include the following in the embed"
+            hasChildLabel={false}>
+            <EuiSwitch
+              name="switch"
+              id="asdf2"
+              label="Current time range"
+              checked={true}
+              onChange={() => {}}
+            />
+          </EuiFormRow>
+          <EuiSpacer />
+          <EuiButton fill>Copy iFrame code</EuiButton>
+        </div>
+      ),
+    },
+  ];
 
   const button = (
     <EuiButton iconType="arrowDown" iconSide="right" onClick={onButtonClick}>
