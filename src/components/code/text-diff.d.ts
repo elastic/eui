cb7f3ef81f7b60502c31806431f300b94e6691a1
Anish Aggarwal <anish17122000@gmail.com>
2020-05-11 21:22:43 +0530

    populated text-diff module defination

--- src/components/code/text-diff.d.ts
+++ src/components/code/text-diff.d.ts
@@ -1,1 +1,13 @@
-declare module 'text-diff';
+declare module 'text-diff' {
+  interface ConstructorProps {
+    timeout: number;
+  }
+
+  type DiffElement = [-1 | 0 | 1, string];
+
+  class Diff {
+    constructor({ timeout }: ConstructorProps);
+    main: (initialText: string, currentText: string) => DiffElement[];
+  }
+  export = Diff;
+}
