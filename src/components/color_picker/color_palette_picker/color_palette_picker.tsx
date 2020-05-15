16aeb06dd6554391707987524cdf338aac9efbd9
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2020-05-15 12:34:11 +0100

    Update src/components/color_picker/color_palette_picker/color_palette_picker.tsx
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>

--- src/components/color_picker/color_palette_picker/color_palette_picker.tsx
+++ src/components/color_picker/color_palette_picker/color_palette_picker.tsx
@@ -42,7 +42,7 @@ export interface EuiColorPalettePickerPaletteProps {
    * Specify if the palette is
    * `fixed`: individual color blocks; or
    * `gradient`: each color fades into the next; or
-   * `text`: a text as an option (a title is required).
+   * `text`: a text only option (a title is required).
    */
   type: 'fixed' | 'gradient' | 'text';
   /**
