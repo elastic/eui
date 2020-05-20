fca53cbe8f739676cb7fc7a6e5ef69ef7756e03b
Anish Aggarwal <43617894+anishagg17@users.noreply.github.com>
2020-05-21 01:21:16 +0530

    Update src-docs/src/views/text_diff/text_diff_custom_components.js
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>

--- src-docs/src/views/text_diff/text_diff_custom_components.js
+++ src-docs/src/views/text_diff/text_diff_custom_components.js
@@ -18,8 +18,6 @@ export default () => {
     'One difference is that interfaces create a new name that is used everywhere. Type aliases don’t create a new name — for instance, error messages won’t use the alias name. In the code below, hovering over interfaced in an editor will show that it returns an Interface, but will show that aliased returns object literal type.Hope you will be happy every day';
 
   const [rendered, textDiffObject] = useEuiTextDiff({
-    fontSize: 'm',
-    paddingSize: 'm',
     initialText,
     currentText,
     DeletionComponent: () => null,
