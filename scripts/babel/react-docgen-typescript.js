0a3b3ff7b1a71012783a74381a5eba091d1e1ce5
Ashik Meerankutty <ashik9591@gmail.com>
2020-06-04 22:42:50 +0530

    Use ts.createProgram

--- scripts/babel/react-docgen-typescript.js
+++ scripts/babel/react-docgen-typescript.js
@@ -16,105 +16,74 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const _ = require('lodash');
 const propsParser = require('react-docgen-typescript');
+const template = require('@babel/template');
+const ts = require('typescript');
+const glob = require('glob');
+const util = require('util');
 
-module.exports = function({ types: t }) {
+const files = glob.sync('src/**/*.{ts,tsx}', { absolute: true });
+const program = ts.createProgram(files, {});
+
+module.exports = function() {
   return {
     visitor: {
-      Program: {
-        exit(path, state) {
-          injectReactDocInfo(path, state, this.file.code, t);
-        },
+      Program(path, state) {
+        const { filename } = state.file.opts;
+
+        // these files causing some issues needs to fix
+        if (filename.includes('index.ts')) return;
+        if (filename.includes('flex_item.tsx')) return;
+        if (filename.includes('flex_grid.tsx')) return;
+
+        // find if components extends types from other modules
+        const componentExtends = [];
+
+        let docgenResults = [];
+        try {
+          docgenResults = propsParser
+            .withDefaultConfig({
+              propFilter: prop => {
+                if (prop.parent) {
+                  if (
+                    prop.parent.name === 'DOMAttributes' &&
+                    !componentExtends.includes('DOMAttributes')
+                  ) {
+                    componentExtends.push('DOMAttributes');
+                  }
+                  return !prop.parent.fileName.includes('node_modules');
+                }
+                return true;
+              },
+              shouldExtractLiteralValuesFromEnum: true,
+              shouldRemoveUndefinedFromOptional: true,
+            })
+            .parseWithProgramProvider(filename, () => program);
+        } catch (e) {
+          console.log(e);
+        }
+        if (docgenResults.length === 0) return;
+        docgenResults.forEach(function(docgenResult) {
+          const exportName = docgenResult.displayName;
+
+          // Forwardref components extracted export name = 'ForwardRefExoticComponent'
+          if (exportName === 'ForwardRefExoticComponent') {
+            return;
+          }
+          if (!exportName) return;
+          docgenResult.extends = componentExtends;
+          path.node.body.push(
+            template.default.ast(`            
+            ${exportName}.__docgenInfo = ${util.inspect(docgenResult, {
+              showHidden: false,
+              depth: null,
+            })}
+          `)
+          );
+        });
       },
     },
   };
 };
-
-function injectReactDocInfo(path, state, code, t) {
-  const { filename } = state.file.opts;
-
-  if (filename.includes('index.ts')) return;
-
-  const program = path.scope.getProgramParent().path;
-  const whiteListedHtmlProps = ['className'];
-
-  const componentExtends = [];
-  let docgenResults = [];
-
-  try {
-    const data = propsParser.withCustomConfig('tsconfig.json', {
-      propFilter: prop => {
-        if (whiteListedHtmlProps.includes(prop.name)) {
-          return true;
-        }
-        if (prop.parent) {
-          if (
-            prop.parent.name === 'DOMAttributes' &&
-            !componentExtends.includes('DOMAttributes')
-          ) {
-            componentExtends.push('DOMAttributes');
-          }
-          return !prop.parent.fileName.includes('node_modules');
-        }
-        return true;
-      },
-      shouldExtractLiteralValuesFromEnum: true,
-      shouldRemoveUndefinedFromOptional: true,
-    }).parse;
-    docgenResults = data(filename);
-  } catch (e) {
-    console.log(e);
-  }
-  docgenResults.forEach(function(docgenResult) {
-    const exportName = docgenResult.displayName;
-    docgenResult.extends = componentExtends;
-    const docNode = buildObjectExpression(docgenResult, t);
-    const docgenInfo = t.expressionStatement(
-      t.assignmentExpression(
-        '=',
-        t.memberExpression(
-          t.identifier(exportName),
-          t.identifier('__docgenInfo')
-        ),
-        docNode
-      )
-    );
-    program.pushContainer('body', docgenInfo);
-  });
-}
-
-function buildObjectExpression(obj, t) {
-  if (_.isPlainObject(obj)) {
-    const children = [];
-    for (const key in obj) {
-      if (key === 'actualName') continue;
-      if (!obj.hasOwnProperty(key) || _.isUndefined(obj[key])) continue;
-      children.push(
-        t.objectProperty(
-          t.stringLiteral(key),
-          buildObjectExpression(obj[key], t)
-        )
-      );
-    }
-    return t.objectExpression(children);
-  } else if (_.isString(obj)) {
-    return t.stringLiteral(obj);
-  } else if (_.isBoolean(obj)) {
-    return t.booleanLiteral(obj);
-  } else if (_.isNumber(obj)) {
-    return t.numericLiteral(obj);
-  } else if (_.isArray(obj)) {
-    const children = [];
-    obj.forEach(function(val) {
-      children.push(buildObjectExpression(val, t));
-    });
-    // eslint-disable-next-line new-cap
-    return t.ArrayExpression(children);
-  } else if (_.isNull(obj)) {
-    return t.nullLiteral();
-  }
-}
