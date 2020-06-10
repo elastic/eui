db2c68b12dfedae832dac44f703caa7cf37fd7f2
Caroline Horn <549577+cchaos@users.noreply.github.com>
2020-06-10 18:12:58 -0400

    Quick change to docs text

--- src-docs/src/views/form_validation/form_validation_example.js
+++ src-docs/src/views/form_validation/form_validation_example.js
@@ -24,11 +24,11 @@ export const FormValidationExample = {
       text: (
         <p>
           Validation is achieved by applying <EuiCode>isInvalid</EuiCode> and
-          optionally error props onto the <strong>EuiForm</strong> or{' '}
-          <strong>EuiFormRow</strong> components. Errors are optional and are
-          passed as an array in case you need to list more than one. You can
-          also hide the callout by passing
-          <EuiCode>invalidCallout=&ldquo;none&ldquo;</EuiCode>
+          the optional <EuiCode>error<EuiCode> props onto the{' '}
+          <strong>EuiForm</strong> and <strong>EuiFormRow</strong> components. 
+          Errors are optional and are passed as an array in case you need to 
+          list more than one. You can also hide the callout by passing{' '}
+          <EuiCode language="ts">{'invalidCallout="none"'}</EuiCode>
         </p>
       ),
       source: [
