d03ff8c93a83a7051aeebdeb8f198def09d41e21
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2020-06-11 22:03:50 -0400

    enables new ban-tslint-comment eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -97,6 +97,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/ban-tslint-comment': 'error',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
