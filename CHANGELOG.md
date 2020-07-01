c19ca9f1c2e70e8f8b0295fb6eb416837646fb45
Caroline Horn <549577+cchaos@users.noreply.github.com>
2020-07-01 12:27:50 -0400

    (Trying to) Fix z-indexes with header and flyouts & adding more EuiHeader docs (#3655)
    * [EuiOverlayMask] Added `headerAdjacent` prop for altering z-index based on EuiHeader
    
    * [EuiFlyout] Adjusts mask for header and add `maskProps`
    
    - By default EuiFlyout’s mask will always display below the header, but this can be adjusted by with `maskProps`
    
    * Fix demo page setup script with folder location
    
    * [EuiCollapsibleNav] Applying `headerAdjacent="below”` to mask and adding `maskProps` to allow customization
    
    * [Docs] Fixing header examples (esp Alerts)
    
    * Tweak to Amsterdam EuiHeaderBreadcrumb sizing
    
    * Fix demo page setup script with folder location (pt 2)
    
    * [EuiBreadcrumbs] Fix for allowing `onClick`
    
    And font-weight fix for amsterdam header breadcrumbs
    
    * [SASS] Fix flyout vs collapsible nav clip-path
    
    * [EuiBreadcrumbs] Fix for allowing `onClick` (SNAPS)
    
    * [Docs] Elastic Header Pattern (as TSX)
    
    * Convert example to JS and optimize for codesandbox
    
    * [EuiBadge] Allow any valid chroma.js color string
    
    * [EuiHeaderLinks] Fix margins for any children
    
    * Cleanup
    
    * Update src-docs/src/views/header/header_example.js
    
    Co-authored-by: Greg Thompson <thompson.glowe@gmail.com>
    
    * Update src-docs/src/views/overlay_mask/overlay_mask_example.js
    
    Co-authored-by: Greg Thompson <thompson.glowe@gmail.com>
    
    * Change `headerAdjacent` to `headerZindexLocation`
    
    * cl
    
    * cl
    
    Co-authored-by: Greg Thompson <thompson.glowe@gmail.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,11 +5,19 @@
 - Updated props of `EuiCode` and `EuiCodeBlock` to reflect only functional props ([#3647](https://github.com/elastic/eui/pull/3647))
 - Updated `EuiResizableContainer` `onPanelWidthChange` callback method to include all panel widths ([#3630](https://github.com/elastic/eui/pull/3630))
 - Extended `Query` / `EuiSearchBar` to allow any character inside double-quoted phrases ([#3432](https://github.com/elastic/eui/pull/3432))
+- Added `headerZindexLocation` prop to `EuiOverlayMask` ([#3655](https://github.com/elastic/eui/pull/3655))
+- Added `maskProps` prop to `EuiFlyout` and `EuiCollapsibleNav` ([#3655](https://github.com/elastic/eui/pull/3655))
 
 **Bug fixes**
 
 - Fixed `EuiContextMenu` panel `onAnimationEnd` transition bug in Chrome ([#3656](https://github.com/elastic/eui/pull/3656))
 - Fixed `EuiSkipLink` interactive props and Safari click issue ([#3665](https://github.com/elastic/eui/pull/3665))
+- Fixed `z-index` issues with `EuiHeader`, `EuiFlyout`, and other portal content ([#3655](https://github.com/elastic/eui/pull/3655))
+- Fixed `color` prop error in `EuiBadge` to be more flexible with what format it accepts ([#3655](https://github.com/elastic/eui/pull/3655))
+
+**Theme: Amsterdam**
+
+- Fixed `EuiHeaderBreadcrumb` height, `onClick`, border-radius, and single item display ([#3655](https://github.com/elastic/eui/pull/3655))
 
 ## [`26.1.0`](https://github.com/elastic/eui/tree/v26.1.0)
 
--- src-docs/src/views/overlay_mask/overlay_mask.js
+++ src-docs/src/views/overlay_mask/overlay_mask.js
@@ -4,73 +4,51 @@ import {
   EuiOverlayMask,
   EuiButton,
   EuiSpacer,
-  EuiFlyout,
   EuiTitle,
-  EuiFlyoutHeader,
 } from '../../../../src/components';
 
 export default () => {
-  const [modalOpen, changeModal] = useState(false);
-  const [selectedModal, selectModal] = useState(1);
-  const [flyOut, changeFlyOut] = useState(false);
+  const [maskOpen, changeMask] = useState(false);
+  const [maskWithClickOpen, changeMaskWithClick] = useState(false);
 
-  const openModal = modal => {
-    selectModal(modal);
-    changeModal(!modalOpen);
-  };
-
-  const closeModal = () => {
-    changeModal(!modalOpen);
-  };
-
-  const toggleFlyOut = () => {
-    changeFlyOut(!flyOut);
-  };
-
-  if (modalOpen) {
-    if (selectedModal === 1) {
-      return (
-        <React.Fragment>
-          <EuiOverlayMask onClick={closeModal}>
-            <EuiTitle>
-              <h2> Click anywhere to close overlay. </h2>
-            </EuiTitle>
-          </EuiOverlayMask>
-        </React.Fragment>
-      );
-    }
-
-    return (
-      <EuiOverlayMask>
-        <EuiButton onClick={closeModal}>Click this button to close</EuiButton>
+  const modal = (
+    <React.Fragment>
+      <EuiOverlayMask
+        onClick={() => {
+          changeMask(false);
+        }}>
+        <EuiTitle>
+          <h2> Click anywhere to close overlay. </h2>
+        </EuiTitle>
       </EuiOverlayMask>
-    );
-  }
+    </React.Fragment>
+  );
 
-  if (flyOut) {
-    return (
-      <React.Fragment>
-        <EuiOverlayMask onClick={toggleFlyOut} />
-        <EuiFlyout size="s" onClose={toggleFlyOut}>
-          <EuiFlyoutHeader>
-            <EuiTitle>
-              <h1>Click outside this flyout to close overlay. </h1>
-            </EuiTitle>
-          </EuiFlyoutHeader>
-        </EuiFlyout>
-      </React.Fragment>
-    );
-  }
+  const maskWithClick = (
+    <EuiOverlayMask>
+      <EuiButton
+        onClick={() => {
+          changeMaskWithClick(false);
+        }}>
+        Click this button to close
+      </EuiButton>
+    </EuiOverlayMask>
+  );
 
   return (
     <React.Fragment>
-      <EuiButton onClick={() => openModal(1)}>Overlay with onClick</EuiButton>
-      <EuiSpacer size="xxl" />
-      <EuiButton onClick={() => openModal(2)}>Overlay with button</EuiButton>
-      <EuiSpacer size="xxl" />
-      <EuiButton onClick={() => toggleFlyOut()}>
-        Overlay as a sibling of a flyout
+      <EuiButton
+        onClick={() => {
+          changeMask(true);
+        }}>
+        Overlay with onClick
       </EuiButton>
+      <EuiSpacer size="xxl" />
+      <EuiButton onClick={() => changeMaskWithClick(true)}>
+        Overlay with button
+      </EuiButton>
+      {maskOpen ? modal : undefined}
+      {maskWithClickOpen ? maskWithClick : undefined}
     </React.Fragment>
   );
 };
