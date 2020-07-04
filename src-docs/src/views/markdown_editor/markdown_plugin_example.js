1eb2ebf73f30800be48f2c5b1c31887513d24046
Dave Snider <dave.snider@gmail.com>
2020-07-04 12:36:47 -0700

    basic intro fo plugin page, needs more content and an example still

--- src-docs/src/views/markdown_editor/markdown_plugin_example.js
+++ src-docs/src/views/markdown_editor/markdown_plugin_example.js
@@ -8,6 +8,7 @@ import {
   EuiMarkdownFormat,
   EuiText,
   EuiSpacer,
+  EuiDescriptionList,
 } from '../../../../src/components';
 
 import { Link } from 'react-router-dom';
@@ -16,9 +17,88 @@ import MarkdownFormat from './markdown_format';
 const markdownFormatSource = require('!!raw-loader!./markdown_format');
 const markdownFormatHtml = renderToHtml(MarkdownFormat);
 
-import MarkdownFormatSink from './markdown_format_sink';
-const markdownFormatSinkSource = require('!!raw-loader!./markdown_format_sink');
-const markdownFormatSinkHtml = renderToHtml(MarkdownFormatSink);
+const pluginConcepts = [
+  {
+    title: 'uiPlugin',
+    description: (
+      <span>
+        Provides the <strong>UI</strong> for the button in the toolbar as well
+        as any modals or extra UI that provides content to the editor.
+      </span>
+    ),
+  },
+  {
+    title: 'parsingPlugin',
+    description: (
+      <span>
+        Provides the logic to identify the new syntax and parse it into an{' '}
+        <strong>AST node</strong>.
+      </span>
+    ),
+  },
+  {
+    title: 'processingPlugin',
+    description: (
+      <span>
+        Provides the logic to process the new <strong>AST node</strong> into a{' '}
+        <strong>React node</strong>.
+      </span>
+    ),
+  },
+];
+
+const uiPluginConcepts = [
+  {
+    title: 'name',
+    description: (
+      <span>
+        The name of your plugin. Use the <strong>button.label</strong> listed
+        below if you need a more friendly display name. The button can be
+        ommitted if you wish the user to only utilize syntax to author the
+        content.
+      </span>
+    ),
+  },
+  {
+    title: 'button',
+    description: (
+      <span>
+        Takes a <strong>label</strong> and an <strong>icon type</strong>. This
+        forms the button that appear in the toolbar. Clicking the button will
+        trigger either the <strong>editor</strong> or <strong>formatter</strong>
+        .
+      </span>
+    ),
+  },
+  {
+    title: 'editor',
+    description: (
+      <span>
+        Provides UI controls (like an interactive modal) for how to build the
+        inital content. <strong>Must exist if formatting does not</strong>.
+      </span>
+    ),
+  },
+  {
+    title: 'formatter',
+    description: (
+      <span>
+        If no <strong>editor</strong> is provided, a React component can be
+        providing to handle formatting.
+      </span>
+    ),
+  },
+  {
+    title: 'helpText',
+    description: (
+      <span>
+        Contains a React node. Should contain some information and an example
+        for how to utlize the syntax. Appears when the markdown icon is clicked
+        on the bottom of the editor.
+      </span>
+    ),
+  },
+];
 
 export const MarkdownPluginExample = {
   title: 'Markdown plugins',
@@ -26,13 +106,31 @@ export const MarkdownPluginExample = {
     <Fragment>
       <EuiText>
         <p>
-          <strong>EuiMarkdownFormat</strong> is a read-only way to render
-          markdown-style content in a page. It is a peer component to{' '}
+          Both{' '}
           <strong>
             <Link to="/editors-syntax/markdown-editor/">EuiMarkdownEditor</Link>
           </strong>{' '}
-          and has the ability to be modified by additional{' '}
-          <Link to="">markdown plugins</Link>.
+          and{' '}
+          <strong>
+            <Link to="/editors-syntax/markdown-format/">EuiMarkdownFormat</Link>
+          </strong>{' '}
+          utilize the same underlying plugin architecture to transform string
+          based syntax into React components. At a high level{' '}
+          <Link to="">Unified JS</Link> is used in comination with{' '}
+          <Link to="">Remark</Link> to provide EUI&apos;s markdown components,
+          which are separated into a <strong>parsing</strong> and{' '}
+          <strong>processing</strong> layer. These two concepts are kept
+          distinct in EUI components to provide concrete locations for your
+          plugins to be injected, be it editing or rendering. Finally you
+          provide <strong>UI</strong> to the component to handle interactions
+          with the editor.
+        </p>
+        <p>
+          In addition to running the full pipeline,{' '}
+          <strong>EuiMarkdownEditor</strong> uses just the parsing configuration
+          to determine the input&apos;s validity, provide messages back to the
+          application, and allow the toolbar buttons to interact with existing
+          markdown tags.
         </p>
       </EuiText>
       <EuiSpacer size="xxl" />
@@ -50,17 +148,29 @@ export const MarkdownPluginExample = {
           code: markdownFormatHtml,
         },
       ],
-      title: 'Built in plugins',
+      title: 'Plugin development',
       text: (
-        <p>
-          <strong>EuiMarkdownFormat</strong> is a wrapper that will render
-          Markdown provided. EuiMarkdownFormat uses{' '}
-          <Link to="https://github.com/remarkjs/remark)">Remark</Link> by
-          default. The translation layer automatically substitutes raw HTML
-          output with their EUI equivilant. This means anchor and code blocks
-          will become <strong>EuiLink</strong> and <strong>EuiCodeBlock</strong>{' '}
-          components respectively.
-        </p>
+        <Fragment>
+          <p>
+            An <strong>EuiMarkdown plugin</strong> is comprised of three major
+            pieces, which are passed searpately into the editor component.
+          </p>
+          <EuiDescriptionList
+            compressed
+            listItems={pluginConcepts}
+            type="responsiveColumn"
+            titleProps={{ style: { width: '20%' } }}
+            descriptionProps={{ style: { width: '80%' } }}
+          />
+          <h3>uiPlugin</h3>
+          <EuiDescriptionList
+            compressed
+            listItems={uiPluginConcepts}
+            type="responsiveColumn"
+            titleProps={{ style: { width: '20%' } }}
+            descriptionProps={{ style: { width: '80%' } }}
+          />
+        </Fragment>
       ),
       props: {
         EuiMarkdownFormat,
