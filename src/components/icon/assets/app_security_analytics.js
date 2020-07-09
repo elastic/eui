7634fa0fd887f8c37c96a66f1ffbca24d2401c7c
miukimiu <elizabet.oliveira@elastic.co>
2020-07-09 18:53:55 +0100

    removing fill none

--- src/components/icon/assets/app_security_analytics.js
+++ src/components/icon/assets/app_security_analytics.js
@@ -4,7 +4,6 @@ const EuiIconAppSecurityAnalytics = ({ title, titleId, ...props }) => (
   <svg
     width={32}
     height={32}
-    fill="none"
     viewBox="0 0 32 32"
     xmlns="http://www.w3.org/2000/svg"
     aria-labelledby={titleId}
