aa7d31af70f104e7d3333d8872119528d0e1c22e
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2020-07-17 16:34:54 +0100

    Update src-docs/src/views/combo_box/combo_box_example.js
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>

--- src-docs/src/views/combo_box/combo_box_example.js
+++ src-docs/src/views/combo_box/combo_box_example.js
@@ -439,7 +439,7 @@ export const ComboBoxExample = {
           </p>
           <p>
             <strong>Note:</strong> Creating custom options might not be obvious,
-            so provide a help text explaining that this option is available.
+            so provide help text explaining that this option is available.
           </p>
         </Fragment>
       ),
