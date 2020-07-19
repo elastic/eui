9fdcf7b345d82c91114d947ed515ac2791cd0098
cchaos <caroline.horn@elastic.co>
2020-07-19 11:06:27 -0400

    Very basic start of a selectable template for sitewide search

---
+++ src-docs/src/views/selectable/search.tsx
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
+
+import { EuiIcon } from '../../../../src/components/icon';
+import { EuiHighlight } from '../../../../src/components/highlight';
+import { EuiText, EuiTextColor } from '../../../../src/components/text';
+import { EuiBadge } from '../../../../src/components/badge';
+import {
+  EuiSelectable,
+  EuiSelectableTemplateSitewide,
+} from '../../../../src/components/selectable';
+import { EuiFlexGroup, EuiFlexItem } from '../../../../src/components/flex';
+import { EuiLink } from '../../../../src/components/link';
+import { EuiPopover } from '../../../../src/components/popover';
+import { EuiAvatar } from '../../../../src/components/avatar';
+
+export type EuiSelectableProps = React.ComponentProps<typeof EuiSelectable>;
+export type EuiSelectableOptionsProps = Required<EuiSelectableProps>['options'];
+export type EuiSelectableOptionProps = EuiSelectableOptionsProps[number];
+
+import { searchData, recents } from './data';
+
+function createAppendNodes(space?: string) {
+  const spaceAvatar = space ? (
+    <EuiAvatar type="space" name={space} size="s" />
+  ) : (
+    undefined
+  );
+
+  return (
+    <EuiFlexGroup responsive={false} gutterSize="s">
+      {spaceAvatar && <EuiFlexItem grow={false}>{spaceAvatar}</EuiFlexItem>}
+      <EuiFlexItem grow={false}>
+        <EuiBadge
+          aria-hidden={true}
+          className="kibanaChromeSearch__itemGotoBadge"
+          color="hollow">
+          Go to <small>↩</small>
+        </EuiBadge>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+}
+
+const allSearches = searchData.concat(recents);
+const data: EuiSelectableOptionsProps = allSearches.map(item => {
+  return {
+    key: item.title,
+    label: `${item.title} ${item.type.title}`,
+    prepend: item.type.iconType ? (
+      <EuiIcon type={item.type.iconType} size="m" color="subdued" />
+    ) : (
+      undefined
+    ),
+    className: 'kibanaChromeSearch__item',
+    append: createAppendNodes(item.space),
+  };
+});
+
+const recentData: EuiSelectableOptionsProps = recents.map(item => {
+  return {
+    key: item.title,
+    label: `${item.title} ${item.type.title}`,
+    prepend: <EuiIcon type="clock" size="m" color="subdued" />,
+    className: 'kibanaChromeSearch__item',
+    append: createAppendNodes(item.space),
+  };
+});
+
+export default () => {
+  const options = data;
+  const [searchValue, setSearchValue] = useState('');
+  const searchValueExists = searchValue && searchValue !== '';
+
+  let inputRef: HTMLInputElement;
+
+  useEffect(() => {
+    window.addEventListener('keydown', onWindowKeyDown);
+
+    return function cleanup() {
+      window.removeEventListener('resize', onWindowKeyDown);
+    };
+  });
+
+  const onWindowKeyDown = (e: any) => {
+    if (e.metaKey && e.key.toLowerCase() === 'k') {
+      window.addEventListener('keyup', onWindowKeyUp);
+    }
+  };
+
+  const onWindowKeyUp = () => {
+    inputRef && inputRef.focus();
+    window.removeEventListener('keyup', onWindowKeyUp);
+  };
+
+  const onChange = (updatedOptions: EuiSelectableOptionProps[]) => {
+    const clickedItem = _.find(updatedOptions, { checked: 'on' });
+    if (!clickedItem) return;
+    const searchItem = _.find(allSearches, { title: clickedItem.key });
+    if (searchItem && searchItem.url) console.log(searchItem.url);
+  };
+
+  const renderOption = (
+    option: EuiSelectableOptionProps,
+    searchValue: string
+  ) => {
+    const moreInfo = _.find(allSearches, { title: option.key });
+    if (!moreInfo) return 'Missing info';
+
+    return (
+      <>
+        <strong>
+          <EuiHighlight search={searchValue}>{moreInfo.title}</EuiHighlight>
+        </strong>
+        <br />
+        <small>
+          {moreInfo.type.title && (
+            <EuiTextColor color="secondary">
+              <strong>
+                <EuiHighlight search={searchValue}>
+                  {moreInfo.type.title}
+                </EuiHighlight>
+              </strong>
+            </EuiTextColor>
+          )}
+          {moreInfo.meta && (
+            <EuiTextColor color="subdued">
+              &ensp;•&ensp;
+              <EuiHighlight search={searchValue}>{moreInfo.meta}</EuiHighlight>
+            </EuiTextColor>
+          )}
+        </small>
+      </>
+    );
+  };
+
+  return (
+    <EuiSelectableTemplateSitewide
+      options={searchValueExists ? options : recentData}
+      searchProps={{
+        append: '⌘K',
+        // onKeyUpCapture: (e: any) => setSearchValue(e.currentTarget.value),
+        compressed: 'true',
+        className: 'customSearchClass',
+      }}
+      listProps={{
+        className: 'customListClass',
+      }}
+      popoverProps={{
+        className: 'customPopoverClass',
+        width: '50vw',
+      }}
+      popoverFooter={
+        <EuiText color="subdued" size="xs">
+          <EuiFlexGroup alignItems="center" gutterSize="s" responsive={false}>
+            <EuiFlexItem grow={false}>
+              <EuiLink>View more results</EuiLink>
+            </EuiFlexItem>
+            <EuiFlexItem />
+            <EuiFlexItem grow={false}>Quickly search using</EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiBadge>Command + K</EuiBadge>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiText>
+      }
+    />
+  );
+};
---
+++ src/components/selectable/selectable_templates/selectable_template_sitewide.tsx
@@ -0,0 +1,175 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import React, {
+  HTMLAttributes,
+  FunctionComponent,
+  ReactNode,
+  useState,
+  CSSProperties,
+} from 'react';
+import { CommonProps } from '../../common';
+import classNames from 'classnames';
+import { EuiAvatarProps } from '../../avatar/avatar';
+import { EuiIconProps } from '../../icon';
+import { EuiSelectable, EuiSelectableProps } from '../selectable';
+import { EuiSelectableOption } from '../selectable_option';
+import { EuiPopoverTitle, EuiPopoverFooter } from '../../popover';
+import { EuiPopover, Props as PopoverProps } from '../../popover/popover';
+import { useEuiI18n } from '../../i18n';
+import { EuiText } from '../../text';
+import { EuiFlexGroup, EuiFlexItem } from '../../flex';
+import { EuiLink } from '../../link';
+import { EuiBadge } from '../../badge';
+import { EuiSelectableListItemProps } from '../selectable_list/selectable_list_item';
+
+interface MetaData {
+  text: ReactNode;
+  type: 'app' | 'deployment' | 'article' | 'case' | 'platform' | 'other';
+  color?: string;
+  fontWeight?: 'normal' | 'medium' | 'bold';
+}
+
+export type EuiSelectableTemplateSitewideSchema = EuiSelectableOption & {
+  label: ReactNode;
+  icon?: EuiIconProps;
+  avatar?: EuiAvatarProps;
+  meta?: MetaData[];
+  [key: string]: any;
+};
+
+export type EuiSelectableTemplateSitewideProps = Omit<
+  EuiSelectableProps,
+  'options'
+> & {
+  options: EuiSelectableTemplateSitewideSchema[];
+  /**
+   * Override some of the EuiPopover props housing the list.
+   * The default width is `600`
+   */
+  popoverProps?: PopoverProps & { width?: CSSProperties['width'] };
+  /**
+   * Optionally provide a title for the popover
+   */
+  popoverTitle?: ReactNode;
+  /**
+   * Optionally provide a footer for the popover
+   */
+  popoverFooter?: ReactNode;
+};
+
+export const EuiSelectableTemplateSitewide: FunctionComponent<
+  EuiSelectableTemplateSitewideProps
+> = ({
+  children,
+  className,
+  options,
+  popoverProps,
+  popoverTitle,
+  popoverFooter,
+  searchProps,
+  listProps,
+  ...rest
+}) => {
+  /**
+   * i18n text
+   */
+  const [searchPlaceholder] = useEuiI18n(
+    ['euiSelectableTemplateSitewide.searchPlaceholder'],
+    ['Search for anything...']
+  );
+
+  /**
+   * Internal states
+   */
+  const [inputHasFocus, setInputHasFocus] = useState(false);
+
+  /**
+   * Popover helpers
+   */
+  const closePopover = () => {
+    setInputHasFocus(false);
+    popoverProps && popoverProps.closePopover();
+  };
+  // Width applied to the internal div
+  let popoverWidth: CSSProperties['width'] = 600;
+  if (popoverProps) {
+    // So it also needs to be removed from the spread
+    const { width, ...popoverRest } = popoverProps;
+    popoverWidth = popoverProps.width;
+    popoverProps = popoverRest;
+  }
+
+  /**
+   * Classes
+   */
+  const classes = classNames('euiSelectableTemplateSitewide', className);
+  const searchClasses = classNames(
+    'euiSelectableTemplateSitewide__search',
+    searchProps && searchProps.className
+  );
+  const listClasses = classNames(
+    'euiSelectableTemplateSitewide__list',
+    listProps && listProps.className
+  );
+
+  return (
+    <EuiSelectable
+      className={classes}
+      searchable
+      options={options}
+      // onChange={onChange}
+      // renderOption={renderOption}
+      // height={300}
+      singleSelection={true}
+      searchProps={{
+        placeholder: searchPlaceholder,
+        onFocus: () => setInputHasFocus(true),
+        // inputRef: (ref: HTMLInputElement) => (inputRef = ref),
+        isClearable: true,
+        ...searchProps,
+        className: searchClasses,
+      }}
+      listProps={{
+        rowHeight: 68,
+        showIcons: false,
+        ...listProps,
+        className: listClasses,
+      }}
+      {...rest}>
+      {(list, search) => (
+        <EuiPopover
+          panelPaddingSize="none"
+          display="block"
+          isOpen={inputHasFocus}
+          {...popoverProps}
+          button={search}
+          closePopover={closePopover}>
+          <div style={{ width: popoverWidth }}>
+            {popoverTitle && <EuiPopoverTitle>{popoverTitle}</EuiPopoverTitle>}
+            {list}
+            {popoverFooter && (
+              <EuiPopoverFooter>{popoverFooter}</EuiPopoverFooter>
+            )}
+          </div>
+        </EuiPopover>
+      )}
+    </EuiSelectable>
+  );
+};
