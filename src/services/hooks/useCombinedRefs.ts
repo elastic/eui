c1971266b43ce5e5927342709ecb00275d0812e4
Caroline Horn <549577+cchaos@users.noreply.github.com>
2020-07-23 12:29:02 -0400

    Using `/* */` comment format

--- src/services/hooks/useCombinedRefs.ts
+++ src/services/hooks/useCombinedRefs.ts
@@ -19,12 +19,13 @@
 
 import { MutableRefObject, Ref, useCallback } from 'react';
 
-// For use when a component needs to set `ref` objects from multiple sources.
-// For instance, if a component accepts a `ref` prop but also needs its own
-// local reference for calculations, etc.
-// This hook handles setting multiple `ref`s of any available `ref` type
-// in a single callback function.
-
+/*
+ * For use when a component needs to set `ref` objects from multiple sources.
+ * For instance, if a component accepts a `ref` prop but also needs its own
+ * local reference for calculations, etc.
+ * This hook handles setting multiple `ref`s of any available `ref` type
+ * in a single callback function.
+ * /
 export const useCombinedRefs = <T>(
   refs: Array<Ref<T> | MutableRefObject<T | undefined> | undefined>
 ) => {
