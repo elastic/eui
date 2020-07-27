fa9e62662016c4d7a85966d2450d8f66525d679d
Caroline Horn <549577+cchaos@users.noreply.github.com>
2020-07-27 13:28:00 -0400

    [EuiFieldPassword] Support toggling of obfuscation (#3751)
    * [EuiFieldPassword] Support toggling obfuscation
    
    - Added props `type` and `canToggleVisibility`
    
    * Removed `canToggleVisibility` in favor of a third `type` called ‘dual’
    
    * Put focus back into input after toggling visibility
    
    * Back to using `Ref` instead of `RefCallback`
    
    * Use new `useEuiI18n` method
    
    * Quick non-related fix, remove `zindex` completely from popovers in datagrid example
    
    Unnecessary to have a custom z-index as the default is much higher than the supplied one.
    
    * Removing Edge's inernal reveal button when we're providing a custom one
    
    * useCombinedRefs
    
    * comment
    
    * Using `/* */` comment format
    
    * Whoops
    
    * cl
    
    Co-authored-by: Greg Thompson <thompson.glowe@gmail.com>

--- src/services/hooks/useCombinedRefs.ts
+++ src/services/hooks/useCombinedRefs.ts
@@ -17,5 +17,29 @@
  * under the License.
  */
 
-export * from './useCombinedRefs';
-export * from './useDependentState';
+import { MutableRefObject, Ref, useCallback } from 'react';
+
+/*
+ * For use when a component needs to set `ref` objects from multiple sources.
+ * For instance, if a component accepts a `ref` prop but also needs its own
+ * local reference for calculations, etc.
+ * This hook handles setting multiple `ref`s of any available `ref` type
+ * in a single callback function.
+ */
+export const useCombinedRefs = <T>(
+  refs: Array<Ref<T> | MutableRefObject<T | undefined> | undefined>
+) => {
+  return useCallback(
+    (node: T) =>
+      refs.forEach(ref => {
+        if (!ref) return;
+
+        if (typeof ref === 'function') {
+          ref(node);
+        } else {
+          (ref as MutableRefObject<T>).current = node;
+        }
+      }),
+    [refs]
+  );
+};
