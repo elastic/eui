d5c42350bcea646d377f193f5c9c216d3846bbcd
Anish Aggarwal <anish17122000@gmail.com>
2020-08-04 23:34:51 +0530

    [Playground] EuiLink

---
+++ src-docs/src/views/link/playground.js
@@ -0,0 +1,65 @@
+import { PropTypes } from 'react-view';
+import { EuiLink } from '../../../../src/components/';
+import { propUtilityForPlayground } from '../../services/playground';
+import * as t from '@babel/types';
+
+export default () => {
+  const docgenInfo = Array.isArray(EuiLink.__docgenInfo)
+    ? EuiLink.__docgenInfo[0]
+    : EuiLink.__docgenInfo;
+  const propsToUse = propUtilityForPlayground(docgenInfo.props);
+
+  propsToUse.href = {
+    type: PropTypes.String,
+    value: 'http://www.elastic.co',
+  };
+  propsToUse.target = {
+    type: PropTypes.String,
+    value: '_blank',
+  };
+
+  propsToUse.children = {
+    value: 'Link',
+    type: PropTypes.String,
+    description: 'Link text.',
+    hidden: false,
+  };
+
+  propsToUse.onClick = {
+    ...propsToUse.onClick,
+    type: PropTypes.Custom,
+    value: undefined,
+    custom: {
+      ...propsToUse.onClick.custom,
+      use: 'switch',
+      label: 'Simulate',
+    },
+  };
+  return {
+    config: {
+      componentName: 'EuiLink',
+      props: propsToUse,
+      scope: {
+        EuiLink,
+      },
+      imports: {
+        '@elastic/eui': {
+          named: ['EuiLink'],
+        },
+      },
+      customProps: {
+        onClick: {
+          generate: val => {
+            if (!val) return null;
+            const obj = t.arrowFunctionExpression(
+              [],
+              t.blockStatement([]),
+              false
+            );
+            return obj;
+          },
+        },
+      },
+    },
+  };
+};
