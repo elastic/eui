30bbe405e60c72dfeebecd64884cd04bfcd55e5c
Anish Aggarwal <anish17122000@gmail.com>
2020-08-13 15:30:39 +0530

    [Playground] EuiRange, EuiDualRange

---
+++ src-docs/src/views/range/playground.js
@@ -0,0 +1,106 @@
+import { PropTypes } from 'react-view';
+import { EuiRange, EuiDualRange } from '../../../../src/components/';
+import {
+  propUtilityForPlayground,
+  dummyFunction,
+} from '../../services/playground';
+
+export const rangeConfig = () => {
+  const docgenInfo = Array.isArray(EuiRange.__docgenInfo)
+    ? EuiRange.__docgenInfo[0]
+    : EuiRange.__docgenInfo;
+  const propsToUse = propUtilityForPlayground(docgenInfo.props);
+
+  propsToUse.value = {
+    ...propsToUse.value,
+    type: PropTypes.String,
+  };
+
+  propsToUse.onChange = {
+    ...propsToUse.onChange,
+    type: PropTypes.Custom,
+    value: undefined,
+    custom: {
+      ...propsToUse.onChange.custom,
+      use: 'switch',
+      label: 'Simulate',
+    },
+  };
+
+  propsToUse.showInput = {
+    ...propsToUse.showInput,
+    type: PropTypes.Boolean,
+  };
+  propsToUse.valueAppend = {
+    ...propsToUse.valueAppend,
+    type: PropTypes.String,
+  };
+  propsToUse.valuePrepend = {
+    ...propsToUse.valuePrepend,
+    type: PropTypes.String,
+  };
+
+  return {
+    config: {
+      componentName: 'EuiRange',
+      props: propsToUse,
+      scope: {
+        EuiRange,
+      },
+      imports: {
+        '@elastic/eui': {
+          named: ['EuiRange'],
+        },
+      },
+      customProps: {
+        onChange: dummyFunction,
+      },
+    },
+  };
+};
+
+export const dualRangeConfig = () => {
+  const docgenInfo = Array.isArray(EuiDualRange.__docgenInfo)
+    ? EuiDualRange.__docgenInfo[0]
+    : EuiDualRange.__docgenInfo;
+  const propsToUse = propUtilityForPlayground(docgenInfo.props);
+
+  propsToUse.value = {
+    ...propsToUse.value,
+    type: PropTypes.String,
+  };
+
+  propsToUse.onChange = {
+    ...propsToUse.onChange,
+    type: PropTypes.Custom,
+    value: undefined,
+    custom: {
+      ...propsToUse.onChange.custom,
+      use: 'switch',
+      label: 'Simulate',
+    },
+  };
+
+  propsToUse.showInput = {
+    ...propsToUse.showInput,
+    type: PropTypes.Boolean,
+  };
+
+  return {
+    config: {
+      componentName: 'EuiDualRange',
+      props: propsToUse,
+      scope: {
+        EuiDualRange,
+      },
+      imports: {
+        '@elastic/eui': {
+          named: ['EuiDualRange'],
+        },
+      },
+      customProps: {
+        onChange: dummyFunction,
+      },
+    },
+  };
+};
