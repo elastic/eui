3ec057038204257dfc694678d5e76ecc74e9c331
Greg Thompson <thompson.glowe@gmail.com>
2020-08-20 11:36:35 -0600

    direct splice

--- src-docs/.babelrc.js
+++ src-docs/.babelrc.js
@@ -1,8 +1,10 @@
-const baseConfig = require("../.babelrc.js");
-const index = baseConfig.plugins.indexOf("./scripts/babel/proptypes-from-ts-props");
-const plugins = [...baseConfig.plugins];
-plugins.splice(index + 1, 0, "./scripts/babel/react-docgen-typescript")
-module.exports = {
-  ...baseConfig,
-  plugins,
-};
+const baseConfig = require('../.babelrc.js');
+const index = baseConfig.plugins.indexOf(
+  './scripts/babel/proptypes-from-ts-props'
+);
+baseConfig.plugins.splice(
+  index + 1,
+  0,
+  './scripts/babel/react-docgen-typescript'
+);
+module.exports = baseConfig;
