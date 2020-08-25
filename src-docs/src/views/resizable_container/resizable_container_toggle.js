81780f2a9ffe4c35b26f6d51f3ba957b4b89a476
Andrea Del Rio <delrio.andre@gmail.com>
2020-08-25 15:47:52 -0700

    working on the example

--- src-docs/src/views/resizable_container/resizable_container_toggle.js
+++ src-docs/src/views/resizable_container/resizable_container_toggle.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { EuiText, EuiResizableContainer } from '../../../../src/components';
+import React, { useState } from 'react';
+import {
+  EuiText,
+  EuiResizableContainer,
+  EuiListGroup,
+  EuiListGroupItem,
+  EuiPanel,
+} from '../../../../src/components';
 import { fake } from 'faker';
 
 const text = (
   <>
     <p>{fake('{{lorem.paragraphs}}')}</p>
-    <p>{fake('{{lorem.paragraphs}}')}</p>
-    <p>{fake('{{lorem.paragraphs}}')}</p>
   </>
 );
 
-export default () => (
-  <>
-    <EuiResizableContainer style={{ height: '200px' }}>
+const texts = [];
+
+for (let i = 0; i < 4; i++) {
+  texts.push(<p>{fake('{{lorem.paragraphs}}')}</p>);
+}
+
+console.log(texts);
+
+export default () => {
+  // const handleOnClick = () => {
+  //   alert('Item was clicked');
+  // };
+
+  const [item, setItem] = useState('First');
+
+  return (
+    <EuiResizableContainer style={{ height: '320px' }}>
       {(EuiResizablePanel, EuiResizableButton) => (
         <>
           <EuiResizablePanel
@@ -21,70 +39,44 @@ export default () => (
               notCollapsedIcon: 'menuRight',
               collapsedIcon: 'menuLeft',
             }}
-            initialSize={50}
-            minSize="30%">
-            <EuiText>
-              <p>sidebar</p>
-              {/* <a href="">Hello world</a> */}
-            </EuiText>
+            initialSize={20}
+            minSize="10%">
+            <EuiListGroup flush={true}>
+              <EuiListGroupItem
+                isActive
+                onClick={() => setItem('First')}
+                label="First item"
+              />
+
+              <EuiListGroupItem
+                onClick={() => setItem('Second')}
+                label="Second item"
+              />
+
+              <EuiListGroupItem
+                onClick={() => setItem('Third')}
+                label="Third item"
+              />
+
+              <EuiListGroupItem
+                onClick={() => setItem('Fourth')}
+                label="Fourth item"
+              />
+            </EuiListGroup>
           </EuiResizablePanel>
 
           <EuiResizableButton />
 
-          <EuiResizablePanel initialSize={50} minSize="200px">
-            <EuiText>
-              <p>content</p>
-            </EuiText>
+          <EuiResizablePanel initialSize={80} minSize="200px">
+            <EuiPanel paddingSize="l" style={{ height: '280px' }}>
+              <EuiText>
+                <p>{item} item</p>
+              </EuiText>
+              <EuiText>{text}</EuiText>
+            </EuiPanel>
           </EuiResizablePanel>
         </>
       )}
     </EuiResizableContainer>
-    <EuiResizableContainer style={{ height: '200px' }}>
-      {(EuiResizablePanel, EuiResizableButton) => (
-        <>
-          <EuiResizablePanel initialSize={50} minSize="30%">
-            <EuiText>
-              <p>sidebar</p>
-              {/* <a href="">Hello world</a> */}
-            </EuiText>
-          </EuiResizablePanel>
-
-          <EuiResizableButton />
-
-          <EuiResizablePanel
-            toggling={{
-              // active: true,
-              notCollapsedIcon: 'alert',
-              collapsedIcon: 'help',
-            }}
-            initialSize={50}
-            minSize="200px">
-            <EuiText>
-              <p>content</p>
-            </EuiText>
-          </EuiResizablePanel>
-        </>
-      )}
-    </EuiResizableContainer>
-    <EuiResizableContainer style={{ height: '200px' }}>
-      {(EuiResizablePanel, EuiResizableButton) => (
-        <>
-          <EuiResizablePanel initialSize={50} minSize="30%">
-            <EuiText>
-              <p>sidebar</p>
-              {/* <a href="">Hello world</a> */}
-            </EuiText>
-          </EuiResizablePanel>
-
-          <EuiResizableButton />
-
-          <EuiResizablePanel initialSize={50} minSize="200px">
-            <EuiText>
-              <p>content</p>
-            </EuiText>
-          </EuiResizablePanel>
-        </>
-      )}
-    </EuiResizableContainer>
-  </>
-);
+  );
+};
