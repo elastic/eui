c1240941c5c261f5696223dccdf049e968af968b
Kelvin <>
2020-09-02 11:09:08 +1000

    [Docs] Fix EuiToast's codesandbox link for Toast list in documentation (#3876).

--- src-docs/src/views/toast/toast_example.js
+++ src-docs/src/views/toast/toast_example.js
@@ -6,20 +6,17 @@ import { renderToHtml } from '../../services';
 import { GuideSectionTypes } from '../../components';
 
 import {
-  EuiButton,
   EuiCode,
   EuiToast,
   EuiGlobalToastList,
   EuiGlobalToastListItem,
-  EuiFlexGroup,
-  EuiFlexItem,
   EuiText,
   EuiSpacer,
 } from '../../../../src/components';
 import Guidelines from './guidelines';
 import toastConfig from './playground';
 
-import ToastList, { addToast, removeAllToasts } from './toast_list';
+import ToastList from './toast_list';
 const toastListSource = require('!!raw-loader!./toast_list');
 const toastListHtml = renderToHtml(ToastList);
 const toastListSnippet = [
@@ -127,22 +124,9 @@ export const ToastExample = {
         EuiGlobalToastListItem,
       },
       demo: (
-        <div style={{ maxWidth: 320 }}>
-          <EuiFlexGroup gutterSize="s">
-            <EuiFlexItem>
-              <EuiButton onClick={addToast}>
-                Add toast to global toast list
-              </EuiButton>
-            </EuiFlexItem>
-            <EuiFlexItem>
-              <EuiButton onClick={removeAllToasts} color="danger">
-                Remove all toasts
-              </EuiButton>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-
+        <>
           <ToastList />
-        </div>
+        </>
       ),
       snippet: toastListSnippet,
     },
