b1e89aad867b2ef8605dfe0b67194dc5ea3fcfe1
Casper Hübertz <casper@formgeist.com>
2020-09-07 09:52:12 +0200

    Copy changes

--- src-docs/src/views/badge/badge_example.js
+++ src-docs/src/views/badge/badge_example.js
@@ -216,15 +216,7 @@ export const BadgeExample = {
         <div>
           <p>
             Badges can work as health status indicators in places where there
-            are a lot of repeated statuses e.g. in tables.
-          </p>
-          <p>
-            The status colors can be determined from the{' '}
-            <EuiCode>euiPaletteForStatus</EuiCode> in the{' '}
-            <Link to="/utilities/color-palettes">
-              <strong>Eui Color Palettes</strong>
-            </Link>
-            .
+            are a lot of repeated statuses, e.g. in tables.
           </p>
         </div>
       ),
