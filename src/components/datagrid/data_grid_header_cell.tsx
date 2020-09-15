22290ebd3770bd4d96bd6a2f34a85e5ef4b9672a
Andrea Del Rio <delrio.andre@gmail.com>
2020-09-15 10:37:17 -0700

    fix focus of menu (again) and add mixin

--- src/components/datagrid/data_grid_header_cell.tsx
+++ src/components/datagrid/data_grid_header_cell.tsx
@@ -311,7 +311,6 @@ export const EuiDataGridHeaderCell: FunctionComponent<EuiDataGridHeaderCellProps
           <div className="euiDataGridHeaderCell__content">{display || id}</div>
           <EuiPopover
             className="euiDataGridHeaderCell__popover"
-            anchorClassName="euiDataGridHeaderCell__popoverAnchor"
             panelPaddingSize="none"
             anchorPosition="downRight"
             button={
