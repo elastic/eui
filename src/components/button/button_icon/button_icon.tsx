259c792c206ca3e570e8f1c3fc8f2d96e3c73fc7
Chandler Prall <chandler.prall@gmail.com>
2020-09-22 09:17:55 -0600

    Removed 'disabled' color from button components

--- src/components/button/button_icon/button_icon.tsx
+++ src/components/button/button_icon/button_icon.tsx
@@ -41,7 +41,6 @@ import { ButtonSize } from '../button';
 export type EuiButtonIconColor =
   | 'accent'
   | 'danger'
-  | 'disabled'
   | 'ghost'
   | 'primary'
   | 'subdued'
@@ -85,7 +84,6 @@ type Props = ExclusiveUnion<
 const colorToClassNameMap: { [color in EuiButtonIconColor]: string } = {
   accent: 'euiButtonIcon--accent',
   danger: 'euiButtonIcon--danger',
-  disabled: 'euiButtonIcon--disabled',
   ghost: 'euiButtonIcon--ghost',
   primary: 'euiButtonIcon--primary',
   subdued: 'euiButtonIcon--subdued',
