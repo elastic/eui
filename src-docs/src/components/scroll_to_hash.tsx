8b7ee474781baf2d8576a8641006d08a3856a930
miukimiu <elizabet.oliveira@elastic.co>
2020-09-23 16:18:43 +0100

    Removing setTimeout()

--- src-docs/src/components/scroll_to_hash.tsx
+++ src-docs/src/components/scroll_to_hash.tsx
@@ -4,17 +4,15 @@ import { useLocation } from 'react-router-dom';
 const ScrollToHash: FunctionComponent = () => {
   const location = useLocation();
   useEffect(() => {
-    setTimeout(() => {
-      const element = document.getElementById(location.hash.replace('#', ''));
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-      } else {
-        window.scrollTo({
-          behavior: 'auto',
-          top: 0,
-        });
-      }
-    }, 500);
+    const element = document.getElementById(location.hash.replace('#', ''));
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      window.scrollTo({
+        behavior: 'auto',
+        top: 0,
+      });
+    }
   }, [location]);
   return null;
 };
