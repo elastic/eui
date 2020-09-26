dc5fc99080341201b1fb3b3bfbfc83cc57b82c5a
TAY TS <taytzushieh@gmail.com>
2020-09-26 18:12:42 +0800

    Updated badge documentation
    - Removed console.log and window.alert

--- src-docs/src/views/badge/playground.js
+++ src-docs/src/views/badge/playground.js
@@ -25,7 +25,6 @@ export const badgeConfig = () => {
       use: 'switch',
       label: 'Simulate',
       modifyOtherProps: (val, state, set) => {
-        console.log(val, 'state', state);
         if (val) {
           if (!state.onClickAriaLabel.value) {
             set('onClickAriaLabel', 'onClickAriaLabel');
