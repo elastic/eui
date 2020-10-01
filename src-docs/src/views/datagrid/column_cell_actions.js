bbb8f11ae448efd272b14e870afa5b6f0141e405
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2020-10-01 22:57:21 +0200

    Update src-docs/src/views/datagrid/column_cell_actions.js
    Co-authored-by: Andrea Del Rio <delrio.andre@gmail.com>

--- src-docs/src/views/datagrid/column_cell_actions.js
+++ src-docs/src/views/datagrid/column_cell_actions.js
@@ -69,7 +69,7 @@ const columns = [
         iconType: 'brush',
         label: 'Paint this city',
         callback: (rowIndex, colIndex) =>
-          alert(`Paint sent from ${rowIndex + 1}, column "${colIndex}"`),
+          alert(`Paint sent from row ${rowIndex + 1}, column "${colIndex}"`),
       },
     ],
   },
