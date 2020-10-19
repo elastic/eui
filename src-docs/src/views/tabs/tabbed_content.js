853174dc63783275d503724d1c5cc3c31e2a9cbe
TAY TS <taytzushieh@gmail.com>
2020-10-19 22:09:00 +0800

    Reverted tabbed content logging to show callback content

--- src-docs/src/views/tabs/tabbed_content.js
+++ src-docs/src/views/tabs/tabbed_content.js
@@ -93,8 +93,8 @@ export default () => {
       tabs={tabs}
       initialSelectedTab={tabs[1]}
       autoFocus="selected"
-      onTabClick={(tabs) => {
-        console.log(tabs);
+      onTabClick={(tab) => {
+        console.log('clicked tab', tab);
       }}
     />
   );
