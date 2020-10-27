ff6540557b0fa3885dd4965f514f360e0b0549e6
Caroline Horn <549577+cchaos@users.noreply.github.com>
2020-10-27 11:51:53 -0400

    EuiBottomBar: allow customization of whether the component makes room for itself (#4156)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 - Improved `EuiButtonGroup` focus, hover, selected and disabled states ([#4142](https://github.com/elastic/eui/pull/4142))
 - Added `display` prop to `EuiToolTip` for common display block needs ([#4148](https://github.com/elastic/eui/pull/4148))
 - Added support for more colors in `EuiProgress` such as `vis0` through `vis9`, `warning`, `success`  and custom colors ([#4130](https://github.com/elastic/eui/pull/4130))
+- Added `affordForDisplacement` prop to `EuiBottomBar` ([#4156](https://github.com/elastic/eui/pull/4156))
 
 **Bug fixes**
 
---
+++ src-docs/src/views/bottom_bar/bottom_bar_displacement.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+import {
+  EuiBottomBar,
+  EuiButtonGroup,
+  EuiButtonEmpty,
+  EuiFlexGroup,
+  EuiFlexItem,
+} from '../../../../src/components';
+
+export default () => {
+  const [toggleIdSelected, setToggleIdSelected] = useState(null);
+
+  const toggleButtons = [
+    {
+      id: 'bottomBarStandard',
+      label: 'Show bottom bar',
+    },
+    {
+      id: 'bottomBarWithoutAffordForDisplacement',
+      label: 'Show bottom bar (without affordForDisplacement behavior)',
+    },
+  ];
+
+  const onChange = (optionId) => {
+    setToggleIdSelected(optionId);
+  };
+
+  return (
+    <div>
+      <EuiButtonGroup
+        legend="Bottom Bar demo toggle buttons group"
+        type="single"
+        buttonSize="m"
+        options={toggleButtons}
+        idSelected={toggleIdSelected}
+        onChange={(id) => onChange(id)}
+      />
+
+      {toggleIdSelected && (
+        <EuiBottomBar
+          affordForDisplacement={toggleIdSelected === 'bottomBarStandard'}>
+          <EuiFlexGroup justifyContent="flexEnd">
+            <EuiFlexItem grow={false}>
+              <EuiButtonEmpty
+                onClick={() => setToggleIdSelected(null)}
+                color="ghost"
+                size="s"
+                iconType="cross">
+                close
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiBottomBar>
+      )}
+    </div>
+  );
+};
