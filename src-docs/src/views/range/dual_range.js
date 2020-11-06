a6a2f5b5e2741f35380afe488487e881056e88d1
Greg Thompson <thompson.glowe@gmail.com>
2020-11-06 08:42:46 -0700

    Revert "revert me"
    This reverts commit 8240e6f9e93a1f8e7e9e4f87224133a6d6a936b9.

--- src-docs/src/views/range/dual_range.js
+++ src-docs/src/views/range/dual_range.js
@@ -5,34 +5,22 @@ import { EuiDualRange } from '../../../../src/components';
 import { htmlIdGenerator } from '../../../../src/services';
 
 export default () => {
-  const [value, setValue] = useState(['20', '100']);
-  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState(['', '']);
 
   const onChange = (value) => {
     setValue(value);
   };
 
-  const onToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <div>
-      <button type="button" onClick={onToggle}>
-        Toggle
-      </button>
-      <div className={isOpen ? '' : 'eui-showFor'}>
-        <EuiDualRange
-          id={htmlIdGenerator()()}
-          min={0}
-          max={200}
-          step={10}
-          value={value}
-          onChange={onChange}
-          showLabels
-          aria-label="An example of EuiDualRange"
-        />
-      </div>
-    </div>
+    <EuiDualRange
+      id={htmlIdGenerator()()}
+      min={-100}
+      max={200}
+      step={10}
+      value={value}
+      onChange={onChange}
+      showLabels
+      aria-label="An example of EuiDualRange"
+    />
   );
 };
