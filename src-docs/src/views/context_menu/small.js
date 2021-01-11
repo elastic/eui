6e4e58204987aba5ca4839847eb99224d69d5661
Andrea Del Rio <delrio.andre@gmail.com>
2021-01-11 10:37:53 -0800

    re-simplifying small example

--- src-docs/src/views/context_menu/small.js
+++ src-docs/src/views/context_menu/small.js
@@ -1,119 +1,58 @@
 import React, { useState } from 'react';
-import { EuiComment } from '../../../../src/components/comment_list';
-import { EuiButtonIcon } from '../../../../src/components/button';
-import { EuiText } from '../../../../src/components/text';
-import { EuiPopover } from '../../../../src/components/popover';
+
 import {
+  EuiButton,
   EuiContextMenuPanel,
   EuiContextMenuItem,
-} from '../../../../src/components/context_menu';
-
-const body = (
-  <EuiText size="s">
-    <p>Click the gear icon to see a small context menu.</p>
-  </EuiText>
-);
+  // EuiIcon,
+  EuiPopover,
+} from '../../../../src/components';
 
 export default () => {
-  const [isPopoverOpen1, setIsPopoverOpen1] = useState(false);
-  const [isPopoverOpen2, setIsPopoverOpen2] = useState(false);
+  const [isPopoverOpen, setPopover] = useState(false);
 
-  const togglePopover1 = () => {
-    setIsPopoverOpen1(!isPopoverOpen1);
+  const onButtonClick = () => {
+    setPopover(!isPopoverOpen);
   };
 
-  const closePopover1 = () => {
-    setIsPopoverOpen1(false);
+  const closePopover = () => {
+    setPopover(false);
   };
 
-  const togglePopover2 = () => {
-    setIsPopoverOpen2(!isPopoverOpen2);
-  };
+  const items = [
+    <EuiContextMenuItem
+      key="copy"
+      icon="copy"
+      onClick={() => {
+        closePopover();
+      }}>
+      Copy
+    </EuiContextMenuItem>,
+    <EuiContextMenuItem
+      key="edit"
+      icon="pencil"
+      onClick={() => {
+        closePopover();
+      }}>
+      Edit
+    </EuiContextMenuItem>,
+  ];
 
-  const closePopover2 = () => {
-    setIsPopoverOpen2(false);
-  };
-
-  const customActions1 = (
-    <EuiPopover
-      button={
-        <EuiButtonIcon
-          aria-label="Actions"
-          iconType="gear"
-          size="s"
-          color="text"
-          onClick={togglePopover1}
-        />
-      }
-      isOpen={isPopoverOpen1}
-      closePopover={closePopover1}
-      panelPaddingSize="none"
-      anchorPosition="leftCenter">
-      <EuiContextMenuPanel
-        size="s"
-        items={[
-          <EuiContextMenuItem key="A" icon="pencil" onClick={closePopover1}>
-            Edit
-          </EuiContextMenuItem>,
-          <EuiContextMenuItem key="B" icon="share" onClick={closePopover1}>
-            Share
-          </EuiContextMenuItem>,
-          <EuiContextMenuItem key="C" icon="copy" onClick={closePopover1}>
-            Copy
-          </EuiContextMenuItem>,
-        ]}
-      />
-    </EuiPopover>
-  );
-
-  const customActions2 = (
-    <EuiPopover
-      button={
-        <EuiButtonIcon
-          aria-label="Actions"
-          iconType="gear"
-          size="s"
-          color="text"
-          onClick={togglePopover2}
-        />
-      }
-      isOpen={isPopoverOpen2}
-      closePopover={closePopover2}
-      panelPaddingSize="none"
-      anchorPosition="leftCenter">
-      <EuiContextMenuPanel
-        size="s"
-        items={[
-          <EuiContextMenuItem key="A" icon="pencil" onClick={closePopover2}>
-            Edit
-          </EuiContextMenuItem>,
-          <EuiContextMenuItem key="B" icon="share" onClick={closePopover2}>
-            Share
-          </EuiContextMenuItem>,
-          <EuiContextMenuItem key="C" icon="copy" onClick={closePopover2}>
-            Copy
-          </EuiContextMenuItem>,
-        ]}
-      />
-    </EuiPopover>
+  const button = (
+    <EuiButton iconType="arrowDown" iconSide="right" onClick={onButtonClick}>
+      Small context menu
+    </EuiButton>
   );
 
   return (
-    <div>
-      <EuiComment
-        username="janed"
-        event="added a comment"
-        actions={customActions1}
-        timestamp="Jan 1, 2021">
-        {body}
-      </EuiComment>
-      <EuiComment
-        username="danib"
-        event="requested changes"
-        actions={customActions2}
-        timestamp="Jan 2, 2021">
-        {body}
-      </EuiComment>
-    </div>
+    <EuiPopover
+      id="contextMenuExample"
+      button={button}
+      isOpen={isPopoverOpen}
+      closePopover={closePopover}
+      panelPaddingSize="none"
+      anchorPosition="downLeft">
+      <EuiContextMenuPanel items={items} size="s" />
+    </EuiPopover>
   );
 };
