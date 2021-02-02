82839da8ee699aa393634a40eaad3a9e483ecccd
Greg Thompson <thompson.glowe@gmail.com>
2021-02-02 15:27:02 -0600

    revert me

--- src-docs/src/views/combo_box/combo_box.js
+++ src-docs/src/views/combo_box/combo_box.js
@@ -1,87 +1,32 @@
 import React, { useState } from 'react';
 
-import { EuiComboBox } from '../../../../src/components';
-import { DisplayToggles } from '../form_controls/display_toggles';
+import { EuiComboBox, EuiFieldText } from '../../../../src/components';
 
-const optionsStatic = [
-  {
-    label: 'Titan',
-    'data-test-subj': 'titanOption',
-  },
-  {
-    label: 'Enceladus is disabled',
-    disabled: true,
-  },
-  {
-    label: 'Mimas',
-  },
-  {
-    label: 'Dione',
-  },
-  {
-    label: 'Iapetus',
-  },
-  {
-    label: 'Phoebe',
-  },
-  {
-    label: 'Rhea',
-  },
-  {
-    label:
-      "Pandora is one of Saturn's moons, named for a Titaness of Greek mythology",
-  },
-  {
-    label: 'Tethys',
-  },
-  {
-    label: 'Hyperion',
-  },
-];
 export default () => {
-  const [options, setOptions] = useState(optionsStatic);
-  const [selectedOptions, setSelected] = useState([options[2], options[4]]);
+  const [name, setName] = useState('');
 
-  const onChange = (selectedOptions) => {
-    setSelected(selectedOptions);
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+    window.alert('form submitted');
   };
 
-  const onCreateOption = (searchValue, flattenedOptions = []) => {
-    const normalizedSearchValue = searchValue.trim().toLowerCase();
+  const options = [
+    { label: 'Chandler', color: 'hotpink' },
+    { label: 'Rachel', color: 'green' },
+  ];
 
-    if (!normalizedSearchValue) {
-      return;
-    }
-
-    const newOption = {
-      label: searchValue,
-    };
-
-    // Create the option if it doesn't exist.
-    if (
-      flattenedOptions.findIndex(
-        (option) => option.label.trim().toLowerCase() === normalizedSearchValue
-      ) === -1
-    ) {
-      setOptions([...options, newOption]);
-    }
-
-    // Select the option.
-    setSelected([...selectedOptions, newOption]);
-  };
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
   return (
-    /* DisplayToggles wrapper for Docs only */
-    <DisplayToggles canDisabled={false} canReadOnly={false}>
+    <form onSubmit={onSubmit}>
+      <EuiFieldText value={name} onChange={(ev) => setName(ev.target.value)} />
       <EuiComboBox
-        placeholder="Select or create options"
         options={options}
+        onChange={(options) => setSelectedOptions(options)}
         selectedOptions={selectedOptions}
-        onChange={onChange}
-        onCreateOption={onCreateOption}
-        isClearable={true}
-        data-test-subj="demoComboBox"
       />
-    </DisplayToggles>
+      {/* <input type="submit" /> */}
+    </form>
   );
 };
