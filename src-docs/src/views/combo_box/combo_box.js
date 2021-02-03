cb328a7e5a371e1cb1edc8764f8a83cb0966f8f8
Greg Thompson <thompson.glowe@gmail.com>
2021-02-03 12:56:45 -0600

    Revert "revert me"
    This reverts commit 82839da8ee699aa393634a40eaad3a9e483ecccd.

--- src-docs/src/views/combo_box/combo_box.js
+++ src-docs/src/views/combo_box/combo_box.js
@@ -1,32 +1,87 @@
 import React, { useState } from 'react';
 
-import { EuiComboBox, EuiFieldText } from '../../../../src/components';
+import { EuiComboBox } from '../../../../src/components';
+import { DisplayToggles } from '../form_controls/display_toggles';
 
+const optionsStatic = [
+  {
+    label: 'Titan',
+    'data-test-subj': 'titanOption',
+  },
+  {
+    label: 'Enceladus is disabled',
+    disabled: true,
+  },
+  {
+    label: 'Mimas',
+  },
+  {
+    label: 'Dione',
+  },
+  {
+    label: 'Iapetus',
+  },
+  {
+    label: 'Phoebe',
+  },
+  {
+    label: 'Rhea',
+  },
+  {
+    label:
+      "Pandora is one of Saturn's moons, named for a Titaness of Greek mythology",
+  },
+  {
+    label: 'Tethys',
+  },
+  {
+    label: 'Hyperion',
+  },
+];
 export default () => {
-  const [name, setName] = useState('');
+  const [options, setOptions] = useState(optionsStatic);
+  const [selectedOptions, setSelected] = useState([options[2], options[4]]);
 
-  const onSubmit = (ev) => {
-    ev.preventDefault();
-    ev.stopPropagation();
-    window.alert('form submitted');
+  const onChange = (selectedOptions) => {
+    setSelected(selectedOptions);
   };
 
-  const options = [
-    { label: 'Chandler', color: 'hotpink' },
-    { label: 'Rachel', color: 'green' },
-  ];
+  const onCreateOption = (searchValue, flattenedOptions = []) => {
+    const normalizedSearchValue = searchValue.trim().toLowerCase();
 
-  const [selectedOptions, setSelectedOptions] = useState([]);
+    if (!normalizedSearchValue) {
+      return;
+    }
+
+    const newOption = {
+      label: searchValue,
+    };
+
+    // Create the option if it doesn't exist.
+    if (
+      flattenedOptions.findIndex(
+        (option) => option.label.trim().toLowerCase() === normalizedSearchValue
+      ) === -1
+    ) {
+      setOptions([...options, newOption]);
+    }
+
+    // Select the option.
+    setSelected([...selectedOptions, newOption]);
+  };
 
   return (
-    <form onSubmit={onSubmit}>
-      <EuiFieldText value={name} onChange={(ev) => setName(ev.target.value)} />
+    /* DisplayToggles wrapper for Docs only */
+    <DisplayToggles canDisabled={false} canReadOnly={false}>
       <EuiComboBox
+        placeholder="Select or create options"
         options={options}
-        onChange={(options) => setSelectedOptions(options)}
         selectedOptions={selectedOptions}
+        onChange={onChange}
+        onCreateOption={onCreateOption}
+        isClearable={true}
+        data-test-subj="demoComboBox"
       />
-      {/* <input type="submit" /> */}
-    </form>
+    </DisplayToggles>
   );
 };
