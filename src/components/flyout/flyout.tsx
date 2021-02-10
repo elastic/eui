438bafb7e4c5ad400fd3e034de72df9aa1efab35
Akash Gupta <akashgp9@gmail.com>
2021-02-10 08:37:45 +0530

    Fixed onClose invoking with unexpected parameter

--- src/components/flyout/flyout.tsx
+++ src/components/flyout/flyout.tsx
@@ -134,7 +134,9 @@ export const EuiFlyout: FunctionComponent<EuiFlyoutProps> = ({
             iconType="cross"
             color="text"
             aria-label={closeButtonAriaLabel || closeAriaLabel}
-            onClick={onClose}
+            onClick={() => {
+              onClose();
+            }}
             data-test-subj="euiFlyoutCloseButton"
           />
         )}
