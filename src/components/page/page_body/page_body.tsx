ac3c7df07336e3696e58ebc502554c1c7c2b3cc9
cchaos <caroline.horn@elastic.co>
2021-02-12 15:50:31 -0500

    Using restrictedWidth helper on EuiPageBody

--- src/components/page/page_body/page_body.tsx
+++ src/components/page/page_body/page_body.tsx
@@ -20,19 +20,16 @@
 import React, { PropsWithChildren, ComponentType, ComponentProps } from 'react';
 import classNames from 'classnames';
 import { CommonProps } from '../../common';
+import {
+  EuiPageRestrictWidth,
+  setPropsForRestrictedPageWidth,
+} from '../_restrict_width';
 
 type ComponentTypes = keyof JSX.IntrinsicElements | ComponentType<any>;
 
 export type EuiPageBodyProps<T extends ComponentTypes = 'main'> = CommonProps &
-  ComponentProps<T> & {
-    /**
-     * Sets the max-width of the page,
-     * set to `true` to use the default size,
-     * set to `false` to not restrict the width,
-     * set to a number for a custom width in px,
-     * set to a string for a custom width in custom measurement.
-     */
-    restrictWidth?: boolean | number | string;
+  ComponentProps<T> &
+  EuiPageRestrictWidth & {
     /**
      * Sets the HTML element for `EuiPageBody`.
      */
@@ -47,19 +44,18 @@ export const EuiPageBody = <T extends ComponentTypes>({
   component: Component = 'main' as T,
   ...rest
 }: PropsWithChildren<EuiPageBodyProps<T>>) => {
-  let widthClassname;
-  let newStyle;
+  const { widthClassName, newStyle } = setPropsForRestrictedPageWidth(
+    restrictWidth,
+    style
+  );
 
-  if (restrictWidth === true) {
-    widthClassname = 'euiPageBody--restrictWidth-default';
-  } else if (restrictWidth !== false) {
-    widthClassname = 'euiPageBody--restrictWidth-custom';
-    const value =
-      typeof restrictWidth === 'number' ? `${restrictWidth}px` : restrictWidth;
-    newStyle = { ...style, maxWidth: value };
-  }
-
-  const classes = classNames('euiPageBody', widthClassname, className);
+  const classes = classNames(
+    'euiPageBody',
+    {
+      [`euiPageBody--${widthClassName}`]: widthClassName,
+    },
+    className
+  );
 
   return (
     <Component className={classes} style={newStyle || style} {...rest}>
