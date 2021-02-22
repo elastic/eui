160d47d3f62bd504f2e27262912f9524d0d02f93
cchaos <caroline.horn@elastic.co>
2021-02-22 16:07:32 -0500

    pretty

--- src-docs/src/views/page_header/page_header_example.js
+++ src-docs/src/views/page_header/page_header_example.js
@@ -144,10 +144,10 @@ description="Example of a description."
       text: (
         <>
           <p>
-            The page header content props are helpful props to push
-            content into established Elastic page layout patterns. They are
-            completely optional and by design, inflexible. If you need a layout
-            that does not match these patterns you can pass in your own{' '}
+            The page header content props are helpful props to push content into
+            established Elastic page layout patterns. They are completely
+            optional and by design, inflexible. If you need a layout that does
+            not match these patterns you can pass in your own{' '}
             <EuiCode>children</EuiCode> utilizing the{' '}
             <strong>EuiPageHeaderSection</strong> components.
           </p>
