89c7f792c977aa68f3e844f9c3ec0c30da19c71f
miukimiu <elizabet.oliveira@elastic.co>
2021-02-23 14:54:01 +0000

    More docs

--- src-docs/src/views/notification_event/notification_event_example.js
+++ src-docs/src/views/notification_event/notification_event_example.js
@@ -7,6 +7,9 @@ import {
   EuiNotificationEvent,
   EuiText,
   EuiContextMenuItem,
+  EuiSpacer,
+  EuiCode,
+  EuiTextColor,
 } from '../../../../src/components';
 import { notificationEventReadButtonConfig } from './playground';
 import { EuiNotificationEventPrimaryAction } from './props';
@@ -18,6 +21,29 @@ const notificationEventHtml = renderToHtml(NotificationEvent);
 import NotificationsFeed from './notifications_feed';
 const notificationsFeedSource = require('!!raw-loader!./notifications_feed');
 const notificationsFeedHtml = renderToHtml(NotificationsFeed);
+
+const notificationEventSnippet = `<EuiNotificationEvent
+  id={id}
+  meta={{
+    type: 'Alert',
+    severity: 'Warning',
+    iconType: 'logoMaps',
+    badgeColor: 'warning',
+    eventName: 'alert-warning-01',
+    time: '1 min ago',
+  }}
+  title={title}
+  isRead={isRead}
+  primaryAction={primaryAction}
+  messages={messages}
+  onRead={onRead}
+  contextMenuItems={contextMenuItems}
+  onOpenContextMenu={onOpenContextMenu}
+  onClickPrimaryAction={() => {}}
+  onClickTitle={() => {}}
+/>`;
+
+const required = <EuiTextColor color="danger">(required)</EuiTextColor>;
 
 export const NotificationEventExample = {
   title: 'Notification event',
@@ -38,6 +64,7 @@ export const NotificationEventExample = {
           adapts the design according to the passed props.
         </p>
       </EuiText>
+      <EuiSpacer />
     </Fragment>
   ),
   sections: [
@@ -52,12 +79,79 @@ export const NotificationEventExample = {
           code: notificationEventHtml,
         },
       ],
+      title: 'Props and methods',
+      text: (
+        <>
+          <EuiText>
+            <p>
+              A <strong>EuiNotificationEvent</strong> is comprised of different
+              props.
+            </p>
+            <ul>
+              <li>
+                <EuiCode>{'id'}</EuiCode> {required}: a unique id
+              </li>
+              <li>
+                <EuiCode>{'meta'}</EuiCode> {required}: an object with multiple
+                props like <EuiCode>{'type'}</EuiCode>,{' '}
+                <EuiCode>{'severity'}</EuiCode>,{' '}
+                <EuiCode>{'badgeColor'}</EuiCode>,{' '}
+                <EuiCode>{'iconType'}</EuiCode> and <EuiCode>{'time'}</EuiCode>.
+              </li>
+              <li>
+                <EuiCode>{'title'}</EuiCode> {required}: the{' '}
+                <EuiCode>{'title'}</EuiCode> of the event
+              </li>
+              <li>
+                <EuiCode>{'primaryAction'}</EuiCode>: an object with a{' '}
+                <EuiCode>{'label'}</EuiCode> and other{' '}
+                <EuiCode>{'EuiButtonEmptyProps'}</EuiCode> props like a{' '}
+                <EuiCode>{'iconType'}</EuiCode>
+              </li>
+              <li>
+                <EuiCode>{'contextMenuItems'}</EuiCode>: an array of{' '}
+                <EuiCode>{'<EuiContextMenuItem />'}</EuiCode>
+              </li>
+              <li>
+                <EuiCode>{'messages'}</EuiCode> {required}: notification
+                messages as an array of strings
+              </li>
+            </ul>
+          </EuiText>
+          <EuiSpacer />
+          <EuiText>
+            <p>Methods for helping to deal to common action types:</p>
+            <ul>
+              <li>
+                <EuiCode>onOpenContextMenu</EuiCode>: returns the{' '}
+                <EuiCode>id</EuiCode>, <EuiCode>isRead</EuiCode> and{' '}
+                <EuiCode>type</EuiCode> of the open context menu
+              </li>
+              <li>
+                <EuiCode>onClickPrimaryAction</EuiCode>: returns the{' '}
+                <EuiCode>id</EuiCode> and applies an <EuiCode>onClick</EuiCode>{' '}
+                handler to the <EuiCode>primaryAction</EuiCode>
+              </li>
+              <li>
+                <EuiCode>onClickTitle</EuiCode>: returns the{' '}
+                <EuiCode>id</EuiCode> and applies an <EuiCode>onClick</EuiCode>{' '}
+                handler to the <EuiCode>title</EuiCode>
+              </li>
+            </ul>
+          </EuiText>
+          <EuiSpacer />
+          <EuiText>
+            <p>See the snippet and props table for full details.</p>
+          </EuiText>
+        </>
+      ),
       props: {
         EuiNotificationEvent,
         EuiNotificationEventMeta,
         EuiNotificationEventPrimaryAction,
         EuiContextMenuItem,
       },
+      snippet: notificationEventSnippet,
       demo: <NotificationEvent />,
     },
     {
