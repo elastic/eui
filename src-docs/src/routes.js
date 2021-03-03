a7410f6e02c57788dc9ae7f1fa1e1ee49efd43d1
Akash Gupta <akashgp9@gmail.com>
2021-03-03 15:01:41 +0530

    added ability to navigate with keyboard arrows

--- src-docs/src/routes.js
+++ src-docs/src/routes.js
@@ -1,5 +1,7 @@
 import React, { createElement, Fragment } from 'react';
 
+import { createHashHistory } from 'history';
+
 import { GuidePage, GuideSection } from './components';
 
 import { EuiErrorBoundary } from '../../src/components';
@@ -500,6 +502,7 @@ const allRoutes = navigation.reduce((accummulatedRoutes, section) => {
 }, []);
 
 export default {
+  history: createHashHistory(), // eslint-disable-line react-hooks/rules-of-hooks
   navigation,
 
   getRouteForPath: (path) => {
