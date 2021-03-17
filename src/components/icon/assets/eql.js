5a867a168fc717702d2890c2a8743b8c087bfd33
Ryan Keairns <contactryank@gmail.com>
2021-03-17 16:51:34 -0500

    Set eql glyph height to 16

--- src/components/icon/assets/eql.js
+++ src/components/icon/assets/eql.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 const EuiIconEql = ({ title, titleId, ...props }) => (
   <svg
     width={16}
-    height={14}
+    height={16}
     viewBox="0 0 16 16"
     xmlns="http://www.w3.org/2000/svg"
     aria-labelledby={titleId}
