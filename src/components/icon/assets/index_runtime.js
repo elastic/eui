749dd07e8be23b7f27650b5dc9265c12992ee856
Ryan Keairns <contactryank@gmail.com>
2021-03-18 09:14:15 -0500

    Remove circle

--- src/components/icon/assets/index_runtime.js
+++ src/components/icon/assets/index_runtime.js
@@ -9,11 +9,8 @@ const EuiIconIndexRuntime = ({ title, titleId, ...props }) => (
     aria-labelledby={titleId}
     {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
-    <path d="M10.999 5H5V4h5.999v1zM3 4v1h1V4H3zm0 3v1h1V7H3zm0 3v1h1v-1H3z" />
-    <path
-      fillRule="evenodd"
-      d="M7.672 14H1V1h12v5.758A4.5 4.5 0 117.672 14zM2 2h10v4.256A4.492 4.492 0 0010.5 6a4.481 4.481 0 00-2.828 1H5v1h1.758a4.473 4.473 0 00-.73 2H5v1h1.027c.082.734.34 1.416.73 2H2V2zm7 11l4-2.5L9 8v5z"
-    />
+    <path d="M12 2H2v11h6v1H1V1h12v6.839l-1-.707V2z" />
+    <path d="M8 11v-1H5v1h3zm0-3.055c0-.342.082-.664.23-.945H5v1h3v-.055zM5 5h5.999V4H5v1zM3 4v1h1V4H3zm0 3v1h1V7H3zm0 3v1h1v-1H3zm11.607.2l-4.32-3.055C9.727 6.75 9 7.204 9 7.945v6.108c0 .74.726 1.196 1.287.8l4.32-3.055c.524-.37.524-1.228 0-1.598z" />
   </svg>
 );
 
