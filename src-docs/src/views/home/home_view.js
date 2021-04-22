8a400795915d799fb171d61994dc47fcf3d6a7b5
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2021-04-22 12:37:38 +0100

    [Docs] Rebrand (#4727)
    * Adding new logo and new hero illustration
    
    * Adding Figma community file for Amsterdam
    
    * Replacing favicon
    
    * Adding elastic logo and link into the footer
    
    * Adding react helmet. Favicons for dev and prod.
    
    * social media thumbnail
    
    * Social media tags
    
    * Footer elastic logo
    
    * Moving react-helmet to devDependencies
    
    * Reconciling meta tags
    
    * Adding title and description meta tags into index.html

--- src-docs/src/views/home/home_view.js
+++ src-docs/src/views/home/home_view.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import imageIcons from '../../images/icons.svg';
 import imageButtons from '../../images/buttons.svg';
 import imageTables from '../../images/tables.svg';
@@ -10,7 +10,6 @@ import imagePages from '../../images/page.svg';
 import imageText from '../../images/text.svg';
 import imageCharts from '../../images/charts.svg';
 import HomeIllustration from './home_illustration';
-
 import {
   EuiCard,
   EuiFlexGroup,
@@ -25,7 +24,7 @@ import {
   EuiPageContent,
   EuiPageContentBody,
 } from '../../../../src/components';
-import { Link } from 'react-router-dom';
+import { HomeFooterElasticLogo } from './home_footer_elastic_logo';
 
 export const HomeView = () => (
   <EuiPageContent
@@ -36,8 +35,11 @@ export const HomeView = () => (
     borderRadius="none">
     <EuiPageContentBody restrictWidth>
       <EuiPanel color="subdued" hasShadow={false} paddingSize="none">
-        <EuiFlexGroup alignItems="center" gutterSize="none">
-          <EuiFlexItem style={{ padding: 24 }}>
+        <EuiFlexGroup
+          alignItems="center"
+          gutterSize="none"
+          className="guideHome__hero">
+          <EuiFlexItem>
             <EuiTitle size="l">
               <h1>Elastic UI</h1>
             </EuiTitle>
@@ -73,7 +75,7 @@ export const HomeView = () => (
               </EuiFlexGroup>
             </EuiText>
           </EuiFlexItem>
-          <EuiFlexItem className="eui-textRight">
+          <EuiFlexItem>
             <HomeIllustration />
           </EuiFlexItem>
         </EuiFlexGroup>
@@ -202,14 +204,27 @@ export const HomeView = () => (
       </EuiFlexGroup>
 
       <EuiSpacer size="xl" />
-      <EuiText size="xs" textAlign="center" color="subdued">
-        <p>
-          EUI is licensed under{' '}
-          <EuiLink href="https://github.com/elastic/eui/blob/master/LICENSE">
-            Apache License 2.0
-          </EuiLink>
-        </p>
-      </EuiText>
+      <div>
+        <EuiText size="xs" textAlign="center" color="subdued">
+          <p>
+            EUI is licensed under{' '}
+            <EuiLink href="https://github.com/elastic/eui/blob/master/LICENSE">
+              Apache License 2.0
+            </EuiLink>{' '}
+            | Crafted with{' '}
+            <span
+              role="img"
+              aria-label="love"
+              className="guideHome__footerHeart">
+              ❤️
+            </span>{' '}
+            by{' '}
+            <EuiLink href="http://elastic.co/" external={false} target="_blank">
+              <HomeFooterElasticLogo />
+            </EuiLink>
+          </p>
+        </EuiText>
+      </div>
     </EuiPageContentBody>
   </EuiPageContent>
 );
