c01449ef2c02e9b4d5f5d76c20274cbdb7545563
majidsajadi <majidsajadi93@gmail.com>
2021-05-13 02:41:54 +0430

    refactor(services): remove lodash times

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -17,20 +17,19 @@
  * under the License.
  */
 
-import _times from 'lodash/times';
 import _memoize from 'lodash/memoize';
-
-// wrap the lodash functions to avoid having lodash's TS type definition from being
-// exported, which can conflict with the lodash namespace if other versions are used
 
 export function times(count: number): number[];
 export function times<T>(count: number, iteratee: (index: number) => T): T[];
 export function times<T>(count: number, iteratee?: (index: number) => T) {
-  if (iteratee === undefined) {
-    return _times(count);
-  }
-  return _times(count, iteratee);
+  return Array.from({ length: count }).map((_, index) =>
+    iteratee === undefined ? index : iteratee(index)
+  );
 }
+
+
+// wrap the lodash functions to avoid having lodash's TS type definition from being
+// exported, which can conflict with the lodash namespace if other versions are used
 
 export function memoize<T extends (...args: any[]) => any>(
   func: T,
