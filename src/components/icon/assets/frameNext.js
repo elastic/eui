c73f3087bae8a9e9370194f5ad8223594dcde839
miukimiu <elizabet.oliveira@elastic.co>
2021-05-19 16:10:56 +0100

    Renaming `previousFrame` and `nextFrame` to `frameNext` and `framePrevious`

--- src/components/icon/assets/frameNext.js
+++ src/components/icon/assets/frameNext.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const EuiIconPreviousFrame = ({ title, titleId, ...props }) => (
+const EuiIconFrameNext = ({ title, titleId, ...props }) => (
   <svg
     width={16}
     height={16}
@@ -11,10 +11,10 @@ const EuiIconPreviousFrame = ({ title, titleId, ...props }) => (
     {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
     <path
-      d="M13 2a1 1 0 011 1v10a1 1 0 11-2 0V3a1 1 0 011-1zm-5.146.22l-7.2 4.581a1.425 1.425 0 000 2.398l7.2 4.581c.936.595 2.146-.088 2.146-1.199V3.42c0-1.111-1.21-1.794-2.146-1.199z"
+      d="M3 2a1 1 0 00-1 1v10a1 1 0 102 0V3a1 1 0 00-1-1zm5.146.22l7.2 4.581a1.425 1.425 0 010 2.398l-7.2 4.581C7.21 14.375 6 13.692 6 12.581V3.42c0-1.112 1.21-1.795 2.146-1.2z"
       clipRule="evenodd"
     />
   </svg>
 );
 
-export const icon = EuiIconPreviousFrame;
+export const icon = EuiIconFrameNext;
