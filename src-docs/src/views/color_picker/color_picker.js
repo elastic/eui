0e2b42625b4616739a84b870db82a04a5be363e2
Greg Thompson <thompson.glowe@gmail.com>
2021-05-25 09:43:31 -0500

    clean up

--- src-docs/src/views/color_picker/color_picker.js
+++ src-docs/src/views/color_picker/color_picker.js
@@ -7,12 +7,7 @@ export default () => {
   const [color, setColor, errors] = useColorPickerState('#D36086');
   return (
     <EuiFormRow label="Pick a color" isInvalid={!!errors} error={errors}>
-      <EuiColorPicker
-        onChange={setColor}
-        color={color}
-        isInvalid={!!errors}
-        onBlur={() => console.log('blur')}
-      />
+      <EuiColorPicker onChange={setColor} color={color} isInvalid={!!errors} />
     </EuiFormRow>
   );
 };
