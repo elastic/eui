021f7f3a966035a43eb6bda3d25409e9a8bbab10
miukimiu <elizabet.oliveira@elastic.co>
2021-06-04 19:34:07 +0100

    wip

--- src-docs/src/views/range/levels.js
+++ src-docs/src/views/range/levels.js
@@ -61,7 +61,6 @@ export default () => {
           { label: '100kb', value: 100 },
         ]}
         showInput
-        compressed
         levels={levels}
         aria-label="An example of EuiDualRange with levels prop"
         aria-describedby="levelsHelp3"
