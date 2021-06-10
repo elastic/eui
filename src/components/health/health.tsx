ed5ae17718c1f596868619ff6f16e5057d69d0cf
cchaos <caroline.horn@elastic.co>
2021-06-10 15:22:44 -0400

    Using the same color set for buttons and icon buttons
    - Added `success` and `accent` color options to EuiButton
    - Starting to deprecate `secondary`

--- src/components/health/health.tsx
+++ src/components/health/health.tsx
@@ -38,7 +38,7 @@ export type EuiHealthProps = CommonProps &
   Omit<HTMLAttributes<HTMLDivElement>, 'color'> & {
     /**
      * Sets the color of the dot icon.
-     * It accepts any `IconColor`: `default`, `primary`, `secondary`, `success`, `accent`, `warning`, `danger`, `text`,
+     * It accepts any `IconColor`: `default`, `primary`, `success`, `accent`, `warning`, `danger`, `text`,
      * `subdued` or `ghost`; or any valid CSS color value as a `string`
      */
     color?: IconColor;
