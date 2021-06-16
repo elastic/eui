30ec3b14f32791c0722de4cd789e193212e33eed
Greg Thompson <thompson.glowe@gmail.com>
2021-06-16 16:05:51 -0500

    Revert "revert me"
    This reverts commit f8c0fd9b95b524934fc532ac4bcbd82bde022150.

--- src-docs/src/views/resizable_container/resizable_container_basic.js
+++ src-docs/src/views/resizable_container/resizable_container_basic.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { EuiText, EuiResizableContainer } from '../../../../src/components';
 import { fake } from 'faker';
 
@@ -10,34 +10,23 @@ const text = (
   </>
 );
 
-export default () => {
-  const dom = useRef();
-  const [styles, setStyles] = useState({ display: 'none' });
-  useEffect(() => {
-    if (!dom.current) return;
-    dom.current.innerHTML = 'YOLO';
-  });
-  useEffect(() => {
-    setTimeout(() => setStyles({ display: 'flex' }), 1000);
-  }, []);
-  return (
-    <EuiResizableContainer style={{ ...styles, height: '200px' }}>
-      {(EuiResizablePanel, EuiResizableButton) => (
-        <>
-          <EuiResizablePanel initialSize={50} minSize="30%">
-            <EuiText>
-              <div>{text}</div>
-              <a href="">Hello world</a>
-            </EuiText>
-          </EuiResizablePanel>
+export default () => (
+  <EuiResizableContainer style={{ height: '200px' }}>
+    {(EuiResizablePanel, EuiResizableButton) => (
+      <>
+        <EuiResizablePanel initialSize={50} minSize="30%">
+          <EuiText>
+            <div>{text}</div>
+            <a href="">Hello world</a>
+          </EuiText>
+        </EuiResizablePanel>
 
-          <EuiResizableButton />
+        <EuiResizableButton />
 
-          <EuiResizablePanel initialSize={50} minSize="200px">
-            <div ref={dom} />
-          </EuiResizablePanel>
-        </>
-      )}
-    </EuiResizableContainer>
-  );
-};
+        <EuiResizablePanel initialSize={50} minSize="200px">
+          <EuiText>{text}</EuiText>
+        </EuiResizablePanel>
+      </>
+    )}
+  </EuiResizableContainer>
+);
