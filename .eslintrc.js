49c213d1f132a717c157224198bfca3680a499e8
Chandler Prall <chandler.prall@gmail.com>
2021-07-07 09:11:44 -0600

    Include SSPL license

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
-const ELASTIC_2_0_LICENSE_HEADER = `
+const SSPL_ELASTIC_2_0_LICENSE_HEADER = `
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 `;
 
@@ -45,7 +46,7 @@ module.exports = {
     'local/require-license-header': [
       'warn',
       {
-        license: ELASTIC_2_0_LICENSE_HEADER,
+        license: SSPL_ELASTIC_2_0_LICENSE_HEADER,
       },
     ],
     'no-use-before-define': 'off',
