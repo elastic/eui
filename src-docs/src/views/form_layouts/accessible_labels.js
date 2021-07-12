b94f98adfebb0da4d1bbcaf5bfa73741c2d8d5ed
cchaos <caroline.horn@elastic.co>
2021-07-12 16:04:30 -0400

    A few edits

--- src-docs/src/views/form_layouts/accessible_labels.js
+++ src-docs/src/views/form_layouts/accessible_labels.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import {
   EuiButton,
   EuiCode,
-  EuiDescribedFormGroup,
   EuiForm,
   EuiFormRow,
   EuiSwitch,
@@ -13,51 +12,39 @@ export default () => {
 
   return (
     <EuiForm component="form">
-      <EuiDescribedFormGroup
-        title={<h3>Not as simple as they look</h3>}
-        description={
+      <EuiFormRow
+        label="Settings"
+        helpText={
           <>
-            Some form controls come with their own label and don&lsquo;t need
-            the one provided by <strong>EuiFormRow</strong>. Include{' '}
-            <EuiCode>hasChildLabel:false</EuiCode> on form rows that wrap:{' '}
-            <EuiCode>EuiSwitch</EuiCode>, <EuiCode>EuiButton</EuiCode>, or{' '}
-            <EuiCode>EuiLink</EuiCode>.
+            Navigate to this Switch with a screen reader. With{' '}
+            <EuiCode>hasChildLabel = false</EuiCode> the name of this is
+            &ldquo;Dark mode?&rdquo; instead of &ldquo;Settings&rdquo;.
+          </>
+        }
+        hasChildLabel={false}>
+        <EuiSwitch
+          name="switch"
+          label="Dark mode?"
+          onChange={() => {
+            setCheckedState(false);
+            setTimeout(() => {
+              setCheckedState(true);
+            }, 500);
+          }}
+          checked={checkedState}
+        />
+      </EuiFormRow>
+      <EuiFormRow
+        label="Your cluster data"
+        helpText={
+          <>
+            Navigate to this Switch with a screen reader. With{' '}
+            <EuiCode>hasChildLabel = true</EuiCode> the name of this switch is
+            &ldquo;Your cluster data&rdquo; instead of &ldquo;Download&rdquo;.
           </>
         }>
-        <EuiFormRow
-          label="Settings"
-          helpText={
-            <>
-              Navigate to this Switch with a screen reader - with{' '}
-              <EuiCode>hasChildLabel:false</EuiCode> the name of this is
-              &rdquo;Dark mode?&ldquo; instead of &rdquo;Settings&ldquo;.
-            </>
-          }
-          hasChildLabel={false}>
-          <EuiSwitch
-            name="switch"
-            label="Dark mode?"
-            onChange={() => {
-              setCheckedState(false);
-              setTimeout(() => {
-                setCheckedState(true);
-              }, 500);
-            }}
-            checked={checkedState}
-          />
-        </EuiFormRow>
-        <EuiFormRow
-          label="Your cluster data"
-          helpText={
-            <>
-              Navigate to this Switch with a screen reader - without{' '}
-              <EuiCode>hasChildLabel:false</EuiCode> the name of this switch is
-              &rdquo;Your cluster data&ldquo; instead of &rdquo;Download&ldquo;.
-            </>
-          }>
-          <EuiButton>Download</EuiButton>
-        </EuiFormRow>
-      </EuiDescribedFormGroup>
+        <EuiButton>Download</EuiButton>
+      </EuiFormRow>
     </EuiForm>
   );
 };
