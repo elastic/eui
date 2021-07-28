a2b497cafa4d03159e3052d08e6d9807371b3056
Greg Thompson <thompson.glowe@gmail.com>
2021-07-28 10:41:26 -0500

    clean up

--- src-docs/src/views/code/code_example.js
+++ src-docs/src/views/code/code_example.js
@@ -45,15 +45,12 @@ export const CodeExample = {
         <p>
           <strong>EuiCode</strong> and <strong>EuiCodeBlock</strong> are
           intended to render static lines or blocks of code in{' '}
-          <strong>read-only</strong>
-          contexts. If you need capabilities to edit, or want to print long code
-          (e.g., printing JSON from an API), we recommend installing a version
-          of Monaco. If you are building within the Kibana platform, you can use
-          their{' '}
-          <EuiLink
-            external
-            href="https://github.com/elastic/kibana/tree/master/src/plugins/kibana_react/public/code_editor">
-            <strong>CodeEditor</strong> (within Kibana)
+          <strong>read-only</strong> contexts. If you need capabilities to edit,
+          or want to print long code (e.g., printing JSON from an API), we
+          recommend installing a version of Monaco. If you are building within
+          the Kibana platform, you can use their{' '}
+          <EuiLink href="https://github.com/elastic/kibana/tree/master/src/plugins/kibana_react/public/code_editor">
+            <strong>CodeEditor</strong>
           </EuiLink>
           .
         </p>
