4bce8da4bfc7dfb78c997176730961178e1fc340
Greg Thompson <thompson.glowe@gmail.com>
2021-07-28 10:35:00 -0500

    console.warn check

--- src/services/console/warn_once.ts
+++ src/services/console/warn_once.ts
@@ -9,7 +9,7 @@
 const log: { [id: string]: boolean } = {};
 
 export const warnOnce = (id: string, message: string) => {
-  if (!log[id]) {
+  if (!log[id] && console && console.warn) {
     log[id] = true;
     console.warn(message);
   }
