5538ad51a8f5a5a49373c3334693d9e1f5f8730a
Greg Thompson <thompson.glowe@gmail.com>
2021-07-29 11:55:47 -0500

    docs

---
+++ src-docs/src/views/code/line_numbers.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { EuiCodeBlock, EuiSpacer } from '../../../../src/components';
+
+export default () => (
+  <div>
+    <EuiCodeBlock language="json" fontSize="m" paddingSize="m" lineNumbers>
+      {`{
+"id": "1",
+"rawResponse": {
+  "took": 19,
+  "timed_out": false,
+  "_shards": {
+    "total": 1,
+    "successful": 1,
+    "skipped": 0,
+    "failed": 0
+  }
+}`}
+    </EuiCodeBlock>
+
+    <EuiSpacer />
+
+    <EuiCodeBlock
+      language="json"
+      fontSize="m"
+      paddingSize="m"
+      isCopyable
+      lineNumbers={{ start: 32 }}>
+      {`"OriginLocation": [
+  {
+    "coordinates": [
+      -97.43309784,
+      37.64989853
+    ],
+    "type": "Point"
+  }
+],`}
+    </EuiCodeBlock>
+  </div>
+);
