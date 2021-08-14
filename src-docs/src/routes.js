cda2a5c3e86e485c7726f12188dd4934028578fe
cchaos <caroline.horn@elastic.co>
2021-08-14 17:13:10 -0400

    Tree view docs

--- src-docs/src/routes.js
+++ src-docs/src/routes.js
@@ -364,10 +364,10 @@ const navigation = [
       KeyPadMenuExample,
       LinkExample,
       PaginationExample,
-      TreeViewExample,
       SideNavExample,
       StepsExample,
       TabsExample,
+      TreeViewExample,
     ].map((example) => createExample(example)),
   },
   {
