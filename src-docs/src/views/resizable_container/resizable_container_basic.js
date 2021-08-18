0fb333eb9e2a6fb4f9f04c9317685213b59e35fc
Greg Thompson <thompson.glowe@gmail.com>
2021-08-18 12:36:05 -0500

    Revert "revert me"
    This reverts commit 6a99e4b0d65218af550ea69584bc5c3bf8398840.

--- src-docs/src/views/resizable_container/resizable_container_basic.js
+++ src-docs/src/views/resizable_container/resizable_container_basic.js
@@ -1,9 +1,5 @@
-import React, { useState } from 'react';
-import {
-  EuiText,
-  EuiResizableContainer,
-  EuiPagination,
-} from '../../../../src/components';
+import React from 'react';
+import { EuiText, EuiResizableContainer } from '../../../../src/components';
 import { fake } from 'faker';
 
 const text = (
@@ -14,30 +10,11 @@ const text = (
   </>
 );
 
-const Pagination = () => {
-  const [activePage, setActivePage] = useState(0);
-  const PAGE_COUNT = 22;
-
-  const goToPage = (pageNumber) => {
-    setActivePage(pageNumber);
-  };
-
-  return (
-    <EuiPagination
-      aria-label="Many pages example"
-      pageCount={PAGE_COUNT}
-      activePage={activePage}
-      onPageClick={(activePage) => goToPage(activePage)}
-    />
-  );
-};
-
 export default () => (
   <EuiResizableContainer style={{ height: '200px' }}>
     {(EuiResizablePanel, EuiResizableButton) => (
       <>
         <EuiResizablePanel initialSize={50} minSize="30%">
-          <Pagination />
           <EuiText>
             <div>{text}</div>
             <a href="">Hello world</a>
@@ -47,7 +24,6 @@ export default () => (
         <EuiResizableButton />
 
         <EuiResizablePanel initialSize={50} minSize="200px">
-          <Pagination />
           <EuiText>{text}</EuiText>
         </EuiResizablePanel>
       </>
