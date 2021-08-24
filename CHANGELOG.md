ecaaa0acda0e84e17163ac8290ef15e30112d019
Caroline Horn <549577+cchaos@users.noreply.github.com>
2021-08-24 15:48:49 -0400

    [EuiTooltip] Revert "Remove tooltip focus on mousemove (#3335)" and force visibility while element is focused (#5066)
    * Revert "Remove tooltip focus on mousemove (#3335)"
    * Applying `onFocus` and `onBlur` methods to child clone
    * Put default value for `display` in `defaultProps` so it populates in the props table
    * [EuiIconTip] Removing `children` from TS

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 ## [`master`](https://github.com/elastic/eui/tree/master)
 
-No public interface changes since `37.3.1`.
+- Updated `EuiToolTip` to remain showing tooltip while child element is in focus ([#5066](https://github.com/elastic/eui/pull/5066))
+- Removed `children` from TypeScript definition in `EuiIconTip` ([#5066](https://github.com/elastic/eui/pull/5066))
+
+**Bug fixes**
+
+- Fixed location of default value of `EuiToolTip`'s `display` prop ([#5066](https://github.com/elastic/eui/pull/5066))
 
 ## [`37.3.1`](https://github.com/elastic/eui/tree/v37.3.1)
 
--- src-docs/src/views/tool_tip/tool_tip_example.js
+++ src-docs/src/views/tool_tip/tool_tip_example.js
@@ -1,8 +1,6 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-import { renderToHtml } from '../../services';
-
 import { GuideSectionTypes } from '../../components';
 
 import {
@@ -10,20 +8,22 @@ import {
   EuiCode,
   EuiToolTip,
   EuiIconTip,
-  EuiSpacer,
-  EuiTitle,
+  EuiText,
 } from '../../../../src/components';
-import toolTipConfig from './playground';
+import { toolTipConfig, iconTipConfig } from './playground';
 
 import ToolTip from './tool_tip';
 const toolTipSource = require('!!raw-loader!./tool_tip');
-const toolTipHtml = renderToHtml(ToolTip);
 const tooltipSnippet = [
-  `<EuiToolTip position="top" content="Tooltip text">
+  `<EuiToolTip content="Tooltip text">
   <!-- An inline element to trigger the tooltip -->
 </EuiToolTip>
 `,
   `<EuiToolTip title="Tooltip title" content="Tooltip text">
+  <!-- An inline element to trigger the tooltip -->
+</EuiToolTip>
+`,
+  `<EuiToolTip content="Right side tooltip" position="right">
   <!-- An inline element to trigger the tooltip -->
 </EuiToolTip>
 `,
@@ -33,9 +33,11 @@ const tooltipSnippet = [
 `,
 ];
 
+import ToolTipComponent from './tool_tip_components';
+const toolTipComponentSource = require('!!raw-loader!./tool_tip_components');
+
 import IconTip from './icon_tip';
 const infoTipSource = require('!!raw-loader!./icon_tip');
-const infoTipHtml = renderToHtml(IconTip);
 const infoTipSnippet = `<EuiIconTip
   content="Tooltip text for the icon"
   position="top"
@@ -46,13 +48,29 @@ const infoTipSnippet = `<EuiIconTip
 export const ToolTipExample = {
   title: 'Tooltip',
   intro: (
-    <EuiCallOut title="EuiToolTip only applies to inline elements">
+    <EuiText>
       <p>
-        EuiToolTip wraps its children in a span element, so if you pass in a
-        block-level child (e.g. a div) the resulting DOM will be in violation of
-        the HTML5 spec.
+        Generally, tooltips should provide short, <strong>non-essential</strong>
+        , contextual information, usually naming or describing with more detail.
+        If you need interactive content or anything other than text, we
+        recommend using{' '}
+        <Link to="/layout/popover">
+          <strong>EuiPopover</strong>
+        </Link>{' '}
+        instead.
       </p>
-    </EuiCallOut>
+
+      <EuiCallOut
+        iconType="accessibility"
+        color="warning"
+        title={
+          <>
+            Putting anything other than plain text in a tooltip is lost on
+            screen readers.
+          </>
+        }
+      />
+    </EuiText>
   ),
   sections: [
     {
@@ -60,29 +78,11 @@ export const ToolTipExample = {
         <>
           <p>
             Wrap <strong>EuiToolTip</strong> around any item that you need a
-            tooltip for. The <EuiCode>position</EuiCode> prop will take a
-            suggested position, but will change it if the tooltip gets too close
-            to the edge of the screen.
-          </p>
-
-          <EuiTitle size="xs">
-            <h2>
-              <small>Applying tooltips to custom components</small>
-            </h2>
-          </EuiTitle>
-
-          <p>
-            Internally, <strong>EuiToolTip</strong> applies{' '}
-            <EuiCode>onFocus</EuiCode>, <EuiCode>onBlur</EuiCode>,{' '}
-            <EuiCode>onMouseOver</EuiCode>, and <EuiCode>onMouseOut</EuiCode>{' '}
-            props to whatever you pass as <EuiCode>children</EuiCode>. If you
-            pass in a custom component, then you&rsquo;ll need to make sure
-            these props are applied to the root element rendered by your
-            component. The best way to do that is to follow{' '}
-            <a href="https://github.com/elastic/eui/blob/master/wiki/component-design.md#pass-through-props">
-              EUI&rsquo;s guidelines on pass-through props
-            </a>
-            .
+            tooltip for and provide the <EuiCode>content</EuiCode> and
+            optionally the <EuiCode>title</EuiCode>. The{' '}
+            <EuiCode>position</EuiCode> prop will take a suggested position, but
+            will change it if the tooltip gets too close to the edge of the
+            screen.
           </p>
 
           <EuiCallOut
@@ -95,23 +95,6 @@ export const ToolTipExample = {
               </>
             }
           />
-
-          <EuiSpacer size="l" />
-
-          <EuiCallOut
-            iconType="accessibility"
-            color="warning"
-            title={
-              <>
-                Putting anything other than plain text in a tooltip is lost on
-                screen readers. Consider switching to{' '}
-                <Link to="/layout/popover">
-                  <strong>EuiPopover</strong>
-                </Link>{' '}
-                if you need more content inside a tooltip.
-              </>
-            }
-          />
         </>
       ),
       source: [
@@ -119,28 +102,52 @@ export const ToolTipExample = {
           type: GuideSectionTypes.JS,
           code: toolTipSource,
         },
+      ],
+      props: { EuiToolTip },
+      snippet: tooltipSnippet,
+      demo: <ToolTip />,
+      playground: toolTipConfig,
+    },
+    {
+      title: 'Wrapping components',
+      text: (
+        <>
+          <p>
+            <strong>EuiToolTip</strong> wraps its children in a{' '}
+            <EuiCode>{'<span>'}</EuiCode> element that is{' '}
+            <EuiCode language="css">{'display: inline-block'}</EuiCode>. If you
+            are wrapping a block-level child (e.g. a{' '}
+            <EuiCode>{'<div>'}</EuiCode>
+            ), you may need to change this by passing{' '}
+            <EuiCode language="tsx">{'display="block"'}</EuiCode> but the
+            resulting DOM may be in violation of the HTML5 spec.
+          </p>
+          <p>
+            It also applies <EuiCode>onFocus</EuiCode> and{' '}
+            <EuiCode>onBlur</EuiCode> props the the cloned{' '}
+            <EuiCode>children</EuiCode>. If you pass in a custom component, then
+            you&rsquo;ll need to make sure these props are applied to the root
+            element rendered by your component. The best way to do that is to
+            follow{' '}
+            <a href="https://github.com/elastic/eui/blob/master/wiki/component-design.md#pass-through-props">
+              EUI&rsquo;s guidelines on pass-through props
+            </a>
+            .
+          </p>
+        </>
+      ),
+      source: [
         {
-          type: GuideSectionTypes.HTML,
-          code: toolTipHtml,
+          type: GuideSectionTypes.JS,
+          code: toolTipComponentSource,
         },
       ],
 
       props: { EuiToolTip },
-      snippet: tooltipSnippet,
-      demo: <ToolTip />,
+      demo: <ToolTipComponent />,
     },
     {
       title: 'IconTip',
-      source: [
-        {
-          type: GuideSectionTypes.JS,
-          code: infoTipSource,
-        },
-        {
-          type: GuideSectionTypes.HTML,
-          code: infoTipHtml,
-        },
-      ],
       text: (
         <Fragment>
           <p>
@@ -157,10 +164,16 @@ export const ToolTipExample = {
           </p>
         </Fragment>
       ),
-      props: { EuiToolTip, EuiIconTip },
+      source: [
+        {
+          type: GuideSectionTypes.JS,
+          code: infoTipSource,
+        },
+      ],
+      props: { EuiIconTip },
       snippet: infoTipSnippet,
       demo: <IconTip />,
+      playground: iconTipConfig,
     },
   ],
-  playground: toolTipConfig,
 };
