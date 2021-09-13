b8e4c456bbcf2f0ef59c3da2ca5445f33caaf7c2
Greg Thompson <thompson.glowe@gmail.com>
2021-09-13 10:32:14 -0500

    docs updates

--- src-docs/src/views/code/code_block_pre.js
+++ src-docs/src/views/code/code_block_pre.js
@@ -12,7 +12,6 @@ export default () => (
       overflowHeight={300}
       whiteSpace="pre"
       isCopyable
-      lineNumbers
     >
       {`export default () => (
   <div>In this example, the whiteSpace property is set to pre. All the whitespaces will be kept as is and the text only wraps when line breaks are in the content.</div>
