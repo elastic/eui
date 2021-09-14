6ee12a16a125a3a24cff1571daee81278b5fba9c
cchaos <caroline.horn@elastic.co>
2021-09-14 16:49:17 -0400

    Fixed up scrollbar function

--- src/global_styling/mixins/_helpers.ts
+++ src/global_styling/mixins/_helpers.ts
@@ -10,9 +10,74 @@ import chroma from 'chroma-js';
 import { useEuiTheme } from '../../services/theme/hooks';
 import { transparentize } from '../../services/color';
 import { useOverflowShadow } from './_shadow';
+import { CSSProperties } from 'react';
 
 /**
- * NOTE: These were quick conversions of their Sass counterparts.
+ * Set scroll bar appearance on Chrome (and firefox).
+ * All parameters are optional and default to specific global settings.
+ */
+export const useScrollBar = ({
+  thumbColor: _thumbColor,
+  trackColor = 'transparent',
+  width = 'thin',
+  size: _size,
+  corner: _corner,
+}: {
+  thumbColor?: CSSProperties['backgroundColor'];
+  trackColor?: CSSProperties['backgroundColor'];
+  /**
+   * Defaults to `thin`. Use `auto` only for large page scrollbars
+   */
+  width?: CSSProperties['scrollbarWidth'];
+  /**
+   * Overall width (height for horizontal scrollbars)
+   */
+  size?: CSSProperties['width'];
+  /**
+   * Corner sizes are usually determined by `width` and
+   * are used as an inset border and therefore a smaller corner size means a larger thumb
+   */
+  corner?: CSSProperties['borderWidth'];
+} = {}) => {
+  const {
+    euiTheme: { colors, size },
+  } = useEuiTheme();
+
+  // Set defaults from theme
+  const thumbColor = _thumbColor || transparentize(colors.darkShade, 0.5);
+  const scrollBarSize = _size || size.base;
+  const scrollBarCorner =
+    _corner || width === 'thin' ? `calc(${size.s} * 0.75)` : size.xs;
+
+  // Firefox's scrollbar coloring cascades, but the sizing does not,
+  // so it's being added to this mixin for allowing support wherever custom scrollbars are
+  const firefoxSupport = `scrollbar-color: ${thumbColor} ${trackColor};`;
+
+  return `scrollbar-width: ${width};
+
+    &::-webkit-scrollbar {
+      width: ${scrollBarSize};
+      height: ${scrollBarSize};
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background-color: ${thumbColor};
+      background-clip: content-box;
+      border-radius: ${scrollBarSize};
+      border: ${scrollBarCorner} solid ${trackColor};
+    }
+
+    &::-webkit-scrollbar-corner,
+    &::-webkit-scrollbar-track {
+      background-color: ${trackColor};
+    }
+
+    ${firefoxSupport}
+  `;
+};
+
+/**
+ * NOTE: The ones below this comment were quick conversions of their Sass counterparts.
  *       They have yet to be used/tested.
  */
 
@@ -48,53 +113,6 @@ export const useInnerBorder = ({
       content: '';
       pointer-events: none;
       border: 1px solid ${color};
-    }
-  `;
-};
-
-// Set scroll bar appearance on Chrome (and firefox).
-export const useScrollBar = ({
-  thumbColor: _thumbColor,
-  trackBackgroundColor = 'transparent',
-  width = 'thin',
-}: {
-  thumbColor?: string;
-  trackBackgroundColor?: string;
-  width?: 'thin' | 'thick';
-} = {}) => {
-  const {
-    euiTheme: { colors, size },
-  } = useEuiTheme();
-  const thumbColor = _thumbColor || colors.darkShade;
-  return `
-    // Firefox's scrollbar coloring cascades, but the sizing does not,
-    // so it's being added to this mixin for allowing support wherever custom scrollbars are
-    // sass-lint:disable-block no-misspelled-properties
-    scrollbar-color: ${transparentize(
-      thumbColor,
-      0.5
-    )} ${trackBackgroundColor}; // Firefox support
-
-    ${width === 'thin' ? 'scrollbar-width: thin' : ''};
-
-    &::-webkit-scrollbar {
-      width: ${size.base};
-      height: ${size.base};
-    }
-
-    &::-webkit-scrollbar-thumb {
-      background-color: ${transparentize(thumbColor, 0.5)};
-      background-clip: content-box;
-      border-radius: ${size.base};
- 
-      border: ${
-        width === 'thin' ? `calc(${size.s} * 0.75)` : size.xs
-      } solid ${trackBackgroundColor};
-    }
-
-    &::-webkit-scrollbar-corner,
-    &::-webkit-scrollbar-track {
-      background-color: ${trackBackgroundColor};
     }
   `;
 };
