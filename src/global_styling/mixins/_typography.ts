6a96180b1c5409a64fa6d08c91f50deaeb12240f
cchaos <caroline.horn@elastic.co>
2021-09-14 17:22:34 -0400

    Removed `useEuiFont` in favor of a single reset on the elements that need it

--- src/global_styling/mixins/_typography.ts
+++ src/global_styling/mixins/_typography.ts
@@ -6,24 +6,8 @@
  * Side Public License, v 1.
  */
 
-import { useEuiTheme, isLegacyTheme } from '../../services/theme';
+// import { useEuiTheme, isLegacyTheme } from '../../services/theme';
 
-// Some mixins that help us deal with browser scaling of text more consistently.
+// Some functions that help us deal with browser scaling of text more consistently.
 // Essentially, fonts across eui should scale against the root html element, not
 // against parent inheritance.
-
-// Our base fonts
-
-export const useEuiFont = () => {
-  const {
-    euiTheme: { font, themeName },
-  } = useEuiTheme();
-  const legacyTheme = isLegacyTheme(themeName);
-  return `
-    font-family: ${font.family};
-    font-weight: ${font.weight.regular};
-    letter-spacing: ${legacyTheme ? '-.005em' : 'normal'};
-    text-size-adjust: 100%;
-    font-kerning: normal;
-  `;
-};
