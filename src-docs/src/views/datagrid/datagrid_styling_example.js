a82612d8ccdee94dd1d3c58077c1eabc96037579
miukimiu <elizabet.oliveira@elastic.co>
2021-09-20 18:59:14 +0100

    Better `gridStyle` font size explanation

--- src-docs/src/views/datagrid/datagrid_styling_example.js
+++ src-docs/src/views/datagrid/datagrid_styling_example.js
@@ -171,8 +171,9 @@ export const DataGridStylingExample = {
             setting in <EuiCode>toolbarVisibility</EuiCode> means the user has
             the ability to override the padding and font size passed into{' '}
             <EuiCode>gridStyle</EuiCode> by the engineer. The font size
-            overriding only works when using units relative to the parent
-            container, like an <EuiCode>em</EuiCode>.
+            overriding only works with text or elements that can inherit the
+            parent font size or elements that use units relative to the parent
+            container.
           </p>
           <EuiCodeBlock language="javascript" paddingSize="s" isCopyable>
             {gridSnippet}
