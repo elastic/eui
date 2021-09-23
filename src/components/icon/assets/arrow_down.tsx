40653ddc8c7857a3c4f0e71fafd3236aa10080d9
Constance Chen <constance.chen.3@gmail.com>
2021-09-23 11:26:18 -0700

    Fix SVG attribute error (caught by Typescript!)
    yay, it works

--- src/components/icon/assets/arrow_down.tsx
+++ src/components/icon/assets/arrow_down.tsx
@@ -29,7 +29,7 @@ const EuiIconArrowDown = ({
   >
     {title ? <title id={titleId}>{title}</title> : null}
     <path
-      fillRule="non-zero"
+      fillRule="nonzero"
       d="M13.069 5.157L8.384 9.768a.546.546 0 01-.768 0L2.93 5.158a.552.552 0 00-.771 0 .53.53 0 000 .759l4.684 4.61c.641.631 1.672.63 2.312 0l4.684-4.61a.53.53 0 000-.76.552.552 0 00-.771 0z"
     />
   </svg>
