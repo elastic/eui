f463296996ada7d0330d40ecf088348cdaaea0d7
cchaos <caroline.horn@elastic.co>
2021-10-05 19:02:26 -0400

    [EuiIcon] Supporting `ghost` and `text` colors on logos

--- src/components/icon/assets/logo_elastic.tsx
+++ src/components/icon/assets/logo_elastic.tsx
@@ -20,6 +20,7 @@ const EuiIconLogoElastic = ({
   ...props
 }: React.SVGProps<SVGSVGElement> & SVGRProps) => (
   <svg
+    data-type="logoElastic"
     xmlns="http://www.w3.org/2000/svg"
     width={32}
     height={32}
