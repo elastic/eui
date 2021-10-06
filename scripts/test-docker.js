56e1c2883a406f4d58795fd6f019a7323cd18355
Chandler Prall <chandler.prall@gmail.com>
2021-10-06 16:44:53 -0600

    Adding Cypress for browser-based component unit testing (#5042)
    * Added Cypress and added an example e2e integration test for EuiFocusTrap as well as a component test example for the same
    
    * cypress.json
    
    * Removed some default cypress files, cleaned up the related configurations
    
    * converting cypress spec to typescript but have some issues
    
    * Add documentation
    
    + update a few current testing docs to point to new Cypress doc
    
    * newline nit
    
    * bump node version, update docker image
    
    * It is nice to run the things you meant to run
    
    * set a specific OS for CI jobs
    
    * Use modern node version in the deploy docs CI job
    
    * Use the CI docker image in the build_docs job too
    
    * PR feedback
    
    Co-authored-by: Constance Chen <constance.chen.3@gmail.com>
    Co-authored-by: Constance <constancecchen@users.noreply.github.com>

--- scripts/test-docker.js
+++ scripts/test-docker.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 
-execSync('docker pull docker.elastic.co/eui/puppeteer:latest', {
+execSync('docker pull docker.elastic.co/eui/ci:1.0', {
   stdio: 'inherit',
 });
 /* eslint-disable-next-line no-multi-str */
@@ -8,10 +8,11 @@ execSync("docker run \
   -i --rm --cap-add=SYS_ADMIN --volume=$(pwd):/app --workdir=/app \
   -e GIT_COMMITTER_NAME=test -e GIT_COMMITTER_EMAIL=test -e HOME=/tmp \
   --user=$(id -u):$(id -g) \
-  docker.elastic.co/eui/puppeteer:latest \
+  docker.elastic.co/eui/ci:1.0 \
   bash -c 'npm config set spin false \
     && /opt/yarn*/bin/yarn \
-    && npm run test \
+    && yarn cypress install \
+    && NODE_OPTIONS=\"--max-old-space-size=2048\" npm run test-ci \
     && npm run start-test-server-and-a11y-test \
     && npm run build'", {
   stdio: 'inherit',
