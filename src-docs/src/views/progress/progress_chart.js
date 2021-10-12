3470254a2358db5ef7e0a8852a1c860e9ea608ce
Greg Thompson <thompson.glowe@gmail.com>
2021-10-12 12:58:56 -0500

    Revert "revert me"
    This reverts commit bf3a3630703d74726de5c13b0f7d469ca370a718.

--- src-docs/src/views/progress/progress_chart.js
+++ src-docs/src/views/progress/progress_chart.js
@@ -1,12 +1,6 @@
 import React, { Fragment } from 'react';
 
-import {
-  EuiBadge,
-  EuiPopover,
-  EuiButton,
-  EuiProgress,
-  EuiSpacer,
-} from '../../../../src/components';
+import { EuiProgress, EuiSpacer } from '../../../../src/components';
 
 const data = [
   { label: 'Basic percentage', value: '80' },
@@ -19,57 +13,36 @@ const data = [
   { label: "Women's Accessories", value: '24.0256' },
 ];
 
-export default () => {
-  const [isPopoverOpen, setIsPopoverOpen] = React.useState(false);
-
-  const onButtonClick = () =>
-    setIsPopoverOpen((isPopoverOpen) => !isPopoverOpen);
-  const closePopover = () => setIsPopoverOpen(false);
-
-  const button = (
-    <EuiButton iconType="arrowDown" iconSide="right" onClick={onButtonClick}>
-      Show popover
-    </EuiButton>
-  );
-
-  return (
-    <Fragment>
-      <EuiPopover
-        button={button}
-        isOpen={isPopoverOpen}
-        closePopover={closePopover}
-      >
-        <EuiBadge>Text</EuiBadge>
-        <div style={{ maxWidth: 160 }}>
-          {data.map((item) => (
-            <>
-              <EuiProgress
-                valueText={true}
-                max={100}
-                color="success"
-                size="s"
-                {...item}
-              />
-              <EuiSpacer size="s" />
-            </>
-          ))}
-        </div>
-        <EuiSpacer size="m" />
-        <div style={{ maxWidth: 200 }}>
-          {data.map((item) => (
-            <>
-              <EuiProgress
-                valueText={true}
-                max={100}
-                color="primary"
-                size="m"
-                {...item}
-              />
-              <EuiSpacer size="s" />
-            </>
-          ))}
-        </div>
-      </EuiPopover>
-    </Fragment>
-  );
-};
+export default () => (
+  <Fragment>
+    <div style={{ maxWidth: 160 }}>
+      {data.map((item) => (
+        <>
+          <EuiProgress
+            valueText={true}
+            max={100}
+            color="success"
+            size="s"
+            {...item}
+          />
+          <EuiSpacer size="s" />
+        </>
+      ))}
+    </div>
+    <EuiSpacer size="m" />
+    <div style={{ maxWidth: 200 }}>
+      {data.map((item) => (
+        <>
+          <EuiProgress
+            valueText={true}
+            max={100}
+            color="primary"
+            size="m"
+            {...item}
+          />
+          <EuiSpacer size="s" />
+        </>
+      ))}
+    </div>
+  </Fragment>
+);
