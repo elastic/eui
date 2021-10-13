38a872acef0042136fc98527fe04635f8e3cc97b
cchaos <caroline.horn@elastic.co>
2021-10-13 18:07:07 -0400

    Generated id for example

--- src-docs/src/views/accordion/accordion_buttonElement.tsx
+++ src-docs/src/views/accordion/accordion_buttonElement.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 
 import { EuiAccordion, EuiLink, EuiPanel } from '../../../../src/components';
+import { useGeneratedHtmlId } from '../../../../src/services';
 
-export default () => (
-  <EuiAccordion
-    id="accordion123"
-    buttonElement="div"
-    buttonContent={
-      <EuiLink
-        onClick={(e) => e.stopPropagation()}
-        href="#/layout/accordion#interactive-content-in-the-trigger"
-      >
-        This is a nested link
-      </EuiLink>
-    }
-  >
-    <EuiPanel color="subdued">
-      Any content inside of <strong>EuiAccordion</strong> will appear here.
-    </EuiPanel>
-  </EuiAccordion>
-);
+export default () => {
+  const buttonElementAccordionId = useGeneratedHtmlId({
+    prefix: 'buttonElementAccordion',
+  });
+
+  return (
+    <EuiAccordion
+      id={buttonElementAccordionId}
+      buttonElement="div"
+      buttonContent={
+        <EuiLink
+          onClick={(e) => e.stopPropagation()}
+          href="#/layout/accordion#interactive-content-in-the-trigger"
+        >
+          This is a nested link
+        </EuiLink>
+      }
+    >
+      <EuiPanel color="subdued">
+        Any content inside of <strong>EuiAccordion</strong> will appear here.
+      </EuiPanel>
+    </EuiAccordion>
+  );
+};
