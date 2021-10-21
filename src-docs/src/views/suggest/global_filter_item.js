4c73be1f8818cef1b5686d17cc34554f87042e73
1Copenut <trevor.pierce@elastic.co>
2021-10-21 15:25:13 -0500

    Updating props on EuiSuggest to pass axe check * global_filter_items.js#88 was passing a tabindex -1 to a button icon * Button already had an implicit tabindex of 0, throwing a violation * Updated comments in badge.tsx to highlight differences in tabindex 0, -1

--- src-docs/src/views/suggest/global_filter_item.js
+++ src-docs/src/views/suggest/global_filter_item.js
@@ -85,11 +85,9 @@ export const GlobalFilterItem = (props) => {
       iconSide="right"
       onClick={togglePopover}
       onClickAriaLabel="Filter actions"
-      closeButtonProps={{
-        // Removing tab focus on close button because the same option can be optained through the context menu
-        // Also, we may want to add a `DEL` keyboard press functionality
-        tabIndex: '-1',
-      }}
+      // Removing `closeButtonProps` in this case because the button has an implicit tabindex of 0.
+      // The icon inside it cannot have a declared tabindex or axe throws a violation.
+      // See components > badge > badge.tsx#255
       {...rest}
     >
       {prefix}
