e10a3c0dee37f2eacf7e58dd242a4fcc6db764e6
1Copenut <trevor.pierce@elastic.co>
2021-11-16 17:26:47 -0600

    Removing tabIndex from beta_badge for axe violation

--- src/components/badge/beta_badge/beta_badge.tsx
+++ src/components/badge/beta_badge/beta_badge.tsx
@@ -224,7 +224,7 @@ export const EuiBetaBadge: FunctionComponent<EuiBetaBadgeProps> = ({
           content={tooltipContent}
           title={title || label}
         >
-          <span tabIndex={0} className={classes} {...rest}>
+          <span className={classes} {...rest}>
             {icon || label}
           </span>
         </EuiToolTip>
