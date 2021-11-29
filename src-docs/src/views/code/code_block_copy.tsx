12666f5f042d59f65d07d8a1f0df2ba6850dcf57
Constance Chen <constance.chen.3@gmail.com>
2021-11-29 10:37:18 -0800

    [REVERT ME] Repro

--- src-docs/src/views/code/code_block_copy.tsx
+++ src-docs/src/views/code/code_block_copy.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import { EuiCodeBlock } from '../../../../src/components';
 
-const htmlCode = `<!-- I'm an example of HTML -->
-<h1>Hello world!</h1>
-<p>Lorem ipsum dolor sit amet.</p>`;
+// const htmlCode = `<!-- I'm an example of HTML -->
+// <h1>Hello world!</h1>
+// <p>Lorem ipsum dolor sit amet.</p>`;
 
 export default () => (
-  <EuiCodeBlock language="html" isCopyable>
-    {htmlCode}
+  <EuiCodeBlock isCopyable>
+    <div />
   </EuiCodeBlock>
 );
