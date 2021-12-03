a6a3680aafcc8efee134319645d5f789361ed5bf
Constance <constancecchen@users.noreply.github.com>
2021-12-03 14:25:33 -0800

    Fix broken TSX demos/codepen links (#5443)
    - we were previously only checking for .JS types and should have .TSX as well

--- src-docs/src/components/guide_section/guide_section_parts/guide_section_code.tsx
+++ src-docs/src/components/guide_section/guide_section_parts/guide_section_code.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState, useEffect } from 'react';
+import React, { FunctionComponent, useState, useEffect, useMemo } from 'react';
 import { EuiCodeBlock } from '../../../../../src/components/code';
 import { EuiButtonEmpty } from '../../../../../src/components/button';
 // @ts-ignore Not TS
@@ -16,10 +16,15 @@ export const GuideSectionExampleCode: FunctionComponent<GuideSectionExampleCode>
   code,
   type = GuideSectionTypes.JS,
 }) => {
+  const isJavascript = useMemo(
+    () => [GuideSectionTypes.JS, GuideSectionTypes.TSX].includes(type),
+    [type]
+  );
+
   const [codeToRender, setCodeToRender] = useState();
 
   useEffect(() => {
-    if (type === GuideSectionTypes.JS) {
+    if (isJavascript) {
       setCodeToRender(renderJsSourceCode(code));
     } else {
       setCodeToRender(code);
@@ -28,20 +33,19 @@ export const GuideSectionExampleCode: FunctionComponent<GuideSectionExampleCode>
     return () => {
       setCodeToRender(undefined);
     };
-  }, [code, type]);
+  }, [code, isJavascript]);
 
-  const codeSandboxLink =
-    type === GuideSectionTypes.JS ? (
-      <CodeSandboxLink
-        className="guideSectionExampleCode__link"
-        content={code.default}
-        type={type.toLowerCase()}
-      >
-        <EuiButtonEmpty size="xs" iconType="logoCodesandbox">
-          Try out this demo on Code Sandbox
-        </EuiButtonEmpty>
-      </CodeSandboxLink>
-    ) : undefined;
+  const codeSandboxLink = isJavascript ? (
+    <CodeSandboxLink
+      className="guideSectionExampleCode__link"
+      content={code.default}
+      type={type.toLowerCase()}
+    >
+      <EuiButtonEmpty size="xs" iconType="logoCodesandbox">
+        Try out this demo on Code Sandbox
+      </EuiButtonEmpty>
+    </CodeSandboxLink>
+  ) : undefined;
 
   return (
     <>
