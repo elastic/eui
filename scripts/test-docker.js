60b9b4d09f384c9983a63fea352544e68f1d4f97
Constance Chen <constance.chen.3@gmail.com>
2021-12-08 11:08:58 -0800

    Increment Docker CI image

--- scripts/test-docker.js
+++ scripts/test-docker.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 
-execSync('docker pull docker.elastic.co/eui/ci:1.0', {
+execSync('docker pull docker.elastic.co/eui/ci:2.0', {
   stdio: 'inherit',
 });
 /* eslint-disable-next-line no-multi-str */
@@ -8,7 +8,7 @@ execSync("docker run \
   -i --rm --cap-add=SYS_ADMIN --volume=$(pwd):/app --workdir=/app \
   -e GIT_COMMITTER_NAME=test -e GIT_COMMITTER_EMAIL=test -e HOME=/tmp \
   --user=$(id -u):$(id -g) \
-  docker.elastic.co/eui/ci:1.0 \
+  docker.elastic.co/eui/ci:2.0 \
   bash -c 'npm config set spin false \
     && /opt/yarn*/bin/yarn \
     && yarn cypress install \
