f39188706f0b4a127d92ffc7d7473ae155598298
Chandler Prall <chandler.prall@gmail.com>
2021-12-15 10:49:12 -0700

    [EuiDataGrid] Added focus management cypress tests for data grid (#5467)
    * Added focus management cypress tests for data grid
    
    * Updated snapshots
    
    * euiDataGridExpansionPanel -> euiDataGridExpansionPopover

--- src/components/datagrid/data_grid.spec.tsx
+++ src/components/datagrid/data_grid.spec.tsx
@@ -6,8 +6,10 @@
  * Side Public License, v 1.
  */
 
-import React from 'react';
-import { EuiDataGrid, EuiDataGridProps } from './index';
+import React, { ReactNode } from 'react';
+import { EuiDataGrid, EuiDataGridColumn, EuiDataGridProps } from './index';
+import { EuiLink } from '../link';
+import { EuiButtonEmpty } from '../button';
 
 const baseProps: EuiDataGridProps = {
   'aria-label': 'grid for testing',
@@ -79,6 +81,258 @@ describe('EuiDataGrid', () => {
             .invoke('outerHeight')
             .should('be.greaterThan', firstHeight);
         });
+    });
+  });
+
+  describe('focus management', () => {
+    const columns: EuiDataGridColumn[] = [
+      {
+        id: 'no_interactive',
+        display: '0 interactive',
+        isExpandable: false,
+        actions: false,
+      },
+      {
+        id: 'no_interactive_expandable',
+        display: '0 interactive',
+        actions: false,
+      },
+      {
+        id: 'one_interactive',
+        display: '1 interactive',
+        isExpandable: false,
+        actions: false,
+      },
+      {
+        id: 'one_interactive_expandable',
+        display: '1 interactive',
+        actions: false,
+      },
+      {
+        id: 'two_interactives',
+        display: '2 interactives',
+        isExpandable: false,
+        actions: false,
+      },
+      {
+        id: 'two_interactives_expandable',
+        display: '2 interactives',
+        actions: false,
+      },
+    ];
+    const columnVisibility = {
+      visibleColumns: columns.map(({ id }) => id),
+      setVisibleColumns: () => {},
+    };
+
+    const columnValueMap: { [key: string]: ReactNode } = {
+      no_interactive: <span>value</span>,
+      no_interactive_expandable: <span>value</span>,
+
+      one_interactive: (
+        <span>
+          <EuiLink href="#" data-test-subj="focusOnMe">
+            value
+          </EuiLink>
+        </span>
+      ),
+      one_interactive_expandable: (
+        <span>
+          <EuiLink href="#" data-test-subj="focusOnMe">
+            value
+          </EuiLink>
+        </span>
+      ),
+
+      two_interactives: (
+        <span>
+          <EuiButtonEmpty size="xs" data-test-subj="btn-yes" onClick={() => {}}>
+            Yes
+          </EuiButtonEmpty>
+          <EuiButtonEmpty
+            size="xs"
+            data-test-subj="btn-no"
+            color="danger"
+            onClick={() => {}}
+          >
+            No
+          </EuiButtonEmpty>
+        </span>
+      ),
+      two_interactives_expandable: (
+        <span>
+          <EuiButtonEmpty size="xs" data-test-subj="btn-yes" onClick={() => {}}>
+            Yes
+          </EuiButtonEmpty>
+          <EuiButtonEmpty
+            size="xs"
+            data-test-subj="btn-no"
+            color="danger"
+            onClick={() => {}}
+          >
+            No
+          </EuiButtonEmpty>
+        </span>
+      ),
+    };
+
+    const renderCellValue: EuiDataGridProps['renderCellValue'] = ({
+      columnId,
+    }) => columnValueMap[columnId];
+
+    const renderFooterCellValue = undefined;
+
+    const focusManagementBaseProps = {
+      ...baseProps,
+      columns,
+      columnVisibility,
+      renderCellValue,
+      renderFooterCellValue,
+    };
+
+    it('focuses a cell when clicked', () => {
+      cy.mount(<EuiDataGrid {...focusManagementBaseProps} />);
+
+      getGridData();
+
+      // starts with body in focus
+      cy.focused().should('not.exist');
+
+      cy.get('[data-gridcell-id="1,1"]').click();
+      cy.focused().should('have.attr', 'data-gridcell-id', '1,1');
+    });
+
+    describe('cell keyboard interactions', () => {
+      it('tabbing to the grid the controls, then the first cell, then off', () => {
+        cy.mount(
+          <>
+            <EuiDataGrid {...focusManagementBaseProps} />
+            <span tabIndex={0} id="final-tabbable" />
+          </>
+        );
+
+        getGridData();
+
+        // starts with body in focus
+        cy.focused().should('not.exist');
+
+        // tab through the control bar
+        cy.get('body')
+          .tab()
+          .should('have.attr', 'data-test-subj', 'dataGridColumnSelectorButton')
+          .tab()
+          .should(
+            'have.attr',
+            'data-test-subj',
+            'dataGridDisplaySelectorButton'
+          )
+          .tab()
+          .should('have.attr', 'data-test-subj', 'dataGridFullScreenButton');
+
+        // tab into the grid, should focus first cell after a short delay
+        cy.focused().tab();
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,0');
+
+        cy.focused().tab().should('have.id', 'final-tabbable');
+      });
+
+      it('arrow-keying focuses another cell, unless it has only one interactive element', () => {
+        cy.mount(<EuiDataGrid {...focusManagementBaseProps} />);
+
+        getGridData();
+
+        cy.get('[data-test-subj=dataGridWrapper]').focus();
+
+        // first cell is non-interactive and non-expandable = focus cell
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,0');
+
+        // already left-most, should be no-op
+        cy.focused().type('{leftarrow}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,0');
+
+        // arrow right, expandable cell with no interactive = focus cell
+        cy.focused().type('{rightarrow}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,1');
+
+        // arrow right, non-expandable cell with one interactive = focus interactive
+        cy.focused().type('{rightarrow}');
+        cy.focused().should('have.attr', 'data-test-subj', 'focusOnMe');
+
+        // arrow right, non-expandable cell with two interactives = focus cell
+        cy.focused().type('{rightarrow}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,3');
+
+        // arrow right, expandable cell with two interactives = focus cell
+        cy.focused().type('{rightarrow}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,4');
+      });
+
+      it('cell expansion/interaction', () => {
+        cy.mount(<EuiDataGrid {...focusManagementBaseProps} />);
+
+        getGridData();
+
+        cy.get('[data-test-subj=dataGridWrapper]').focus();
+
+        // first cell is non-interactive and non-expandable, enter should have no effect
+        cy.focused().type('{enter}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,0');
+
+        // second cell is expandable
+        cy.get('[data-gridcell-id="0,1"]').click();
+        cy.focused().type('{enter}');
+        cy.focused().should(
+          'have.attr',
+          'data-test-subj',
+          'euiDataGridExpansionPopover'
+        );
+        cy.focused().type('{esc}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,1');
+
+        // third cell is non-expandable & interactive, click should focus on the link
+        cy.get('[data-gridcell-id="0,2"]').click();
+        cy.focused().type('{enter}');
+        cy.focused().should('have.attr', 'data-test-subj', 'focusOnMe');
+
+        // fourth cell is non-expandable with multiple interactives, click should focus on the cell
+        cy.get('[data-gridcell-id="0,3"]').click();
+        cy.focused().type('{enter}');
+        cy.focused().should('have.attr', 'data-test-subj', 'focusOnMe'); // focus trap focuses the link
+        cy.focused().type('{esc}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,3');
+
+        // fifth cell is non-expandable & no-actions with multiple interactives, click should focus cell
+        cy.get('[data-gridcell-id="0,4"]').click('topLeft'); // top left to avoid clicking a button
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,4');
+        // enable interactives & focus trap
+        cy.focused().type('{enter}');
+        cy.focused().should('have.attr', 'data-test-subj', 'btn-yes');
+        cy.focused().tab();
+        cy.focused().should('have.attr', 'data-test-subj', 'btn-no');
+        cy.focused().tab();
+        cy.focused().should('have.attr', 'data-test-subj', 'btn-yes');
+        cy.focused().type('{esc}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,4');
+
+        // sixth cell is expandable cell with two interactives, click should focus on the cell
+        cy.get('[data-gridcell-id="0,5"]').click('topLeft', { force: true }); // top left to avoid clicking a button
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,5');
+        cy.focused().type('{enter}'); // trigger expansion popover
+        cy.focused().should('have.attr', 'data-test-subj', 'btn-yes'); // focus trap should move focus to the first button
+        cy.focused().parentsUntil(
+          '[data-test-subj="euiDataGridExpansionPopover"]'
+        ); // ensure focus is in the popover
+        cy.focused().tab();
+        cy.focused().should('have.attr', 'data-test-subj', 'btn-no');
+        cy.focused().tab();
+        cy.focused().should(
+          'have.attr',
+          'data-test-subj',
+          'euiDataGridExpansionPopover'
+        );
+        cy.focused().type('{esc}');
+        cy.focused().should('have.attr', 'data-gridcell-id', '0,5');
+      });
     });
   });
 });
