b51a614d3ddc6be161a1958758d1ca7e1bea858d
Constance Chen <constance.chen.3@gmail.com>
2021-12-21 10:18:20 -0800

    Add documentation + example + props

--- src/components/datagrid/data_grid_types.ts
+++ src/components/datagrid/data_grid_types.ts
@@ -284,6 +284,13 @@ export type EuiDataGridProps = OneOf<
 >;
 
 export interface EuiDataGridRefProps {
+  /**
+   * Allows manually focusing the specified cell in the grid.
+   *
+   * Using this method is an accessibility requirement if your EuiDataGrid
+   * toggles a modal or flyout - focus must be restored to the grid on close
+   * to prevent keyboard or screen reader users from being stranded.
+   */
   setFocusedCell({ rowIndex, colIndex }: EuiDataGridCellLocation): void;
 }
 
