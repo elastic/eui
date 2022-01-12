5b49b4f351e6df4f12e0802370ff3d18e6b6450a
Bree Hall <briannajdhall@gmail.com>
2022-01-12 15:45:28 -0500

    Had to remove changes to the fiter_group_multi that weren't supposed to be in this branch

--- src-docs/src/views/filter_group/filter_group_multi.js
+++ src-docs/src/views/filter_group/filter_group_multi.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 
 import {
   EuiPopover,
+  EuiPopoverTitle,
+  EuiFieldSearch,
+  EuiFilterSelectItem,
+  EuiLoadingChart,
+  EuiSpacer,
+  EuiIcon,
   EuiFilterGroup,
   EuiFilterButton,
-  EuiSelectable,
 } from '../../../../src/components';
 import { useGeneratedHtmlId } from '../../../../src/services';
 
@@ -24,26 +29,49 @@ export default () => {
   });
 
   const [items, setItems] = useState([
-    { label: 'Johann Sebastian Bach', checked: 'on' },
-    { label: 'Wolfgang Amadeus Mozart', checked: 'on' },
-    { label: 'Antonín Dvořák', checked: 'off' },
-    { label: 'Dmitri Shostakovich' },
-    { label: 'Felix Mendelssohn-Bartholdy' },
-    { label: 'Franz Liszt' },
-    { label: 'Franz Schubert' },
-    { label: 'Frédéric Chopin' },
-    { label: 'Georg Friedrich Händel' },
-    { label: 'Giuseppe Verdi' },
-    { label: 'Gustav Mahler' },
-    { label: 'Igor Stravinsky' },
-    { label: 'Johannes Brahms' },
-    { label: 'Joseph Haydn' },
-    { label: 'Ludwig van Beethoven' },
-    { label: 'Piotr Illitch Tchaïkovsky' },
-    { label: 'Robert Schumann' },
-    { label: 'Sergej S. Prokofiew' },
-    { label: 'Wolfgang Amadeus Mozart' },
+    { name: 'Johann Sebastian Bach', checked: 'on' },
+    { name: 'Wolfgang Amadeus Mozart', checked: 'on' },
+    { name: 'Antonín Dvořák', checked: 'off' },
+    { name: 'Dmitri Shostakovich' },
+    { name: 'Felix Mendelssohn-Bartholdy' },
+    { name: 'Franz Liszt' },
+    { name: 'Franz Schubert' },
+    { name: 'Frédéric Chopin' },
+    { name: 'Georg Friedrich Händel' },
+    { name: 'Giuseppe Verdi' },
+    { name: 'Gustav Mahler' },
+    { name: 'Igor Stravinsky' },
+    { name: 'Johannes Brahms' },
+    { name: 'Joseph Haydn' },
+    { name: 'Ludwig van Beethoven' },
+    { name: 'Piotr Illitch Tchaïkovsky' },
+    { name: 'Robert Schumann' },
+    { name: 'Sergej S. Prokofiew' },
+    { name: 'Wolfgang Amadeus Mozart' },
   ]);
+
+  function updateItem(index) {
+    if (!items[index]) {
+      return;
+    }
+
+    const newItems = [...items];
+
+    switch (newItems[index].checked) {
+      case 'on':
+        newItems[index].checked = 'off';
+        break;
+
+      case 'off':
+        newItems[index].checked = undefined;
+        break;
+
+      default:
+        newItems[index].checked = 'on';
+    }
+
+    setItems(newItems);
+  }
 
   const button = (
     <EuiFilterButton
@@ -67,20 +95,40 @@ export default () => {
         closePopover={closePopover}
         panelPaddingSize="none"
       >
-        <EuiSelectable
-          aria-label="Filter group multiselect example"
-          searchable
-          options={items}
-          onChange={(newItems) => setItems(newItems)}
-          allowExclusions
-        >
-          {(items, search) => (
-            <>
-              {search}
-              {items}
-            </>
-          )}
-        </EuiSelectable>
+        <EuiPopoverTitle paddingSize="s">
+          <EuiFieldSearch compressed />
+        </EuiPopoverTitle>
+        <div className="euiFilterSelect__items">
+          {items.map((item, index) => (
+            <EuiFilterSelectItem
+              checked={item.checked}
+              key={index}
+              onClick={() => updateItem(index)}
+            >
+              {item.name}
+            </EuiFilterSelectItem>
+          ))}
+          {/*
+                Use when loading items initially
+              */}
+          <div className="euiFilterSelect__note">
+            <div className="euiFilterSelect__noteContent">
+              <EuiLoadingChart size="m" />
+              <EuiSpacer size="xs" />
+              <p>Loading filters</p>
+            </div>
+          </div>
+          {/*
+                Use when no results are returned
+              */}
+          <div className="euiFilterSelect__note">
+            <div className="euiFilterSelect__noteContent">
+              <EuiIcon type="minusInCircle" />
+              <EuiSpacer size="xs" />
+              <p>No filters found</p>
+            </div>
+          </div>
+        </div>
       </EuiPopover>
     </EuiFilterGroup>
   );
