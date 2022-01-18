2ebeee1e903f46fb951672bef11015fe840ee94f
Constance Chen <constance.chen.3@gmail.com>
2022-01-18 13:54:03 -0800

    [enhancement] Account for openCellPopover being called on cells out of view
    + write bonus Cypress test for useScroll's focus effect now that we have access to the imperative ref

--- src/components/datagrid/utils/scrolling.ts
+++ src/components/datagrid/utils/scrolling.ts
@@ -8,6 +8,8 @@
 
 import { useContext, useEffect, useCallback, MutableRefObject } from 'react';
 import { VariableSizeGrid as Grid } from 'react-window';
+
+import { DataGridCellPopoverContext } from '../body/data_grid_cell_popover';
 import { DataGridFocusContext } from './focus';
 
 interface ScrollCellIntoView {
@@ -42,6 +44,18 @@ export const useScroll = (args: Dependencies) => {
       });
     }
   }, [focusedCell, scrollCellIntoView]);
+
+  const { popoverIsOpen, openCellLocation } = useContext(
+    DataGridCellPopoverContext
+  );
+  useEffect(() => {
+    if (popoverIsOpen) {
+      scrollCellIntoView({
+        rowIndex: openCellLocation.rowIndex,
+        colIndex: openCellLocation.colIndex,
+      });
+    }
+  }, [popoverIsOpen, openCellLocation, scrollCellIntoView]);
 
   return { scrollCellIntoView };
 };
