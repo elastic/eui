490670c8cf604b7220ab6387bd8404da7deb920f
1Copenut <trevor.pierce@elastic.co>
2022-01-28 12:40:29 -0600

    Updating TS comments for hasInteractiveChildren prop, for clarity.

--- src/components/drag_and_drop/draggable.tsx
+++ src/components/drag_and_drop/draggable.tsx
@@ -41,8 +41,8 @@ export interface EuiDraggableProps
    */
   customDragHandle?: boolean;
   /**
-   * Whether the container is draggable and should have `role="group"` instead of `"button"`.
-   * Setting this flag ensures your drag & drop container is keyboard & screen reader accessible.
+   * Whether the container has interactive children and should have `role="group"` instead of `"button"`.
+   * Setting this flag ensures your drag & drop container is keyboard and screen reader accessible.
    */
   hasInteractiveChildren?: boolean;
   /**
