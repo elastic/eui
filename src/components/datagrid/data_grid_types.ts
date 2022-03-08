22aeee28d52fbdc6ad2bca98e956c97f846dac5c
Greg Thompson <thompson.glowe@gmail.com>
2022-03-08 17:45:06 -0600

    make SharedRenderCellElementProps.schema optional

--- src/components/datagrid/data_grid_types.ts
+++ src/components/datagrid/data_grid_types.ts
@@ -407,7 +407,7 @@ interface SharedRenderCellElementProps {
   /**
    * The schema type of the column being rendered
    */
-  schema: string | undefined | null;
+  schema?: string | null;
 }
 
 export type EuiDataGridSetCellProps = CommonProps &
