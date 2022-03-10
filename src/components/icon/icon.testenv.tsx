4c171b0a968be0dc73ecd251b9704ac2f4b15ac2
Yulia Cech <yulia.cech@elastic.co>
2022-03-10 18:24:38 +0100

    [A11y] Update icon `testenv` file to render `aria-label` as text

--- src/components/icon/icon.testenv.tsx
+++ src/components/icon/icon.testenv.tsx
@@ -8,14 +8,18 @@
 
 import React, { ComponentType } from 'react';
 
-// aria-label is not allowed on a span element, so it's removed from props
 export const EuiIcon = ({ type, 'aria-label': ariaLabel, ...rest }: any) => (
   <span
     data-euiicon-type={
       typeof type === 'string' ? type : type.displayName || type.name
     }
     {...rest}
-  />
+  >
+    {
+      // render aria-label as text, if it's passed in props
+      ariaLabel ?? ''
+    }
+  </span>
 );
 
 export const appendIconComponentCache = (_: {
