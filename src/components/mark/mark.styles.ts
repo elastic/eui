e7b3917dec4301dda0518530c3cc2828efe128fb
Greg Thompson <thompson.glowe@gmail.com>
2022-03-15 11:24:56 -0500

    [CSS-in-JS] Convert `EuiMark` (#4575)
    * placeholder docs
    
    * CL TODO
    
    * converting euimark styles to emotion
    
    * import location
    
    * move CL
    
    * rename style file; style factory pattern
    
    * use simple return
    
    * update generated classname pattern
    
    * update snapshots
    
    * better class name generation
    
    * cl
    
    * Update src/components/mark/mark.styles.ts
    
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * clean up
    
    * better wiki
    
    * more wiki
    
    * Update wiki/creating-components-manually.md
    
    Co-authored-by: Constance <constancecchen@users.noreply.github.com>
    
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>
    Co-authored-by: Constance <constancecchen@users.noreply.github.com>

---
+++ src/components/mark/mark.styles.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { css } from '@emotion/react';
+import { UseEuiTheme, transparentize } from '../../services';
+
+export const euiMarkStyles = ({ euiTheme, colorMode }: UseEuiTheme) => {
+  // TODO: Was $euiFocusBackgroundColor
+  const transparency = { LIGHT: 0.1, DARK: 0.3 };
+
+  return css`
+    background-color: ${transparentize(
+      euiTheme.colors.primary,
+      transparency[colorMode]
+    )};
+    font-weight: ${euiTheme.font.weight.bold};
+    // Override the browser's black color.
+    // Can't use 'inherit' because the text to background color contrast may not be sufficient
+    color: ${euiTheme.colors.text};
+  `;
+};
