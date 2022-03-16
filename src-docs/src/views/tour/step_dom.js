6fc9d2d89a7a42578e255de45381815ae8ba3416
Greg Thompson <thompson.glowe@gmail.com>
2022-03-16 11:45:42 -0500

    second example

--- src-docs/src/views/tour/step_dom.js
+++ src-docs/src/views/tour/step_dom.js
@@ -9,7 +9,8 @@ import {
 } from '../../../../src/components';
 
 export default () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpenRef, setIsOpenRef] = useState(true);
+  const [isOpenSelector, setIsOpenSelector] = useState(true);
   const anchorRef = useRef();
   return (
     <div>
@@ -18,23 +19,49 @@ export default () => {
         content={
           <EuiText>
             <p>
-              Popover is attached to the <EuiCode>#anchor</EuiCode> button
+              Popover is attached to the <EuiCode>anchorRef</EuiCode> button
             </p>
           </EuiText>
         }
-        isStepOpen={isOpen}
+        isStepOpen={isOpenRef}
         minWidth={300}
-        onFinish={() => setIsOpen(false)}
+        onFinish={() => setIsOpenRef(false)}
+        step={1}
+        stepsTotal={1}
+        title="React ref as anchor location"
+        anchorPosition="rightDown"
+      />
+      <EuiButtonIcon
+        onClick={() => setIsOpenRef(!isOpenRef)}
+        iconType="globe"
+        aria-label="Anchor"
+        buttonRef={anchorRef}
+      />
+
+      <EuiSpacer size="xxl" />
+      <EuiSpacer size="xxl" />
+
+      <EuiTourStep
+        anchor="#anchorTarget"
+        content={
+          <EuiText>
+            <p>
+              Popover is attached to the <EuiCode>#anchorTarget</EuiCode> button
+            </p>
+          </EuiText>
+        }
+        isStepOpen={isOpenSelector}
+        minWidth={300}
+        onFinish={() => setIsOpenSelector(false)}
         step={1}
         stepsTotal={1}
         title="DOM selector as anchor location"
         anchorPosition="rightUp"
       />
       <EuiButtonIcon
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => setIsOpenSelector(!isOpenSelector)}
         iconType="globe"
         aria-label="Anchor"
-        buttonRef={anchorRef}
         id="anchorTarget"
       />
       <EuiSpacer size="xxl" />
