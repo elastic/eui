c3fd70e17905a7954f7a35b89fcf541ea6369dd1
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2022-03-21 16:34:37 +0000

    Updated `textSize` in markdown editor example

--- src-docs/src/views/timeline/timeline_item_event.tsx
+++ src-docs/src/views/timeline/timeline_item_event.tsx
@@ -81,6 +81,7 @@ export default () => (
           }
           onChange={() => {}}
           initialViewMode="viewing"
+          markdownFormatProps={{ textSize: 's' }}
         />
       }
     />
