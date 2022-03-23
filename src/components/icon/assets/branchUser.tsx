924437e28b1e617ef7c51272dca98f5b2080fedb
Paulo Henrique <paulo.scape@gmail.com>
2022-03-23 11:40:47 -0300

    icons improvements

--- src/components/icon/assets/branchUser.tsx
+++ src/components/icon/assets/branchUser.tsx
@@ -9,7 +9,6 @@
 // THIS IS A GENERATED FILE. DO NOT MODIFY MANUALLY. @see scripts/compile-icons.js
 
 import * as React from 'react';
-import { htmlIdGenerator } from '../../../services';
 interface SVGRProps {
   title?: string;
   titleId?: string;
@@ -19,44 +18,23 @@ const EuiIconBranchUser = ({
   title,
   titleId,
   ...props
-}: React.SVGProps<SVGSVGElement> & SVGRProps) => {
-  const generateId = htmlIdGenerator('branchUser');
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={16}
-      height={16}
-      fill="none"
-      viewBox="0 0 16 16"
-      aria-labelledby={titleId}
-      {...props}
-    >
-      {title ? <title id={titleId}>{title}</title> : null}
-      <g clipPath={`url(#${generateId('clip0_1210_934')})`}>
-        <path d="M3.188 8.482a3.053 3.053 0 012.187-.92h.875c1.077 0 1.972-.351 2.154-1.376a1.75 1.75 0 11.883.015C9.09 7.704 7.806 8.438 6.25 8.438h-.875c-1.084 0-1.985.789-2.157 2.25a1.75 1.75 0 11-.905-.008V4.445a1.75 1.75 0 11.874 0v4.037zM2.75 3.625a.875.875 0 100-1.75.875.875 0 000 1.75zm0 9.625a.875.875 0 100-1.75.875.875 0 000 1.75zm6.125-7.875a.875.875 0 100-1.75.875.875 0 000 1.75z" />
-      </g>
-      <g clipPath={`url(#${generateId('clip1_1210_934')})`}>
-        <circle cx={11.938} cy={10.406} r={1.531} />
-        <g clipPath={`url(#${generateId('clip2_1210_934')})`}>
-          <path
-            d="M15 15H8.875c.29-1.248 1.55-2.188 3.063-2.188 1.511 0 2.772.94 3.062 2.188zm-3.058 3.281h-.009.009z"
-            clipRule="evenodd"
-          />
-        </g>
-      </g>
-      <defs>
-        <clipPath id={generateId('clip0_1210_934')}>
-          <path d="M0 0h9.625v13.125H0z" transform="translate(1 1)" />
-        </clipPath>
-        <clipPath id={generateId('clip1_1210_934')}>
-          <path d="M0 0h6.125v6.125H0z" transform="translate(8.875 8.875)" />
-        </clipPath>
-        <clipPath id={generateId('clip2_1210_934')}>
-          <path d="M0 0h6.125v2.625H0z" transform="translate(8.875 12.375)" />
-        </clipPath>
-      </defs>
-    </svg>
-  );
-};
+}: React.SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    viewBox="0 0 16 16"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path
+      fillRule="evenodd"
+      d="M6 7.987a3.49 3.49 0 00-2.5 1.05v-4.1a2 2 0 10-1 0v6.126a2 2 0 101.034.01A2.5 2.5 0 016 8.986h1a3.5 3.5 0 003.47-3.043 2 2 0 10-1.009-.017A2.5 2.5 0 017 7.987H6zM4 3a1 1 0 11-2 0 1 1 0 012 0zm0 10a1 1 0 11-2 0 1 1 0 012 0zm7-9a1 1 0 11-2 0 1 1 0 012 0z"
+      clipRule="evenodd"
+    />
+    <path d="M13.5 10.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM9 15c.284-1.223 1.519-2.143 3-2.143s2.716.92 3 2.143H9z" />
+  </svg>
+);
 
 export const icon = EuiIconBranchUser;
