ab78a2a14fcb6ce3d531f5b80c9dee6aa125f299
cchaos <caroline.horn@elastic.co>
2022-03-29 15:57:09 -0400

    `fullscreen`

--- src-docs/src/views/page/page_template_example.js
+++ src-docs/src/views/page/page_template_example.js
@@ -94,8 +94,8 @@ export const PageTemplateExample = {
       >
         <p>
           You&apos;ll find the code for each in their own tab and if you go to
-          full screen, you can see how they would behave in a typical
-          application layout.
+          fullscreen, you can see how they would behave in a typical application
+          layout.
         </p>
       </EuiCallOut>
       <EuiSpacer />
