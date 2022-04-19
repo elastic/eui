173bb89b9e62988a0a02dac7fb4c52e552bb2e01
Dave Snider <dsnider@elastic.co>
2022-04-19 10:38:22 -0400

    Converts spacer to Emotion

---
+++ src/components/spacer/spacer.styles.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { css } from '@emotion/react';
+import { UseEuiTheme } from '../../services';
+
+export const euiSpacerStyles = ({ euiTheme }: UseEuiTheme) => ({
+  // base
+  euiSpacer: css`
+    flex-shrink: 0; // don't ever let this shrink in height if direct descendent of flex;
+  `,
+  // variants
+  xs: css`
+    height: ${euiTheme.size.xs};
+  `,
+  s: css`
+    height: ${euiTheme.size.s};
+  `,
+  m: css`
+    height: ${euiTheme.size.m};
+  `,
+  l: css`
+    height: ${euiTheme.size.l};
+  `,
+  xl: css`
+    height: ${euiTheme.size.xl};
+  `,
+  xxl: css`
+    height: ${euiTheme.size.xxl};
+  `,
+});
