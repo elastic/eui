f6d4e108de38679bad7f12268da52ed416410551
cchaos <caroline.horn@elastic.co>
2022-04-19 23:04:36 -0400

    [Emotion] Converted EuiLoadingContent

---
+++ src/components/loading/loading_content.styles.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { css, keyframes } from '@emotion/react';
+import { COLOR_MODES_STANDARD, shade, tint, UseEuiTheme } from '../../services';
+
+const loadingContentGradient = keyframes`
+  0% {
+    transform: translateX(-53%);
+  }
+
+  100% {
+    transform: translateX(0);
+  }
+`;
+
+export const euiLoadingContentStyles = ({
+  euiTheme,
+  colorMode,
+}: UseEuiTheme) => {
+  const gradientStartStop =
+    colorMode === COLOR_MODES_STANDARD.dark
+      ? shade(euiTheme.colors.lightShade, 0.12)
+      : tint(euiTheme.colors.lightShade, 0.65);
+  const gradientMiddle =
+    colorMode === COLOR_MODES_STANDARD.dark
+      ? shade(euiTheme.colors.lightShade, 0.24)
+      : tint(euiTheme.colors.lightShade, 0.8);
+
+  return {
+    euiLoadingContent: css``,
+
+    euiLoadingContent__singleLine: css`
+      display: block;
+      width: 100%;
+      height: ${euiTheme.size.base};
+      border-radius: ${euiTheme.border.radius.medium};
+      overflow: hidden;
+
+      &:not(:last-child) {
+        margin-block-end: ${euiTheme.size.s};
+      }
+
+      &:last-child:not(:only-child) {
+        width: 75%;
+      }
+
+      &::after {
+        content: '';
+        display: block;
+        width: 220%;
+        height: 100%;
+        background: red;
+        background: linear-gradient(
+          137deg,
+          ${gradientStartStop} 45%,
+          ${gradientMiddle} 50%,
+          ${gradientStartStop} 55%
+        );
+
+        // TODO: WRAP IN EuiCanAnimate once fix is merged
+        animation: ${loadingContentGradient} 1.5s
+          ${euiTheme.animation.resistance} infinite;
+      }
+    `,
+  };
+};
--- src/components/loading/loading_content.tsx
+++ src/components/loading/loading_content.tsx
@@ -9,6 +9,8 @@
 import React, { FunctionComponent, HTMLAttributes } from 'react';
 import classNames from 'classnames';
 import { CommonProps } from '../common';
+import { useEuiTheme } from '../../services';
+import { euiLoadingContentStyles } from './loading_content.styles';
 
 export type LineRange = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
@@ -22,19 +24,20 @@ export const EuiLoadingContent: FunctionComponent<EuiLoadingContentProps> = ({
   className,
   ...rest
 }) => {
+  const euiTheme = useEuiTheme();
+  const styles = euiLoadingContentStyles(euiTheme);
+  const cssStyles = [styles.euiLoadingContent];
+  const lineCssStyles = [styles.euiLoadingContent__singleLine];
+
   const classes = classNames('euiLoadingContent', className);
   const lineElements = [];
 
   for (let i = 0; i < lines; i++) {
-    lineElements.push(
-      <span key={i} className="euiLoadingContent__singleLine">
-        <span className="euiLoadingContent__singleLineBackground" />
-      </span>
-    );
+    lineElements.push(<span key={i} css={lineCssStyles} />);
   }
 
   return (
-    <span className={classes} {...rest}>
+    <span className={classes} css={cssStyles} {...rest}>
       {lineElements}
     </span>
   );
