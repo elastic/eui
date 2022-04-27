a66b23c01861eb3400fd1ab1ea15de9b5fb65e58
Constance <constancecchen@users.noreply.github.com>
2022-04-27 13:57:58 -0700

    [Emotion] Convert EuiTitle (#5842)
    * Set up basic CSS-in-JS styles
    
    - not yet touching the mixin
    
    * DRY out shared `euiTextBreakWord` and `color` used in Sass mixin
    
    * Create JS euiTitle() mixin and add sizes styling
    
    - and per-theme titles settings - just amsterdam for now, but we can possibly expand this if we add more themes in the future
    
    + delete old global_styling title files in favor of component-specific logic
    
    * Add hook version + unit tests for euiTitle() mixin
    
    * Remove Sass files and className maps
    
    * Update EuiTitle tests/snapshots
    
    + fix test failure due to emotion sometimes generating a random 6 character className
    
    * Update downstream snapshots consuming EuiTitle
    
    * Add changelog entry
    
    * [PR feedback] Move `euiTextBreakWord` out of variables and to mixins
    
    * [PR feedback] add `euiTheme.font.title.weight` key
    
    * [PR feedback] Remove letterSpacing
    
    * [PR feedback] Simplify title font sizes & line heights
    
    * [PR feedback] Move `color` back into mixin
    
    * Update snapshots
    
    * [PR feedback] Copy
    
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * [PR feedback] Standardize EuiStep title font weight
    
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>

---
+++ src/components/title/title.styles.test.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { testCustomHook } from '../../test/internal';
+
+import { TITLE_SIZES } from './title';
+import { useEuiTitle } from './title.styles';
+
+describe('euiTitle mixin', () => {
+  describe('returns a static object of title font properties for each title size', () => {
+    TITLE_SIZES.forEach((size) => {
+      it(size, () => {
+        expect(
+          testCustomHook(() => useEuiTitle(size)).return
+        ).toMatchSnapshot();
+      });
+    });
+  });
+});
