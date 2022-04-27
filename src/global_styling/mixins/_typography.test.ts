c966bca2ea24b4c9a07405c0512899417f6c7a15
Constance Chen <constance.chen.3@gmail.com>
2022-04-27 10:29:24 -0700

    Add unit tests for euiFontSize mixin

---
+++ src/global_styling/mixins/_typography.test.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { testCustomHook } from '../../test/internal';
+
+import { EuiThemeFontScales } from '../variables/typography';
+import { useEuiFontSize } from './_typography';
+
+describe('euiFontSize', () => {
+  describe('returns an object of font-size and line-height for each scale', () => {
+    describe('rem', () => {
+      EuiThemeFontScales.forEach((size) => {
+        test(size, () => {
+          expect(
+            testCustomHook(() => useEuiFontSize(size)).return
+          ).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('em', () => {
+      EuiThemeFontScales.forEach((size) => {
+        test(size, () => {
+          expect(
+            testCustomHook(() => useEuiFontSize(size, 'em')).return
+          ).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('px', () => {
+      EuiThemeFontScales.forEach((size) => {
+        test(size, () => {
+          expect(
+            testCustomHook(() => useEuiFontSize(size, 'px')).return
+          ).toMatchSnapshot();
+        });
+      });
+    });
+  });
+});
