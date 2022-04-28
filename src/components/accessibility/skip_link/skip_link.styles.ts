466050813b4f8426523f15cea6548dcd1eceac19
Greg Thompson <thompson.glowe@gmail.com>
2022-04-28 14:39:54 -0500

    [CSS-in-JS] Convert `EuiSkipLink` (#5851)
    * convert euiskiplink to emotion styling
    
    * enforce non-null levels values
    
    * CL
    
    * Update src/components/accessibility/skip_link/skip_link.styles.ts
    
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>
    
    * snapshot
    
    Co-authored-by: Caroline Horn <549577+cchaos@users.noreply.github.com>

---
+++ src/components/accessibility/skip_link/skip_link.styles.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { css } from '@emotion/react';
+import { UseEuiTheme } from '../../../services';
+
+export const euiSkipLinkStyles = ({ euiTheme }: UseEuiTheme) => {
+  return {
+    euiSkipLink: css`
+      transition: none !important;
+
+      &:focus {
+        animation: none !important;
+      }
+    `,
+    // Positions
+    // Set positions on focus only as to not override screenReaderOnly position
+    // When positioned absolutely, consumers still need to tell it WHERE (top,left,etc...)
+    absolute: css`
+      &:focus {
+        position: absolute;
+      }
+    `,
+    fixed: css`
+      &:focus {
+        position: fixed;
+        inset-block-start: ${euiTheme.size.xs};
+        inset-inline-start: ${euiTheme.size.xs};
+        z-index: ${Number(euiTheme.levels.header) + 1};
+      }
+    `,
+  };
+};
