ac5c03dfee8c2f43ac2f371aff8d4eb67f2e2af7
cchaos <caroline.horn@elastic.co>
2022-04-28 15:42:14 -0400

    Added tests for `background-color` util

---
+++ src/global_styling/mixins/_color.test.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { testCustomHook } from '../../test/internal';
+import { BACKGROUND_COLORS, useEuiBackgroundColor } from './_color';
+
+describe('useEuiBackgroundColor mixin returns a static background-color property', () => {
+  describe('for each color', () => {
+    BACKGROUND_COLORS.forEach((color) => {
+      it(color, () => {
+        expect(
+          testCustomHook(() => useEuiBackgroundColor(color)).return
+        ).toMatchSnapshot();
+      });
+    });
+  });
+});
