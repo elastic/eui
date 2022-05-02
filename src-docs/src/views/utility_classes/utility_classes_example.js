ec27f55b2b477ab13495b5ebcdcf3016507d8524
Constance Chen <constance.chen.3@gmail.com>
2022-05-02 10:24:01 -0700

    [PR feedback] Add callouts to CSS utilities docs page

--- src-docs/src/views/utility_classes/utility_classes_example.js
+++ src-docs/src/views/utility_classes/utility_classes_example.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { EuiSpacer, EuiText, EuiTitle } from '../../../../src/components';
-import { EuiHorizontalRule } from '../../../../src/components/horizontal_rule';
+import {
+  EuiSpacer,
+  EuiText,
+  EuiCallOut,
+  EuiHorizontalRule,
+} from '../../../../src/components';
 import UtilityClassesDisplay from './utility_classes';
 import UtilityClassesVertAlign from './utility_classes_vert_align';
 import UtilityClassesResponsive from './utility_classes_responsive';
@@ -10,12 +14,24 @@ import UtilityClassesResponsive from './utility_classes_responsive';
 export const UtilityClassesExample = {
   title: 'CSS utility classes',
   intro: (
-    <EuiText grow={false}>
-      <p>
-        The following CSS-only classes are provided as helper utilities. They
-        are useful for making micro-adjustments to existing React components.
-      </p>
-    </EuiText>
+    <>
+      <EuiText grow={false}>
+        <p>
+          The following CSS-only classes are provided as helper utilities. They
+          are useful for making micro-adjustments to existing React components.
+        </p>
+      </EuiText>
+      <EuiSpacer size="m" />
+      <EuiCallOut title="Scroll utilities have moved" iconType="symlink">
+        For overflow and scrolling specific utilities, go to the{' '}
+        <Link to="/utilities/scroll">Scroll documentation page</Link>.
+      </EuiCallOut>
+      <EuiSpacer size="m" />
+      <EuiCallOut title="Text utilities have moved" iconType="symlink">
+        For text and typography specific utilities, go to the{' '}
+        <Link to="/utilities/text">Text documentation page</Link>.
+      </EuiCallOut>
+    </>
   ),
   sections: [
     {
@@ -49,30 +65,6 @@ export const UtilityClassesExample = {
         <>
           <EuiSpacer />
           <UtilityClassesResponsive />
-        </>
-      ),
-    },
-    {
-      // We avoid using the `title` key here to prevent these sections from showing up as sidebar links
-      text: (
-        <>
-          <EuiHorizontalRule />
-          <EuiTitle>
-            <h2>Scroll</h2>
-          </EuiTitle>
-          <p>
-            For overflow and scrolling specific utilities, go to the{' '}
-            <Link to="/utilities/scroll">Scroll documentation page</Link>.
-          </p>
-          <EuiHorizontalRule />
-          <EuiTitle>
-            <h2>Text</h2>
-          </EuiTitle>
-          <p>
-            For text and typography specific utilities, go to the{' '}
-            <Link to="/utilities/text">Text documentation page</Link>.
-          </p>
-          <EuiSpacer size="xs" />
         </>
       ),
     },
