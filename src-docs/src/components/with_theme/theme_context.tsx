057c7507018ad52e58aa39bfc1ea8ea93d3911a7
Constance Chen <constance.chen.3@gmail.com>
2022-05-03 11:49:47 -0700

    Add logic to load theme language based on url param

--- src-docs/src/components/with_theme/theme_context.tsx
+++ src-docs/src/components/with_theme/theme_context.tsx
@@ -3,7 +3,8 @@ import { EUI_THEMES, EUI_THEME } from '../../../../src/themes';
 // @ts-ignore importing from a JS file
 import { applyTheme } from '../../services';
 
-export const STYLE_STORAGE_KEY = 'js_vs_sass_preference';
+const STYLE_STORAGE_KEY = 'js_vs_sass_preference';
+const URL_PARAM_KEY = 'themeLanguage';
 
 export type THEME_LANGUAGES = {
   id: 'language--js' | 'language--sass';
@@ -48,15 +49,11 @@ export class ThemeProvider extends React.Component<object, State> {
   constructor(props: object) {
     super(props);
 
-    let themeLanguage = localStorage.getItem(
-      STYLE_STORAGE_KEY
-    ) as THEME_LANGUAGES['id'];
-    if (!themeLanguage || !THEME_LANGS.includes(themeLanguage))
-      themeLanguage = defaultState.themeLanguage;
-
     let theme = localStorage.getItem('theme');
     if (!theme || !THEME_NAMES.includes(theme)) theme = defaultState.theme;
     applyTheme(theme);
+
+    const themeLanguage = this.getThemeLanguage();
 
     this.state = {
       theme,
@@ -69,6 +66,26 @@ export class ThemeProvider extends React.Component<object, State> {
       localStorage.setItem('theme', themeValue);
       applyTheme(themeValue);
     });
+  };
+
+  getThemeLanguage = () => {
+    // Allow theme language to be set by URL param, so we can link people
+    // to specific docs, e.g. ?themeLanguage=js, ?themeLanguage=sass
+    // Note that because of our hash router, this logic only works on page load/full reload
+    const urlParams = window?.location?.href?.split('?')[1]; // Note: we can't use location.search because of our hash router
+    const fromUrlParam = new URLSearchParams(urlParams).get(URL_PARAM_KEY);
+    // Otherwise, obtain it from localStorage
+    const fromLocalStorage = localStorage.getItem(STYLE_STORAGE_KEY);
+
+    let themeLanguage = (fromUrlParam
+      ? `language--${fromUrlParam}`
+      : fromLocalStorage) as THEME_LANGUAGES['id'];
+
+    // If not set by either param or storage, or an invalid value, use the default
+    if (!themeLanguage || !THEME_LANGS.includes(themeLanguage))
+      themeLanguage = defaultState.themeLanguage;
+
+    return themeLanguage;
   };
 
   changeThemeLanguage = (language: THEME_LANGUAGES['id']) => {
