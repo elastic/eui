54172b4f2a542b8526fad811d2355e5643bc3ea3
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2022-05-06 15:32:44 +0100

    Better gap styles

--- src/components/facet/facet_group.styles.ts
+++ src/components/facet/facet_group.styles.ts
@@ -25,7 +25,7 @@ const _facetGroupGutterSize = ({
       ? `
       display: flex;
       flex-direction: row;
-      gap: 0 calc(${gutterSize});`
+      gap: 0 ${gutterSize};`
       : `
       > [class*='euiFacetButton'] {
         width: 100%;
