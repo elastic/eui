f117767d7dce913e3ebdee1f17fa3c7dab65ff98
Bree Hall <briannajdhall@gmail.com>
2022-06-14 16:40:55 -0400

    Converted the remaining styles for DescriptionList to Emotion. Converted the euiBreakopint mixin to TypeScript

--- src/components/description_list/description_list.styles.ts
+++ src/components/description_list/description_list.styles.ts
@@ -7,13 +7,15 @@
  */
 
 import { css } from '@emotion/react';
-import { UseEuiTheme } from '../../services';
+import { UseEuiTheme, tint } from '../../services';
 import { euiTitle } from '../title/title.styles';
 import { euiText } from '../text/text.styles';
-import { euiFontSize } from '../../global_styling';
+import { euiFontSize, euiBreakpoint } from '../../global_styling';
 
 export const euiDescriptionListStyles = (euiThemeContext: UseEuiTheme) => {
   const euiTheme = euiThemeContext.euiTheme;
+  const colorMode = euiThemeContext.colorMode;
+  const { lineHeight } = euiFontSize(euiThemeContext, 's');
 
   return {
     euiDescriptionList: css`
@@ -38,7 +40,7 @@ export const euiDescriptionListStyles = (euiThemeContext: UseEuiTheme) => {
         &.euiDescriptionList--compressed {
           .euiDescriptionList__title {
             ${euiTitle(euiThemeContext, 'xxs')}
-            line-height: ${euiTheme.size.l};
+            line-height: ${lineHeight};
           }
 
           .euiDescriptionList__description {
@@ -53,7 +55,7 @@ export const euiDescriptionListStyles = (euiThemeContext: UseEuiTheme) => {
 
             .euiDescriptionList__description {
               ${euiTitle(euiThemeContext, 'xxs')};
-              line-height: ${euiTheme.size.l};
+              line-height: ${lineHeight};
             }
           }
         }
@@ -76,13 +78,14 @@ export const euiDescriptionListStyles = (euiThemeContext: UseEuiTheme) => {
 
           .euiDescriptionList__description {
             ${euiTitle(euiThemeContext, 'xs')};
-            line-height: ${euiTheme.size.l};
+            line-height: ${lineHeight};
           }
         }
+
         &.euiDescriptionList--compressed {
           .euiDescriptionList__title {
             ${euiTitle(euiThemeContext, 'xxs')};
-            line-height: ${euiTheme.size.l};
+            line-height: ${lineHeight};
           }
 
           .euiDescriptionList__description {
@@ -96,18 +99,54 @@ export const euiDescriptionListStyles = (euiThemeContext: UseEuiTheme) => {
             }
 
             .euiDescriptionList__description {
-              ${euiTitle(euiThemeContext, 'xs')};
-              line-height: ${euiTheme.size.l};
+              ${euiTitle(euiThemeContext, 'xxs')};
+              line-height: ${lineHeight};
             }
           }
         }
-        //->
+
+        &.euiDescriptionList--responsiveColumn {
+          ${euiBreakpoint(['xs', 's'])} {
+            &.euiDescriptionList--center {
+              .euiDescriptionList__title,
+              .euiDescriptionList__description {
+                text-align: center;
+              }
+            }
+
+            &.euiDescriptionList--reverse {
+              .euiDescriptionList__title {
+                ${euiFontSize(euiThemeContext, 's')};
+              }
+
+              .euiDescriptionList__description {
+                ${euiTitle(euiThemeContext, 'xs')};
+              }
+            }
+          }
+
+          &.euiDescriptionList--inline {
+            &.euiDescriptionList--compressed {
+              .euiDescriptionList__title {
+                ${euiFontSize(euiThemeContext, 'xs')};
+                padding: 0 ${euiTheme.size.xs};
+              }
+
+              .euiDescriptionList__description {
+                ${euiFontSize(euiThemeContext, 'xs')};
+              }
+            }
+            &.euiDescriptionList--center {
+              text-align: center;
+            }
+          }
+        }
       }
     `,
     'euiDescriptionList-row': css`
-      &.euiDescriptionList__title {
+      .euiDescriptionList__title {
         ${euiTitle(euiThemeContext, 'xs')}
-        line-height: ${euiTheme.size.l};
+        line-height: ${lineHeight};
         margin-top: ${euiTheme.size.base};
 
         // Make sure the first <dt> doesn't get a margin.
@@ -115,7 +154,7 @@ export const euiDescriptionListStyles = (euiThemeContext: UseEuiTheme) => {
           margin-top: 0;
         }
       }
-      &.euiDescriptionList__description {
+      .euiDescriptionList__description {
         ${euiFontSize(euiThemeContext, 's')};
       }
     `,
@@ -134,15 +173,15 @@ export const euiDescriptionListStyles = (euiThemeContext: UseEuiTheme) => {
         margin-top: 0;
       }
 
-      &.euiDescriptionList__title {
+      .euiDescriptionList__title {
         ${euiTitle(euiThemeContext, 'xs')};
-        line-height: ${euiTheme.size.l};
+        line-height: ${lineHeight};
         width: 50%; // Flex-basis doesn't work in IE with padding
         padding-right: ${euiTheme.size.s};
       }
 
-      &.euiDescriptionList__description {
-        @include euiFontSize;
+      .euiDescriptionList__description {
+        ${euiFontSize(euiThemeContext, 's')};
         width: 50%; // Flex-basis doesn't work in IE with padding
         padding-left: ${euiTheme.size.s};
       }
@@ -164,17 +203,56 @@ export const euiDescriptionListStyles = (euiThemeContext: UseEuiTheme) => {
 
       &.euiDescriptionList__title {
         ${euiTitle(euiThemeContext, 'xs')};
-        line-height: ${euiTheme.size.l};
+        line-height: ${lineHeight};
         width: 50%; // Flex-basis doesn't work in IE with padding
         padding-right: ${euiTheme.size.s};
       }
 
       &.euiDescriptionList__description {
-        @include euiFontSize;
+        ${euiFontSize(euiThemeContext, 's')};
         width: 50%; // Flex-basis doesn't work in IE with padding
         padding-left: ${euiTheme.size.s};
       }
+
+      ${euiBreakpoint(['xs', 's'])} {
+        display: block;
+
+        &.euiDescriptionList__title,
+        &.euiDescriptionList__description {
+          width: 100%;
+          padding: 0;
+        }
+
+        &.euiDescriptionList__description {
+          ${euiFontSize(euiThemeContext, 's')};
+          margin-top: 0;
+        }
+      }
     `,
-    'euiDescriptionList-inline': css``,
+
+    'euiDescriptionList-inline': css`
+      &.euiDescriptionList__title {
+        ${euiFontSize(euiThemeContext, 's')};
+        display: inline;
+        border-radius: ${euiTheme.border.radius.small};
+        font-weight: ${euiTheme.font.weight.medium};
+        background-color: ${colorMode === 'DARK'
+          ? tint(euiTheme.colors.lightestShade, 0.5)
+          : euiTheme.colors.lightestShade};
+        padding: 1px ${euiTheme.size.xs};
+        margin: 0 ${euiTheme.size.xs};
+
+        // Make sure the first <dt> doesn't get a margin.
+        &:first-of-type {
+          margin-left: 0;
+        }
+        ${euiTheme.colors.lightestShade}
+      }
+
+      &.euiDescriptionList__description {
+        ${euiFontSize(euiThemeContext, 's')};
+        display: inline;
+      }
+    `,
   };
 };
---
+++ src/global_styling/mixins/_responsive.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export const BREAKPOINT_KEYS = {
+  xs: 0,
+  s: 575,
+  m: 768,
+  l: 992,
+  xl: 1200,
+} as const;
+export type EuiBreakpointKeys = keyof typeof BREAKPOINT_KEYS;
+
+export const euiBreakpoint = (sizes: EuiBreakpointKeys[]) => {
+  // Check to see if both min-width and max-width should be set
+
+  // If sizes only contains one element and it is the smallest size (xs), don't set a min-width
+  if (sizes.length === 1 && sizes[0] === 'xs') {
+    return `@media only screen and (max-width: ${BREAKPOINT_KEYS[sizes[0]]}px)`;
+  } // If sizes only contains one elements and it is the largest size (xl), don't set a max-width
+  else if (sizes.length === 1 && sizes[0] === 'xl') {
+    return `@media only screen and (min-width: ${BREAKPOINT_KEYS[sizes[0]]}px)`;
+  } // Else, set a min-width and max-width
+  else {
+    // minSize should equal the first element in the sizes array and the mazSize should equal the last element
+    const minSize = sizes[0];
+    const maxSize = sizes[sizes.length - 1];
+
+    // -1px to prevent the overlap breakpoints
+    return `@media only screen and (min-width: ${
+      BREAKPOINT_KEYS[minSize]
+    }px) and (max-width: ${BREAKPOINT_KEYS[maxSize] - 1}px)`;
+  }
+};
