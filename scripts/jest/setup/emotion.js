b5240d263eb0770cea1d8fe36f1620845a843181
Greg Thompson <thompson.glowe@gmail.com>
2022-06-15 11:53:40 -0500

    replace css-[hash] prefix with emotion

--- scripts/jest/setup/emotion.js
+++ scripts/jest/setup/emotion.js
@@ -2,7 +2,18 @@ import { createSerializer, matchers } from "@emotion/jest";
 
 expect.extend(matchers);
 
+const trimEuiSelector = (selector) => {
+  // Matches the Emotion className pattern + the EUI variant naming convention.
+  // Starts with `css-[hash]-`.
+  // Contains `eui[ComponentName] or `Eui[ComponentName]`.
+  // Capture the component name (from above) and all variant additions until the end of the string.
+  const euiMatch = selector.match(/css-[\d\w]{6,}-(?<euiComponent>[eE]ui[A-Z][\d\w-]*$)/);
+  // Use the captured group (`euiComponent`) if available and prepend with `emotion-`,
+  // otherwise use the full selector.
+  return euiMatch?.groups?.euiComponent ? `emotion-${euiMatch.groups.euiComponent}` : selector;
+};
+
 module.exports = createSerializer({
-  classNameReplacer: (className) => className,
+  classNameReplacer: trimEuiSelector,
   includeStyles: false,
 });
