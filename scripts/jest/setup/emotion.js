ed269f0136ed3797f1ff6200929bb7b2afd6f9e4
Greg Thompson <thompson.glowe@gmail.com>
2022-06-15 12:49:38 -0500

    move to tests dir; add unit tests

--- scripts/jest/setup/emotion.js
+++ scripts/jest/setup/emotion.js
@@ -1,19 +1,9 @@
 import { createSerializer, matchers } from "@emotion/jest";
+import { replaceEmotionPrefix } from '../../../src/test'
 
 expect.extend(matchers);
 
-const trimEuiSelector = (selector) => {
-  // Matches the Emotion className pattern + the EUI variant naming convention.
-  // Starts with `css-[hash]-`.
-  // Contains `eui[ComponentName] or `Eui[ComponentName]`.
-  // Capture the component name (from above) and all variant additions until the end of the string.
-  const euiMatch = selector.match(/css-[\d\w]{6,}-(?<euiComponent>[eE]ui[A-Z][\d\w-]*$)/);
-  // Use the captured group (`euiComponent`) if available and prepend with `emotion-`,
-  // otherwise use the full selector.
-  return euiMatch?.groups?.euiComponent ? `emotion-${euiMatch.groups.euiComponent}` : selector;
-};
-
 module.exports = createSerializer({
-  classNameReplacer: trimEuiSelector,
+  classNameReplacer: replaceEmotionPrefix,
   includeStyles: false,
 });
---
+++ src/test/emotion-prefix.test.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { replaceEmotionPrefix } from './emotion-prefix';
+
+describe('replaceEmotionPrefix', () => {
+  it('matches EUI component Emotion classes', () => {
+    expect(replaceEmotionPrefix('css-abc1234-euiComponent')).toBe(
+      'emotion-euiComponent'
+    );
+  });
+  it('matches EUI component Emotion classes with variants', () => {
+    expect(replaceEmotionPrefix('css-abc1234-euiComponent-primary-m')).toBe(
+      'emotion-euiComponent-primary-m'
+    );
+  });
+  it('matches chained EUI component Emotion classes', () => {
+    expect(
+      replaceEmotionPrefix('css-abc1234-euiComponentStyles-EuiComponent')
+    ).toBe('emotion-euiComponentStyles-EuiComponent');
+  });
+  it('does not match EUI Sass classes', () => {
+    expect(replaceEmotionPrefix('euiMark')).toBe('euiMark');
+    expect(replaceEmotionPrefix('euiButton--primary')).toBe(
+      'euiButton--primary'
+    );
+  });
+  it('does not match non-EUI classes', () => {
+    expect(replaceEmotionPrefix('css-euiMark')).toBe('css-euiMark');
+    expect(
+      replaceEmotionPrefix('css-abc123-kibanaClassName-euiMockComponent')
+    ).toBe('css-abc123-kibanaClassName-euiMockComponent');
+    expect(replaceEmotionPrefix('kibanaClassName-euiMockComponent')).toBe(
+      'kibanaClassName-euiMockComponent'
+    );
+  });
+});
