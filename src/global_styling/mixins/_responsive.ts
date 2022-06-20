9e5d1e607d0fc8502e7aa22a2d97041fcc68f332
Bree Hall <briannajdhall@gmail.com>
2022-06-20 11:08:42 -0400

    CHANGELOG

--- src/global_styling/mixins/_responsive.ts
+++ src/global_styling/mixins/_responsive.ts
@@ -6,24 +6,28 @@
  * Side Public License, v 1.
  */
 
-export const BREAKPOINT_KEYS = {
+export const EUI_BREAKPOINT_KEYS = {
   xs: 0,
   s: 575,
   m: 768,
   l: 992,
   xl: 1200,
 } as const;
-export type EuiBreakpointKeys = keyof typeof BREAKPOINT_KEYS;
+export type EuiBreakpointKeys = keyof typeof EUI_BREAKPOINT_KEYS;
 
 export const euiBreakpoint = (sizes: EuiBreakpointKeys[]) => {
   // Check to see if both min-width and max-width should be set
 
   // If sizes only contains one element and it is the smallest size (xs), don't set a min-width
   if (sizes.length === 1 && sizes[0] === 'xs') {
-    return `@media only screen and (max-width: ${BREAKPOINT_KEYS[sizes[0]]}px)`;
+    return `@media only screen and (max-width: ${
+      EUI_BREAKPOINT_KEYS[sizes[0]]
+    }px)`;
   } // If sizes only contains one elements and it is the largest size (xl), don't set a max-width
   else if (sizes.length === 1 && sizes[0] === 'xl') {
-    return `@media only screen and (min-width: ${BREAKPOINT_KEYS[sizes[0]]}px)`;
+    return `@media only screen and (min-width: ${
+      EUI_BREAKPOINT_KEYS[sizes[0]]
+    }px)`;
   } // Else, set a min-width and max-width
   else {
     // minSize should equal the first element in the sizes array and the mazSize should equal the last element
@@ -32,7 +36,7 @@ export const euiBreakpoint = (sizes: EuiBreakpointKeys[]) => {
 
     // -1px to prevent the overlap breakpoints
     return `@media only screen and (min-width: ${
-      BREAKPOINT_KEYS[minSize]
-    }px) and (max-width: ${BREAKPOINT_KEYS[maxSize] - 1}px)`;
+      EUI_BREAKPOINT_KEYS[minSize]
+    }px) and (max-width: ${EUI_BREAKPOINT_KEYS[maxSize] - 1}px)`;
   }
 };
