51c3edc5539b2b9fb4f6dc86dcea999310029f0f
Greg Thompson <thompson.glowe@gmail.com>
2022-06-23 10:06:37 -0500

    remove styles export

--- src/components/accessibility/screen_reader_only/index.ts
+++ src/components/accessibility/screen_reader_only/index.ts
@@ -8,7 +8,4 @@
 
 export type { EuiScreenReaderOnlyProps } from './screen_reader_only';
 export { EuiScreenReaderOnly } from './screen_reader_only';
-export {
-  euiScreenReaderOnly,
-  euiScreenReaderOnlyStyles,
-} from './screen_reader_only.styles';
+export { euiScreenReaderOnly } from './screen_reader_only.styles';
