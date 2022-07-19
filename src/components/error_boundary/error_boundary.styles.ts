002416208fd007cfd09363a958bf8297bf43c511
Greg Thompson <thompson.glowe@gmail.com>
2022-07-19 11:17:59 -0500

    safari 15.4 repeating-linear-gradient fix

--- src/components/error_boundary/error_boundary.styles.ts
+++ src/components/error_boundary/error_boundary.styles.ts
@@ -21,8 +21,9 @@ export const euiErrorBoundaryStyles = ({ euiTheme }: UseEuiTheme) => {
         ${color1},
         ${color1} 1px,
         ${color2} 1px,
-        ${color2} 20px
+        ${color2} 19px
       );
+      background-size: 54px 54px; // Fix for Safari 15.4+
       ${logicalCSS('padding-horizontal', euiTheme.size.base)};
       ${logicalCSS('padding-vertical', euiTheme.size.base)};
     `,
