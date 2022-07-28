eb486b59163c22f134dc77b1eb811af205afd958
Constance Chen <constance.chen.3@gmail.com>
2022-07-28 13:35:34 -0700

    Add basic EuiPopover RTL tests

---
+++ src/components/popover/popover.rtl.test.tsx
@@ -0,0 +1,105 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React, { useState } from 'react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+
+import { requiredProps } from '../../test/required_props';
+
+import { EuiPopover } from './';
+
+describe('EuiPopover', () => {
+  describe('snapshot testing', () => {
+    it('renders', () => {
+      const { baseElement } = render(
+        <EuiPopover
+          button={<button />}
+          closePopover={() => {}}
+          {...requiredProps}
+        />
+      );
+
+      // NOTE: Using baseElement instead of container is required for components that use portals
+      expect(baseElement).toMatchSnapshot();
+    });
+
+    it('renders with popover contents', () => {
+      const { baseElement } = render(
+        <EuiPopover
+          button={<button />}
+          closePopover={() => {}}
+          isOpen={true}
+          {...requiredProps}
+        >
+          Hello world
+        </EuiPopover>
+      );
+
+      expect(baseElement).toMatchSnapshot();
+    });
+  });
+
+  const mockPopoverInteraction = jest.fn();
+  const MockPopoverComponent = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const togglePopover = () => setIsOpen(!isOpen);
+    const closePopover = () => setIsOpen(false);
+
+    return (
+      <EuiPopover
+        button={<button onClick={togglePopover}>Open popover</button>}
+        closePopover={closePopover}
+        isOpen={isOpen}
+        data-testid="popover"
+      >
+        Popover content
+        <button onClick={mockPopoverInteraction}>Button inside popover</button>
+      </EuiPopover>
+    );
+  };
+
+  describe('open/close behavior', () => {
+    it('opens the popover, contents', () => {
+      render(<MockPopoverComponent />);
+
+      expect(screen.queryByText('Popover content')).toBeFalsy();
+
+      fireEvent.click(screen.getByText('Open popover'));
+
+      expect(screen.queryByText('Popover content')).toBeTruthy();
+    });
+
+    it('allows interacting with popover children', async () => {
+      render(<MockPopoverComponent />);
+
+      fireEvent.click(screen.getByText('Open popover'));
+      await waitFor(() => {
+        expect(screen.queryByText('Popover content')).toBeTruthy();
+      });
+
+      fireEvent.click(screen.getByText('Button inside popover'));
+      expect(mockPopoverInteraction).toHaveBeenCalledTimes(1);
+    });
+
+    it('closes the popover on escape key press', async () => {
+      render(<MockPopoverComponent />);
+
+      fireEvent.click(screen.getByText('Open popover'));
+
+      await waitFor(() => {
+        expect(screen.queryByText('Popover content')).toBeTruthy();
+      });
+
+      fireEvent.keyDown(screen.queryByTestId('popover')!, { key: 'Escape' });
+
+      await waitFor(() => {
+        expect(screen.queryByText('Popover content')).toBeFalsy();
+      });
+    });
+  });
+});
