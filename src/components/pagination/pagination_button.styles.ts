9ca485c8f9d8c2d3e2440b43ba6197be18faa36f
Greg Thompson <thompson.glowe@gmail.com>
2022-08-04 13:55:08 -0500

    convert euipaginationbutton and euipaginationbuttonarrow to emotion styling

---
+++ src/components/pagination/pagination_button.styles.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { css } from '@emotion/react';
+import {
+  logicalCSS,
+  logicalTextAlignCSS,
+  euiFontSize,
+} from '../../global_styling';
+import { UseEuiTheme } from '../../services';
+
+export const euiPaginationButtonStyles = (euiThemeContext: UseEuiTheme) => {
+  const { euiTheme } = euiThemeContext;
+  const fontSizeS = euiFontSize(euiThemeContext, 's');
+  const halfSizeM = parseInt(euiTheme.size.m.replace('px', '')) / 2;
+
+  return {
+    // Base
+    euiPaginationButton: css`
+      ${fontSizeS};
+      padding: 0;
+      ${logicalTextAlignCSS('center')}
+      border-radius: ${euiTheme.border.radius.medium};
+      outline-offset: -${euiTheme.focus.width};
+    `,
+    // States
+    isActive: css`
+      font-weight: ${euiTheme.font.weight.bold};
+      color: ${euiTheme.colors.primary};
+
+      // TODO: update with new selector
+      .euiButtonEmpty__content {
+        cursor: default;
+      }
+
+      &,
+      &:hover {
+        text-decoration: underline;
+      }
+    `,
+    isPlaceholder: css`
+      align-items: baseline;
+      // TODO: update with var from button.styles.ts
+      color: $euiButtonColorDisabledText;
+      ${fontSizeS};
+      ${logicalCSS('padding-top', `${halfSizeM}px`)};
+      ${logicalCSS('padding-bottom', 0)};
+      ${logicalCSS('padding-horizontal', euiTheme.size.s)};
+      ${logicalCSS('height', euiTheme.size.l)};
+    `,
+  };
+};
+
+export const euiPaginationButtonArrowStyles = ({ euiTheme }: UseEuiTheme) => ({
+  euiPaginationArrowButton: css`
+    outline-offset: -${euiTheme.focus.width};
+  `,
+});
