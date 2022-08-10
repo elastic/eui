c88f1ea5fa7261dad552bd68ba58ff9d833b7aee
Constance Chen <constance.chen.3@gmail.com>
2022-08-10 09:54:00 -0700

    Convert text globals/utilities to Emotion
    + fix missing CSS logical property on text truncation mixin
    + convert textLeft/Right to logical CSS
    - note that we can't use the logicalTextAlign util for this as !important can't be passed to that util

--- src/global_styling/utility/utility.tsx
+++ src/global_styling/utility/utility.tsx
@@ -10,10 +10,43 @@ import React from 'react';
 import { Global, css } from '@emotion/react';
 
 import { euiScreenReaderOnly } from '../../components/accessibility/screen_reader_only/screen_reader_only.styles';
+import { euiTextBreakWord, euiTextTruncate } from '../mixins';
 
 const globalStyles = css`
   .euiScreenReaderOnly {
     ${euiScreenReaderOnly()}
+  }
+
+  // Text
+  .eui-textCenter {
+    text-align: center !important;
+  }
+  .eui-textLeft {
+    text-align: start !important;
+  }
+  .eui-textRight {
+    text-align: end !important;
+  }
+  .eui-textNoWrap {
+    white-space: nowrap !important;
+  }
+  .eui-textInheritColor {
+    color: inherit !important;
+  }
+  .eui-textBreakWord {
+    ${euiTextBreakWord()}
+  }
+  .eui-textBreakAll {
+    overflow-wrap: break-word !important; // Fixes FF when dashes are involved #2288
+    word-break: break-all !important;
+  }
+  .eui-textBreakNormal {
+    overflow-wrap: normal !important;
+    word-wrap: normal !important;
+    word-break: normal !important;
+  }
+  .eui-textTruncate {
+    ${euiTextTruncate()}
   }
 `;
 export const EuiUtilityClasses = () => <Global styles={globalStyles} />;
