fc66fbe7e009756a83f81289145df266e0c535e4
Trevor Pierce <1Copenut@users.noreply.github.com>
2022-08-25 09:38:05 -0500

    Log Cypress a11y violations without test failures. (#6173)
    * Log Cypress a11y violations without test failures.
    * Refactoring common violation handling behavior.
    * Refactoring to a facade pattern to capture violations if reports can't.
    * Final refactor to ensure good console reporting only.
    * Removing Node fs declaration as not needed.
    * Moving smoke test to its own file. Object syntax for config object.

--- cypress/support/a11y/checkAxe.ts
+++ cypress/support/a11y/checkAxe.ts
@@ -11,8 +11,28 @@
 import { defaultContext, defaultAxeConfig } from './defaultAxeConfig';
 import { Result } from 'axe-core';
 
-function logViolations(violations: Result[]) {
-  // Print any violations to the console using a custom callback
+function _handleViolations(violations: Result[], skipTestFailure?: boolean) {
+  // Destructure keys from the violations object to create a readable array
+  const violationData = violations.map(({ id, description, impact, nodes }) => ({
+    id,
+    description,
+    impact,
+    nodes: nodes.length
+  }));
+
+  // Print reporting only message to the console
+  // https://github.com/component-driven/cypress-axe#skipfailures-optional-defaults-to-false
+  if (skipTestFailure) {
+    cy.task(
+      'log',
+      `
+========================================
+* A11Y REPORT MODE ONLY
+========================================`
+    );
+  }
+
+  // Print violations to the console using a custom callback
   // https://github.com/component-driven/cypress-axe#using-the-violationcallback-argument
   cy.task(
     'log',
@@ -21,20 +41,31 @@ function logViolations(violations: Result[]) {
     } ${violations.length === 1 ? 'was' : 'were'} detected.`
   );
 
-  // Destructure keys from the violations object to create a readable table
-  const violationData = violations.map(({ id, description, impact, nodes }) => ({
-      id,
-      description,
-      impact,
-      nodes: nodes.length
-    })
-  );
-
   // Print the table of violations to the console
   cy.task('table', violationData);
 }
 
-Cypress.Commands.add('checkAxe', (context, axeConfig, callback) => {
+function logViolationsAndThrow(violations: Result[]) {
+  _handleViolations(violations);
+}
+
+function logViolationsToConsoleOnly(violations: Result[]) {
+  _handleViolations(violations, true);
+}
+
+Cypress.Commands.add('checkAxe', ({
+  skipFailures,
+  context,
+  axeConfig,
+  callback
+} = {}) => {
   cy.injectAxe();
-  cy.checkA11y(context ?? defaultContext, axeConfig ?? defaultAxeConfig, callback ?? logViolations);
+  cy.checkA11y(
+    context ?? defaultContext,
+    axeConfig ?? defaultAxeConfig,
+    callback ?? skipFailures
+      ? logViolationsToConsoleOnly
+      : logViolationsAndThrow,
+    skipFailures
+  );
 });
