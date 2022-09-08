698feab1be91638201e02abae78920e7e6375dc0
Trevor Pierce <1Copenut@users.noreply.github.com>
2022-09-08 15:47:54 -0500

    Upgrade Cypress to v10 (#6212)
    * WIP. Loading everything except theme CSS file.
    
    * now with infinity% more global styles
    
    * Headless Chrome tests all passing.
    
    * Refactoring location of config files.
    
    * Skipping Data Grid footer regression test.
    
    * Get cypress code coverage working again
    
    + `cypress/` folder is getting included in coverage now for some reason, so ignore anything not in src
    
    + misc prettier auto fixes
    
    * Updating context type cast to use existing TS type.
    
    * it.skip comments
    
    Co-authored-by: Chandler Prall <chandler.prall@gmail.com>
    Co-authored-by: Constance Chen <constance.chen.3@gmail.com>
    Co-authored-by: Constance <constancecchen@users.noreply.github.com>

---
+++ cypress.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'cypress';
+import webpackConfig from './cypress/webpack.config';
+
+export default defineConfig({
+  retries: {
+    runMode: 2,
+    openMode: 2,
+  },
+
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+      webpackConfig,
+    },
+    setupNodeEvents(on, config) {
+      require('@cypress/code-coverage/task')(on, config);
+      on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+        table(message) {
+          console.table(message);
+          return null;
+        },
+      });
+
+      return config;
+    },
+    specPattern: './src/**/*.spec.tsx',
+    video: false,
+  },
+});
