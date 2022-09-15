5f4af08b2527af9ea11b840724048b796c5e1861
Constance Chen <constance.chen.3@gmail.com>
2022-09-14 18:48:09 -0700

    Add support for testing child componentProps to `shouldRenderCustomStyles`

--- src/test/internal/render_custom_styles.tsx
+++ src/test/internal/render_custom_styles.tsx
@@ -10,28 +10,64 @@ import React, { ReactElement } from 'react';
 import { render } from '@testing-library/react';
 import { css } from '@emotion/react';
 
+const customStyles = {
+  className: 'hello',
+  css: css`
+    color: red;
+  `,
+  style: { content: "'world'" },
+};
+
+const assertOutputStyles = (rendered: HTMLElement) => {
+  // className
+  const componentNode = rendered.querySelector('.hello');
+  expect(componentNode).not.toBeNull();
+  // css
+  expect(componentNode!.getAttribute('class')).toEqual(
+    expect.stringMatching(/css-[\d\w-]{6,}-css/) // should have generated an emotion class ending with -css
+  );
+  // style
+  expect(componentNode!.getAttribute('style')).toContain("content: 'world';");
+};
+
 /**
  * Use this test helper to quickly check that the component supports custom
  * `className`, `css`, and `style` properties.
  *
+ * Use the second childProps arg to ensure that any child component props
+ * also correctly accept custom css/classes/styles.
+ *
  * Example usage:
  *
  * shouldRenderCustomStyles(<EuiMark {...requiredProps} />Marked</EuiMark>);
+ * shouldRenderCustomStyles(<EuiPageSection />, ['contentProps']);
  */
-export const shouldRenderCustomStyles = (component: ReactElement) => {
+export const shouldRenderCustomStyles = (
+  component: ReactElement,
+  childProps?: string[]
+) => {
   it('should render custom classNames, css, and styles', () => {
     const { baseElement } = render(
       <div>{React.cloneElement(component, customStyles)}</div>
     );
-
-    // className
-    const componentNode = baseElement.querySelector('.hello');
-    expect(componentNode).not.toBeNull();
-    // css
-    expect(componentNode!.getAttribute('class')).toEqual(
-      expect.stringMatching(/css-[\d\w-]{6,}-css/) // should have generated an emotion class ending with -css
-    );
-    // style
-    expect(componentNode!.getAttribute('style')).toContain("content: 'world';");
+    assertOutputStyles(baseElement);
   });
+
+  if (childProps) {
+    childProps.forEach((_childProps) => {
+      it(`should render custom classNames, css, and styles on ${_childProps}`, () => {
+        const { baseElement } = render(
+          <div>
+            {React.cloneElement(component, {
+              [_childProps]: {
+                ...(component.props[_childProps] || {}),
+                ...customStyles,
+              },
+            })}
+          </div>
+        );
+        assertOutputStyles(baseElement);
+      });
+    });
+  }
 };
