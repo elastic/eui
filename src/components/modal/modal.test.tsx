4fc41bff4ba4c31a0e8bf442c5315bcbf7ca20b0
Constance Chen <constance.chen.3@gmail.com>
2022-10-26 12:53:10 -0700

    Remove old scroll workaround and use new react-focus-on API

--- src/components/modal/modal.test.tsx
+++ src/components/modal/modal.test.tsx
@@ -7,7 +7,6 @@
  */
 
 import React from 'react';
-import { fireEvent } from '@testing-library/dom';
 import { render } from '../../test/rtl';
 import { requiredProps } from '../../test';
 
@@ -23,28 +22,5 @@ describe('EuiModal', () => {
 
     // NOTE: Using baseElement instead of container is required for components that use portals
     expect(baseElement).toMatchSnapshot();
-  });
-
-  // TODO: Remove this onFocus scroll workaround after react-focus-on supports focusOptions
-  // @see https://github.com/elastic/eui/issues/6304
-  describe('focus/scroll workaround', () => {
-    it('scrolls back to the original window position on initial modal focus', () => {
-      window.scrollTo = jest.fn();
-
-      const { getByTestSubject } = render(
-        <EuiModal data-test-subj="modal" onClose={() => {}}>
-          children
-        </EuiModal>
-      );
-
-      // For whatever reason, react-focus-lock doesn't appear to trigger focus in RTL so we'll do it manually
-      fireEvent.focusIn(getByTestSubject('modal'));
-      // Confirm that scrolling does not occur more than once
-      fireEvent.focusIn(getByTestSubject('modal'));
-      fireEvent.focusIn(getByTestSubject('modal'));
-
-      expect(window.scrollTo).toHaveBeenCalledTimes(1);
-      jest.restoreAllMocks();
-    });
   });
 });
