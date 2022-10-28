bdea710534e2e78ab56949284b36d8593b9c6860
Constance <constancecchen@users.noreply.github.com>
2022-10-28 15:00:43 -0700

    [EuiModal] Temporary workaround for scroll-jumping behavior (#6327)
    * Add removable workaround until react-focus-on supports focusOptions
    
    * unit tests - convert modal tests to RTL; add removable scroll/focus test
    
    * [misc bugfix] Remove body width & scroll jumping occurring in Chrome
    
    - use `react-focus-on`'s scrollLock API to prevent this (they manage preserving the width of the scrollbar for us)
    
    * changelog

--- src/components/modal/modal.test.tsx
+++ src/components/modal/modal.test.tsx
@@ -7,19 +7,44 @@
  */
 
 import React from 'react';
-import { mount } from 'enzyme';
-import { requiredProps, takeMountedSnapshot } from '../../test';
+import { fireEvent } from '@testing-library/dom';
+import { render } from '../../test/rtl';
+import { requiredProps } from '../../test';
 
 import { EuiModal } from './modal';
 
-test('renders EuiModal', () => {
-  const component = (
-    <EuiModal onClose={() => {}} {...requiredProps}>
-      children
-    </EuiModal>
-  );
+describe('EuiModal', () => {
+  it('renders', () => {
+    const { baseElement } = render(
+      <EuiModal onClose={() => {}} {...requiredProps}>
+        children
+      </EuiModal>
+    );
 
-  expect(
-    takeMountedSnapshot(mount(component), { hasArrayOutput: true })
-  ).toMatchSnapshot();
+    // NOTE: Using baseElement instead of container is required for components that use portals
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  // TODO: Remove this onFocus scroll workaround after react-focus-on supports focusOptions
+  // @see https://github.com/elastic/eui/issues/6304
+  describe('focus/scroll workaround', () => {
+    it('scrolls back to the original window position on initial modal focus', () => {
+      window.scrollTo = jest.fn();
+
+      const { getByTestSubject } = render(
+        <EuiModal data-test-subj="modal" onClose={() => {}}>
+          children
+        </EuiModal>
+      );
+
+      // For whatever reason, react-focus-lock doesn't appear to trigger focus in RTL so we'll do it manually
+      fireEvent.focusIn(getByTestSubject('modal'));
+      // Confirm that scrolling does not occur more than once
+      fireEvent.focusIn(getByTestSubject('modal'));
+      fireEvent.focusIn(getByTestSubject('modal'));
+
+      expect(window.scrollTo).toHaveBeenCalledTimes(1);
+      jest.restoreAllMocks();
+    });
+  });
 });
