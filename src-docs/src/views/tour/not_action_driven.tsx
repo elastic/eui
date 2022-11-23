edf22f304c24781db35b6d83c4a369ff3c94f828
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2022-11-23 19:10:47 +0000

    [EuiTour] Add ability to pass an array of buttons to `footerAction` (#6384)
    * Add ability to pass an array of buttons in `footerAction`
    
    * Adding conditional button
    
    * Adding CL
    
    * Adding test
    
    * Improving demo
    
    * Added new CL
    
    * Adding unique `anchorRef`
    
    * Adding unique `STORAGE_KEY`
    
    * [PR feedback] Mobile behavior
    
    * [PR feedback] unnecessary const declaration
    
    - it isn't used in more than one place and it isn't significantly more readable
    
    * [PR feedback] Copy/grammar tweaks
    
    'Not action driven' is the most opinionated change - apologies if it's unwelcome, I just thought it sounded awkward vs using the word 'passive'
    
    Co-authored-by: Constance Chen <constance.chen.3@gmail.com>

---
+++ src-docs/src/views/tour/not_action_driven.tsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from 'react';
+
+import {
+  EuiButton,
+  EuiButtonEmpty,
+  EuiSpacer,
+  EuiTourStep,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPanel,
+  EuiButtonIcon,
+} from '../../../../src/components';
+
+const demoTourSteps = [
+  {
+    step: 1,
+    title: 'Preview mode',
+    content: <p>See what your project looks like.</p>,
+    anchorRef: 'notActionDrivenStep1',
+    iconType: 'eye',
+  },
+  {
+    step: 2,
+    title: 'Build Mode',
+    content: <p>Build your project.</p>,
+    anchorRef: 'notActionDrivenStep2',
+    iconType: 'editorCodeBlock',
+  },
+  {
+    step: 3,
+    title: 'Comment mode',
+    content: <p>Collaborate with your colleagues.</p>,
+    anchorRef: 'notActionDrivenStep3',
+    iconType: 'editorComment',
+  },
+  {
+    step: 2,
+    title: 'Share',
+    content: <p>Share your project.</p>,
+    anchorRef: 'notActionDrivenStep4',
+    iconType: 'share',
+  },
+];
+
+const tourConfig = {
+  currentTourStep: 1,
+  isTourActive: true,
+  tourPopoverWidth: 360,
+  tourSubtitle: 'Demo tour',
+};
+
+const STORAGE_KEY = 'notActionDrivenDemoTour';
+
+export default () => {
+  const [state, setState] = useState(() => {
+    let initialState: any = localStorage.getItem(STORAGE_KEY);
+    if (initialState) {
+      initialState = JSON.parse(initialState);
+    } else {
+      initialState = tourConfig;
+    }
+    return initialState;
+  });
+  useEffect(() => {
+    // Store the tour data
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(state));
+  }, [state]);
+
+  const incrementStep = () => {
+    setState({
+      ...state,
+      currentTourStep: state.currentTourStep + 1,
+    });
+  };
+
+  const handleClick = () => {
+    incrementStep();
+  };
+
+  const resetTour = () => {
+    setState({
+      ...state,
+      currentTourStep: 1,
+      isTourActive: true,
+    });
+  };
+
+  const finishTour = () => {
+    setState({
+      ...state,
+      isTourActive: false,
+    });
+  };
+
+  return (
+    <div>
+      <EuiPanel style={{ width: 'max-content' }}>
+        <EuiFlexGroup responsive={false}>
+          {demoTourSteps.map((step, index) => (
+            <EuiFlexItem grow={false}>
+              <EuiTourStep
+                content={step.content}
+                isStepOpen={
+                  state.isTourActive && state.currentTourStep === index + 1
+                }
+                minWidth={state.tourPopoverWidth}
+                onFinish={finishTour}
+                step={index + 1}
+                stepsTotal={demoTourSteps.length}
+                subtitle={state.tourSubtitle}
+                title={step.title}
+                anchorPosition="rightUp"
+                footerAction={
+                  // if it's the last step, we don't want to show the next button
+                  index === demoTourSteps.length - 1 ? (
+                    <EuiButton color="success" size="s" onClick={finishTour}>
+                      Finish tour
+                    </EuiButton>
+                  ) : (
+                    [
+                      <EuiButtonEmpty
+                        size="s"
+                        color="text"
+                        onClick={finishTour}
+                      >
+                        Close tour
+                      </EuiButtonEmpty>,
+                      <EuiButton color="success" size="s" onClick={handleClick}>
+                        Next
+                      </EuiButton>,
+                    ]
+                  )
+                }
+              >
+                <EuiButtonIcon iconType={step.iconType} color="text" />
+              </EuiTourStep>
+            </EuiFlexItem>
+          ))}
+        </EuiFlexGroup>
+      </EuiPanel>
+      <EuiSpacer size="m" />
+      <EuiButtonEmpty iconType="refresh" onClick={resetTour}>
+        Reset tour
+      </EuiButtonEmpty>
+    </div>
+  );
+};
