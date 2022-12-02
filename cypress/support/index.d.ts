a0c38025b828331fee0db376bbf2bcd33b5cee96
Trevor Pierce <1Copenut@users.noreply.github.com>
2022-12-02 14:29:37 -0600

    Adding a11y tests for Control Bar, Drag and Drop, Card. (#6449)
    * Adding a11y spec for Control Bar.
    * Updating checkAxe TS definition.
    * Adding a11y and keyboard spec to Drag and Drop.
    * Adding a11y and keyboard spec for Card.

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -9,12 +9,12 @@ declare global {
     interface Chainable<Subject> {
       // We are adding `/// <reference types="../../../cypress/support"/>` to
       // Cypress specs so VSCode will recognize custom command types.
-  
+
       /**
-       * Convenience method to run the axe-core accessibility scanner without having to establish 
-       * `cy.injectAxe()` in a `beforeEach` block. This method also reports axe violations in the 
+       * Convenience method to run the axe-core accessibility scanner without having to establish
+       * `cy.injectAxe()` in a `beforeEach` block. This method also reports axe violations in the
        * console output for debugging.
-       * 
+       *
        * @param skipFailures Set to true to report failures to the console without Cypress failing the test.
        * @param context Any valid node or CSS selector. Defaults to the Cypress containing `<div>`.
        * @param axeConfig Add or change rules in the `axe.run` config object
@@ -23,12 +23,11 @@ declare global {
        * @see https://www.deque.com/axe/core-documentation/api-documentation/#results-object
        */
       checkAxe(options?: {
-        skipFailures?: boolean,
-        context?: ContextObject,
-        axeConfig?: RunOptions,
-        callback?: (violations: Result[]) => void
+        skipFailures?: boolean;
+        context?: ContextObject | string;
+        axeConfig?: RunOptions;
+        callback?: (violations: Result[]) => void;
       }): void;
-  
 
       /**
        * This ensures the correct testing window has focus when using Cypress Real Events.
@@ -41,7 +40,11 @@ declare global {
        * @param keyToPress Any valid key or array of keys https://docs.cypress.io/api/commands/type#Arguments
        * @param count Number of times to invoke `realPress()`. Defaults to 2.
        */
-      repeatRealPress(keyToPress: KeyOrShortcut, count?: number, options?: RealPressOptions): void;
+      repeatRealPress(
+        keyToPress: KeyOrShortcut,
+        count?: number,
+        options?: RealPressOptions
+      ): void;
     }
   }
 }
