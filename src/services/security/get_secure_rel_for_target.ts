2f8b19cc5478b7d2bce8109689c4df12c8ab2e1d
Jason Stoltzfus <jastoltz24@gmail.com>
2023-01-18 16:23:26 -0500

    Removed domain check from EUILink

--- src/services/security/get_secure_rel_for_target.ts
+++ src/services/security/get_secure_rel_for_target.ts
@@ -10,10 +10,8 @@
  * Secures outbound links. For more info:
  * https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/
  */
-import { isDomainSecure } from '../url';
 
 export const getSecureRelForTarget = ({
-  href,
   target = '',
   rel,
 }: {
@@ -21,14 +19,11 @@ export const getSecureRelForTarget = ({
   target?: '_blank' | '_self' | '_parent' | '_top' | string;
   rel?: string;
 }) => {
-  const isElasticHref = !!href && isDomainSecure(href);
   const relParts = !!rel
     ? rel.split(' ').filter((part) => !!part.length && part !== 'noreferrer')
     : [];
 
-  if (!isElasticHref) {
-    relParts.push('noreferrer');
-  }
+  relParts.push('noreferrer');
 
   if (target.includes('_blank') && relParts.indexOf('noopener') === -1) {
     relParts.push('noopener');
