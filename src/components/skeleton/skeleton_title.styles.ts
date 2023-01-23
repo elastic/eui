835e745cb96307b7be331796205cab5bfc5f4c5a
Constance Chen <constance.chen.3@gmail.com>
2023-01-23 12:59:11 -0800

    Remove hard-coded `euiSkeletonTitleStyles` heights map
    - in favor of calling `euiTitle`'s font utils and grabbing `lineHeight` directly from it

--- src/components/skeleton/skeleton_title.styles.ts
+++ src/components/skeleton/skeleton_title.styles.ts
@@ -10,6 +10,7 @@ import { css } from '@emotion/react';
 import { euiCanAnimate, logicalCSS } from '../../global_styling';
 import { euiAnimSkeletonGradient } from '../../global_styling/utility/animations';
 import { COLOR_MODES_STANDARD, shade, tint, UseEuiTheme } from '../../services';
+import { euiTitle } from '../title/title.styles';
 
 export const euiSkeletonTitleStyles = (euiThemeContext: UseEuiTheme) => {
   const gradientStartStop =
@@ -20,15 +21,6 @@ export const euiSkeletonTitleStyles = (euiThemeContext: UseEuiTheme) => {
     euiThemeContext.colorMode === COLOR_MODES_STANDARD.dark
       ? shade(euiThemeContext.euiTheme.colors.lightShade, 0.24)
       : tint(euiThemeContext.euiTheme.colors.lightShade, 0.8);
-
-  const skeletonHeadingSizes = {
-    l: '2.4286rem',
-    m: '1.9286rem',
-    s: '1.5714rem',
-    xs: '1.1429rem',
-    xxs: '1rem',
-    xxxs: '0.8571rem',
-  };
 
   return {
     euiSkeleton__title: css`
@@ -56,32 +48,32 @@ export const euiSkeletonTitleStyles = (euiThemeContext: UseEuiTheme) => {
     `,
     // Sizes
     l: css`
-      ${logicalCSS('height', skeletonHeadingSizes.l)};
+      ${logicalCSS('height', euiTitle(euiThemeContext, 'l').lineHeight)};
       border-radius: ${euiThemeContext.euiTheme.border.radius.medium};
       ${logicalCSS('width', '45%')};
     `,
     m: css`
-      ${logicalCSS('height', skeletonHeadingSizes.m)};
+      ${logicalCSS('height', euiTitle(euiThemeContext, 'm').lineHeight)};
       border-radius: ${euiThemeContext.euiTheme.border.radius.medium};
       ${logicalCSS('width', '45%')};
     `,
     s: css`
-      ${logicalCSS('height', skeletonHeadingSizes.s)};
+      ${logicalCSS('height', euiTitle(euiThemeContext, 's').lineHeight)};
       border-radius: ${euiThemeContext.euiTheme.border.radius.medium};
       ${logicalCSS('width', '45%')};
     `,
     xs: css`
-      ${logicalCSS('height', skeletonHeadingSizes.xs)};
+      ${logicalCSS('height', euiTitle(euiThemeContext, 'xs').lineHeight)};
       border-radius: ${euiThemeContext.euiTheme.border.radius.medium};
       ${logicalCSS('width', '45%')};
     `,
     xxs: css`
-      ${logicalCSS('height', skeletonHeadingSizes.xxs)};
+      ${logicalCSS('height', euiTitle(euiThemeContext, 'xxs').lineHeight)};
       border-radius: ${euiThemeContext.euiTheme.border.radius.medium};
       ${logicalCSS('width', '45%')};
     `,
     xxxs: css`
-      ${logicalCSS('height', skeletonHeadingSizes.xxxs)};
+      ${logicalCSS('height', euiTitle(euiThemeContext, 'xxxs').lineHeight)};
       border-radius: ${euiThemeContext.euiTheme.border.radius.medium};
       ${logicalCSS('width', '45%')};
     `,
