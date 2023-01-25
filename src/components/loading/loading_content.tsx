2084c3e153f96a8cf88c6a5ab62f2292bd5c7f3a
Constance Chen <constance.chen.3@gmail.com>
2023-01-25 11:10:25 -0800

    Deprecate EuiLoadingContent + dogfood EuiSkeletonText

--- src/components/loading/loading_content.tsx
+++ src/components/loading/loading_content.tsx
@@ -6,46 +6,18 @@
  * Side Public License, v 1.
  */
 
-import React, { FunctionComponent, HTMLAttributes } from 'react';
-import classNames from 'classnames';
-import { CommonProps } from '../common';
-import { useEuiTheme } from '../../services';
-import { useLoadingAriaLabel } from './_loading_strings';
-import { euiLoadingContentStyles } from './loading_content.styles';
+import React, { FunctionComponent } from 'react';
 
-export type LineRange = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+import { EuiSkeletonText, EuiSkeletonTextProps } from '../skeleton';
 
-export type EuiLoadingContentProps = CommonProps &
-  HTMLAttributes<HTMLDivElement> & {
-    lines?: LineRange;
-  };
+/**
+ * @deprecated Use `EuiSkeletonTextProps` instead
+ */
+export type EuiLoadingContentProps = EuiSkeletonTextProps;
 
-export const EuiLoadingContent: FunctionComponent<EuiLoadingContentProps> = ({
-  lines = 3,
-  className,
-  'aria-label': ariaLabel,
-  ...rest
-}) => {
-  const euiTheme = useEuiTheme();
-  const styles = euiLoadingContentStyles(euiTheme);
-  const lineCssStyles = [styles.euiLoadingContent__singleLine];
-  const defaultLabel = useLoadingAriaLabel();
-
-  const classes = classNames('euiLoadingContent', className);
-  const lineElements = [];
-
-  for (let i = 0; i < lines; i++) {
-    lineElements.push(<span key={i} css={lineCssStyles} />);
-  }
-
-  return (
-    <span
-      className={classes}
-      role="progressbar"
-      aria-label={ariaLabel || defaultLabel}
-      {...rest}
-    >
-      {lineElements}
-    </span>
-  );
-};
+/**
+ * @deprecated Use `EuiSkeletonText` instead
+ */
+export const EuiLoadingContent: FunctionComponent<EuiSkeletonTextProps> = (
+  props
+) => <EuiSkeletonText {...props} />;
