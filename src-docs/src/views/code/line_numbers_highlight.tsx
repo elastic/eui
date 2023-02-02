42306d98548f384c607e7859bad6ac1bbc1b17e4
Elizabet Oliveira <elizabet.oliveira@elastic.co>
2023-02-02 16:10:50 +0000

    Better demo example

--- src-docs/src/views/code/line_numbers_highlight.tsx
+++ src-docs/src/views/code/line_numbers_highlight.tsx
@@ -1,37 +1,31 @@
 import React from 'react';
 
-import {
-  EuiCodeBlock,
-  EuiFlexGroup,
-  EuiFlexItem,
-} from '../../../../src/components';
+import { EuiCodeBlock, EuiSpacer } from '../../../../src/components';
 
 import { EuiCodeBlockAnnotationButton } from '../../../../src/components/code/code_block_annotation_button';
 
 export default () => (
-  <EuiFlexGroup>
-    <EuiFlexItem>
-      <EuiCodeBlockAnnotationButton />
-    </EuiFlexItem>
-    <EuiFlexItem>
-      <EuiCodeBlock
-        language="json"
-        fontSize="m"
-        paddingSize="m"
-        lineNumbers={{
-          highlight: '7, 8, 9',
-          annotations: {
-            3: (
-              <>
-                The <strong>shard</strong> is the unit at which Elasticsearch
-                distributes data around the cluster.
-              </>
-            ),
-            5: 'Some quick popover text',
-          },
-        }}
-      >
-        {`"OriginLocation": [
+  <>
+    <EuiCodeBlockAnnotationButton />
+    <EuiSpacer />
+    <EuiCodeBlock
+      language="json"
+      fontSize="m"
+      paddingSize="m"
+      lineNumbers={{
+        highlight: '7, 8, 9',
+        annotations: {
+          3: (
+            <>
+              The <strong>shard</strong> is the unit at which Elasticsearch
+              distributes data around the cluster.
+            </>
+          ),
+          5: 'Some quick popover text',
+        },
+      }}
+    >
+      {`"OriginLocation": [
   {
     "coordinates": [
       -97.43309784,
@@ -40,7 +34,6 @@ export default () => (
     "type": "Point"
   }
 ],`}
-      </EuiCodeBlock>
-    </EuiFlexItem>
-  </EuiFlexGroup>
+    </EuiCodeBlock>
+  </>
 );
