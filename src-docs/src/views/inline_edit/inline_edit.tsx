be9f7f76d3df6b46706bc592774f6ed005d79909
Bree Hall <briannajdhall@gmail.com>
2023-02-07 16:24:58 -0500

    Initial structure for EuiInlineEdit docs

---
+++ src-docs/src/views/inline_edit/inline_edit.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { EuiInlineEdit } from '../../../../src';
+
+export default () => <EuiInlineEdit></EuiInlineEdit>;
---
+++ src-docs/src/views/inline_edit/inline_edit_example.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { GuideSectionTypes } from '../../components';
+
+import { EuiText, EuiInlineEdit } from '../../../../src';
+
+import InlineEdit from './inline_edit';
+const inlineEditSource = require('!!raw-loader!./inline_edit');
+
+export const InlineEditExample = {
+  title: 'Inline edit',
+  intro: (
+    <>
+      <EuiText></EuiText>
+    </>
+  ),
+  sections: [
+    {
+      title: 'InlineEdit',
+      text: (
+        <>
+          <p>
+            Description needed: how to use the <strong>EuiInlineEdit</strong>{' '}
+            component.
+          </p>
+        </>
+      ),
+      source: [
+        {
+          type: GuideSectionTypes.JS,
+          code: inlineEditSource,
+        },
+      ],
+      demo: <InlineEdit />,
+      props: { EuiInlineEdit },
+    },
+  ],
+};
