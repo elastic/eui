fd9d198069969119d542e354c8d2419d20ce97ec
Constance Chen <constance.chen.3@gmail.com>
2023-02-07 15:03:51 -0800

    Set up API & documentation

--- src-docs/src/views/code/line_numbers_highlight.tsx
+++ src-docs/src/views/code/line_numbers_highlight.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-import { EuiCodeBlock } from '../../../../src/components';
+import { EuiCodeBlock, EuiCode } from '../../../../src/components';
 
 export default () => (
   <EuiCodeBlock
     language="json"
     fontSize="m"
     paddingSize="m"
-    lineNumbers={{ start: 32, highlight: '32, 34-37, 40' }}
+    lineNumbers={{
+      start: 32,
+      highlight: '32, 34-37, 40',
+      annotations: {
+        34: 'Coordinates can be obtained from Elastic Maps',
+        38: (
+          <>
+            Allowed types: <EuiCode>Point</EuiCode>, <EuiCode>Area</EuiCode>
+          </>
+        ),
+      },
+    }}
   >
     {`"OriginLocation": [
   {
