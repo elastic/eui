647feccc8f6f139ee98168ffa09eaa9bf6bcaa64
Katie Hughes <kate.hughes@elastic.co>
2023-02-13 15:03:07 -0800

    Add docs for using EuiImage with SVGs

---
+++ src-docs/src/views/image/image_svg.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { EuiImage } from '../../../../src/components';
+
+export default () => (
+  <EuiImage
+    size="l"
+    width={400}
+    hasShadow
+    caption="Flowchart diagram"
+    alt="Flowchart diagram"
+    src="https://upload.wikimedia.org/wikipedia/commons/c/cf/Clustal_Omega_Algorithm_Flowchart.svg"
+    allowFullScreen
+  />
+);
