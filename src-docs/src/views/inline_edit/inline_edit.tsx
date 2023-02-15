c1f83f109a2a25a1b4eae2fe57530a3d1412d872
Bree Hall <briannajdhall@gmail.com>
2023-02-15 15:59:03 -0500

    Added button groups to documentation to make toggling button sizes easier

--- src-docs/src/views/inline_edit/inline_edit.tsx
+++ src-docs/src/views/inline_edit/inline_edit.tsx
@@ -1,23 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { EuiInlineEdit, EuiSpacer } from '../../../../src';
+import {
+  EuiInlineEdit,
+  EuiSpacer,
+  EuiButtonGroup,
+  TextSize,
+  EuiTitleSize,
+} from '../../../../src';
 
-export default () => (
-  <>
-    <p>EuiInlineEdit - Text</p>
-    <EuiInlineEdit
-      display="text"
-      inputAriaLabel="textControlInput"
-      defaultValue="Hello World!"
-    />
+export default () => {
+  // Display Text Toggles
+  const textSizeButtons = [
+    {
+      id: 'xs',
+      label: 'Extra Small',
+    },
+    {
+      id: 's',
+      label: 'Small',
+    },
+    {
+      id: 'm',
+      label: 'Medium',
+    },
+    {
+      id: 'relative',
+      label: 'Relative',
+    },
+  ];
 
-    <EuiSpacer />
+  const [toggleTextButtonSize, setToggleTextButtonSize] = useState<TextSize>(
+    'm'
+  );
 
-    <p>EuiInlineEdit - Title</p>
-    <EuiInlineEdit
-      display="title"
-      inputAriaLabel="textControlInput"
-      defaultValue="Hello World (but as a title)!"
-    />
-  </>
-);
+  const textOnChange = (optionId: TextSize) => {
+    setToggleTextButtonSize(optionId);
+  };
+
+  // Display Title Toggles
+  const titleSizeButtons = [
+    {
+      id: 'xxxs',
+      label: '3X Small',
+    },
+    {
+      id: 'xxs',
+      label: '2X Small',
+    },
+    {
+      id: 'xs',
+      label: 'Extra Small',
+    },
+    {
+      id: 's',
+      label: 'Small',
+    },
+    {
+      id: 'm',
+      label: 'Medium',
+    },
+    {
+      id: 'l',
+      label: 'Large',
+    },
+  ];
+
+  const [toggleTitleButtonSize, setToggleTitleButtonSize] = useState<
+    EuiTitleSize
+  >('m');
+
+  const titleOnChange = (optionId: EuiTitleSize) => {
+    setToggleTitleButtonSize(optionId);
+  };
+
+  return (
+    <>
+      <p>EuiInlineEdit - Text</p>
+
+      <EuiSpacer />
+
+      <EuiButtonGroup
+        legend="This is a basic group"
+        options={textSizeButtons}
+        idSelected={toggleTextButtonSize}
+        onChange={(id) => textOnChange(id as TextSize)}
+      />
+
+      <EuiSpacer />
+
+      <EuiInlineEdit
+        display="text"
+        inputAriaLabel="textControlInput"
+        defaultValue="Hello World!"
+        size={toggleTextButtonSize}
+      />
+
+      <EuiSpacer />
+
+      <p>EuiInlineEdit - Title</p>
+
+      <EuiSpacer />
+
+      <EuiButtonGroup
+        legend="This is a basic group"
+        options={titleSizeButtons}
+        idSelected={toggleTitleButtonSize}
+        onChange={(id) => titleOnChange(id as EuiTitleSize)}
+      />
+
+      <EuiSpacer />
+
+      <EuiInlineEdit
+        display="title"
+        inputAriaLabel="textControlInput"
+        defaultValue="Hello World (but as a title)!"
+        size={toggleTitleButtonSize}
+      />
+    </>
+  );
+};
