6dc960b68cc316433875eb28a76dca2038f3f0de
Constance Chen <constance.chen.3@gmail.com>
2023-02-28 16:02:25 -0800

    Set up header & footer render

--- src/components/datagrid/body/data_grid_body_custom.tsx
+++ src/components/datagrid/body/data_grid_body_custom.tsx
@@ -6,10 +6,96 @@
  * Side Public License, v 1.
  */
 
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
+
+import { useDefaultColumnWidth, useColumnWidths } from '../utils/col_widths';
 
 import { EuiDataGridBodyProps } from '../data_grid_types';
+import { useDataGridHeader } from './header';
+import { useDataGridFooter } from './footer';
 
-export const EuiDataGridBodyCustomRender: FunctionComponent<EuiDataGridBodyProps> = () => {
-  return <div className="euiDataGrid__customRenderBody">TODO</div>;
+export const EuiDataGridBodyCustomRender: FunctionComponent<EuiDataGridBodyProps> = ({
+  renderCustomGridBody,
+  leadingControlColumns,
+  trailingControlColumns,
+  columns,
+  schema,
+  schemaDetectors,
+  visibleRows,
+  renderCellPopover,
+  renderFooterCellValue,
+  interactiveCellId,
+  headerIsInteractive,
+  handleHeaderMutation,
+  setVisibleColumns,
+  switchColumnPos,
+  onColumnResize,
+  gridWidth,
+}) => {
+  /**
+   * Columns & widths
+   */
+  const visibleColumns = useMemo(() => {
+    return [...leadingControlColumns, ...columns, ...trailingControlColumns];
+  }, [columns, leadingControlColumns, trailingControlColumns]);
+
+  // compute the default column width from the container's width and count of visible columns
+  const defaultColumnWidth = useDefaultColumnWidth(
+    gridWidth,
+    leadingControlColumns,
+    trailingControlColumns,
+    columns
+  );
+
+  const { columnWidths, setColumnWidth } = useColumnWidths({
+    columns,
+    leadingControlColumns,
+    trailingControlColumns,
+    defaultColumnWidth,
+    onColumnResize,
+  });
+
+  /**
+   * Header & footer
+   */
+  const { headerRow } = useDataGridHeader({
+    headerIsInteractive,
+    handleHeaderMutation,
+    switchColumnPos,
+    setVisibleColumns,
+    leadingControlColumns,
+    trailingControlColumns,
+    columns,
+    columnWidths,
+    defaultColumnWidth,
+    setColumnWidth,
+    schema,
+    schemaDetectors,
+  });
+
+  const { footerRow } = useDataGridFooter({
+    renderFooterCellValue,
+    renderCellPopover,
+    rowIndex: visibleRows.visibleRowCount,
+    visibleRowIndex: visibleRows.visibleRowCount,
+    interactiveCellId,
+    leadingControlColumns,
+    trailingControlColumns,
+    columns,
+    columnWidths,
+    defaultColumnWidth,
+    schema,
+  });
+
+  return (
+    <div className="euiDataGrid__customRenderBody">
+      {headerRow}
+      {renderCustomGridBody!({
+        visibleColumns,
+        visibleRowData: visibleRows,
+        Cell: () => null, // TODO
+      })}
+      {footerRow}
+    </div>
+  );
 };
