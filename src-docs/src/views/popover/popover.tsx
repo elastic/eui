ecafc39110e290ee61190e20a35f02213cf40c6f
Constance Chen <constance.chen.3@gmail.com>
2023-04-06 12:46:50 -0700

    Revert "[REVERT ME] Reproduction of bug"
    This reverts commit 3d6f396062d00818d5dcad763a09ab32d6f32311.

--- src-docs/src/views/popover/popover.tsx
+++ src-docs/src/views/popover/popover.tsx
@@ -1,74 +1,33 @@
 import React, { useState } from 'react';
-import moment from 'moment';
-import {
-  EuiPopover,
-  EuiPopoverTitle,
-  EuiPopoverFooter,
-  EuiButton,
-  EuiButtonEmpty,
-  EuiHorizontalRule,
-  EuiDatePickerRange,
-  EuiDatePicker,
-} from '../../../../src';
+
+import { EuiPopover, EuiButtonEmpty, EuiText } from '../../../../src';
 
 export default () => {
-  const [isPopoverOpen3, setIsPopoverOpen3] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
-  const onButtonClick3 = () =>
-    setIsPopoverOpen3((isPopoverOpen3) => !isPopoverOpen3);
-  const closePopover3 = () => setIsPopoverOpen3(false);
+  const onButtonClick = () =>
+    setIsPopoverOpen((isPopoverOpen) => !isPopoverOpen);
+  const closePopover = () => setIsPopoverOpen(false);
 
-  const [startDate, setStartDate] = useState(moment());
-  const [endDate, setEndDate] = useState(moment().add(11, 'd'));
+  const button = (
+    <EuiButtonEmpty
+      iconType="documentation"
+      iconSide="right"
+      onClick={onButtonClick}
+    >
+      How it works
+    </EuiButtonEmpty>
+  );
 
   return (
     <EuiPopover
-      button={
-        <EuiButtonEmpty
-          iconType="questionInCircle"
-          iconSide="right"
-          onClick={onButtonClick3}
-        >
-          With title and footer button
-        </EuiButtonEmpty>
-      }
-      isOpen={isPopoverOpen3}
-      closePopover={closePopover3}
-      anchorPosition="upCenter"
+      button={button}
+      isOpen={isPopoverOpen}
+      closePopover={closePopover}
     >
-      <EuiPopoverTitle>Edit schedule</EuiPopoverTitle>
-      <EuiDatePickerRange
-        isInvalid={startDate > endDate}
-        startDateControl={
-          <EuiDatePicker
-            selected={startDate}
-            onChange={(date) => date && setStartDate(date)}
-            startDate={startDate}
-            endDate={endDate}
-            aria-label="Start date"
-            showTimeSelect
-          />
-        }
-        endDateControl={
-          <EuiDatePicker
-            selected={endDate}
-            onChange={(date) => date && setEndDate(date)}
-            startDate={startDate}
-            endDate={endDate}
-            aria-label="End date"
-            showTimeSelect
-          />
-        }
-      />
-      <EuiHorizontalRule margin="m" />
-      <EuiButton fill fullWidth>
-        Save schedule
-      </EuiButton>
-      <EuiPopoverFooter>
-        <EuiButton fullWidth color="danger">
-          Delete schedule
-        </EuiButton>
-      </EuiPopoverFooter>
+      <EuiText style={{ width: 300 }}>
+        <p>Popover content that&rsquo;s wider than the default width</p>
+      </EuiText>
     </EuiPopover>
   );
 };
