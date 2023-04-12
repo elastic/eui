7ca44eefffc997b56843afc470100551d1e04519
Constance Chen <constance.chen.3@gmail.com>
2023-04-11 17:47:36 -0700

    [EuiFormControlLayout] Clean up positioning override CSS & side CSS
    - Positioning: prefer a prop instead, so that each usage (e.g. delimited, ranges) can pass in their own custom control over absolute vs static positioning
    
    - Side: clean up EuiFormLayoutControlIcons to render separate for left vs right side, which makes for better and more correct DOM order, + clean up classes
    
    + misc cleanup

--- src/components/form/form_control_layout/form_control_layout_delimited.tsx
+++ src/components/form/form_control_layout/form_control_layout_delimited.tsx
@@ -57,7 +57,7 @@ export const EuiFormControlLayoutDelimited: FunctionComponent<EuiFormControlLayo
   });
 
   return (
-    <EuiFormControlLayout className={classes} {...rest}>
+    <EuiFormControlLayout className={classes} iconsPosition="static" {...rest}>
       {addClassesToControl(startControl)}
       <EuiFormControlDelimiter
         delimiter={delimiter}
