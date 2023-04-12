a4e2f59d943fa00baa3ba270c78cb6a4ee6614bd
1Copenut <trevor.pierce@elastic.co>
2023-04-12 16:37:25 -0500

    Added Cypress and Jest tests for isAutoFocused prop.

--- src/components/toast/toast.a11y.tsx
+++ src/components/toast/toast.a11y.tsx
@@ -6,7 +6,9 @@
  * Side Public License, v 1.
  */
 
-/// <reference types="../../../cypress/support"/>
+/// <reference types="cypress" />
+/// <reference types="cypress-real-events" />
+/// <reference types="../../../cypress/support" />
 
 import React from 'react';
 import { EuiButton } from '../button';
@@ -17,7 +19,7 @@ describe('EuiToast', () => {
     cy.viewport(1024, 768); // medium breakpoint
   });
 
-  describe('Keyboard and screen reader accessibility', () => {
+  describe('Automated accessibility check', () => {
     it('renders with default props', () => {
       cy.mount(
         <div style={{ maxWidth: '60ch', marginInline: 'auto' }}>
@@ -28,23 +30,35 @@ describe('EuiToast', () => {
       );
       cy.checkAxe();
     });
+  });
 
-    it('renders with default props', () => {
+  describe('Keyboard and screen reader accessibility', () => {
+    it('renders with autoFocus', () => {
+      const toastActionSpy = cy.spy().as('toastActionSpy');
       cy.mount(
         <div style={{ maxWidth: '60ch', marginInline: 'auto' }}>
-          <EuiToast title="Actionable toast">
+          <EuiToast title="Actionable toast" onClose={() => {}} isAutoFocused>
             <>
               <p>This is a security measure.</p>
               <p>
                 Please click the button below or move your mouse to show that
                 you&rsquo;re still using Kibana.
               </p>
-              <EuiButton autoFocus>Extend my session</EuiButton>
+              <EuiButton
+                data-test-subj="toastActionButton"
+                onClick={toastActionSpy}
+              >
+                Extend my session
+              </EuiButton>
             </>
           </EuiToast>
         </div>
       );
-      cy.focused().contains('Extend my session');
+      cy.get('[data-test-subj="toastCloseButton"]').should('have.focus');
+      cy.realPress('Tab');
+      cy.get('[data-test-subj="toastActionButton"]').should('have.focus');
+      cy.realPress('Enter');
+      cy.get('@toastActionSpy').should('have.been.called');
       cy.checkAxe();
     });
   });
