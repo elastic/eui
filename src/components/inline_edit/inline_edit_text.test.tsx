283f8a056455e16807f7f62489276faf6129962b
Bree Hall <briannajdhall@gmail.com>
2023-04-18 16:08:58 -0400

    Added snapshots for read mode and edit mode for the Text and Title variations for EuiInlineEdit

--- src/components/inline_edit/inline_edit_text.test.tsx
+++ src/components/inline_edit/inline_edit_text.test.tsx
@@ -10,20 +10,70 @@ import React from 'react';
 import { render } from '../../test/rtl';
 import { requiredProps } from '../../test/required_props';
 
-import { EuiInlineEditText } from './inline_edit_text';
+import { EuiInlineEditText, EuiInlineEditTextProps } from './inline_edit_text';
+import { TEXT_SIZES } from '../text/text';
 
-describe('EuiInlineEditText', () => {
+describe('EuiInlineEditTitle', () => {
+  test('renders as title in readMode', () => {
+    const { container } = render(
+      <EuiInlineEditText
+        inputAriaLabel={'textInputReadModeTest'}
+        defaultValue="hello world"
+        {...requiredProps}
+      />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('renders as title in editMode', () => {
+    const { container } = render(
+      <EuiInlineEditText
+        inputAriaLabel={'textInputEditModeTest'}
+        defaultValue="hello world"
+        startWithEditOpen={true}
+        {...requiredProps}
+      />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
   describe('props', () => {
-    test('renders as text', () => {
-      const { container } = render(
-        <EuiInlineEditText
-          inputAriaLabel={'textInput'}
-          defaultValue="hello world"
-          {...requiredProps}
-        />
-      );
+    describe('sizes', () => {
+      // Remove 'relative' from text sizes available for EuiInlineEditText
+      const usedTextSizes = TEXT_SIZES.filter((size) => size !== 'relative');
 
-      expect(container.firstChild).toMatchSnapshot();
+      usedTextSizes.forEach((size: string) => {
+        test(`${size} is rendered in readMode`, () => {
+          const component = render(
+            <EuiInlineEditText
+              size={size as EuiInlineEditTextProps['size']}
+              defaultValue="Hello World!"
+              inputAriaLabel="inlineEditLabel"
+              saveButtonAriaLabel="saveEditButton"
+            />
+          );
+
+          expect(component).toMatchSnapshot();
+        });
+      });
+
+      usedTextSizes.forEach((size: string) => {
+        test(`${size} is rendered in editMode`, () => {
+          const component = render(
+            <EuiInlineEditText
+              size={size as EuiInlineEditTextProps['size']}
+              defaultValue="Hello World!"
+              inputAriaLabel="inlineEditLabel"
+              saveButtonAriaLabel="saveEditButton"
+              startWithEditOpen={true}
+            />
+          );
+
+          expect(component).toMatchSnapshot();
+        });
+      });
     });
   });
 });
--- src/components/inline_edit/inline_edit_title.test.tsx
+++ src/components/inline_edit/inline_edit_title.test.tsx
@@ -11,20 +11,70 @@ import { render } from '../../test/rtl';
 import { requiredProps } from '../../test/required_props';
 
 import { EuiInlineEditTitle } from './inline_edit_title';
+import { TITLE_SIZES } from '../title/title';
 
 describe('EuiInlineEditTitle', () => {
-  describe('props', () => {
-    test('renders as title', () => {
-      const { container } = render(
-        <EuiInlineEditTitle
-          inputAriaLabel={'textInput'}
-          defaultValue="hello world"
-          heading="h2"
-          {...requiredProps}
-        />
-      );
+  test('renders as title in readMode', () => {
+    const { container } = render(
+      <EuiInlineEditTitle
+        inputAriaLabel={'textInputReadModeTest'}
+        defaultValue="hello world"
+        heading="h1"
+        {...requiredProps}
+      />
+    );
 
-      expect(container.firstChild).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('renders as title in editMode', () => {
+    const { container } = render(
+      <EuiInlineEditTitle
+        inputAriaLabel={'textInputEditModeTest'}
+        defaultValue="hello world"
+        heading="h1"
+        startWithEditOpen={true}
+        {...requiredProps}
+      />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  describe('props', () => {
+    describe('sizes', () => {
+      TITLE_SIZES.forEach((size) => {
+        test(`${size} is rendered in readMode`, () => {
+          const component = render(
+            <EuiInlineEditTitle
+              size={size}
+              defaultValue="Hello World!"
+              inputAriaLabel="inlineEditLabel"
+              saveButtonAriaLabel="saveEditButton"
+              heading="h1"
+            />
+          );
+
+          expect(component).toMatchSnapshot();
+        });
+      });
+
+      TITLE_SIZES.forEach((size) => {
+        test(`${size} is rendered in editMode`, () => {
+          const component = render(
+            <EuiInlineEditTitle
+              size={size}
+              defaultValue="Hello World!"
+              inputAriaLabel="inlineEditLabel"
+              saveButtonAriaLabel="saveEditButton"
+              startWithEditOpen={true}
+              heading="h1"
+            />
+          );
+
+          expect(component).toMatchSnapshot();
+        });
+      });
     });
   });
 });
