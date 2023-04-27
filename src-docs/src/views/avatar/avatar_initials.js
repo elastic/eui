c4324065fc746ad1a364a7f55c661cbe6b2100f4
Constance Chen <constance.chen.3@gmail.com>
2023-04-27 12:10:30 -0700

    Update avatar docs with new `casing` prop
    + use flex/gap instead of spaces

--- src-docs/src/views/avatar/avatar_initials.js
+++ src-docs/src/views/avatar/avatar_initials.js
@@ -1,31 +1,45 @@
 import React from 'react';
 
-import { EuiAvatar, EuiTitle, EuiSpacer } from '../../../../src/components';
+import {
+  EuiAvatar,
+  EuiTitle,
+  EuiSpacer,
+  EuiFlexGroup,
+} from '../../../../src/components';
 
 export default () => (
-  <div>
+  <>
     <EuiTitle size="xs">
       <h3>Single vs multi-word</h3>
     </EuiTitle>
     <EuiSpacer />
-    <EuiAvatar name="Single" />
-    &emsp;
-    <EuiAvatar name="Two Words" />
-    &emsp;
-    <EuiAvatar name="More Than Two Words" />
-    &emsp;
-    <EuiAvatar name="lowercase words" />
+    <EuiFlexGroup responsive={false} gutterSize="xs">
+      <EuiAvatar name="Single" />
+      <EuiAvatar name="Two Words" />
+      <EuiAvatar name="More Than Two Words" />
+      <EuiAvatar name="lower_case_single" />
+    </EuiFlexGroup>
+    <EuiSpacer />
+    <EuiTitle size="xs">
+      <h3>Letter casing</h3>
+    </EuiTitle>
+    <EuiSpacer />
+    <EuiFlexGroup responsive={false} gutterSize="xs">
+      <EuiAvatar name="capitalized - default" />
+      <EuiAvatar name="upper case" casing="uppercase" />
+      <EuiAvatar name="LOWER CASE" casing="lowercase" />
+      <EuiAvatar name="weird Casing but Okay" casing="none" />
+    </EuiFlexGroup>
     <EuiSpacer />
     <EuiTitle size="xs">
       <h4>Custom</h4>
     </EuiTitle>
     <EuiSpacer />
-    <EuiAvatar name="Kibana" initialsLength={2} />
-    &emsp;
-    <EuiAvatar name="Leonardo Dude" initialsLength={1} />
-    &emsp;
-    <EuiAvatar name="Not provided" initials="?" />
-    &emsp;
-    <EuiAvatar name="Engineering User" initials="En" initialsLength={2} />
-  </div>
+    <EuiFlexGroup responsive={false} gutterSize="xs">
+      <EuiAvatar name="Kibana" initialsLength={2} />
+      <EuiAvatar name="Leonardo Dude" initialsLength={1} />
+      <EuiAvatar name="Not provided" initials="?" />
+      <EuiAvatar name="Engineering User" initials="En" initialsLength={2} />
+    </EuiFlexGroup>
+  </>
 );
