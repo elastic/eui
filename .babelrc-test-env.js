f19e2eb6029eb34ddb2be5fb66c9fbf8530595aa
Constance Chen <constance.chen.3@gmail.com>
2023-05-16 12:41:49 -0700

    Remove unnecessary `@babel/plugin-syntax-dynamic-import` plugin definition

--- .babelrc-test-env.js
+++ .babelrc-test-env.js
@@ -1,9 +1,8 @@
 module.exports = {
-  "extends": "./.babelrc.js",
-  "plugins": [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-transform-async-to-generator",
-    "@babel/plugin-syntax-dynamic-import",
-    "dynamic-import-node"
+  extends: './.babelrc.js',
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-async-to-generator',
+    'dynamic-import-node',
   ],
 };
