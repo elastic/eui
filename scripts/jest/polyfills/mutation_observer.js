8eec966bb66099dd51c202bd0f09c33026d78cfa
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-05-24 00:03:25 +0200

    test: bump MutationObserver max listeners to 45

--- scripts/jest/polyfills/mutation_observer.js
+++ scripts/jest/polyfills/mutation_observer.js
@@ -524,7 +524,7 @@ var MutationNotifier = /** @class */ (function (_super) {
   __extends(MutationNotifier, _super);
   function MutationNotifier() {
     var _this = _super.call(this) || this;
-    _this.setMaxListeners(30); // bump this as needed, it's mostly dependant on test parallelization
+    _this.setMaxListeners(45); // bump this as needed, it's mostly dependent on test parallelization
     return _this;
   }
   MutationNotifier.getInstance = function () {
