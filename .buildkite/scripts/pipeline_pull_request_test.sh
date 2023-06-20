69a12d787f898353348297afb3f36d6044d44477
1Copenut <trevor.pierce@elastic.co>
2023-06-20 09:57:22 -0500

    Upgrading jobs to use Node 18.

--- .buildkite/scripts/pipeline_pull_request_test.sh
+++ .buildkite/scripts/pipeline_pull_request_test.sh
@@ -6,9 +6,8 @@ docker run \
   -i --rm --cap-add=SYS_ADMIN --volume="$(pwd):/app" --workdir=/app \
   -e GIT_COMMITTER_NAME=test -e GIT_COMMITTER_EMAIL=test -e HOME=/tmp \
   --user="$(id -u):$(id -g)" \
-  docker.elastic.co/ci-agent-images/eui-buildkite-ci:1.0 \
-  bash -c "npm config set spin false \
-  && /opt/yarn*/bin/yarn \
+  docker.elastic.co/eui/ci:5.0 \
+  bash -c "/opt/yarn*/bin/yarn \
   && yarn cypress install \
   && NODE_OPTIONS=\"--max-old-space-size=2048\" npm run test-ci \
   && npm run build"
