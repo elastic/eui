8faca284f8b913983ee09efea610615487efdc35
Cee Chen <constance.chen@elastic.co>
2023-06-22 13:35:46 -0700

    [docs] Tweak `EuiNotificationBadge` example to show all colors

--- src-docs/src/views/badge/notification_badge.tsx
+++ src-docs/src/views/badge/notification_badge.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiNotificationBadge,
-} from '../../../../src/components';
+import { EuiFlexGroup, EuiNotificationBadge } from '../../../../src/components';
 
 export default () => (
   <EuiFlexGroup responsive={false} gutterSize="s">
-    <EuiFlexItem grow={false}>
-      <EuiNotificationBadge>3</EuiNotificationBadge>
-    </EuiFlexItem>
-    <EuiFlexItem grow={false}>
-      <EuiNotificationBadge color="success">3</EuiNotificationBadge>
-    </EuiFlexItem>
+    <EuiNotificationBadge>1</EuiNotificationBadge>
+    <EuiNotificationBadge color="success">2</EuiNotificationBadge>
+    <EuiNotificationBadge color="subdued">3</EuiNotificationBadge>
   </EuiFlexGroup>
 );
