1feeb97f022d2d6e4595d086a03e8d5996e305fb
Tomasz Kajtoch <tomek@kajto.ch>
2023-06-30 11:21:01 +0200

    build: update react-router-dom and cleanup related dependencies usage (#6892)
    * build: update react-router-dom and @types/react-router-dom to latest v5
    
    * build: Remove react-router and replace its usages to import from react-router-dom
    
    * build: remove react-router-redux and its broken usages
    
    * build: remove duplicated and unused history instances in favor of <HashRouter>
    
    * refactor: replace withRouter() with react router hooks

--- src-docs/src/store/configure_store.js
+++ src-docs/src/store/configure_store.js
@@ -1,14 +1,10 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { routerMiddleware, routerReducer } from 'react-router-redux';
-import { createHashHistory } from 'history';
 
 import Routes from '../routes';
 
 import localeReducer from './reducers/locale_reducer';
 import themeReducer from './reducers/theme_reducer';
-
-export const history = createHashHistory();
 
 /**
  * @param {Object} initialState An object defining the application's initial
@@ -17,16 +13,14 @@ export const history = createHashHistory();
 export default function configureStore(initialState) {
   function rootReducer(state = {}, action) {
     return {
-      routing: routerReducer(state.routing, action),
       theme: themeReducer(state.theme, action),
       locale: localeReducer(state.locale, action),
       routes: Routes,
     };
   }
 
-  const finalStore = compose(applyMiddleware(thunk, routerMiddleware(history)))(
-    createStore
-  )(rootReducer, initialState);
-
-  return finalStore;
+  return compose(applyMiddleware(thunk))(createStore)(
+    rootReducer,
+    initialState
+  );
 }
