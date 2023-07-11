e0be51880df2e0c352bcc0fa893670d3adc18267
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-07-11 21:49:41 +0200

    refactor: convert Cypress mount and realMount commands to TypeScript

--- cypress/support/setup/mount.tsx
+++ cypress/support/setup/mount.tsx
@@ -6,10 +6,15 @@
  * Side Public License, v 1.
  */
 
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { mount as cypressMount } from 'cypress/react';
 import { EuiProvider } from '../../../src';
 
-Cypress.Commands.add('mount', (children) => {
+const mountCommand = (children: ReactNode): ReturnType<typeof cypressMount> => {
   return cypressMount(<EuiProvider>{children}</EuiProvider>);
-});
+};
+
+// Export only the type to not confuse code-completion tools
+export type mountCommand = typeof mountCommand;
+
+Cypress.Commands.add('mount', mountCommand);
