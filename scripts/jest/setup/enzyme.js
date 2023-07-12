69ab23abfcd09f4d454134998ab8ab398bfe1ccc
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-07-12 19:25:35 +0200

    build: add @cfaester/enzyme-adapter-react-18 enzyme adapter and use it when REACT_VERSION is 18

--- scripts/jest/setup/enzyme.js
+++ scripts/jest/setup/enzyme.js
@@ -1,4 +1,13 @@
 import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+// Pick Enzyme adapter based on which React version is currently being tested.
+// It has to be directly compared against process.env.REACT_VERSION
+// for tree-shaking to work.
+let Adapter;
+if (process.env.REACT_VERSION === '18') {
+  Adapter = require('@cfaester/enzyme-adapter-react-18').default;
+} else {
+  Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
+}
 
 configure({ adapter: new Adapter() });
