34bb28541560c9e7a62c171a91e542128e50da1e
Cee Chen <constance.chen@elastic.co>
2023-07-14 13:16:33 -0700

    Update EuiBasicTable's PaginationBar to use `EuiTablePagination` defaults

--- src/components/basic_table/pagination_bar.test.tsx
+++ src/components/basic_table/pagination_bar.test.tsx
@@ -11,6 +11,8 @@ import { fireEvent } from '@testing-library/react';
 import { render } from '../../test/rtl';
 import { requiredProps } from '../../test';
 
+import { EuiProvider } from '../provider';
+
 import { PaginationBar } from './pagination_bar';
 
 describe('PaginationBar', () => {
@@ -46,5 +48,66 @@ describe('PaginationBar', () => {
 
     fireEvent.click(getByLabelText('Page 2 of 2'));
     expect(onPageChange).toHaveBeenCalledWith(1);
+  });
+
+  describe('EuiTablePagination component defaults', () => {
+    it('falls back to EuiTablePagination defaults', () => {
+      const { getByText, getByTestSubject } = render(
+        <PaginationBar
+          {...props}
+          pagination={{ pageIndex: 1, totalItemCount: 100 }}
+        />
+      );
+
+      expect(getByText('Rows per page: 50')).toBeTruthy();
+      fireEvent.click(getByTestSubject('tablePaginationPopoverButton'));
+      expect(getByTestSubject('tablePagination-10-rows')).toBeTruthy();
+      expect(getByTestSubject('tablePagination-20-rows')).toBeTruthy();
+      expect(getByTestSubject('tablePagination-50-rows')).toBeTruthy();
+      expect(getByTestSubject('tablePagination-100-rows')).toBeTruthy();
+    });
+
+    it('correctly uses configured EuiTablePagination defaults', () => {
+      const { getByText, getByTestSubject } = render(
+        <EuiProvider
+          componentDefaults={{
+            EuiTablePagination: {
+              itemsPerPage: 5,
+              itemsPerPageOptions: [5, 15],
+            },
+          }}
+        >
+          <PaginationBar
+            {...props}
+            pagination={{ pageIndex: 1, totalItemCount: 100 }}
+          />
+        </EuiProvider>,
+        { wrapper: undefined }
+      );
+
+      expect(getByText('Rows per page: 5')).toBeTruthy();
+      fireEvent.click(getByTestSubject('tablePaginationPopoverButton'));
+      expect(getByTestSubject('tablePagination-5-rows')).toBeTruthy();
+      expect(getByTestSubject('tablePagination-15-rows')).toBeTruthy();
+    });
+
+    it('correctly overrides all defaults', () => {
+      const { getByText, getByTestSubject } = render(
+        <PaginationBar
+          {...props}
+          pagination={{
+            pageIndex: 3,
+            totalItemCount: 20,
+            pageSize: 4,
+            pageSizeOptions: [2, 4],
+          }}
+        />
+      );
+
+      expect(getByText('Rows per page: 4')).toBeTruthy();
+      fireEvent.click(getByTestSubject('tablePaginationPopoverButton'));
+      expect(getByTestSubject('tablePagination-2-rows')).toBeTruthy();
+      expect(getByTestSubject('tablePagination-4-rows')).toBeTruthy();
+    });
   });
 });
