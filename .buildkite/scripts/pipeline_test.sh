c74794ffe83487177a77ffc437bb14f9bf7b59b6
Trevor Pierce <1Copenut@users.noreply.github.com>
2023-07-17 15:56:21 -0500

    [BUILDKITE] Adding job to deploy docs on push to `main` (#6947)
    * Adding env check and job to deploy docs on push to main.
    * Bumping Node version to latest 18 LTS slim.
    * Renaming bash scripts for better reuse.
    * Adding a BUILDKITE variable to test docs deploy routing.
    * Forgot to capture an env variable.
    * Reverting GH branch logic change.
    * Testing missing PR number env variable.
    * Replacing prior Jenkins PR string.
    * Added logic for Jenkins and BK with clear commenting.
    * Added CI boolean guard clauses for both environments.
    * Passing env variables through to Docker run.
    * Refactoring CI env variables for uniqueness.

--- .buildkite/scripts/pipeline_test.sh
+++ .buildkite/scripts/pipeline_test.sh
@@ -6,7 +6,7 @@ docker run \
   -i --rm --cap-add=SYS_ADMIN --volume="$(pwd):/app" --workdir=/app \
   -e GIT_COMMITTER_NAME=test -e GIT_COMMITTER_EMAIL=test -e HOME=/tmp \
   --user="$(id -u):$(id -g)" \
-  docker.elastic.co/eui/ci:5.0 \
+  docker.elastic.co/eui/ci:5.1 \
   bash -c "/opt/yarn*/bin/yarn \
   && yarn cypress install \
   && NODE_OPTIONS=\"--max-old-space-size=2048\" npm run test-ci"
