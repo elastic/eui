6dcff8ac779fca5a58b392ffd62b5c6ef87ec87c
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-07-20 01:07:11 +0200

    test: convert EuiHeader unit tests using enzyme render to RTL

--- src/components/header/header_links/header_links.test.tsx
+++ src/components/header/header_links/header_links.test.tsx
@@ -7,9 +7,9 @@
  */
 
 import React from 'react';
-import { render } from 'enzyme';
 import { requiredProps } from '../../../test';
 import { shouldRenderCustomStyles } from '../../../test/internal';
+import { render } from '../../../test/rtl';
 
 import { EuiHeaderLinks, GUTTER_SIZES } from './header_links';
 
@@ -19,24 +19,26 @@ describe('EuiHeaderLinks', () => {
   });
 
   test('is rendered', () => {
-    const component = render(<EuiHeaderLinks {...requiredProps} />);
+    const { container } = render(<EuiHeaderLinks {...requiredProps} />);
 
-    expect(component).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   describe('gutterSize', () => {
     GUTTER_SIZES.forEach((gutterSize) => {
       test(`${gutterSize} is rendered`, () => {
-        const component = render(<EuiHeaderLinks gutterSize={gutterSize} />);
+        const { container } = render(
+          <EuiHeaderLinks gutterSize={gutterSize} />
+        );
 
-        expect(component).toMatchSnapshot();
+        expect(container.firstChild).toMatchSnapshot();
       });
     });
   });
 
   describe('popover props', () => {
     test('is rendered', () => {
-      const component = render(
+      const { container } = render(
         <EuiHeaderLinks
           popoverBreakpoints={['xs', 's', 'm', 'l', 'xl']}
           popoverButtonProps={{
@@ -47,13 +49,15 @@ describe('EuiHeaderLinks', () => {
         />
       );
 
-      expect(component).toMatchSnapshot();
+      expect(container.firstChild).toMatchSnapshot();
     });
 
     test('is never rendered with "none"', () => {
-      const component = render(<EuiHeaderLinks popoverBreakpoints={'none'} />);
+      const { container } = render(
+        <EuiHeaderLinks popoverBreakpoints={'none'} />
+      );
 
-      expect(component).toMatchSnapshot();
+      expect(container.firstChild).toMatchSnapshot();
     });
   });
 });
