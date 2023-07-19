be91b67f29d7845b4cd26ca5954edfb613dde9d8
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-07-20 00:24:49 +0200

    test: convert EuiForm unit tests using enzyme render to RTL

--- src/components/form/text_area/text_area.test.tsx
+++ src/components/form/text_area/text_area.test.tsx
@@ -7,32 +7,29 @@
  */
 
 import React from 'react';
-import { render } from 'enzyme';
 import { requiredProps } from '../../../test/required_props';
+import { render } from '../../../test/rtl';
 
 import { EuiForm } from '../form';
 import { EuiTextArea } from './text_area';
 
 describe('EuiTextArea', () => {
   test('is rendered', () => {
-    const component = render(<EuiTextArea {...requiredProps} />);
+    const { container } = render(<EuiTextArea {...requiredProps} />);
 
-    expect(component).toMatchSnapshot();
+    expect(container.firstChild).toMatchSnapshot();
   });
 
   describe('inherits', () => {
     test('fullWidth from <EuiForm />', () => {
-      const component = render(
+      const { container } = render(
         <EuiForm fullWidth>
           <EuiTextArea />
         </EuiForm>
       );
 
-      if (!component.find('.euiTextArea').hasClass('euiTextArea--fullWidth')) {
-        throw new Error(
-          'expected EuiTextArea to inherit fullWidth from EuiForm'
-        );
-      }
+      const element = container.querySelector('.euiTextArea');
+      expect(element).toHaveClass('euiTextArea--fullWidth');
     });
   });
 });
