c6b39d53450f9a95fc7f1567f76446566c684100
Cee Chen <constance.chen@elastic.co>
2023-07-21 18:29:50 -0700

    Convert more Sass styles
    - simplify border

--- src/components/filter_group/filter_select_item.styles.ts
+++ src/components/filter_group/filter_select_item.styles.ts
@@ -9,12 +9,32 @@
 import { css } from '@emotion/react';
 
 import { UseEuiTheme } from '../../services';
-import { euiFontSize } from '../../global_styling';
+import {
+  logicalCSS,
+  logicalShorthandCSS,
+  logicalTextAlignCSS,
+  euiFontSize,
+} from '../../global_styling';
 
 export const euiFilterSelectItemStyles = (euiThemeContext: UseEuiTheme) => {
+  const { euiTheme } = euiThemeContext;
   return {
     euiFilterSelectItem: css`
+      display: block; /* Necessary to make sure it doesn't force the whole popover to be too wide */
+      ${logicalCSS('width', '100%')}
+      ${logicalShorthandCSS(
+        'padding',
+        `${euiTheme.size.xs} ${euiTheme.size.m}`
+      )}
+
       ${euiFontSize(euiThemeContext, 's')}
+      ${logicalTextAlignCSS('left')}
+
+      color: ${euiTheme.colors.text};
+      ${logicalCSS(
+        'border-bottom',
+        `${euiTheme.border.width.thin} solid ${euiTheme.colors.lightestShade}`
+      )}
     `,
   };
 };
