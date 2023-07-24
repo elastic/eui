8945de5dd469525b9166900b568a584d65a9bb5a
Cee Chen <549407+cee-chen@users.noreply.github.com>
2023-07-24 10:50:05 -0700

    [Emotion] Add new `focus.transparency` and `focus.backgroundColor` theme tokens (#6984)
    * Add `focus.transparency` and `focus.backgroundColor` to EUI theme
    
    * Update focus states comments
    
    - remove unused `outline` var, unlikely we'll use this at any point
    
    - removed TODO esque comment
    
    * changelog

--- src/global_styling/variables/states.ts
+++ src/global_styling/variables/states.ts
@@ -9,18 +9,6 @@
 import { ColorModeSwitch } from '../../services/theme/types';
 import { CSSProperties } from 'react';
 
-/**
- * NOTE: These were quick conversions of their Sass counterparts.
- *       The commented out keys have not been established as necessary yet.
- */
-
-export interface _EuiThemeFocusOutline {
-  /**
-   * A single CSS property: value
-   */
-  [key: string]: ColorModeSwitch;
-}
-
 export interface _EuiThemeFocus {
   /**
    * Default color of the focus ring, some components may override this property
@@ -33,15 +21,13 @@ export interface _EuiThemeFocus {
    */
   width: CSSProperties['borderWidth'];
   /**
-   * Used to transparentize any color at certain values
+   * Used to transparentize the focus background color
+   * - Default value: { LIGHT: 0.1, DARK: 0.2 }
    */
-  // transparency: ColorModeSwitch<number>;
+  transparency: ColorModeSwitch<number>;
   /**
-   * Default color plus transparency
+   * Default focus background color. Not all components set a background color on focus
+   * - Default value: `colors.primary` computed with `focus.transparency`
    */
-  // backgroundColor: ColorModeSwitch;
-  /**
-   * Using `outline` is new for Amsterdam but is set to `none` in legacy theme
-   */
-  // outline: _EuiThemeFocusOutline;
+  backgroundColor: ColorModeSwitch;
 }
