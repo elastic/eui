6436c999b2705088a7bf31d760197e27e7e94120
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-07-25 14:43:17 +0200

    fix: fix custom table docs title typing

--- src-docs/src/views/tables/custom/custom.tsx
+++ src-docs/src/views/tables/custom/custom.tsx
@@ -296,7 +296,7 @@ export default class extends Component<{}, State> {
             size="m"
             style={{ verticalAlign: 'text-top' }}
           />{' '}
-          {title}
+          {title as ReactNode}
         </span>
       ),
     },
