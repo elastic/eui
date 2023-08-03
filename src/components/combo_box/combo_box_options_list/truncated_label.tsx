83bd86addf25d05860a44101f96ba6cdb9e5454b
dej611 <dej611@gmail.com>
2023-08-03 12:30:06 +0200

    :ok_hand: Remove canvas usage + remove dependency

--- src/components/combo_box/combo_box_options_list/truncated_label.tsx
+++ src/components/combo_box/combo_box_options_list/truncated_label.tsx
@@ -25,20 +25,43 @@ const separator = `…`;
 const LABEL_VISIBLE_LENGTH = 8; // empirically found?
 const COMBOBOX_PADDINGS = 40; // empirically found?
 
-const createContext = () =>
-  document.createElement('canvas').getContext('2d') as CanvasRenderingContext2D;
+let divEl: HTMLDivElement | null = null;
 
-// extracted from getTextWidth for performance
-let context: CanvasRenderingContext2D | null = null;
+const createDivEl = (font: string) => {
+  const el = document.createElement('div');
+  el.style.position = 'absolute';
+  el.style.float = 'left';
+  el.style.whiteSpace = 'no-wrap';
+  el.style.visibility = 'hidden';
+  el.style.left = '-1000px';
+  el.style.top = '-1000px';
+  el.style.font = font;
+  return el;
+};
+
+const ensureDivEl = (font: string) => {
+  if (divEl) {
+    return;
+  }
+  divEl = createDivEl(font);
+  document.body.appendChild(divEl);
+};
+
+const cleanup = () => {
+  if (!divEl) {
+    return;
+  }
+  document.body.removeChild(divEl);
+  divEl = null;
+};
 
 const getTextWidth = (text: string, font: string) => {
-  if (context == null) {
-    context = createContext();
+  ensureDivEl(font);
+  if (!divEl) {
+    return 0;
   }
-  const ctx = context;
-  ctx.font = font;
-  const metrics = ctx.measureText(text);
-  return metrics.width;
+  divEl.textContent = text;
+  return divEl.clientWidth;
 };
 
 const truncateLabel = (
@@ -49,7 +72,8 @@ const truncateLabel = (
   labelFn: (label: string, length: number) => string
 ) => {
   let output = labelFn(label, approximateLength);
-  const initialApproximateLength = approximateLength;
+
+  ensureDivEl(font);
 
   while (getTextWidth(output, font) > width) {
     approximateLength = approximateLength - 1;
@@ -59,7 +83,7 @@ const truncateLabel = (
     }
     output = newOutput;
   }
-  console.log({ label, output, initialApproximateLength, approximateLength });
+  cleanup();
   return output;
 };
 
@@ -99,7 +123,11 @@ export const TruncatedLabel = function ({
   defaultComboboxWidth,
   truncation,
 }: TruncatedLabelProps) {
-  const textWidth = useMemo(() => getTextWidth(label, font), [label, font]);
+  // avoid measure if truncation is at the end, CSS will take care of it
+  const textWidth = useMemo(
+    () => (truncation === 'end' ? 0 : getTextWidth(label, font)),
+    [truncation, label, font]
+  );
   const usableWidth = defaultComboboxWidth - COMBOBOX_PADDINGS;
 
   // Use CSS truncation when available
