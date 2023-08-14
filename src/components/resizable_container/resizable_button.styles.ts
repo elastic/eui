a7eb4b5b2d0bd1d572ac59589372f5bfff2334bc
Cee Chen <constance.chen@elastic.co>
2023-08-14 13:20:49 -0700

    Convert more base styles to Emotion
    + remove $euiResizableButtonTransitionSpeed

--- src/components/resizable_container/resizable_button.styles.ts
+++ src/components/resizable_container/resizable_button.styles.ts
@@ -8,16 +8,58 @@
 
 import { css } from '@emotion/react';
 
-import { UseEuiTheme } from '../../services';
+import { UseEuiTheme, transparentize } from '../../services';
+import { logicalCSS, mathWithUnits } from '../../global_styling';
 
 export const euiResizableButtonStyles = (euiThemeContext: UseEuiTheme) => {
   const { euiTheme } = euiThemeContext;
+
+  const transitionSpeed = euiTheme.animation.fast;
+  const transition = `${transitionSpeed} ease`;
 
   return {
     euiResizableButton: css`
       position: relative;
       flex-shrink: 0;
       z-index: 1;
+
+      &::before,
+      &::after {
+        content: '';
+        display: block;
+        position: absolute;
+        ${logicalCSS('top', '50%')}
+        ${logicalCSS('left', '50%')}
+        background-color: ${euiTheme.colors.darkestShade};
+        transition: width ${transition}, height ${transition},
+          transform ${transition}, background-color ${transition};
+      }
+
+      /* Lighten the "grab" icon on :hover */
+      &:hover {
+        &::before,
+        &::after {
+          background-color: ${euiTheme.colors.mediumShade};
+          /* Delay transition on hover so animation is not accidentally triggered on mouse over */
+          transition-delay: ${transitionSpeed};
+        }
+      }
+
+      /* Add a transparent background to the container and
+         emphasize the "grab" icon with primary color on :focus */
+      &:focus {
+        background-color: ${transparentize(euiTheme.colors.primary, 0.1)};
+
+        &::before,
+        &::after {
+          background-color: ${euiTheme.colors.primary};
+
+          /* Overrides default transition so that "grab" icon background-color doesn't animate */
+          transition: width ${transition}, height ${transition},
+            transform ${transition};
+          transition-delay: ${mathWithUnits(transitionSpeed, (x) => x / 2)};
+        }
+      }
     `,
   };
 };
