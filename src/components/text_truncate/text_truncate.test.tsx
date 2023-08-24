4a96de881f2a47b71299e1640646a970eb78b56c
Cee Chen <constance.chen@elastic.co>
2023-08-24 14:27:43 -0700

    [misc cleanup] improve tests
    - add basic unit test for confirming that the right utils areb eing called
    
    - add missing E2E test for resize observer behavior

--- src/components/text_truncate/text_truncate.test.tsx
+++ src/components/text_truncate/text_truncate.test.tsx
@@ -11,9 +11,25 @@ import { render } from '../../test/rtl';
 import { shouldRenderCustomStyles } from '../../test/internal';
 import { requiredProps } from '../../test';
 
+// Util mocks
+const mockEarlyReturn = { checkIfTruncationIsNeeded: () => false };
+const mockCleanup = jest.fn();
+jest.mock('./utils', () => {
+  return {
+    TruncationUtilsWithDOM: jest.fn(() => ({
+      ...mockEarlyReturn,
+      cleanup: mockCleanup,
+    })),
+    TruncationUtilsWithCanvas: jest.fn(() => mockEarlyReturn),
+  };
+});
+import { TruncationUtilsWithCanvas } from './utils';
+
 import { EuiTextTruncate } from './text_truncate';
 
 describe('EuiTextTruncate', () => {
+  beforeEach(() => jest.clearAllMocks());
+
   const props = {
     text: 'Hello world',
     width: 50,
@@ -29,20 +45,40 @@ describe('EuiTextTruncate', () => {
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  it('does not render a resize observer if a width is passed', () => {
-    const onResize = jest.fn();
-    render(<EuiTextTruncate {...props} width={100} onResize={onResize} />);
-    expect(onResize).not.toHaveBeenCalled();
+  describe('resize observer', () => {
+    it('does not render a resize observer if a width is passed', () => {
+      const onResize = jest.fn();
+      render(<EuiTextTruncate {...props} width={100} onResize={onResize} />);
+      expect(onResize).not.toHaveBeenCalled();
+    });
+
+    it('renders a resize observer when no width is passed', () => {
+      const onResize = jest.fn();
+      render(
+        <EuiTextTruncate {...props} width={undefined} onResize={onResize} />
+      );
+      expect(onResize).toHaveBeenCalledWith(0);
+    });
   });
 
-  it('renders a resize observer when no width is passed', () => {
-    const onResize = jest.fn();
-    render(
-      <EuiTextTruncate {...props} width={undefined} onResize={onResize} />
-    );
-    expect(onResize).toHaveBeenCalledWith(0);
+  describe('render API', () => {
+    it('calls the DOM cleanup method after each render', () => {
+      render(<EuiTextTruncate {...props} measurementRenderAPI="dom" />);
+      expect(mockCleanup).toHaveBeenCalledTimes(1);
+    });
+
+    it('allows switching to canvas rendering via `measurementRenderAPI`', () => {
+      render(
+        <EuiTextTruncate
+          width={100}
+          text="Canvas test"
+          measurementRenderAPI="canvas"
+        />
+      );
+      expect(TruncationUtilsWithCanvas).toHaveBeenCalledTimes(1);
+      expect(mockCleanup).not.toHaveBeenCalled();
+    });
   });
 
-  // We can't unit test the actual truncation logic in JSDOM, because
-  // JSDOM doesn't have `offsetWidth`. See the Cypress spec tests instead
+  // We can't unit test the actual truncation logic in JSDOM - see Cypress spec tests instead
 });
