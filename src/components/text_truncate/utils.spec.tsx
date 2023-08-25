30196af4ffa7292604afb1d7d981ed6c540c2e7b
Cee Chen <constance.chen@elastic.co>
2023-08-25 08:55:47 -0700

    Minor test cleanup
    - remove truncation error testing from the main component - we're already testing that in the utils unit tests
    
    - reorganize utils E2E tests slightly for readability
    
    - misc wording fixes

--- src/components/text_truncate/utils.spec.tsx
+++ src/components/text_truncate/utils.spec.tsx
@@ -19,12 +19,6 @@ import React, {
 
 import { TruncationUtilsWithDOM, TruncationUtilsWithCanvas } from './utils';
 
-const sharedProps = {
-  fullText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-  availableWidth: 200,
-  ellipsis: '...',
-};
-
 // CI doesn't have access to the Inter font, so we need to manually include it
 // for font calculations to work correctly
 const font = '14px Inter';
@@ -36,63 +30,57 @@ before(() => {
     'https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap'
   );
   document.head.appendChild(linkElem);
-  cy.wait(1000); // Wait a tick to give the font time to load/swap in
+  cy.wait(1000); // Wait a second to give the font time to load/swap in
 });
 
-// Test utility for outputting the returned strings from each truncation utility
-// in React. Given the same shared props and fonts, both render methods should
-// arrive at the same truncated strings
-const TestSetup: FunctionComponent<{
-  getUtils: () => TruncationUtilsWithDOM | TruncationUtilsWithCanvas;
-}> = ({ getUtils }) => {
-  const [rendered, setRendered] = useState<ReactNode>(null);
+describe('Truncation utils', () => {
+  const sharedProps = {
+    fullText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    availableWidth: 200,
+    ellipsis: '...',
+  };
 
-  useEffect(() => {
-    const utils = getUtils();
-    setRendered(
-      <div style={{ font }}>
-        <div id="start">{utils.truncateStart()}</div>
-        <div id="end">{utils.truncateEnd()}</div>
-        <div id="middle">{utils.truncateMiddle()}</div>
-        <div id="startEnd">{utils.truncateStartEndAtMiddle()}</div>
-        <div id="startEndAt">{utils.truncateStartEndAtPosition(15)}</div>
-      </div>
+  // Test utility for outputting the returned strings from each truncation utility
+  // in React. Given the same shared props and fonts, both render methods should
+  // arrive at the same truncated strings
+  const TestSetup: FunctionComponent<{
+    getUtils: () => TruncationUtilsWithDOM | TruncationUtilsWithCanvas;
+  }> = ({ getUtils }) => {
+    const [rendered, setRendered] = useState<ReactNode>(null);
+
+    useEffect(() => {
+      const utils = getUtils();
+      setRendered(
+        <div style={{ font }}>
+          <div id="start">{utils.truncateStart()}</div>
+          <div id="end">{utils.truncateEnd()}</div>
+          <div id="middle">{utils.truncateMiddle()}</div>
+          <div id="startEnd">{utils.truncateStartEndAtMiddle()}</div>
+          <div id="startEndAt">{utils.truncateStartEndAtPosition(15)}</div>
+        </div>
+      );
+      // @ts-ignore - the `?.` handles canvas which doesn't require a cleanup
+      utils.cleanup?.();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return <>{rendered}</>;
+  };
+
+  const assertExpectedOutput = () => {
+    cy.get('#start').should('have.text', '...t, consectetur adipiscing elit');
+    cy.get('#end').should('have.text', 'Lorem ipsum dolor sit amet, ...');
+    cy.get('#middle').should('have.text', 'Lorem ipsum d...adipiscing elit');
+    cy.get('#startEnd').should(
+      'have.text',
+      '...lor sit amet, consectetur a...'
     );
-    // @ts-ignore - the `?.` handles canvas which doesn't require a cleanup
-    utils.cleanup?.();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  return <>{rendered}</>;
-};
-
-const assertExpectedOutput = () => {
-  cy.get('#start').should('have.text', '...t, consectetur adipiscing elit');
-  cy.get('#end').should('have.text', 'Lorem ipsum dolor sit amet, ...');
-  cy.get('#middle').should('have.text', 'Lorem ipsum d...adipiscing elit');
-  cy.get('#startEnd').should('have.text', '...lor sit amet, consectetur a...');
-  cy.get('#startEndAt').should('have.text', '...rem ipsum dolor sit amet, ...');
-};
-
-describe('TruncationUtilsWithDOM', () => {
-  const container = document.createElement('div');
-  container.style.font = font;
-  const props = { ...sharedProps, container };
-
-  it('truncates text as expected', () => {
-    cy.mount(
-      <TestSetup
-        getUtils={() => {
-          document.body.appendChild(container);
-          return new TruncationUtilsWithDOM(props);
-        }}
-      />
+    cy.get('#startEndAt').should(
+      'have.text',
+      '...rem ipsum dolor sit amet, ...'
     );
-    assertExpectedOutput();
-  });
-});
+  };
 
-describe('TruncationUtilsWithCanvas', () => {
-  describe('container', () => {
+  describe('TruncationUtilsWithDOM', () => {
     const container = document.createElement('div');
     container.style.font = font;
     const props = { ...sharedProps, container };
@@ -102,7 +90,7 @@ describe('TruncationUtilsWithCanvas', () => {
         <TestSetup
           getUtils={() => {
             document.body.appendChild(container);
-            return new TruncationUtilsWithCanvas(props);
+            return new TruncationUtilsWithDOM(props);
           }}
         />
       );
@@ -110,14 +98,34 @@ describe('TruncationUtilsWithCanvas', () => {
     });
   });
 
-  describe('font', () => {
-    const props = { ...sharedProps, font };
+  describe('TruncationUtilsWithCanvas', () => {
+    describe('container', () => {
+      const container = document.createElement('div');
+      container.style.font = font;
+      const props = { ...sharedProps, container };
 
-    it('truncates text as expected', () => {
-      cy.mount(
-        <TestSetup getUtils={() => new TruncationUtilsWithCanvas(props)} />
-      );
-      assertExpectedOutput();
+      it('truncates text as expected', () => {
+        cy.mount(
+          <TestSetup
+            getUtils={() => {
+              document.body.appendChild(container);
+              return new TruncationUtilsWithCanvas(props);
+            }}
+          />
+        );
+        assertExpectedOutput();
+      });
+    });
+
+    describe('font', () => {
+      const props = { ...sharedProps, font };
+
+      it('truncates text as expected', () => {
+        cy.mount(
+          <TestSetup getUtils={() => new TruncationUtilsWithCanvas(props)} />
+        );
+        assertExpectedOutput();
+      });
     });
   });
 });
