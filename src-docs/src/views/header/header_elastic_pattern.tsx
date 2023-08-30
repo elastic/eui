9690c165cf02fdb107648487722ace3049adf50b
Cee Chen <constance.chen@elastic.co>
2023-08-29 18:19:11 -0700

    [docs] Remove now-unnecessary header CSS/mixin

--- src-docs/src/views/header/header_elastic_pattern.tsx
+++ src-docs/src/views/header/header_elastic_pattern.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
@@ -52,14 +52,6 @@ export default () => {
   const guideHeaderDeploymentPopoverId = useGeneratedHtmlId({
     prefix: 'guideHeaderDeploymentPopover',
   });
-
-  useEffect(() => {
-    document.body.classList.add('euiBody--headerIsFixed--double');
-
-    return () => {
-      document.body.classList.remove('euiBody--headerIsFixed--double');
-    };
-  }, []);
 
   /**
    * Collapsible Nav
