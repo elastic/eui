287f2de56edb4000ebe70cd65367637c63fcfd4b
Michael DeFazio <michael.defazio@elastic.co>
2023-08-30 16:26:06 -0400

    Reorders icon grid to go left to right instead of vertically. (#7139)

--- src-docs/src/views/icon/logos.js
+++ src-docs/src/views/icon/logos.js
@@ -71,7 +71,7 @@ export default () => {
             : `<EuiIcon type="logoElasticsearch" size="xl" color="${iconColor}" />`}
         </EuiCodeBlock>
         <EuiSpacer />
-        <EuiFlexGrid direction="column" columns={3}>
+        <EuiFlexGrid direction="row" columns={3}>
           {iconTypes.map((iconType) => (
             <EuiFlexItem key={iconType}>
               <EuiCopy
