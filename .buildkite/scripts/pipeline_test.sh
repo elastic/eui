50343d73fc2b5fbc211b3ed343ad06dddf5dce1b
Cee Chen <549407+cee-chen@users.noreply.github.com>
2023-09-20 10:26:09 -0700

    Revert "[INFRA] Parallelize the Buildkite `test` job on PR (#7197)"
    This reverts commit 57ec9444b5dad6e6ec77db0e910b872462b49e2d.

--- .buildkite/scripts/pipeline_test.sh
+++ .buildkite/scripts/pipeline_test.sh
@@ -2,48 +2,15 @@
 
 set -euo pipefail
 
-DOCKER_OPTIONS=(
-  -i --rm
-  --env GIT_COMMITTER_NAME=test
-  --env GIT_COMMITTER_EMAIL=test
-  --env HOME=/tmp
-  --user="$(id -u):$(id -g)"
-  --volume="$(pwd):/app"
-  --workdir=/app
-  docker.elastic.co/eui/ci:5.3
-)
-
-case $TEST_TYPE in
-  lint)
-    echo "[TASK]: Running linters"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && NODE_OPTIONS=\"--max-old-space-size=2048\" yarn lint")
-    ;;
-
-  unit)
-    echo "[TASK]: Running unit tests"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && NODE_OPTIONS=\"--max-old-space-size=2048\" yarn test-unit")
-    ;;
-
-  cypress:16)
-    echo "[TASK]: Running Cypress tests against React 16"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && NODE_OPTIONS=\"--max-old-space-size=2048\" yarn test-cypress --react-version 16")
-    ;;
-
-  cypress:17)
-    echo "[TASK]: Running Cypress tests against React 17"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && NODE_OPTIONS=\"--max-old-space-size=2048\" yarn test-cypress --react-version 17")
-    ;;
-
-  cypress:18)
-    echo "[TASK]: Running Cypress tests against React 18"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && NODE_OPTIONS=\"--max-old-space-size=2048\" yarn test-cypress")
-    ;;
-
-  *)
-    echo "[ERROR]: Unknown task"
-    echo "Exit code: 1"
-    exit 1
-    ;;
-esac
-
-docker run "${DOCKER_OPTIONS[@]}"
+docker run \
+  -i --rm \
+  --env GIT_COMMITTER_NAME=test \
+  --env GIT_COMMITTER_EMAIL=test \
+  --env HOME=/tmp \
+  --user="$(id -u):$(id -g)" \
+  --volume="$(pwd):/app" \
+  --workdir=/app \
+  docker.elastic.co/eui/ci:5.3 \
+  bash -c "/opt/yarn*/bin/yarn \
+  && yarn cypress install \
+  && NODE_OPTIONS=\"--max-old-space-size=2048\" npm run test-ci"
