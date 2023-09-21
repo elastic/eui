be2d304d8a92c95aa3f44d9651d09985618ec51a
1Copenut <trevor.pierce@elastic.co>
2023-09-21 14:57:54 -0500

    Added an act RTL helper to update one test for React 16, 17.

--- src/test/rtl/index.ts
+++ src/test/rtl/index.ts
@@ -16,4 +16,5 @@ export {
   findByTestSubject,
 } from './data_test_subj_queries';
 export { render, screen, within } from './custom_render';
+export { act } from './act';
 export * from './render_hook';
