9a67186342c7d1b15193f52b7fa4119e1b9b8901
1Copenut <trevor.pierce@elastic.co>
2023-10-10 14:48:32 -0500

    [CHORE] Bumping Docker to use node-18.18.0-slim base image.

--- scripts/test-a11y-docker.js
+++ scripts/test-a11y-docker.js
@@ -9,7 +9,7 @@ execSync(
   -i --rm --cap-add=SYS_ADMIN --volume=$(pwd):/app --workdir=/app \
   -e GIT_COMMITTER_NAME=test -e GIT_COMMITTER_EMAIL=test -e HOME=/tmp \
   --user=$(id -u):$(id -g) \
-  docker.elastic.co/eui/ci:5.3 \
+  docker.elastic.co/eui/ci:5.4 \
   bash -c \'npm config set spin false \
     && /opt/yarn*/bin/yarn \
     && yarn cypress install \
