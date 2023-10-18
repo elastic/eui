f54ac6ff45fc5f80d28bf7a49fa12abf8829592c
1Copenut <trevor.pierce@elastic.co>
2023-10-18 15:10:40 -0500

    Refactoring docker run shell command to use docker compose run.

--- .buildkite/scripts/pipeline_test.sh
+++ .buildkite/scripts/pipeline_test.sh
@@ -6,52 +6,62 @@ DOCKER_OPTIONS=(
   -i --rm
   --env GIT_COMMITTER_NAME=test
   --env GIT_COMMITTER_EMAIL=test
-  --env HOME=/tmp
+  # --env HOME=/tmp
   --user="$(id -u):$(id -g)"
   --volume="$(pwd):/app"
   --workdir=/app
-  docker.elastic.co/eui/ci:5.5
+  # docker.elastic.co/eui/ci:5.5
 )
+
+YARN_COMMAND=""
 
 case $TEST_TYPE in
   lint)
     echo "[TASK]: Running linters"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn lint")
+    # DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn lint")
+    YARN_COMMAND="yarn lint"
     ;;
 
   unit:ts)
     echo "[TASK]: Running .ts and .js unit tests"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react")
+    # DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react")
+    YARN_COMMAND="yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react"
     ;;
 
   unit:tsx:16)
     echo "[TASK]: Running Jest .tsx tests against React 16"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react")
+    # DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react")
+    YARN_COMMAND="yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react"
     ;;
   
   unit:tsx:17)
     echo "[TASK]: Running Jest .tsx tests against React 17"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react")
+    # DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react")
+    YARN_COMMAND="yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react"
     ;;
   
   unit:tsx)
     echo "[TASK]: Running Jest .tsx tests against React 18"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react")
+    # DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react")
+    YARN_COMMAND="yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react"
     ;;
 
   cypress:16)
     echo "[TASK]: Running Cypress tests against React 16"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16")
+    # DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16")
+    YARN_COMMAND="yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16"
     ;;
 
   cypress:17)
     echo "[TASK]: Running Cypress tests against React 17"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17")
+    # DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17")
+    YARN_COMMAND="yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17"
     ;;
 
   cypress:18)
     echo "[TASK]: Running Cypress tests against React 18"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048")
+    # DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048")
+    YARN_COMMAND="yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048"
     ;;
 
   *)
@@ -61,4 +71,4 @@ case $TEST_TYPE in
     ;;
 esac
 
-docker run "${DOCKER_OPTIONS[@]}"
+docker compose "${DOCKER_OPTIONS[@]}" run "${YARN_COMMAND}"
