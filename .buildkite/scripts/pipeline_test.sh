b914e194a3cb9af6b7c799f7cde67d277d66624c
1Copenut <trevor.pierce@elastic.co>
2023-10-19 16:19:09 -0500

    Retrying docker-compose with simple bash echo.

--- .buildkite/scripts/pipeline_test.sh
+++ .buildkite/scripts/pipeline_test.sh
@@ -2,63 +2,65 @@
 
 set -euo pipefail
 
-DOCKER_OPTIONS=(
-  -i --rm
-  --env GIT_COMMITTER_NAME=test
-  --env GIT_COMMITTER_EMAIL=test
-  --env HOME=/tmp
-  --user="$(id -u):$(id -g)"
-  --volume="$(pwd):/app"
-  --workdir=/app
-  docker.elastic.co/eui/ci:5.5
-)
+# DOCKER_OPTIONS=(
+#   -i --rm
+#   --env GIT_COMMITTER_NAME=test
+#   --env GIT_COMMITTER_EMAIL=test
+#   --env HOME=/tmp
+#   --user="$(id -u):$(id -g)"
+#   --volume="$(pwd):/app"
+#   --workdir=/app
+#   docker.elastic.co/eui/ci:5.5
+# )
 
-case $TEST_TYPE in
-  lint)
-    echo "[TASK]: Running linters"
-    DOCKER_OPTIONS+=(bash -c "yarn lint")
-    ;;
+# case $TEST_TYPE in
+#   lint)
+#     echo "[TASK]: Running linters"
+#     DOCKER_OPTIONS+=(bash -c "yarn lint")
+#     ;;
 
-  unit:ts)
-    echo "[TASK]: Running .ts and .js unit tests"
-    DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react")
-    ;;
+#   unit:ts)
+#     echo "[TASK]: Running .ts and .js unit tests"
+#     DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react")
+#     ;;
 
-  unit:tsx:16)
-    echo "[TASK]: Running Jest .tsx tests against React 16"
-    DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react")
-    ;;
+#   unit:tsx:16)
+#     echo "[TASK]: Running Jest .tsx tests against React 16"
+#     DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react")
+#     ;;
   
-  unit:tsx:17)
-    echo "[TASK]: Running Jest .tsx tests against React 17"
-    DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react")
-    ;;
+#   unit:tsx:17)
+#     echo "[TASK]: Running Jest .tsx tests against React 17"
+#     DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react")
+#     ;;
   
-  unit:tsx)
-    echo "[TASK]: Running Jest .tsx tests against React 18"
-    DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react")
-    ;;
+#   unit:tsx)
+#     echo "[TASK]: Running Jest .tsx tests against React 18"
+#     DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react")
+#     ;;
 
-  cypress:16)
-    echo "[TASK]: Running Cypress tests against React 16"
-    DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16")
-    ;;
+#   cypress:16)
+#     echo "[TASK]: Running Cypress tests against React 16"
+#     DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16")
+#     ;;
 
-  cypress:17)
-    echo "[TASK]: Running Cypress tests against React 17"
-    DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17")
-    ;;
+#   cypress:17)
+#     echo "[TASK]: Running Cypress tests against React 17"
+#     DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17")
+#     ;;
 
-  cypress:18)
-    echo "[TASK]: Running Cypress tests against React 18"
-    DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048")
-    ;;
+#   cypress:18)
+#     echo "[TASK]: Running Cypress tests against React 18"
+#     DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048")
+#     ;;
 
-  *)
-    echo "[ERROR]: Unknown task"
-    echo "Exit code: 1"
-    exit 1
-    ;;
-esac
+#   *)
+#     echo "[ERROR]: Unknown task"
+#     echo "Exit code: 1"
+#     exit 1
+#     ;;
+# esac
 
-docker run "${DOCKER_OPTIONS[@]}"
+# docker run "${DOCKER_OPTIONS[@]}"
+
+bash -c "echo COMMAND_1 && echo COMMAND_2 from a composed docker file!"
