67b7da015fad9369ce837e282ab58055a249755a
1Copenut <trevor.pierce@elastic.co>
2023-10-19 15:35:26 -0500

    Simplifying to use smaller, cached Docker image.

--- .buildkite/scripts/pipeline_test.sh
+++ .buildkite/scripts/pipeline_test.sh
@@ -10,51 +10,49 @@ DOCKER_OPTIONS=(
   --user="$(id -u):$(id -g)"
   --volume="$(pwd):/app"
   --workdir=/app
-  docker.elastic.co/eui/ci:5.5
+  docker.elastic.co/eui/ci:5.7
 )
-
-YARN_COMMAND=""
 
 case $TEST_TYPE in
   lint)
     echo "[TASK]: Running linters"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn lint")
+    DOCKER_OPTIONS+=(bash -c "yarn lint")
     ;;
 
   unit:ts)
     echo "[TASK]: Running .ts and .js unit tests"
-    DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react")
+    DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react")
     ;;
 
-  # unit:tsx:16)
-  #   echo "[TASK]: Running Jest .tsx tests against React 16"
-  #   DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react")
-  #   ;;
+  unit:tsx:16)
+    echo "[TASK]: Running Jest .tsx tests against React 16"
+    DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react")
+    ;;
   
-  # unit:tsx:17)
-  #   echo "[TASK]: Running Jest .tsx tests against React 17"
-  #   DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react")
-  #   ;;
+  unit:tsx:17)
+    echo "[TASK]: Running Jest .tsx tests against React 17"
+    DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react")
+    ;;
   
-  # unit:tsx)
-  #   echo "[TASK]: Running Jest .tsx tests against React 18"
-  #   DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react")
-  #   ;;
+  unit:tsx)
+    echo "[TASK]: Running Jest .tsx tests against React 18"
+    DOCKER_OPTIONS+=(bash -c "yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react")
+    ;;
 
-  # cypress:16)
-  #   echo "[TASK]: Running Cypress tests against React 16"
-  #   DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16")
-  #   ;;
+  cypress:16)
+    echo "[TASK]: Running Cypress tests against React 16"
+    DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16")
+    ;;
 
-  # cypress:17)
-  #   echo "[TASK]: Running Cypress tests against React 17"
-  #   DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17")
-  #   ;;
+  cypress:17)
+    echo "[TASK]: Running Cypress tests against React 17"
+    DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17")
+    ;;
 
-  # cypress:18)
-  #   echo "[TASK]: Running Cypress tests against React 18"
-  #   DOCKER_OPTIONS+=(bash -c "/opt/yarn*/bin/yarn && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048")
-  #   ;;
+  cypress:18)
+    echo "[TASK]: Running Cypress tests against React 18"
+    DOCKER_OPTIONS+=(bash -c "yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048")
+    ;;
 
   *)
     echo "[ERROR]: Unknown task"
