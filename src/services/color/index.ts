8b67ea69697980ebe82b43ae25643dd11bbacb66
Cee Chen <549407+cee-chen@users.noreply.github.com>
2023-11-02 09:47:37 -0700

    Remove deprecated `euiPaletteComplimentary` (#7333)

--- src/services/color/index.ts
+++ src/services/color/index.ts
@@ -30,7 +30,6 @@ export {
   euiPaletteColorBlindBehindText,
   euiPaletteForStatus,
   euiPaletteForTemperature,
-  euiPaletteComplimentary,
   euiPaletteComplementary,
   euiPaletteNegative,
   euiPalettePositive,
