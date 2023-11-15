f0748683edd7b0c6b310775b86e25245b96bc418
Cee Chen <constance.chen@elastic.co>
2023-11-14 16:11:52 -0800

    [setup] create new DRY util for rendering the external link icon/indicator
    - will be used by both EuiLink and EuiListGroupItem

---
+++ src/components/link/external_link_icon.tsx
@@ -0,0 +1,67 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React, { FunctionComponent, AnchorHTMLAttributes } from 'react';
+
+import { useEuiTheme } from '../../services';
+import { logicalStyle } from '../../global_styling';
+import { EuiIcon, EuiIconProps } from '../icon';
+import { EuiI18n, useEuiI18n } from '../i18n';
+import { EuiScreenReaderOnly } from '../accessibility';
+
+/**
+ * DRY util for indicating external links both via icon and to
+ * screen readers. Used internally by at EuiLink and EuiListGroupItem
+ */
+
+export type EuiExternalLinkIconProps = {
+  target?: AnchorHTMLAttributes<HTMLAnchorElement>['target'];
+  /**
+   * Set to true to show an icon indicating that it is an external link;
+   * Defaults to true if `target="_blank"`
+   */
+  external?: boolean;
+};
+
+export const EuiExternalLinkIcon: FunctionComponent<
+  EuiExternalLinkIconProps & Partial<EuiIconProps>
+> = ({ target, external, ...rest }) => {
+  const { euiTheme } = useEuiTheme();
+
+  const showExternalLinkIcon =
+    (target === '_blank' && external !== false) || external === true;
+
+  const iconAriaLabel = useEuiI18n(
+    'euiExternalLinkIcon.ariaLabel',
+    'External link'
+  );
+
+  return (
+    <>
+      {showExternalLinkIcon && (
+        <EuiIcon
+          css={logicalStyle('margin-left', euiTheme.size.xs)}
+          aria-label={iconAriaLabel}
+          size="s"
+          type="popout"
+          {...rest}
+        />
+      )}
+      {target === '_blank' && (
+        <EuiScreenReaderOnly>
+          <span>
+            <EuiI18n
+              token="euiExternalLinkIcon.newTarget.screenReaderOnlyText"
+              default="(opens in a new tab or window)"
+            />
+          </span>
+        </EuiScreenReaderOnly>
+      )}
+    </>
+  );
+};
