72b339295569277500575ad0e55678e1dbb48d13
1Copenut <trevor.pierce@elastic.co>
2023-11-16 16:28:50 -0600

    Modifying script to change to app/ directory first to match Docker container.

--- .buildkite/scripts/pipelines/pipeline_test.sh
+++ .buildkite/scripts/pipelines/pipeline_test.sh
@@ -17,48 +17,49 @@ set -euo pipefail
 case $TEST_TYPE in
   lint)
     echo "[TASK]: Running linters"
-    DOCKER_OPTIONS+=(bash -c "yarn lint")
+    cd app && yarn lint
     ;;
 
   unit:ts)
     echo "[TASK]: Running .ts and .js unit tests"
-    yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react
+    cd app && yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react
     ;;
 
   unit:tsx:16)
     echo "[TASK]: Running Jest .tsx tests against React 16"
-    yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react
+    cd app && yarn test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react
     ;;
   
   unit:tsx:17)
     echo "[TASK]: Running Jest .tsx tests against React 17"
-    yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react
+    cd app && yarn test-unit --node-options=--max_old_space_size=2048 --react-version=17 --testMatch=react
     ;;
   
   unit:tsx)
     echo "[TASK]: Running Jest .tsx tests against React 18"
-    yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react
+    cd app && yarn test-unit --node-options=--max_old_space_size=2048 --testMatch=react
     ;;
 
   cypress:16)
     echo "[TASK]: Running Cypress tests against React 16"
-    yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16
+    cd app && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=16
     ;;
 
   cypress:17)
     echo "[TASK]: Running Cypress tests against React 17"
-    yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17
+    cd app && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048 --react-version=17
     ;;
 
   cypress:18)
     echo "[TASK]: Running Cypress tests against React 18"
-    yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048
+    cd app && yarn cypress install && yarn test-cypress --node-options=--max_old_space_size=2048
     ;;
 
   cypress:a11y)
     echo "[TASK]: Running Cypress accessibility tests against React 18"
-    yarn cypress install && yarn run test-cypress-a11y --node-options=--max_old_space_size=2048
+    cd app && yarn cypress install && yarn run test-cypress-a11y --node-options=--max_old_space_size=2048
     ;;
+
   *)
     echo "[ERROR]: Unknown task"
     echo "Exit code: 1"
