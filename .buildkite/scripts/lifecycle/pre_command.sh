0962cafef064b683892d31799a02fc0edfa018f9
1Copenut <trevor.pierce@elastic.co>
2023-11-16 09:21:09 -0600

    Refactored registry into secrets, removed a few hardcoded strings.

--- .buildkite/scripts/lifecycle/pre_command.sh
+++ .buildkite/scripts/lifecycle/pre_command.sh
@@ -6,23 +6,37 @@ set +x
 echo '[SOURCE]: Buildkite dependencies'
 source .buildkite/scripts/common/utils.sh
 
-echo '[INSTALL]: Non-exported variables'
+echo '[INSTALL]: env variables'
+# ======================================== #
+# Paths
+# ======================================== #
 DOCKER_MACHINE_STAGING=secret/ci/elastic-eui/docker-machine-staging
 GITHUB_ACCOUNT=secret/ci/elastic-eui/kibanamachine
 VAULT_ACCOUNT=secret/ci/elastic-eui/bekitzur-kibana-service-account
 
-echo '[INSTALL]: Exported variables'
+# ======================================== #
+# Secrets
+# ======================================== #
 DOCKER_STAGING_USERNAME=$(retry 5 vault read -field=username $DOCKER_MACHINE_STAGING)
 export DOCKER_STAGING_USERNAME
 
 DOCKER_STAGING_PASSWORD=$(retry 5 vault read -field=password $DOCKER_MACHINE_STAGING)
 export DOCKER_STAGING_PASSWORD
 
+DOCKER_STAGING_REGISTRY=$(retry 5 vault read -field=registry $DOCKER_MACHINE_STAGING)
+export DOCKER_STAGING_REGISTRY
+
 GCE_ACCOUNT=$(retry 5 vault read -field=value $VAULT_ACCOUNT)
 export GCE_ACCOUNT
 
 GITHUB_TOKEN=$(retry 5 vault read -field=github_token $GITHUB_ACCOUNT)
 export GITHUB_TOKEN
+
+# ======================================== #
+# Known values
+# ======================================== #
+DOCKER_STAGING_IMAGE=docker-staging/eui/ci
+export DOCKER_STAGING_IMAGE
 
 GPROJECT=elastic-bekitzur
 export GPROJECT
--- .buildkite/scripts/pipelines/pipeline_docker_build.sh
+++ .buildkite/scripts/pipelines/pipeline_docker_build.sh
@@ -3,16 +3,16 @@
 set -euo pipefail
 set +x
 
-echo "[DOCKER]: Get staging registry credentials & login"
-if [[ -z "${DOCKER_STAGING_USERNAME}" ]] || [[ -z "${DOCKER_STAGING_PASSWORD}" ]]; then
+echo "[DOCKER]: Get registry credentials & login"
+if [[ -z "${DOCKER_STAGING_USERNAME}" ]] || [[ -z "${DOCKER_STAGING_PASSWORD}" ]] || [[ -z "${DOCKER_STAGING_REGISTRY}" ]]; then
   echo ":fire: Docker credentials not set." 1>&2
   exit 1
 fi
-echo "${DOCKER_STAGING_PASSWORD}" | docker login -u "${DOCKER_STAGING_USERNAME}" --password-stdin container-registry-test.elastic.co
+echo "${DOCKER_STAGING_PASSWORD}" | docker login -u "${DOCKER_STAGING_USERNAME}" --password-stdin "${DOCKER_STAGING_REGISTRY}"
 
-echo "[DOCKER]: Build and push container to staging registry"
+echo "[DOCKER]: Build and push container to registry"
 docker-compose -f ./scripts/docker-ci/docker-compose.yml build app
-docker push container-registry-test.elastic.co/eui/ci:latest
+docker push "${DOCKER_STAGING_IMAGE}":latest
 
-echo "[DOCKER]: Log out of staging registry"
+echo "[DOCKER]: Log out of registry"
 docker logout
