9060e5b99c05b002cddb8720e1a75838240af160
1Copenut <trevor.pierce@elastic.co>
2023-11-20 16:49:47 -0600

    Bumping local Node and Docker image to v20.9.0.

--- scripts/test-a11y-docker.js
+++ scripts/test-a11y-docker.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 
-execSync('docker pull docker.elastic.co/eui/ci:5.6', {
+execSync('docker pull docker.elastic.co/eui/ci:6.0', {
   stdio: 'inherit',
 });
 /* eslint-disable-next-line no-multi-str */
@@ -9,7 +9,7 @@ execSync(
   -i --rm --cap-add=SYS_ADMIN --volume=$(pwd):/app --workdir=/app --platform=linux/amd64 \
   -e GIT_COMMITTER_NAME=test -e GIT_COMMITTER_EMAIL=test -e HOME=/tmp \
   --user=$(id -u):$(id -g) \
-  docker.elastic.co/eui/ci:5.6 \
+  docker.elastic.co/eui/ci:6.0 \
   bash -c '/opt/yarn*/bin/yarn \
     && yarn cypress install \
     && yarn run test-cypress-a11y \
--- scripts/test-docker.js
+++ scripts/test-docker.js
@@ -1,19 +1,19 @@
 const { execSync } = require('child_process');
 
-execSync('docker pull docker.elastic.co/eui/ci:5.6', {
+execSync('docker pull docker.elastic.co/eui/ci:6.0', {
   stdio: 'inherit',
 });
 /* eslint-disable-next-line no-multi-str */
 execSync(
-  'docker run \
-  -i --rm --cap-add=SYS_ADMIN --volume=$(pwd):/app --workdir=/app \
+  "docker run \
+  -i --rm --cap-add=SYS_ADMIN --volume=$(pwd):/app --workdir=/app --platform=linux/amd64 \
   -e GIT_COMMITTER_NAME=test -e GIT_COMMITTER_EMAIL=test -e HOME=/tmp \
   --user=$(id -u):$(id -g) \
-  docker.elastic.co/eui/ci:5.6 \
-  bash -c \'/opt/yarn*/bin/yarn \
+  docker.elastic.co/eui/ci:6.0 \
+  bash -c '/opt/yarn*/bin/yarn \
     && yarn cypress install \
-    && NODE_OPTIONS="--max-old-space-size=2048" npm run test-ci \
-    && npm run build\'',
+    && yarn test-ci --node-options=--max_old_space_size=2048 \
+    && yarn build'",
   {
     stdio: 'inherit',
   }
