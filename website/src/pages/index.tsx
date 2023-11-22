7f67abed571955ebb16224eff35af6554a4b162f
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-11-23 00:47:51 +0100

    feat(website): update homepage button label

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            EUI Documentation
           </Link>
         </div>
       </div>
