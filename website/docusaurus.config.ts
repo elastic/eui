bcd7f94de17f8368ee07846298e292363284d76c
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2023-11-23 00:17:30 +0100

    feat(website): update theme logo

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -58,10 +58,10 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Elastic UI',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'Elastic UI',
+        src: 'images/eui_logo.svg',
       },
       items: [
         {
