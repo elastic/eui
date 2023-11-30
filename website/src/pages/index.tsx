ed03df11427912cea4370a6a00a2c14ee0cfc52f
Tomasz Kajtoch <tomek@kajto.ch>
2023-11-30 14:28:11 +0000

    Update website/src/pages/index.tsx
    Co-authored-by: Cee Chen <549407+cee-chen@users.noreply.github.com>

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -31,8 +31,9 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description={siteConfig.tagline}
+    >
       <HomepageHeader />
     </Layout>
   );
