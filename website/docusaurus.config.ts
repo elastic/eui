86cb61a013a7254fc89acd069b36c6ad53474cb4
Tomasz Kajtoch <tomek@kajto.ch>
2023-11-30 14:29:11 +0000

    Update website/docusaurus.config.ts
    Co-authored-by: Cee Chen <549407+cee-chen@users.noreply.github.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -35,8 +35,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/elastic/eui/tree/main/website/',
         },
