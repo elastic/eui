1658188ecf1c4255251f269704462d994900a5ef
Tomasz Kajtoch <tomek@kajto.ch>
2023-12-06 21:53:17 +0100

    feat: setup new docs (#7386)
    Co-authored-by: Cee Chen <549407+cee-chen@users.noreply.github.com>

---
+++ website/src/pages/index.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            EUI Documentation
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={siteConfig.title}
+      description={siteConfig.tagline}
+    >
+      <HomepageHeader />
+    </Layout>
+  );
+}
