6f4db95301445d30bd9f27981d45ced46ac20cb5
Tomasz Kajtoch <tomek@kajto.ch>
2023-12-18 18:52:07 +0100

    feat: add a part of existing docs content to the new docs site (#7417)

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs">
             EUI Documentation
           </Link>
         </div>
