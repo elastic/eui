a9dc1dc4cdd70ccf780f8da9685388628e97ffa3
Tomasz Kajtoch <tomek@kajto.ch>
2023-12-18 18:52:07 +0100

    feat: add a part of existing docs content to the new docs site (#7417)

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'rootSidebar',
           position: 'left',
           label: 'Documentation',
         },
