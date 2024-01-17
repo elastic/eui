131ebb0fdf52df2dc9d0a2dadb6107358b744d1e
Tomasz Kajtoch <tomek@kajto.ch>
2024-01-17 17:03:29 +0100

    docs: convert forms section component docs to MDX (#7440)
    Co-authored-by: Cee Chen <549407+cee-chen@users.noreply.github.com>

---
+++ website/src/theme/MDXComponents/index.js
@@ -0,0 +1,9 @@
+import OriginalMDXComponents from '@theme-original/MDXComponents';
+import { Badge } from '@site/src/components/badge';
+
+const MDXComponents = {
+  ...OriginalMDXComponents,
+  Badge,
+};
+
+export default MDXComponents;
