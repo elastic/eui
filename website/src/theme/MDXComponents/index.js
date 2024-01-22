850aaa8df364281c2391b131cecb45a8014c88a1
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-01-22 17:43:27 +0100

    fix(website): combine js and ts MDXComponents overrides

--- website/src/theme/MDXComponents/index.js
+++
@@ -1,9 +0,0 @@
-import OriginalMDXComponents from '@theme-original/MDXComponents';
-import { Badge } from '@site/src/components/badge';
-
-const MDXComponents = {
-  ...OriginalMDXComponents,
-  Badge,
-};
-
-export default MDXComponents;
--- website/src/theme/MDXComponents/index.ts
+++ website/src/theme/MDXComponents/index.ts
@@ -1,8 +1,9 @@
 import MDXComponents from '@theme-original/MDXComponents';
-
+import { Badge } from '@site/src/components/badge';
 import { StoryEmbed } from '@site/src/components/story_embed';
 
 export default {
   ...MDXComponents,
+  Badge,
   StoryEmbed,
 };
