3f4e9ac068062cf9c82feb17d02a2ce1feab472f
Cee Chen <constance.chen@elastic.co>
2024-01-25 11:11:21 -0800

    Wrap `useMemo` around code syntax CSS variables

--- src/components/code/code_syntax.styles.ts
+++ src/components/code/code_syntax.styles.ts
@@ -6,181 +6,186 @@
  * Side Public License, v 1.
  */
 
+import { useMemo } from 'react';
 import {
-  UseEuiTheme,
+  useEuiTheme,
   makeHighContrastColor,
   euiPaletteColorBlind,
 } from '../../services';
 
 const visColors = euiPaletteColorBlind();
 
-export const euiCodeSyntaxColors = (euiThemeContext: UseEuiTheme) => {
-  const { euiTheme } = euiThemeContext;
+// These variables are computationally expensive, so it needs
+// to be a hook in order to memoize it per theme
+export const useEuiCodeSyntaxVariables = () => {
+  const { euiTheme } = useEuiTheme();
 
-  const backgroundColor = euiTheme.colors.lightestShade;
+  return useMemo(() => {
+    const backgroundColor = euiTheme.colors.lightestShade;
 
-  return {
-    backgroundColor: backgroundColor,
-    color: makeHighContrastColor(euiTheme.colors.text)(backgroundColor),
-    inlineCodeColor: makeHighContrastColor(visColors[3])(backgroundColor),
-    selectedBackgroundColor: 'inherit',
-    commentColor: makeHighContrastColor(euiTheme.colors.subduedText)(
-      backgroundColor
-    ),
-    selectorTagColor: 'inherit',
-    stringColor: makeHighContrastColor(visColors[2])(backgroundColor),
-    tagColor: makeHighContrastColor(visColors[1])(backgroundColor),
-    nameColor: makeHighContrastColor(visColors[1])(backgroundColor),
-    numberColor: makeHighContrastColor(visColors[0])(backgroundColor),
-    keywordColor: makeHighContrastColor(visColors[3])(backgroundColor),
-    functionTitleColor: 'inherit',
-    typeColor: makeHighContrastColor(visColors[1])(backgroundColor),
-    attributeColor: 'inherit',
-    symbolColor: makeHighContrastColor(visColors[9])(backgroundColor),
-    paramsColor: 'inherit',
-    metaColor: makeHighContrastColor(euiTheme.colors.subduedText)(
-      backgroundColor
-    ),
-    titleColor: makeHighContrastColor(visColors[7])(backgroundColor),
-    sectionColor: makeHighContrastColor(visColors[9])(backgroundColor),
-    additionColor: makeHighContrastColor(visColors[0])(backgroundColor),
-    deletionColor: makeHighContrastColor(euiTheme.colors.danger)(
-      backgroundColor
-    ),
-    selectorClassColor: 'inherit',
-    selectorIdColor: 'inherit',
-  };
+    return {
+      backgroundColor: backgroundColor,
+      color: makeHighContrastColor(euiTheme.colors.text)(backgroundColor),
+      inlineCodeColor: makeHighContrastColor(visColors[3])(backgroundColor),
+      selectedBackgroundColor: 'inherit',
+      commentColor: makeHighContrastColor(euiTheme.colors.subduedText)(
+        backgroundColor
+      ),
+      selectorTagColor: 'inherit',
+      stringColor: makeHighContrastColor(visColors[2])(backgroundColor),
+      tagColor: makeHighContrastColor(visColors[1])(backgroundColor),
+      nameColor: makeHighContrastColor(visColors[1])(backgroundColor),
+      numberColor: makeHighContrastColor(visColors[0])(backgroundColor),
+      keywordColor: makeHighContrastColor(visColors[3])(backgroundColor),
+      functionTitleColor: 'inherit',
+      typeColor: makeHighContrastColor(visColors[1])(backgroundColor),
+      attributeColor: 'inherit',
+      symbolColor: makeHighContrastColor(visColors[9])(backgroundColor),
+      paramsColor: 'inherit',
+      metaColor: makeHighContrastColor(euiTheme.colors.subduedText)(
+        backgroundColor
+      ),
+      titleColor: makeHighContrastColor(visColors[7])(backgroundColor),
+      sectionColor: makeHighContrastColor(visColors[9])(backgroundColor),
+      additionColor: makeHighContrastColor(visColors[0])(backgroundColor),
+      deletionColor: makeHighContrastColor(euiTheme.colors.danger)(
+        backgroundColor
+      ),
+      selectorClassColor: 'inherit',
+      selectorIdColor: 'inherit',
+
+      get tokensCss() {
+        return `
+  .token.punctuation:not(.interpolation-punctuation):not([class*='attr-']) {
+    opacity: .7;
+  }
+
+  .token.comment,
+  .token.prolog,
+  .token.doctype,
+  .token.cdata,
+  .token.coord,
+  .token.blockquote {
+    color: ${this.commentColor};
+    font-style: italic;
+  }
+
+  .token.selector {
+    color: ${this.selectorTagColor};
+  }
+
+  .token.string,
+  .token.interpolation,
+  .token.interpolation-punctuation,
+  .token.doc-comment .token.keyword,
+  .token.attr-value,
+  .token.url .token.content {
+    color: ${this.stringColor};
+  }
+
+  .token.number,
+  .token.boolean,
+  .token.keyword.nil,
+  .token.regex,
+  .token.variable,
+  .token.unit,
+  .token.hexcode,
+  .token.attr-name,
+  .token.attr-equals {
+    color: ${this.numberColor};
+  }
+
+  .token.atrule .token.rule,
+  .token.keyword {
+    color: ${this.keywordColor};
+  }
+
+  .token.function {
+    color: ${this.functionTitleColor};
+  }
+
+  .token.tag {
+    color: ${this.tagColor};
+  }
+
+  .token.class-name {
+    color: ${this.typeColor};
+  }
+
+  .token.property {
+    color: ${this.attributeColor};
+  }
+
+  .token.console,
+  .token.list-punctuation,
+  .token.url-reference,
+  .token.url .token.url {
+    color: ${this.symbolColor};
+  }
+
+  .token.paramater {
+    color: ${this.paramsColor};
+  }
+
+  .token.meta,
+  .token.important {
+    color: ${this.metaColor};
+  }
+
+  .token.title {
+    color: ${this.titleColor};
+  }
+
+  .token.section {
+    color: ${this.sectionColor};
+  }
+
+  .token.prefix.inserted,
+  .token.prefix.deleted {
+    padding-inline-start: -${euiTheme.size.xs};
+    margin-inline-start:  -${euiTheme.size.xs};
+  }
+
+  .token.prefix.inserted {
+    box-shadow: -${euiTheme.size.xs} 0 ${this.additionColor};
+    color: ${this.additionColor};
+  }
+
+  .token.prefix.deleted {
+    box-shadow: -${euiTheme.size.xs} 0 ${this.deletionColor};
+    color: ${this.deletionColor};
+  }
+
+  .token.selector .token.class {
+    color: ${this.selectorClassColor};
+  }
+
+  .token.selector .token.id {
+    color: ${this.selectorIdColor};
+  }
+
+  .token.italic {
+    font-style: italic;
+  }
+
+  .token.important,
+  .token.bold {
+    font-weight: ${euiTheme.font.weight.bold};
+  }
+
+  .token.url-reference,
+  .token.url .token.url {
+    text-decoration: underline;
+  }
+
+  .token.entity {
+    cursor: help;
+  }`;
+      },
+    };
+  }, [euiTheme]);
 };
 
-export const euiCodeSyntaxTokens = (euiThemeContext: UseEuiTheme) => {
-  const { euiTheme } = euiThemeContext;
-  const euiCodeBlock = euiCodeSyntaxColors(euiThemeContext);
-
-  return `
-    .token.punctuation:not(.interpolation-punctuation):not([class*='attr-']) {
-      opacity: .7;
-    }
-  
-    .token.comment,
-    .token.prolog,
-    .token.doctype,
-    .token.cdata,
-    .token.coord,
-    .token.blockquote {
-      color: ${euiCodeBlock.commentColor};
-      font-style: italic;
-    }
-  
-    .token.selector {
-      color: ${euiCodeBlock.selectorTagColor};
-    }
-  
-    .token.string,
-    .token.interpolation,
-    .token.interpolation-punctuation,
-    .token.doc-comment .token.keyword,
-    .token.attr-value,
-    .token.url .token.content {
-      color: ${euiCodeBlock.stringColor};
-    }
-  
-    .token.number,
-    .token.boolean,
-    .token.keyword.nil,
-    .token.regex,
-    .token.variable,
-    .token.unit,
-    .token.hexcode,
-    .token.attr-name,
-    .token.attr-equals {
-      color: ${euiCodeBlock.numberColor};
-    }
-  
-    .token.atrule .token.rule,
-    .token.keyword {
-      color: ${euiCodeBlock.keywordColor};
-    }
-  
-    .token.function {
-      color: ${euiCodeBlock.functionTitleColor};
-    }
-  
-    .token.tag {
-      color: ${euiCodeBlock.tagColor};
-    }
-  
-    .token.class-name {
-      color: ${euiCodeBlock.typeColor};
-    }
-  
-    .token.property {
-      color: ${euiCodeBlock.attributeColor};
-    }
-  
-    .token.console,
-    .token.list-punctuation,
-    .token.url-reference,
-    .token.url .token.url {
-      color: ${euiCodeBlock.symbolColor};
-    }
-  
-    .token.paramater {
-      color: ${euiCodeBlock.paramsColor};
-    }
-  
-    .token.meta,
-    .token.important {
-      color: ${euiCodeBlock.metaColor};
-    }
-  
-    .token.title {
-      color: ${euiCodeBlock.titleColor};
-    }
-  
-    .token.section {
-      color: ${euiCodeBlock.sectionColor};
-    }
-  
-    .token.prefix.inserted,
-    .token.prefix.deleted {
-      padding-inline-start: -${euiTheme.size.xs};
-      margin-inline-start:  -${euiTheme.size.xs};
-    }
-  
-    .token.prefix.inserted {
-      box-shadow: -${euiTheme.size.xs} 0 ${euiCodeBlock.additionColor};
-      color: ${euiCodeBlock.additionColor};
-    }
-  
-    .token.prefix.deleted {
-      box-shadow: -${euiTheme.size.xs} 0 ${euiCodeBlock.deletionColor};
-      color: ${euiCodeBlock.deletionColor};
-    }
-  
-    .token.selector .token.class {
-      color: ${euiCodeBlock.selectorClassColor};
-    }
-  
-    .token.selector .token.id {
-      color: ${euiCodeBlock.selectorIdColor};
-    }
-  
-    .token.italic {
-      font-style: italic;
-    }
-  
-    .token.important,
-    .token.bold {
-      font-weight: ${euiTheme.font.weight.bold};
-    }
-  
-    .token.url-reference,
-    .token.url .token.url {
-      text-decoration: underline;
-    }
-  
-    .token.entity {
-      cursor: help;
-    }
-  `;
-};
+export type UseEuiCodeSyntaxVariables = ReturnType<
+  typeof useEuiCodeSyntaxVariables
+>;
