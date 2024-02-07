eac6afbf3763b172a3af765122727a0bdb7f1e9b
José Luis González <joseluisgj@gmail.com>
2024-02-07 16:39:26 +0100

    Fixing width and viewBox to 16px

--- src/components/icon/assets/index.tsx
+++ src/components/icon/assets/index.tsx
@@ -21,9 +21,9 @@ const EuiIconIndex = ({
 }: SVGProps<SVGSVGElement> & SVGRProps) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={17}
+    width={16}
     height={16}
-    viewBox="0 0 17 16"
+    viewBox="0 0 16 16"
     aria-labelledby={titleId}
     {...props}
   >
