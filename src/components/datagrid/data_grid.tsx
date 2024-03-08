9c7d2d64376f76fdeeb08948e20bd23f7a8f40d8
Kevin Qualters <kevin.qualters@elastic.co>
2024-03-08 12:41:27 -0800

    Memoize focusContext

--- src/components/datagrid/data_grid.tsx
+++ src/components/datagrid/data_grid.tsx
@@ -275,7 +275,30 @@ export const EuiDataGrid = memo(
     /**
      * Focus
      */
-    const { focusProps: wrappingDivFocusProps, ...focusContext } = useFocus();
+    const {
+      focusProps: wrappingDivFocusProps,
+      onFocusUpdate,
+      focusedCell,
+      setFocusedCell,
+      setIsFocusedCellInView,
+      focusFirstVisibleInteractiveCell,
+    } = useFocus();
+
+    const focusContext = useMemo(() => {
+      return {
+        onFocusUpdate,
+        focusedCell,
+        setFocusedCell,
+        setIsFocusedCellInView,
+        focusFirstVisibleInteractiveCell,
+      };
+    }, [
+      onFocusUpdate,
+      focusedCell,
+      setFocusedCell,
+      setIsFocusedCellInView,
+      focusFirstVisibleInteractiveCell,
+    ]);
 
     /**
      * Cell popover
