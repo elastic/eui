f73bd3d76f02f0b64bf7512dbee5a061375ac9b4
Cee Chen <constance.chen@elastic.co>
2024-03-18 10:37:42 -0700

    Remove notifcation/tour beacon w/ Amsterdam theme

--- src-docs/src/components/guide_theme_selector/guide_theme_selector.tsx
+++ src-docs/src/components/guide_theme_selector/guide_theme_selector.tsx
@@ -11,15 +11,11 @@ import { ThemeContext } from '../with_theme';
 // @ts-ignore Not TS
 import { GuideLocaleSelector } from '../guide_locale_selector';
 import {
-  EuiText,
-  EuiTourStep,
   EuiPopover,
   EuiHorizontalRule,
   EuiButton,
   EuiContextMenuPanel,
   EuiContextMenuItem,
-  EuiLink,
-  EuiIcon,
 } from '../../../../src/components';
 
 type GuideThemeSelectorProps = {
@@ -45,18 +41,9 @@ const GuideThemeSelectorComponent: React.FunctionComponent<
 > = ({ context, onToggleLocale, selectedLocale }) => {
   const isMobileSize = useIsWithinBreakpoints(['xs', 's']);
   const [isPopoverOpen, setPopover] = useState(false);
-  const [isOpen, setIsOpen] = useState(
-    localStorage.getItem(STORAGE_KEY) !== 'dismissed'
-  );
-
-  const onTourDismiss = () => {
-    setIsOpen(false);
-    localStorage.setItem(STORAGE_KEY, 'dismissed');
-  };
 
   const onButtonClick = () => {
     setPopover(!isPopoverOpen);
-    setIsOpen(false);
     localStorage.setItem(STORAGE_KEY, 'dismissed');
   };
 
@@ -102,49 +89,27 @@ const GuideThemeSelectorComponent: React.FunctionComponent<
   );
 
   return (
-    <EuiTourStep
-      content={
-        <EuiText style={{ maxWidth: 320 }}>
-          <p>
-            Amsterdam is now the only theme and the legacy theme has been
-            removed.
-          </p>
-        </EuiText>
-      }
-      isStepOpen={isOpen}
-      onFinish={onTourDismiss}
-      step={1}
-      stepsTotal={1}
-      title={
-        <>
-          <EuiIcon type="bell" size="s" /> &nbsp; Theming update
-        </>
-      }
-      footerAction={<EuiLink onClick={onTourDismiss}>Got it!</EuiLink>}
+    <EuiPopover
+      id="docsThemeSelector"
       repositionOnScroll
+      button={button}
+      isOpen={isPopoverOpen}
+      closePopover={closePopover}
+      panelPaddingSize="none"
+      anchorPosition="downRight"
     >
-      <EuiPopover
-        id="docsThemeSelector"
-        repositionOnScroll
-        button={button}
-        isOpen={isPopoverOpen}
-        closePopover={closePopover}
-        panelPaddingSize="none"
-        anchorPosition="downRight"
-      >
-        <EuiContextMenuPanel size="s" items={items} />
-        {location.host.includes('803') && (
-          <>
-            <EuiHorizontalRule margin="none" />
-            <div style={{ padding: 8 }}>
-              <GuideLocaleSelector
-                onToggleLocale={onToggleLocale}
-                selectedLocale={selectedLocale}
-              />
-            </div>
-          </>
-        )}
-      </EuiPopover>
-    </EuiTourStep>
+      <EuiContextMenuPanel size="s" items={items} />
+      {location.host.includes('803') && (
+        <>
+          <EuiHorizontalRule margin="none" />
+          <div style={{ padding: 8 }}>
+            <GuideLocaleSelector
+              onToggleLocale={onToggleLocale}
+              selectedLocale={selectedLocale}
+            />
+          </div>
+        </>
+      )}
+    </EuiPopover>
   );
 };
