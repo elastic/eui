1f342fa7d306af7756129ab5ae10d5ccfd7ffbe8
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-03-22 18:40:34 +0100

    [Storybook] Add stories for more components (letters M - O) - Part 1 (#7596)

---
+++ src/components/modal/modal_body.stories.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import { EuiModal } from './modal';
+import { EuiModalBody, EuiModalBodyProps } from './modal_body';
+
+const meta: Meta<EuiModalBodyProps> = {
+  title: 'Layout/EuiModal/EuiModalBody',
+  component: EuiModalBody,
+  decorators: [
+    (Story) => (
+      <EuiModal onClose={action('onClose')}>
+        <Story />
+      </EuiModal>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<EuiModalBodyProps>;
+
+export const Playground: Story = {
+  args: {
+    children: 'Modal body',
+  },
+};
---
+++ src/components/modal/modal_footer.stories.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import { EuiModal } from './modal';
+import { EuiModalFooter, EuiModalFooterProps } from './modal_footer';
+
+const meta: Meta<EuiModalFooterProps> = {
+  title: 'Layout/EuiModal/EuiModalFooter',
+  component: EuiModalFooter,
+  decorators: [
+    (Story) => (
+      <EuiModal onClose={action('onClose')}>
+        <Story />
+      </EuiModal>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<EuiModalFooterProps>;
+
+export const Playground: Story = {
+  args: {
+    children: 'Modal footer',
+  },
+};
