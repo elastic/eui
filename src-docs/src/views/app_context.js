1a75efca47688f27465cfd7e8c77c2b9376f333f
Cee Chen <549407+cee-chen@users.noreply.github.com>
2024-03-27 12:25:22 -0700

    Lene saving my butt
    Co-authored-by: Lene Gadewoll <lene.gadewoll@elastic.co>

--- src-docs/src/views/app_context.js
+++ src-docs/src/views/app_context.js
@@ -47,7 +47,7 @@ export const AppContext = ({ children }) => {
   };
 
   const isLocalDev = window.location.host.includes('803');
-  setEuiDevProviderWarning(isLocalDev ? 'error' : 'warning'); // Note: this can't be in a useEffect, otherwise it fires too late for style memoization warnings to error on page reload
+  setEuiDevProviderWarning(isLocalDev ? 'error' : 'warn'); // Note: this can't be in a useEffect, otherwise it fires too late for style memoization warnings to error on page reload
 
   return (
     <EuiProvider
