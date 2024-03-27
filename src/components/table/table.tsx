3d36b088002b4ee48f104259aee5634318b75251
Cee Chen <549407+cee-chen@users.noreply.github.com>
2024-03-27 11:47:55 -0700

    [EuiTable] New `responsiveBreakpoint` prop + initial setup for mobile vs desktop styles (#7625)

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -9,16 +9,33 @@
 import React, { FunctionComponent, TableHTMLAttributes } from 'react';
 import classNames from 'classnames';
 
-import { useEuiMemoizedStyles, useIsWithinMaxBreakpoint } from '../../services';
+import { useEuiMemoizedStyles, type EuiBreakpointSize } from '../../services';
 import { CommonProps } from '../common';
 
+import {
+  useIsEuiTableResponsive,
+  EuiTableIsResponsiveContext,
+} from './mobile/responsive_context';
 import { euiTableStyles } from './table.styles';
 
 export interface EuiTableProps
   extends CommonProps,
     TableHTMLAttributes<HTMLTableElement> {
   compressed?: boolean;
+  /**
+   * @deprecated - use `responsiveBreakpoint` instead
+   */
   responsive?: boolean;
+  /**
+   * Named breakpoint. Below this size, the table will collapse
+   * into responsive cards.
+   *
+   * Pass `false` to never collapse to a mobile view, or inversely,
+   * `true` to always render mobile-friendly cards.
+   *
+   * @default m
+   */
+  responsiveBreakpoint?: EuiBreakpointSize | boolean;
   /**
    * Sets the table-layout CSS property
    */
@@ -30,11 +47,12 @@ export const EuiTable: FunctionComponent<EuiTableProps> = ({
   className,
   compressed,
   tableLayout = 'fixed',
+  responsiveBreakpoint, // Default handled by `useIsEuiTableResponsive`
   responsive = true,
   ...rest
 }) => {
-  // TODO: Make the table responsive breakpoint customizable via prop
-  const isResponsive = useIsWithinMaxBreakpoint('s') && responsive;
+  const isResponsive =
+    useIsEuiTableResponsive(responsiveBreakpoint) && responsive;
 
   const classes = classNames('euiTable', className, {
     'euiTable--responsive': responsive,
@@ -46,11 +64,14 @@ export const EuiTable: FunctionComponent<EuiTableProps> = ({
     styles.layout[tableLayout],
     (!compressed || isResponsive) && styles.uncompressed,
     compressed && !isResponsive && styles.compressed,
+    isResponsive ? styles.mobile : styles.desktop,
   ];
 
   return (
     <table tabIndex={-1} css={cssStyles} className={classes} {...rest}>
-      {children}
+      <EuiTableIsResponsiveContext.Provider value={isResponsive}>
+        {children}
+      </EuiTableIsResponsiveContext.Provider>
     </table>
   );
 };
