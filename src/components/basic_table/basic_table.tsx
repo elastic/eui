4a6a8e87602182275c966ee84099e3f632783f6e
Cee Chen <constance.chen@elastic.co>
2024-04-01 12:28:02 -0700

    ♻️  Move cell `showOnHover` API to basic table `actions` API
    - The goal of this is to limit usage to being controlled by EUI, and reduce potentially hidden information from users
    
    - this allows us to replace `showOnHover` props on basic table columns (of which there is one usage in Kibana)
    
    + remove length logic from `ExpandedItemActions`, and let the correct hover className be determined via p
    rop instead
    
    + update docs example to show custom actions working with `isPrimary` and `showOnHover` logic (previously could not be easily customized with these options)

--- src/components/basic_table/basic_table.tsx
+++ src/components/basic_table/basic_table.tsx
@@ -1139,9 +1139,15 @@ export class EuiBasicTable<T extends object = any> extends Component<
         // If all actions are disabled, do not show any actions but the popover toggle
         actualActions = [];
       } else {
-        // if any of the actions `isPrimary`, add them inline as well, but only the first 2
-        const primaryActions = actualActions.filter((o) => o.isPrimary);
-        actualActions = primaryActions.slice(0, 2);
+        // if any of the actions `isPrimary`, add them inline as well, but only the first 2,
+        // which we'll force to only show on hover for desktop views
+        const primaryActions = actualActions.filter(
+          (action) => action.isPrimary
+        );
+        actualActions = primaryActions.slice(0, 2).map((action) => ({
+          ...action,
+          showOnHover: true,
+        }));
       }
 
       // if we have more than 1 action, we don't show them all in the cell, instead we
@@ -1152,16 +1158,15 @@ export class EuiBasicTable<T extends object = any> extends Component<
 
       actualActions.push({
         name: 'All actions',
-        render: (item: T) => {
-          return (
-            <CollapsedItemActions
-              actions={column.actions}
-              actionsDisabled={allDisabled}
-              itemId={itemId}
-              item={item}
-            />
-          );
-        },
+        render: (item: T) => (
+          <CollapsedItemActions
+            className="euiBasicTable__collapsedActions"
+            actions={column.actions}
+            actionsDisabled={allDisabled}
+            itemId={itemId}
+            item={item}
+          />
+        ),
       });
     }
 
