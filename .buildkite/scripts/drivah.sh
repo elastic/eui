a7e958160ff0fc6c4ef81c3a09350a7ade4cd199
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-04-02 15:06:43 +0200

    ci: authenticate to docker registry and push built drivah images

--- .buildkite/scripts/drivah.sh
+++ .buildkite/scripts/drivah.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 set -euo pipefail
+set +x
 
 BUILD_DIR=${1:-.}
 
+echo "--- :docker: Authenticate to docker.elastic.co"
+vault_path=secret/ci/elastic-eui/docker-machine-production
+username=$(vault read -field=username "${vault_path}")
+password=$(vault read -field=password "${vault_path}")
+echo "${password}" | buildah login --username="${username}" --password-stdin docker.elastic.co
+
 echo "--- :building_construction: Build the drivah container(s)"
 if [[ "${BUILDKITE_PULL_REQUEST}" == "false" ]]; then
-    echo "--- :large_orange_diamond: Add changed-since parameter"
+    echo ":large_orange_diamond: Add changed-since parameter"
     DRIVAH_PARAMETERS="--changed-since=HEAD^"
 fi
 
-drivah build ${DRIVAH_PARAMETERS:-} "${BUILD_DIR}"
+drivah build ${DRIVAH_PARAMETERS:-} --push "${BUILD_DIR}"
