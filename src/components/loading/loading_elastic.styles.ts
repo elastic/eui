bed18656c3e6501eb034501e80552f6ce613ab63
Cee Chen <549407+cee-chen@users.noreply.github.com>
2024-04-04 08:48:56 -0700

    [Emotion][perf] Memoize loading components (#7649)

--- src/components/loading/loading_elastic.styles.ts
+++ src/components/loading/loading_elastic.styles.ts
@@ -33,49 +33,47 @@ const loadingElastic = keyframes`
   }
 `;
 
-export const euiLoadingElasticStyles = () => {
-  return {
-    euiLoadingElastic: css`
-      position: relative;
-      display: inline-block;
+export const euiLoadingElasticStyles = {
+  euiLoadingElastic: css`
+    position: relative;
+    display: inline-block;
 
-      & path {
-        ${euiCanAnimate} {
-          animation-name: ${loadingElastic};
-          animation-fill-mode: forwards;
-          animation-direction: alternate;
-          transform-style: preserve-3d;
-          animation-duration: 1s;
-          animation-timing-function: cubic-bezier(0, 0.63, 0.49, 1);
-          animation-iteration-count: infinite;
-          transform-origin: 50% 50%;
-        }
-
-        /* Hide outline mainly for dark mode */
-        &:nth-of-type(1) {
-          display: none;
-        }
-
-        &:nth-of-type(2) {
-          animation-delay: 0.035s;
-        }
-
-        &:nth-of-type(3) {
-          animation-delay: 0.125s;
-        }
-
-        &:nth-of-type(4) {
-          animation-delay: 0.155s;
-        }
-
-        &:nth-of-type(5) {
-          animation-delay: 0.075s;
-        }
-
-        &:nth-of-type(6) {
-          animation-delay: 0.06s;
-        }
+    & path {
+      ${euiCanAnimate} {
+        animation-name: ${loadingElastic};
+        animation-fill-mode: forwards;
+        animation-direction: alternate;
+        transform-style: preserve-3d;
+        animation-duration: 1s;
+        animation-timing-function: cubic-bezier(0, 0.63, 0.49, 1);
+        animation-iteration-count: infinite;
+        transform-origin: 50% 50%;
       }
-    `,
-  };
+
+      /* Hide outline mainly for dark mode */
+      &:nth-of-type(1) {
+        display: none;
+      }
+
+      &:nth-of-type(2) {
+        animation-delay: 0.035s;
+      }
+
+      &:nth-of-type(3) {
+        animation-delay: 0.125s;
+      }
+
+      &:nth-of-type(4) {
+        animation-delay: 0.155s;
+      }
+
+      &:nth-of-type(5) {
+        animation-delay: 0.075s;
+      }
+
+      &:nth-of-type(6) {
+        animation-delay: 0.06s;
+      }
+    }
+  `,
 };
