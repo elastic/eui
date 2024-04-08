eb1ba9f960d1d8ffc34062c1f56f4dd65ff91ae0
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-04-08 17:51:59 +0200

    ci: refactor `eui-release` scripts

---
+++ .buildkite/scripts/release/step_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Build EUI and publish to npm
+
+set -eo pipefail
+
+# include utils
+source .buildkite/scripts/common/utils.sh
+
+echo "+++ :yarn: Installing dependencies"
+yarn
+
+echo "+++ :yarn: Building @elastic/eui"
+yarn build
+
+echo "+++ :yarn: Built @elastic/eui"
