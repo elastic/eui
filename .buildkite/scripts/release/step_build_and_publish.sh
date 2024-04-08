4db34784c7d17551a47dba83a3084324011d5b03
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-04-08 17:20:17 +0200

    ci: add build_and_publish step to `eui-release` pipeline

---
+++ .buildkite/scripts/release/step_build_and_publish.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Build EUI and publish to npm
+
+set -eo pipefail
+
+# include utils
+source .buildkite/scripts/common/utils.sh
+
+echo "+++ :yarn: Installing dependencies"
+yarn
+
+echo ":yarn: Building @elastic/eui"
+yarn build
+
+echo "+++ :npm: Authenticating to npm"
+
+npm_vault="secret/ci/elastic-eui/npm"
+NPM_TOKEN=$(retry 5 vault read -field=token "${npm_vault}")
+npm config set "//registry.npmjs.org/:_authToken=${NPM_TOKEN}"
+
+echo "+++ :npm: Publishing @elastic/eui"
+
+if [[ "${RELEASE_TYPE}" == "release" ]]; then
+  echo "RELEASE_TYPE=release is not supported yet"
+  exit 1
+else
+  npm publish --dry-run --tag next
+fi
+
+echo "+++ :white_check_mark: Version published!"
