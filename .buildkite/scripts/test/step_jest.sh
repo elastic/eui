83996fcb2065a818f478b18313fbcb5220489db9
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-04-09 17:42:50 +0200

    ci: add jest flags

--- .buildkite/scripts/test/step_jest.sh
+++ .buildkite/scripts/test/step_jest.sh
@@ -7,6 +7,6 @@ source ~/.bash_profile
 echo "+++ :jest: Running jest tests"
 
 export CI="true"
-yarn test-unit
+yarn test-unit --coverage=false --maxWorkers=4
 
 echo "+++ :white_check_mark: Finished running jest tests"
