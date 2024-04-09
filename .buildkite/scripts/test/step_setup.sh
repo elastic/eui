c4bad31b459d581b07155171dc4609c2152b585c
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-04-09 12:19:32 +0200

    ci: debug user

--- .buildkite/scripts/test/step_setup.sh
+++ .buildkite/scripts/test/step_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
+whoami
+pwd
+
 echo "+++ :information_source: Running tests on branch $(BUILDKITE_BRANCH) ($(BUILDKITE_COMMIT))"
 echo "Node.js version: $(node -v)"
 
