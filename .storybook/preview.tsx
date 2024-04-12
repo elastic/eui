b83b75d7e14275415c94f158301d7aada48fb747
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-04-12 15:37:09 +0200

    feat: add loki

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -60,6 +60,8 @@ const preview: Preview = {
         {...(context.componentId === 'theming-euiprovider' && context.args)}
       >
         <div
+          /* #story-wrapper should always be the element that wraps <Story /> */
+          id="story-wrapper"
           css={[
             writingModeStyles.writingMode,
             // @ts-ignore - we're manually ensuring `writingMode` globals match our Emotion style keys
