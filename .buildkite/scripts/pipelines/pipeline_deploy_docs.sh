a6ea926ba02c25c207460e6d61588f4718e36db6
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-04-25 23:09:59 +0200

    build: update buildkite pipelines to use correct working directories when testing and building EUI

--- .buildkite/scripts/pipelines/pipeline_deploy_docs.sh
+++ .buildkite/scripts/pipelines/pipeline_deploy_docs.sh
@@ -13,6 +13,8 @@ if [[ -z "${GITHUB_TOKEN}" ]]; then
   exit 1
 fi
 
+cd packages/eui
+
 # Run EUI build/deploy script, set in the template parameter
 # Expects env: GPROJECT, GCE_ACCOUNT, GIT_BRANCH, GITHUB_TOKEN from pre_command.sh
 ./scripts/deploy/deploy_docs
