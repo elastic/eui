5e90afb30a0019f13fa033516aad1395d6911c79
Cee Chen <constance.chen@elastic.co>
2024-05-06 20:14:30 -0700

    Convert toolbar styles

---
+++ src/components/markdown_editor/markdown_editor_toolbar.styles.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { css } from '@emotion/react';
+
+import { UseEuiTheme } from '../../services';
+import { logicalCSS } from '../../global_styling';
+
+export const euiMarkdownEditorToolbarStyles = (
+  euiThemeContext: UseEuiTheme
+) => {
+  const { euiTheme } = euiThemeContext;
+
+  return {
+    euiMarkdownEditorToolbar: css`
+      display: flex;
+      flex-wrap: wrap;
+      padding: ${euiTheme.size.xs};
+      background-color: ${euiTheme.colors.lightShade};
+      border: ${euiTheme.border.thin};
+      ${logicalCSS('border-bottom', 'none')}
+    `,
+    euiMarkdownEditorToolbar__buttons: css`
+      flex: 1;
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      gap: ${euiTheme.size.xs};
+    `,
+    euiMarkdownEditorToolbar__divider: css`
+      display: block;
+      ${logicalCSS('height', euiTheme.size.l)}
+      ${logicalCSS('margin-left', euiTheme.size.xs)}
+      ${logicalCSS('padding-right', euiTheme.size.xs)}
+      ${logicalCSS(
+        'border-left',
+        `${euiTheme.border.width.thin} solid ${euiTheme.border.color}`
+      )}
+    `,
+  };
+};
