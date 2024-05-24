f0854ceec2fe00f016707fc0b7457299315e7870
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-05-24 12:11:41 +0200

    docs(storybook): add EuiDescribedFormGroup stories

---
+++ packages/eui/src/components/form/described_form_group/described_form_group.stories.tsx
@@ -0,0 +1,59 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { EuiForm } from '../form';
+import { EuiFormRow } from '../form_row';
+
+import {
+  EuiDescribedFormGroup,
+  EuiDescribedFormGroupProps,
+} from './described_form_group';
+import { EuiFieldText } from '../field_text';
+
+const meta: Meta<EuiDescribedFormGroupProps> = {
+  title: 'Forms/EuiForm/EuiDescribedFormGroup',
+  component: EuiDescribedFormGroup,
+  decorators: [
+    (Story, { args }) => (
+      <EuiForm>
+        <Story {...args} />
+      </EuiForm>
+    ),
+  ],
+  argTypes: {
+    description: { control: 'text' },
+  },
+  args: {
+    gutterSize: 'l',
+    fullWidth: false,
+    ratio: 'half',
+    titleSize: 'xs',
+    // set up for easier testing/QA
+    description: '',
+    descriptionFlexItemProps: {},
+    fieldFlexItemProps: {},
+  },
+};
+
+export default meta;
+type Story = StoryObj<EuiDescribedFormGroupProps>;
+
+export const Playground: Story = {
+  args: {
+    title: <h2>Form group title</h2>,
+    description: 'Descriptions are wrapped in a small, subdued EuiTextBlock.',
+    children: (
+      <EuiFormRow label="Text field">
+        <EuiFieldText name="first" />
+      </EuiFormRow>
+    ),
+  },
+};
