e2b934825ad56cf82de6c5e4584689a2117d1a46
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-05-24 12:01:20 +0200

    docs(storybook): add EuiForm stories

---
+++ packages/eui/src/components/form/form.stories.tsx
@@ -0,0 +1,64 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { EuiFieldText } from './field_text';
+import { EuiFormRow } from './form_row';
+import { EuiForm, EuiFormProps } from './form';
+
+const meta: Meta<EuiFormProps> = {
+  title: 'Forms/EuiForm',
+  component: EuiForm,
+  args: {
+    component: 'div',
+    invalidCallout: 'above',
+    // set up for easier testing/QA
+    fullWidth: false,
+    isInvalid: false,
+    error: '',
+  },
+};
+
+export default meta;
+type Story = StoryObj<EuiFormProps>;
+
+export const Playground: Story = {
+  args: {
+    children: (
+      <>
+        <EuiFormRow>
+          <EuiFieldText aria-label="Use aria labels when no actual label is in use" />
+        </EuiFormRow>
+      </>
+    ),
+  },
+};
+
+export const Error: Story = {
+  parameters: {
+    controls: {
+      include: ['error', 'isInvalid', 'invalidCallout'],
+    },
+  },
+  args: {
+    children: (
+      <>
+        <EuiFormRow label="Text field">
+          <EuiFieldText name="first_text" />
+        </EuiFormRow>
+      </>
+    ),
+    isInvalid: true,
+    error: [
+      "Here's an example of an error",
+      'You might have more than one error, so pass an array.',
+    ],
+  },
+};
