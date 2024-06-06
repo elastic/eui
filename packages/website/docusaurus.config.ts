ae5d11be0d642d8e0f4005d0cc835d836b6d395e
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-06-06 13:41:05 +0200

    chore: add copyright comments

--- packages/website/docusaurus.config.ts
+++ packages/website/docusaurus.config.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
