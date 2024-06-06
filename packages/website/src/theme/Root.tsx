dd8abe657318126e6fa91b5d3c0fe90ad4805eb8
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-06-06 13:42:14 +0200

    chore: remove useContext test code

--- packages/website/src/theme/Root.tsx
+++ packages/website/src/theme/Root.tsx
@@ -6,22 +6,15 @@
  * Side Public License, v 1.
  */
 
-import { createContext, useContext } from 'react';
 import { EuiProvider } from '@elastic/eui';
 import { Props } from '@theme/Root';
 
-const c = createContext({});
-
 // Wrap docusaurus root component with <EuiProvider>
 // See https://docusaurus.io/docs/swizzling#wrapper-your-site-with-root
-const Root = ({ children }: Props) => {
-  const a = useContext(c);
-
-  return (
-    <EuiProvider globalStyles={false}>
-      {children}
-    </EuiProvider>
-  );
-}
+const Root = ({ children }: Props) => (
+  <EuiProvider globalStyles={false}>
+    {children}
+  </EuiProvider>
+);
 
 export default Root;
