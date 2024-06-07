1cabbd2e28a2706c06020fa8b0ed4dfc81f1ffd1
Tomasz Kajtoch <tomek@kajto.ch>
2024-06-07 14:49:55 +0200

    feat: move docusaurus website to packages directory (#7815)

---
+++ packages/website/src/pages/index.tsx
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs">
+            EUI Documentation
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={siteConfig.title}
+      description={siteConfig.tagline}
+    >
+      <HomepageHeader />
+    </Layout>
+  );
+}
---
+++ packages/website/src/theme/Root.tsx
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { EuiProvider } from '@elastic/eui';
+import { Props } from '@theme/Root';
+
+// Wrap docusaurus root component with <EuiProvider>
+// See https://docusaurus.io/docs/swizzling#wrapper-your-site-with-root
+const Root = ({ children }: Props) => (
+  <EuiProvider globalStyles={false}>
+    {children}
+  </EuiProvider>
+);
+
+export default Root;
