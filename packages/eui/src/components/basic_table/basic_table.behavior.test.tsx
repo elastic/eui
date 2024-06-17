c1be6f7c845137eac63b939a9c3ec9b6db2496da
Cee Chen <549407+cee-chen@users.noreply.github.com>
2024-06-17 08:51:41 -0700

    [tech debt] Convert remaining table Enzyme tests to RTL (#7836)

--- packages/eui/src/components/basic_table/basic_table.behavior.test.tsx
+++ packages/eui/src/components/basic_table/basic_table.behavior.test.tsx
@@ -7,82 +7,65 @@
  */
 
 import React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
-import { findTestSubject } from '../../test';
+import { fireEvent } from '@testing-library/react';
+import { render } from '../../test/rtl';
 
 import { EuiBasicTable, EuiBasicTableProps } from './basic_table';
 
 describe('EuiBasicTable', () => {
   describe('behavior', () => {
     describe('selected items', () => {
-      let props: EuiBasicTableProps<{ id: string; name: string }>;
-      let component: ReactWrapper;
-
-      beforeEach(() => {
-        props = {
-          items: [
-            { id: '1', name: 'name1' },
-            { id: '2', name: 'name2' },
-          ],
-          itemId: 'id',
-          columns: [
-            {
-              field: 'name',
-              name: 'Name',
-              description: 'description',
-            },
-          ],
-          selection: {
-            onSelectionChange: () => {},
+      const props: EuiBasicTableProps<{ id: string; name: string }> = {
+        items: [
+          { id: '1', name: 'name1' },
+          { id: '2', name: 'name2' },
+        ],
+        itemId: 'id',
+        columns: [
+          {
+            field: 'name',
+            name: 'Name',
+            description: 'description',
           },
-          onChange: () => {},
-        };
-
-        component = mount(<EuiBasicTable {...props} />);
-      });
+        ],
+        selection: {
+          onSelectionChange: () => {},
+        },
+        onChange: () => {},
+      };
 
       test('check the select all checkbox when all are selected', () => {
-        findTestSubject(component, 'checkboxSelectRow-1').simulate('change', {
-          target: { checked: true },
-        });
-        findTestSubject(component, 'checkboxSelectRow-2').simulate('change', {
-          target: { checked: true },
-        });
-        expect(
-          findTestSubject(component, 'checkboxSelectAll').prop('checked')
-        ).toBe(true);
+        const { getByTestSubject } = render(<EuiBasicTable {...props} />);
+
+        fireEvent.click(getByTestSubject('checkboxSelectRow-1'));
+        fireEvent.click(getByTestSubject('checkboxSelectRow-2'));
+        expect(getByTestSubject('checkboxSelectAll')).toBeChecked();
       });
 
       test('uncheck the select all checkbox when some are selected', () => {
-        findTestSubject(component, 'checkboxSelectRow-1').simulate('change', {
-          target: { checked: true },
-        });
-        expect(
-          findTestSubject(component, 'checkboxSelectAll').prop('checked')
-        ).toBe(false);
+        const { getByTestSubject } = render(<EuiBasicTable {...props} />);
+
+        fireEvent.click(getByTestSubject('checkboxSelectRow-1'));
+        expect(getByTestSubject('checkboxSelectAll')).not.toBeChecked();
       });
 
       test('are all selected when the select all checkbox is checked', () => {
-        findTestSubject(component, 'checkboxSelectAll').simulate('change', {
-          target: { checked: true },
-        });
-        expect(
-          findTestSubject(component, 'checkboxSelectRow-1').prop('checked')
-        ).toBe(true);
-        expect(
-          findTestSubject(component, 'checkboxSelectRow-2').prop('checked')
-        ).toBe(true);
+        const { getByTestSubject } = render(<EuiBasicTable {...props} />);
+
+        fireEvent.click(getByTestSubject('checkboxSelectAll'));
+        expect(getByTestSubject('checkboxSelectRow-1')).toBeChecked();
+        expect(getByTestSubject('checkboxSelectRow-2')).toBeChecked();
       });
 
       test('select all checkbox becomes unchecked when selected items are deleted', () => {
-        findTestSubject(component, 'checkboxSelectAll').simulate('change', {
-          target: { checked: true },
-        });
-        props.items = [];
-        component.setProps(props);
-        expect(
-          findTestSubject(component, 'checkboxSelectAll').prop('checked')
-        ).toBe(false);
+        const { getByTestSubject, rerender } = render(
+          <EuiBasicTable {...props} />
+        );
+        fireEvent.click(getByTestSubject('checkboxSelectAll'));
+        expect(getByTestSubject('checkboxSelectAll')).toBeChecked();
+
+        rerender(<EuiBasicTable {...props} items={[]} />);
+        expect(getByTestSubject('checkboxSelectAll')).not.toBeChecked();
       });
     });
   });
