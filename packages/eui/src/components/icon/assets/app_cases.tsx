c8a073a3bea7f0de82992ea2a5c5847812a96721
adcoelho <antonio.coelho@elastic.co>
2024-06-18 11:37:24 +0200

    Update cases icon.

--- packages/eui/src/components/icon/assets/app_cases.tsx
+++ packages/eui/src/components/icon/assets/app_cases.tsx
@@ -31,12 +31,41 @@ const EuiIconAppCases = ({
     {title ? <title id={titleId}>{title}</title> : null}
     <path
       fillRule="evenodd"
-      d="M22.5 24a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11Zm0 2a7.5 7.5 0 1 0 0-15 7.5 7.5 0 0 0 0 15Z"
+      d="M0 1h24v5.72h-2V3H2v24H0V1ZM1.497 30.09C.588 29.415 0 28.368 0 27h2c0 .732.287 1.185.69 1.485.44.327 1.088.515 1.81.515 1.015 0 1.619-.305 1.966-.648C6.82 28.002 7 27.519 7 27h2c0 .98-.345 1.999-1.128 2.773C7.082 30.555 5.935 31 4.5 31c-1.028 0-2.13-.262-3.003-.91Z"
+      className="euiIcon__fillSecondary"
+      clipRule="evenodd"
     />
-    <path d="m30.293 27.707-4-4 1.414-1.414 4 4-1.414 1.414ZM6 4H2v27h25v-3h-2v1H4V6h2V4Zm19 6h2V4h-4v2h2v4Z" />
     <path
       fillRule="evenodd"
-      d="m16.381 3.024.538 1.158h2.274l.636 2.545H9.68l.637-2.545h2.558l.538-1.158c.292-.63.955-1.024 1.484-1.024.53 0 1.193.394 1.485 1.024Zm6.01 5.703-1.636-6.545h-2.56C17.6.898 16.262 0 14.897 0c-1.364 0-2.703.898-3.299 2.182H8.755L7.118 8.727h15.273ZM7 15h6v-2H7v2Zm6 5H7v-2h6v2Zm-6 5h6v-2H7v2Z"
+      d="M7.061 14H31v5.105h-2V16H9.061v11.002h-2V14ZM5 29h11.224v2H5v-2Z"
+      className="euiIcon__fillSecondary"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M9.833 12H7v-2h2.833v2ZM24 12H10.864v-2H24v2Z"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M4 6h23v6.897h-2V8H6v19H4V6Z"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M23 30a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0 2a7 7 0 1 0 0-14 7 7 0 0 0 0 14Z"
+      className="euiIcon__fillSecondary"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M21.9 28.936v-7.98h2v7.98h-2Z"
+      className="euiIcon__fillSecondary"
+      clipRule="evenodd"
+    />
+    <path
+      fillRule="evenodd"
+      d="M26.89 25.946h-7.98v-2h7.98v2Z"
       className="euiIcon__fillSecondary"
       clipRule="evenodd"
     />
