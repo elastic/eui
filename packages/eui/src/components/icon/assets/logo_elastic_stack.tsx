56b6a91ebc41e8eefdf9ebc2e0e241ea3895afe8
Casper Hübertz <casper@formgeist.com>
2024-06-18 09:04:38 +0200

    Recompiled and updated snapshots

--- packages/eui/src/components/icon/assets/logo_elastic_stack.tsx
+++ packages/eui/src/components/icon/assets/logo_elastic_stack.tsx
@@ -23,7 +23,6 @@ const EuiIconLogoElasticStack = ({
     xmlns="http://www.w3.org/2000/svg"
     width={32}
     height={32}
-    fill="none"
     viewBox="0 0 32 32"
     aria-labelledby={titleId}
     {...props}
@@ -33,12 +32,7 @@ const EuiIconLogoElasticStack = ({
       fill="#F04E98"
       d="M0 2.37A2.37 2.37 0 0 1 2.37 0h27.26A2.37 2.37 0 0 1 32 2.37v6.52H0V2.37Z"
     />
-    <path
-      fill="#00BFB3"
-      fillRule="evenodd"
-      d="M0 20.148h32v-8.296H0v8.296Z"
-      clipRule="evenodd"
-    />
+    <path fill="#00BFB3" d="M0 20.148h32v-8.296H0v8.296Z" />
     <path
       fill="#07C"
       d="M0 23.111h32v6.519A2.37 2.37 0 0 1 29.63 32H2.37A2.37 2.37 0 0 1 0 29.63v-6.52Z"
