4ee63eddbc654c5469a8e89cde076e6a0b92ae34
Cee Chen <549407+cee-chen@users.noreply.github.com>
2024-06-25 10:08:37 -0700

    [Emotion] Convert EuiColorPicker (#7845)

--- packages/eui/src/components/color_picker/color_picker.styles.ts
+++ packages/eui/src/components/color_picker/color_picker.styles.ts
@@ -6,16 +6,50 @@
  * Side Public License, v 1.
  */
 
-import { UseEuiTheme } from '../../services';
-import { mathWithUnits } from '../../global_styling';
+import { css } from '@emotion/react';
 
-export const euiColorPickerVariables = (euiThemeContext: UseEuiTheme) => {
+import { UseEuiTheme, transparentize } from '../../services';
+import { logicalCSS, mathWithUnits } from '../../global_styling';
+
+export const euiColorPickerStyles = (euiThemeContext: UseEuiTheme) => {
   const euiTheme = euiThemeContext.euiTheme;
 
+  // 5 columns of swatches + margins + border
+  const colorPickerWidth = mathWithUnits(
+    [euiTheme.size.l, euiTheme.size.s],
+    (x, y) => x * 5 + y * 4
+  );
+
   return {
-    width: mathWithUnits(
-      [euiTheme.size.l, euiTheme.size.s],
-      (x, y) => x * 5 + y * 4
-    ),
+    euiColorPicker: css`
+      position: relative;
+      ${logicalCSS('width', colorPickerWidth)}
+
+      /* Using flex and gap allows us to easily handle spacing between multiple modes + showAlpha */
+      display: flex;
+      flex-direction: column;
+      gap: ${euiTheme.size.s};
+    `,
+
+    euiColorPicker__swatches: css`
+      display: flex;
+      flex-wrap: wrap;
+      gap: ${euiTheme.size.s};
+    `,
+
+    euiColorPicker__alphaRange: css`
+      .euiRangeInput {
+        ${logicalCSS('min-width', 0)}
+      }
+    `,
+
+    // Adds a stroke color for the swatchInput icon. Unlike most EuiIcons it has a stroke in the SVG
+    // Targets a custom className applied directly to the <svg> icon
+    euiColorPicker__swatchInputIcon: css`
+      .euiSwatchInput__stroke {
+        fill: none;
+        stroke: ${transparentize(euiTheme.colors.fullShade, 0.2)};
+      }
+    `,
   };
 };
