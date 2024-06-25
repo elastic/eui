a3b87f1ce6c152fdc6f0f22b4b320a10a594d2aa
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-06-25 11:39:19 +0200

    feat: update admonitions, add callout

---
+++ packages/docusaurus-theme/src/theme/Admonition/Type/Info.tsx
@@ -0,0 +1,16 @@
+import OriginalInfo from '@theme-init/Admonition/Type/Info';
+import type InfoType from '@theme-init/Admonition/Type/Info';
+import type { WrapperProps } from '@docusaurus/types';
+import { CallOut } from '../../../components/call_out';
+
+type Props = WrapperProps<typeof InfoType>;
+
+const Note = (props: Props): JSX.Element => {
+  return (
+    <CallOut variant="info">
+      <OriginalInfo {...props} />
+    </CallOut>
+  );
+};
+
+export default Note;
---
+++ packages/docusaurus-theme/src/theme/Admonition/Type/Warning.tsx
@@ -0,0 +1,16 @@
+import OriginalWarning from '@theme-init/Admonition/Type/Warning';
+import type WarningType from '@theme-init/Admonition/Type/Warning';
+import type { WrapperProps } from '@docusaurus/types';
+import { CallOut } from '../../../components/call_out';
+
+type Props = WrapperProps<typeof WarningType>;
+
+const Warning = (props: Props): JSX.Element => {
+  return (
+    <CallOut variant="warning">
+      <OriginalWarning {...props} />
+    </CallOut>
+  );
+};
+
+export default Warning;
