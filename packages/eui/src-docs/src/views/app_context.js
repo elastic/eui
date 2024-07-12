74ee5d97052d87b9375a7468cf8fbaffbe10bb1d
Jason Stoltzfus <jastoltz24@gmail.com>
2024-07-12 12:18:33 -0400

    read theme from localStorage

--- packages/eui/src-docs/src/views/app_context.js
+++ packages/eui/src-docs/src/views/app_context.js
@@ -49,6 +49,8 @@ export const AppContext = ({ children }) => {
   const isLocalDev = window.location.host.includes('803');
   setEuiDevProviderWarning(isLocalDev ? 'error' : 'warn'); // Note: this can't be in a useEffect, otherwise it fires too late for style memoization warnings to error on page reload
 
+  const overrides = JSON.parse(localStorage.getItem('kbn-theme-overrides') || '{}');
+
   return (
     <EuiProvider
       cache={{
@@ -57,6 +59,7 @@ export const AppContext = ({ children }) => {
       }}
       theme={EUI_THEMES.find((t) => t.value === theme)?.provider}
       colorMode={theme.includes('light') ? 'light' : 'dark'}
+      modify={overrides}
     >
       <Helmet>
         <link
