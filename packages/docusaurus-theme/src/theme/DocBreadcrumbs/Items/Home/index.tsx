3803294291950f59d97893307dcc6e18737e984d
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-07-12 14:03:32 +0200

    feat: update Breadcrumbs styles

---
+++ packages/docusaurus-theme/src/theme/DocBreadcrumbs/Items/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import { translate } from '@docusaurus/Translate';
+import { EuiIcon, useEuiMemoizedStyles } from '@elastic/eui';
+
+import { getItemStyles } from '../item.styles';
+
+export default function HomeBreadcrumbItem(): JSX.Element {
+  const homeHref = useBaseUrl('/');
+
+  const styles = useEuiMemoizedStyles(getItemStyles);
+
+  return (
+    <li className="breadcrumbs__item" css={styles.item}>
+      <Link
+        aria-label={translate({
+          id: 'theme.docs.breadcrumbs.home',
+          message: 'Home page',
+          description: 'The ARIA label for the home page in the breadcrumbs',
+        })}
+        className="breadcrumbs__link"
+        href={homeHref}
+      >
+        EUI+
+      </Link>
+      <EuiIcon type="arrowRight" size="s" css={styles.icon} />
+    </li>
+  );
+}
