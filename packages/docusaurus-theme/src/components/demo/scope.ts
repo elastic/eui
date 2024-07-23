adcb4ca4f94401af737d7a237a09b0dd590a5b50
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-07-23 15:06:11 +0200

    feat(docusaurus-theme): patch `react-live` to use the new `jsx` pragma and inject Emotion's jsx wrapper

--- packages/docusaurus-theme/src/components/demo/scope.ts
+++ packages/docusaurus-theme/src/components/demo/scope.ts
@@ -1,5 +1,14 @@
 import React from 'react';
 import * as EUI from '@elastic/eui';
+import * as EmotionReact from '@emotion/react';
+
+/**
+ * A custom client-side require() alternative to inform users it's not available
+ * and
+ */
+const clientSideRequire = () => {
+  throw new Error('require() is not accessible in the interactive demo environment! All EUI and React exports are available in the global scope for you to use without the need to import them.');
+}
 
 export const demoDefaultScope: Record<string, unknown> = {
   // React
@@ -8,4 +17,9 @@ export const demoDefaultScope: Record<string, unknown> = {
 
   // EUI exports
   ...EUI,
+
+  // Emotion
+  ...EmotionReact,
+
+  require: clientSideRequire,
 };
