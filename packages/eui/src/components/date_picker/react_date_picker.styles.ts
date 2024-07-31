f86783eba83ae5dc56165e0856a9eb7680b3a88d
Cee Chen <constance.chen@elastic.co>
2024-07-31 13:50:04 -0700

    Set up date picker variables + convert absolutely positioned header elements to Emotion
    + handle time select responsive stacking

--- packages/eui/src/components/date_picker/react_date_picker.styles.ts
+++ packages/eui/src/components/date_picker/react_date_picker.styles.ts
@@ -10,16 +10,40 @@
 import { css } from '@emotion/css';
 
 import { UseEuiTheme } from '../../services';
-import { euiMaxBreakpoint, euiFontSize } from '../../global_styling';
+import {
+  euiMaxBreakpoint,
+  euiFontSize,
+  logicalCSS,
+  mathWithUnits,
+} from '../../global_styling';
+
+export const euiDatePickerVariables = ({ euiTheme }: UseEuiTheme) => {
+  return {
+    gapSize: euiTheme.size.xs,
+    get paddingSize() {
+      return mathWithUnits(this.gapSize, (x) => x * 2);
+    },
+
+    headerButtonSize: euiTheme.size.xl,
+    get headerOffset() {
+      return mathWithUnits(
+        [this.headerButtonSize, this.gapSize],
+        (x, y) => x + y
+      );
+    },
+  };
+};
 
 export const euiReactDatePickerStyles = (euiThemeContext: UseEuiTheme) => {
   const { euiTheme } = euiThemeContext;
+  const { gapSize, paddingSize, headerOffset } =
+    euiDatePickerVariables(euiThemeContext);
 
   return {
     euiReactDatePicker: css`
       display: flex;
       justify-content: center;
-      padding: ${euiTheme.size.s};
+      padding: ${paddingSize};
       font-size: ${euiFontSize(euiThemeContext, 'xs').fontSize};
       color: ${euiTheme.colors.text};
       border-radius: ${euiTheme.border.radius.medium};
@@ -38,6 +62,63 @@ export const euiReactDatePickerStyles = (euiThemeContext: UseEuiTheme) => {
         &,
         .react-datepicker__focusTrap {
           flex-direction: column;
+        }
+      }
+
+      /**
+       * Header UI - uses absolute positioning trickery to
+       * move elements out of their normal DOM position
+       */
+
+      .react-datepicker__header__dropdown {
+        position: absolute;
+        ${logicalCSS('top', 0)}
+        ${logicalCSS('horizontal', headerOffset)}
+        padding: ${paddingSize};
+        display: flex;
+        justify-content: center;
+        gap: ${gapSize};
+      }
+
+      .react-datepicker__navigation {
+        z-index: 1;
+        position: absolute;
+        ${logicalCSS('top', paddingSize)}
+
+        /* Remove default EuiButtonIcon translate-Y */
+        &:hover,
+        &:focus {
+          transform: none;
+        }
+      }
+
+      .react-datepicker__navigation--previous {
+        ${logicalCSS('left', paddingSize)}
+      }
+
+      .react-datepicker__navigation--next {
+        ${logicalCSS('right', paddingSize)}
+      }
+
+      /* Layout remaining containers to account for absolutely positioned header */
+      .react-datepicker__month-container {
+        flex-grow: 1;
+        ${logicalCSS('margin-top', headerOffset)}
+      }
+
+      .react-datepicker__time-container {
+        flex-grow: 1;
+        ${logicalCSS(
+          'margin-top',
+          // The time select gets a little extra offset
+          mathWithUnits([headerOffset, gapSize], (x, y) => x + y)
+        )}
+        ${logicalCSS('margin-left', paddingSize)}
+
+        /* Account for vertical stacking of time select on small screens */
+        ${euiMaxBreakpoint(euiThemeContext, 's')} {
+          ${logicalCSS('margin-top', paddingSize)}
+          ${logicalCSS('margin-left', 0)}
         }
       }
     `,
