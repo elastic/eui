036f3349710f7e2407d36216b21d861f93ea1150
Cee Chen <constance.chen@elastic.co>
2024-08-06 12:08:12 -0700

    Convert base EuiComboBox styles
    - class component with generic, so it's easier to use a render prop
    
    - we can avoid having to memoizing styles however by using `logicalStyle()` and `euiFormMaxWidth()` separate from combobox's styles
    
    + remove fullWidth modifier class

--- packages/eui/src/components/combo_box/combo_box.test.tsx
+++ packages/eui/src/components/combo_box/combo_box.test.tsx
@@ -357,10 +357,10 @@ describe('EuiComboBox', () => {
           />
         );
 
+        expect(container.firstElementChild!.className).toContain('-fullWidth');
         expect(container.innerHTML).toContain(
           'euiFormControlLayout--fullWidth'
         );
-        expect(container.innerHTML).toContain('euiComboBox--fullWidth');
         expect(container.innerHTML).toContain(
           'euiComboBox__inputWrap--fullWidth'
         );
