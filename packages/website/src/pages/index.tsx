f92d165c8c7016639d2c542243b14ebb910d241e
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-08-07 18:38:53 +0200

    [EUI+] Implement welcome banner (#7931)

--- packages/website/src/pages/index.tsx
+++ packages/website/src/pages/index.tsx
@@ -8,12 +8,14 @@
 
 import { css } from '@emotion/react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 import Layout from '@theme/Layout';
 import { useEuiMemoizedStyles, UseEuiTheme } from '@elastic/eui';
 
 import { HomepageHeader } from '../components/homepage/header';
 import { HomepageUsers } from '../components/homepage/users';
 import { HomepageHighlights } from '../components/homepage/highlights';
+import { HomepageBanner } from '../components/homepage/banner';
 
 const getStyles = ({ euiTheme }: UseEuiTheme) => ({
   main: css`
@@ -49,17 +51,21 @@ const getStyles = ({ euiTheme }: UseEuiTheme) => ({
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+  const isBrowser = useIsBrowser();
   const styles = useEuiMemoizedStyles(getStyles);
 
   return (
-    <Layout title={siteConfig.title} description={siteConfig.tagline}>
-      <main css={styles.main}>
-        <HomepageHeader />
+    <>
+      {isBrowser && <HomepageBanner />}
+      <Layout title={siteConfig.title} description={siteConfig.tagline}>
+        <main css={styles.main}>
+          <HomepageHeader />
 
-        <HomepageHighlights />
+          <HomepageHighlights />
 
-        <HomepageUsers />
-      </main>
-    </Layout>
+          <HomepageUsers />
+        </main>
+      </Layout>
+    </>
   );
 }
