7101ad28f1060141fb0f223b1fdbb2bc6714a950
Cee Chen <constance.chen@elastic.co>
2024-08-08 12:46:35 -0700

    Delete now-unused icon number classes/utils
    - entirely replaced by new `getIconAffordanceStyles` util, and CSS variables

--- packages/eui/src/components/form/form_control_layout/_num_icons.ts
+++ packages/eui/src/components/form/form_control_layout/_num_icons.ts
@@ -11,41 +11,6 @@ import {
   type EuiFormControlLayoutIconsProps,
 } from './form_control_layout_icons';
 
-/**
- * The `getFormControlClassNameForIconCount` function helps setup the className appendum
- * depending on the form control's current settings/state.
- *
- * @param icon {boolean} Does it contain a static icon like arrowDown
- * @param clear {boolean} Is it currently clearable
- * @param isLoading {boolean} Is is currently loading
- * @param isInvalid {boolean} It is currently invalid
- * @param isDropdown {boolean} It is as dropdown
- * @returns {string | undefined} Returns the string to append to the base className of the form control; or `undefined` if all evaluate to false
- */
-
-export type _EuiFormControlLayoutNumIcons = {
-  icon?: boolean;
-  clear?: boolean;
-  isLoading?: boolean;
-  isInvalid?: boolean;
-  isDropdown?: boolean;
-};
-
-export const getFormControlClassNameForIconCount = ({
-  icon,
-  clear,
-  isLoading,
-  isInvalid,
-  isDropdown,
-}: _EuiFormControlLayoutNumIcons): string | undefined => {
-  const numIcons = [icon, clear, isInvalid, isLoading, isDropdown].filter(
-    (item) => item === true
-  ).length;
-
-  // This className is also specifically used in `src/global_styling/mixins/_form.scss`
-  return numIcons > 0 ? `euiFormControlLayout--${numIcons}icons` : undefined;
-};
-
 export const isRightSideIcon = (
   icon?: EuiFormControlLayoutIconsProps['icon']
 ): boolean => {
