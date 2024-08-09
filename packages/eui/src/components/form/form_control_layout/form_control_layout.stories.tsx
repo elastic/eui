f755e3cae2b4ea41fd96c61e09de2ed568c5fd2d
Cee Chen <constance.chen@elastic.co>
2024-08-09 11:02:49 -0700

    Add VRT for append/prepend/group styles

--- packages/eui/src/components/form/form_control_layout/form_control_layout.stories.tsx
+++ packages/eui/src/components/form/form_control_layout/form_control_layout.stories.tsx
@@ -11,8 +11,14 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { hideStorybookControls } from '../../../../.storybook/utils';
 
-import { EuiIcon } from '../../icon';
+import { useIsWithinMinBreakpoint } from '../../../services';
+import { EuiForm } from '../form';
 import { EuiFieldText } from '../field_text';
+import { EuiIcon } from '../../icon';
+import { EuiIconTip, EuiToolTip } from '../../tool_tip';
+import { EuiPopover } from '../../popover';
+import { EuiButtonEmpty, EuiButtonIcon } from '../../button';
+import { EuiText } from '../../text';
 
 import {
   EuiFormControlLayout,
@@ -89,5 +95,155 @@ export const IconShape: Story = {
     clear: {
       size: 'm',
     },
+  },
+};
+
+export const AppendPrepend: Story = {
+  tags: ['vrt-only'],
+  render: function Render() {
+    const isDesktop = useIsWithinMinBreakpoint('xl');
+    return (
+      <EuiForm
+        fullWidth={isDesktop}
+        css={{ display: 'flex', flexDirection: 'column', gap: 10 }}
+      >
+        <EuiFieldText
+          placeholder="String & text in a tooltip"
+          prepend="String"
+          append={
+            <EuiToolTip content="content">
+              <EuiText size="s">Tooltip</EuiText>
+            </EuiToolTip>
+          }
+        />
+        <EuiFieldText
+          placeholder="XS empty button in a popover & tooltip"
+          prepend={
+            <EuiPopover
+              button={
+                <EuiButtonEmpty size="xs" iconType="arrowDown" iconSide="right">
+                  Popover
+                </EuiButtonEmpty>
+              }
+              closePopover={() => {}}
+            />
+          }
+          append={
+            <EuiToolTip content="content">
+              <EuiButtonEmpty size="xs">Tooltip</EuiButtonEmpty>
+            </EuiToolTip>
+          }
+        />
+        <EuiFieldText
+          placeholder="XS empty buttons with icons"
+          prepend={
+            <EuiButtonEmpty
+              role="button"
+              size="xs"
+              iconType="arrowDown"
+              iconSide="right"
+              aria-label="Calendar dropdown"
+            >
+              <EuiIcon type="calendar" />
+            </EuiButtonEmpty>
+          }
+          append={
+            <EuiButtonEmpty size="xs" iconType="gear">
+              Tooltip
+            </EuiButtonEmpty>
+          }
+        />
+        <EuiFieldText
+          placeholder="Icon & button icon"
+          prepend={<EuiIcon type="vector" />}
+          append={<EuiButtonIcon iconType="gear" aria-label="Gear this" />}
+        />
+        <EuiFieldText
+          placeholder="Icons in buttons and popovers and tooltips"
+          prepend={[
+            <EuiIcon type="vector" />,
+            <EuiButtonIcon iconType="gear" aria-label="Gear this" />,
+          ]}
+          append={[
+            <EuiPopover
+              button={<EuiButtonIcon iconType="gear" aria-label="Gear this" />}
+              closePopover={() => {}}
+            />,
+            <EuiIconTip content="content" />,
+          ]}
+        />
+        <EuiFieldText
+          placeholder="Icon button in popover & tooltip"
+          append={
+            <EuiPopover
+              button={
+                <EuiButtonIcon iconType="arrowDown" aria-label="Popover" />
+              }
+              closePopover={() => {}}
+            />
+          }
+          prepend={
+            <EuiToolTip content="content">
+              <EuiButtonIcon iconType="gear" aria-label="Gear this" />
+            </EuiToolTip>
+          }
+        />
+        <EuiFieldText
+          placeholder="Icon and string & string and icon button"
+          prepend={[<EuiIcon type="vector" />, 'String']}
+          append={[
+            'String',
+            <EuiButtonIcon iconType="gear" aria-label="Gear this" />,
+          ]}
+        />
+        <EuiFieldText
+          placeholder="String and button icon in tooltip & button icon in popover and string"
+          prepend={[
+            'String',
+            <EuiToolTip content="content">
+              <EuiButtonIcon iconType="gear" aria-label="Gear this" />
+            </EuiToolTip>,
+          ]}
+          append={[
+            <EuiPopover
+              button={<EuiButtonIcon iconType="gear" aria-label="Gear this" />}
+              closePopover={() => {}}
+            />,
+            'String',
+          ]}
+        />
+        <EuiFieldText
+          compressed={true}
+          placeholder="Compressed"
+          prepend="String"
+          append={[
+            'String',
+            <EuiButtonIcon iconType="gear" aria-label="Gear this" />,
+          ]}
+        />
+        <EuiFieldText
+          disabled={true}
+          placeholder="Disabled"
+          prepend="String"
+          append={[
+            <EuiPopover
+              button={<EuiButtonIcon iconType="gear" aria-label="Gear this" />}
+              closePopover={() => {}}
+            />,
+          ]}
+        />
+        <EuiFieldText
+          readOnly={true}
+          placeholder="Readonly"
+          prepend={
+            <EuiPopover
+              button={<EuiButtonIcon iconType="gear" aria-label="Gear this" />}
+              closePopover={() => {}}
+            />
+          }
+          append="String"
+        />
+      </EuiForm>
+    );
   },
 };
