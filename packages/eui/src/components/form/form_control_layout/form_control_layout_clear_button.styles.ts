a343047e2922df40a7fefac5313426131aa2f6c8
Cee Chen <constance.chen@elastic.co>
2024-08-12 21:44:42 -0700

    [EuiFormControlLayoutClearButton] Convert styles + cleanup
    TSX:
    - remove className map
    - remove wrapping EuiI18n in favor of hook
    
    CSS:
    - remove unnecessary specific sizes in favor of percentages (50%, 0.5)
    - remove unnecessary `:focus` CSS (same as default browser focus ring)

---
+++ packages/eui/src/components/form/form_control_layout/form_control_layout_clear_button.styles.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { css } from '@emotion/react';
+
+import { UseEuiTheme } from '../../../services';
+
+export const EuiFormControlLayoutClearButtonStyles = ({
+  euiTheme,
+  colorMode,
+}: UseEuiTheme) => {
+  const backgroundColor =
+    colorMode === 'DARK'
+      ? euiTheme.colors.darkShade
+      : euiTheme.colors.mediumShade;
+  return {
+    euiFormControlLayoutClearButton: css`
+      pointer-events: all;
+      background-color: ${backgroundColor};
+      border-radius: 50%;
+      line-height: 0; /* ensures the icon stays vertically centered */
+    `,
+    euiFormControlLayoutClearButton__icon: css`
+      transform: scale(0.5);
+      fill: ${euiTheme.colors.emptyShade};
+      stroke: ${euiTheme.colors.emptyShade};
+    `,
+    size: {
+      s: css`
+        stroke-width: ${euiTheme.size.xs};
+      `,
+      m: css`
+        stroke-width: ${euiTheme.size.xxs};
+      `,
+    },
+  };
+};
