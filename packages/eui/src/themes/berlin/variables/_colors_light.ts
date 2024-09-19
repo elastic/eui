0081b3c8db8c7abe94e37311692633d145613f22
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-09-19 20:14:41 +0200

    feat: add new theme setup

---
+++ packages/eui/src/themes/berlin/variables/_colors_light.ts
@@ -0,0 +1,80 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { shade, tint } from '../../../services/color';
+import { computed } from '../../../services/theme/utils';
+import {
+  makeHighContrastColor,
+  makeDisabledContrastColor,
+} from '../../../services/color/contrast';
+import {
+  _EuiThemeColors,
+  _EuiThemeBrandColors,
+  _EuiThemeBrandTextColors,
+  _EuiThemeShadeColors,
+  _EuiThemeSpecialColors,
+  _EuiThemeTextColors,
+  _EuiThemeColorsMode,
+} from '../../../global_styling/variables/colors';
+
+/*
+ * LIGHT THEME
+ */
+
+export const brand_colors: _EuiThemeBrandColors = {
+  primary: '#07C',
+  accent: '#F04E98',
+  success: '#00BFB3',
+  warning: '#FEC514',
+  danger: '#BD271E',
+};
+
+export const brand_text_colors: _EuiThemeBrandTextColors = {
+  primaryText: computed(makeHighContrastColor('colors.primary')),
+  accentText: computed(makeHighContrastColor('colors.accent')),
+  successText: computed(makeHighContrastColor('colors.success')),
+  warningText: computed(makeHighContrastColor('colors.warning')),
+  dangerText: computed(makeHighContrastColor('colors.danger')),
+};
+
+export const shade_colors: _EuiThemeShadeColors = {
+  emptyShade: '#FFF',
+  lightestShade: '#F1F4FA',
+  lightShade: '#D3DAE6',
+  mediumShade: '#98A2B3',
+  darkShade: '#69707D',
+  darkestShade: '#343741',
+  fullShade: '#000',
+};
+
+export const special_colors: _EuiThemeSpecialColors = {
+  body: computed(
+    ([lightestShade]) => tint(lightestShade, 0.4),
+    ['colors.lightestShade']
+  ),
+  highlight: computed(([warning]) => tint(warning, 0.9), ['colors.warning']),
+  disabled: '#ABB4C4',
+  disabledText: computed(makeDisabledContrastColor('colors.disabled')),
+  shadow: computed(({ colors }) => colors.ink),
+};
+
+export const text_colors: _EuiThemeTextColors = {
+  text: computed(([darkestShade]) => darkestShade, ['colors.darkestShade']),
+  title: computed(([text]) => shade(text, 0.5), ['colors.text']),
+  subduedText: computed(makeHighContrastColor('colors.darkShade')),
+  link: computed(([primaryText]) => primaryText, ['colors.primaryText']),
+};
+
+export const light_colors: _EuiThemeColorsMode = {
+  ...brand_colors,
+  ...shade_colors,
+  ...special_colors,
+  // Need to come after special colors so they can react to `body`
+  ...brand_text_colors,
+  ...text_colors,
+};
