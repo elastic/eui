c6e849b679ab95b8783f1a45f39b9c91bf1f185a
Tomasz Kajtoch <tomek@kajto.ch>
2024-10-15 18:33:29 +0200

    ci: update github PAT token secret path (#8078)

--- .buildkite/scripts/lifecycle/pre_command.sh
+++ .buildkite/scripts/lifecycle/pre_command.sh
@@ -7,14 +7,14 @@ echo '[SOURCE]: Buildkite dependencies'
 source .buildkite/scripts/common/utils.sh
 
 echo '[INSTALL]: Non-exported variables'
-GITHUB_ACCOUNT=secret/ci/elastic-eui/kibanamachine
+GITHUB_ACCOUNT=secret/ci/elastic-eui/github_machine_user
 VAULT_ACCOUNT=secret/ci/elastic-eui/bekitzur-kibana-service-account
 
 echo '[INSTALL]: Exported variables'
 GCE_ACCOUNT=$(retry 5 vault read -field=value $VAULT_ACCOUNT)
 export GCE_ACCOUNT
 
-GITHUB_TOKEN=$(retry 5 vault read -field=github_token $GITHUB_ACCOUNT)
+GITHUB_TOKEN=$(retry 5 vault read -field=kibanamachine_token $GITHUB_ACCOUNT)
 export GITHUB_TOKEN
 
 DOCKER_BASE_IMAGE=docker.elastic.co/eui/ci:6.6
