45c76bc47a642cfb89a5480c97ee8ba2d46504ff
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-12-05 16:28:51 +0100

    refactor: fix docs sass and JSON token loading; remove unexpected deprectation label
    - the CSS hook is useful to prevent having to define variant styles per component

--- packages/eui/src/global_styling/mixins/_color.ts
+++ packages/eui/src/global_styling/mixins/_color.ts
@@ -104,7 +104,6 @@ export const useEuiBackgroundColor = (
 };
 
 /**
- * @deprecated
  * @returns An object map of color keys to CSS,
  * e.g. { danger: css``, success: css``, ... }
  */
@@ -130,7 +129,6 @@ const _euiBackgroundColors = (euiThemeContext: UseEuiTheme) =>
   }, {} as Record<_EuiBackgroundColor, SerializedStyles>);
 
 /**
- * @deprecated - use background tokens directly
  * Hook to retrieve background style for a background color variant
  * @returns An object map of color keys to CSS,
  * e.g. { danger: css``, success: css``, ... }
