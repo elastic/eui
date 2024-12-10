d19f2e6f945d9e3f983efe5b1d04aca9fc66d391
Weronika Olejniczak <weronika.olejniczak@elastic.co>
2024-12-10 19:15:48 +0100

    feat(eslint-plugin): add no-restricted-eui-imports rule

---
+++ packages/eslint-plugin/rules/no_restricted_eui_imports.test.js
@@ -0,0 +1,50 @@
+const { RuleTester } = require('eslint');
+const rule = require('./no_restricted_eui_imports');
+
+const ruleTester = new RuleTester({
+  parser: require.resolve('babel-eslint'),
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+});
+
+ruleTester.run('@elastic/eui/no-restricted-eui-imports', rule, {
+  valid: [
+    {
+      code: "import { EuiButton } from '@elastic/eui';",
+    },
+    {
+      code: "import theme from '@kbn/ui-theme';",
+    },
+  ],
+
+  invalid: [
+    {
+      code: "import theme from '@elastic/eui/dist/eui_theme_light.json';",
+      errors: [
+        {
+          message:
+            'Please use `useEuiTheme` instead. Direct JSON token imports will be removed as per the EUI Deprecation schedule: https://github.com/elastic/eui/issues/1469.',
+        },
+      ],
+    },
+    {
+      code: "import theme from '@kbn/ui-theme';",
+      options: [
+        {
+          patterns: [
+            {
+              pattern: '@kbn/ui-theme',
+              message: 'Please use `useEuiTheme` from `@elastic/eui` instead.',
+            },
+          ],
+        },
+      ],
+      errors: [
+        {
+          message: 'Please use `useEuiTheme` from `@elastic/eui` instead.',
+        },
+      ],
+    },
+  ],
+});
