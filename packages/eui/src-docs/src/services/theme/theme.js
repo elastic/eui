c5af49135b2bd09cae86e1f3956eaea02905bfc9
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-12-12 09:27:52 +0100

    [Visual Refresh] Update color token mappings (#8211)

--- packages/eui/src-docs/src/services/theme/theme.js
+++ packages/eui/src-docs/src/services/theme/theme.js
@@ -8,5 +8,12 @@ export function applyTheme(newTheme, colorMode = 'LIGHT') {
   Object.keys(themes).forEach((theme) =>
     Object.values(themes[theme]).forEach((cssFile) => cssFile.unuse())
   );
+
   themes[newTheme]?.[colorMode]?.use();
+
+  if (newTheme.toLowerCase().includes('borealis')) {
+    document?.body.classList.add('euiTheme--borealis');
+  } else {
+    document?.body.classList.remove('euiTheme--borealis');
+  }
 }
