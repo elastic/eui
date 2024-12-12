578de0bac3e1f4c6af82bf88738125edd41130cf
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-12-12 21:22:26 +0100

    docs: ensure high contrast mode works in EUI+
    - enables workspace EUI version for docusaurus-theme
    
    - fixes theme vs colorMode setting on EuiProvider in EUI+

--- packages/docusaurus-theme/src/components/theme_context/index.tsx
+++ packages/docusaurus-theme/src/components/theme_context/index.tsx
@@ -9,13 +9,13 @@ import { EUI_THEMES, EuiProvider, EuiThemeColorMode } from '@elastic/eui';
 
 import { EuiThemeOverrides } from './theme_overrides';
 
-const EUI_THEME_NAMES = EUI_THEMES.map(
-  ({ value }) => value
-) as EuiThemeColorMode[];
+const EUI_THEME_NAMES = EUI_THEMES.map(({ value }) => value);
 
 const defaultState = {
-  theme: EUI_THEME_NAMES[0] as EuiThemeColorMode,
+  theme: EUI_THEME_NAMES[0] as string,
+  colorMode: 'LIGHT' as EuiThemeColorMode,
   changeTheme: (themeValue: EuiThemeColorMode) => {},
+  changeColorMode: (colorMode: EuiThemeColorMode) => {},
 };
 
 export const AppThemeContext = createContext(defaultState);
@@ -24,25 +24,42 @@ export const AppThemeProvider: FunctionComponent<PropsWithChildren> = ({
   children,
 }) => {
   const isBrowser = useIsBrowser();
-  const [theme, setTheme] = useState<EuiThemeColorMode>(() => {
+
+  const [theme, setTheme] = useState<string>(() => {
     if (isBrowser) {
-      return localStorage.getItem('theme') as EuiThemeColorMode ?? defaultState.theme;
+      return (
+        (localStorage.getItem('theme') as EuiThemeColorMode) ??
+        defaultState.theme
+      );
     }
 
     return defaultState.theme;
   });
 
+  const [colorMode, setColorMode] = useState<EuiThemeColorMode>(() => {
+    if (isBrowser) {
+      return (
+        (localStorage.getItem('colorMode') as EuiThemeColorMode) ??
+        defaultState.colorMode
+      );
+    }
+
+    return defaultState.colorMode;
+  });
+
   return (
     <AppThemeContext.Provider
       value={{
-        theme,
+        theme: theme,
+        colorMode,
         changeTheme: setTheme,
+        changeColorMode: setColorMode,
       }}
     >
       <EuiProvider
         globalStyles={false}
         modify={EuiThemeOverrides}
-        colorMode={theme}
+        colorMode={colorMode}
       >
         {children}
       </EuiProvider>
