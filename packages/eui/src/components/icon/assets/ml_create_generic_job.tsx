4bdfaaab32fad2092036b50b6911a0fa4f272b44
rbrtj <robert.jaszczurek@elastic.co>
2024-12-18 11:33:57 +0100

    cleanup generic job icon

--- packages/eui/src/components/icon/assets/ml_create_generic_job.tsx
+++ packages/eui/src/components/icon/assets/ml_create_generic_job.tsx
@@ -23,7 +23,6 @@ const EuiIconMlCreateGenericJob = ({
     xmlns="http://www.w3.org/2000/svg"
     width={32}
     height={32}
-    fill="none"
     viewBox="0 0 32 32"
     aria-labelledby={titleId}
     {...props}
