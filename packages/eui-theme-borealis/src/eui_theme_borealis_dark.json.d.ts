305c92edf02ac6a2511bf3253f8431e1a6987d36
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-01-20 14:21:48 +0100

    [Visual Refresh] Support new theme tokens on JSON exports (#8277)

--- packages/eui-theme-borealis/src/eui_theme_borealis_dark.json.d.ts
+++ packages/eui-theme-borealis/src/eui_theme_borealis_dark.json.d.ts
@@ -46,69 +46,6 @@ declare module '@elastic/eui-theme-borealis/lib/eui_theme_borealis_dark.json' {
       ghost: string;
       text: string;
     };
-    euiPaletteColorBlind: {
-      euiColorVis0: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis1: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis2: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis3: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis4: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis5: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis6: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis7: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis8: {
-        graphic: string;
-        behindText: string;
-      };
-      euiColorVis9: {
-        graphic: string;
-        behindText: string;
-      };
-    };
-    euiPaletteColorBlindKeys: string;
-    euiColorVis0: string;
-    euiColorVis1: string;
-    euiColorVis2: string;
-    euiColorVis3: string;
-    euiColorVis4: string;
-    euiColorVis5: string;
-    euiColorVis6: string;
-    euiColorVis7: string;
-    euiColorVis8: string;
-    euiColorVis9: string;
-    euiColorVis0_behindText: string;
-    euiColorVis1_behindText: string;
-    euiColorVis2_behindText: string;
-    euiColorVis3_behindText: string;
-    euiColorVis4_behindText: string;
-    euiColorVis5_behindText: string;
-    euiColorVis6_behindText: string;
-    euiColorVis7_behindText: string;
-    euiColorVis8_behindText: string;
-    euiColorVis9_behindText: string;
     euiFontWeightLight: number;
     euiFontWeightRegular: number;
     euiFontWeightMedium: number;
@@ -281,6 +218,118 @@ declare module '@elastic/eui-theme-borealis/lib/eui_theme_borealis_dark.json' {
     euiZFlyout: number;
     euiZMaskBelowHeader: number;
     euiZContent: number;
+    euiPaletteColorBlind: {
+      euiColorVis0: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis1: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis2: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis3: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis4: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis5: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis6: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis7: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis8: {
+        graphic: string;
+        behindText: string;
+      };
+      euiColorVis9: {
+        graphic: string;
+        behindText: string;
+      };
+    };
+    euiPaletteColorBlindKeys: string;
+    euiColorVis0: string;
+    euiColorVis1: string;
+    euiColorVis2: string;
+    euiColorVis3: string;
+    euiColorVis4: string;
+    euiColorVis5: string;
+    euiColorVis6: string;
+    euiColorVis7: string;
+    euiColorVis8: string;
+    euiColorVis9: string;
+    euiColorVis0_behindText: string;
+    euiColorVis1_behindText: string;
+    euiColorVis2_behindText: string;
+    euiColorVis3_behindText: string;
+    euiColorVis4_behindText: string;
+    euiColorVis5_behindText: string;
+    euiColorVis6_behindText: string;
+    euiColorVis7_behindText: string;
+    euiColorVis8_behindText: string;
+    euiColorVis9_behindText: string;
+    euiColorVisAsTextLight0: string;
+    euiColorVisAsTextLight1: string;
+    euiColorVisAsTextLight2: string;
+    euiColorVisAsTextLight3: string;
+    euiColorVisAsTextLight4: string;
+    euiColorVisAsTextLight5: string;
+    euiColorVisAsTextLight6: string;
+    euiColorVisAsTextDark0: string;
+    euiColorVisAsTextDark1: string;
+    euiColorVisAsTextDark2: string;
+    euiColorVisAsTextDark3: string;
+    euiColorVisAsTextDark4: string;
+    euiColorVisAsTextDark5: string;
+    euiColorVisAsTextDark6: string;
+    euiColorVisSuccess0: string;
+    euiColorVisSuccess1: string;
+    euiColorVisWarning0: string;
+    euiColorVisDanger0: string;
+    euiColorVisDanger1: string;
+    euiColorVisNeutral0: string;
+    euiColorVisGrey0: string;
+    euiColorVisGrey1: string;
+    euiColorVisGrey2: string;
+    euiColorVisGrey3: string;
+    euiColorVisWarm0: string;
+    euiColorVisWarm1: string;
+    euiColorVisWarm2: string;
+    euiColorVisCool0: string;
+    euiColorVisCool1: string;
+    euiColorVisCool2: string;
+    euiColorVisComplementary0: string;
+    euiColorVisComplementary1: string;
+    euiColorSeverity0: string;
+    euiColorSeverity1: string;
+    euiColorSeverity2: string;
+    euiColorSeverity3: string;
+    euiColorSeverity4: string;
+    euiColorSeverity5: string;
+    euiColorSeverity6: string;
+    euiColorSeverity7: string;
+    euiColorSeverity8: string;
+    euiColorSeverity9: string;
+    euiColorSeverity10: string;
+    euiColorSeverity11: string;
+    euiColorSeverity12: string;
+    euiColorSeverity13: string;
+    euiColorSeverity14: string;
+    euiColorChartLines: string;
+    euiColorChartBand: string;
     euiColorGhost: string;
     euiColorInk: string;
     euiColorPrimary: string;
@@ -305,13 +354,72 @@ declare module '@elastic/eui-theme-borealis/lib/eui_theme_borealis_dark.json' {
     euiColorPrimaryText: string;
     euiColorSuccessText: string;
     euiColorAccentText: string;
-    euiColorAccentSecondaryText: string;
     euiColorWarningText: string;
     euiColorDangerText: string;
     euiColorDisabledText: string;
     euiLinkColor: string;
-    euiColorChartLines: string;
-    euiColorChartBand: string;
+    euiColorPlainLight: string;
+    euiColorPlainDark: string;
+    euiColorTextPrimary: string;
+    euiColorTextAccent: string;
+    euiColorTextAccentSecondary: string;
+    euiColorTextSuccess: string;
+    euiColorTextWarning: string;
+    euiColorTextDanger: string;
+    euiColorTextParagraph: string;
+    euiColorTextHeading: string;
+    euiColorTextSubdued: string;
+    euiColorTextDisabled: string;
+    euiColorTextInverse: string;
+    euiColorBackgroundBasePrimary: string;
+    euiColorBackgroundBaseAccent: string;
+    euiColorBackgroundBaseAccentSecondary: string;
+    euiColorBackgroundBaseSuccess: string;
+    euiColorBackgroundBaseWarning: string;
+    euiColorBackgroundBaseDanger: string;
+    euiColorBackgroundBaseSubdued: string;
+    euiColorBackgroundBasePlain: string;
+    euiColorBackgroundBaseDisabled: string;
+    euiColorBackgroundBaseHighlighted: string;
+    euiColorBackgroundBaseFormsPrepend: string;
+    euiColorBackgroundBaseFormsControlDisabled: string;
+    euiColorBackgroundBaseInteractiveHover: string;
+    euiColorBackgroundBaseInteractiveSelect: string;
+    euiColorBackgroundBaseInteractiveOverlay: string;
+    euiColorBackgroundBaseSkeletonEdge: string;
+    euiColorBackgroundBaseSkeletonMiddle: string;
+    euiColorBackgroundLightPrimary: string;
+    euiColorBackgroundLightAccent: string;
+    euiColorBackgroundLightAccentSecondary: string;
+    euiColorBackgroundLightSuccess: string;
+    euiColorBackgroundLightWarning: string;
+    euiColorBackgroundLightDanger: string;
+    euiColorBackgroundLightText: string;
+    euiColorBackgroundFilledPrimary: string;
+    euiColorBackgroundFilledAccent: string;
+    euiColorBackgroundFilledAccentSecondary: string;
+    euiColorBackgroundFilledSuccess: string;
+    euiColorBackgroundFilledWarning: string;
+    euiColorBackgroundFilledDanger: string;
+    euiColorBackgroundFilledText: string;
+    euiColorBorderBasePrimary: string;
+    euiColorBorderBaseAccent: string;
+    euiColorBorderBaseAccentSecondary: string;
+    euiColorBorderBaseSuccess: string;
+    euiColorBorderBaseWarning: string;
+    euiColorBorderBaseDanger: string;
+    euiColorBorderBasePlain: string;
+    euiColorBorderBaseSubdued: string;
+    euiColorBorderBaseDisabled: string;
+    euiColorBorderBaseFloating: string;
+    euiColorBorderBaseFormsColorSwatch: string;
+    euiColorBorderBaseFormsControl: string;
+    euiColorBorderStrongPrimary: string;
+    euiColorBorderStrongAccent: string;
+    euiColorBorderStrongAccentSecondary: string;
+    euiColorBorderStrongSuccess: string;
+    euiColorBorderStrongWarning: string;
+    euiColorBorderStrongDanger: string;
   };
   export default sassVariables;
 }
