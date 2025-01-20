43ffc69dead33f6f946936f547d5813a76fbbda2
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-01-20 16:30:28 +0100

    feat: add react hooks for color palette functions

--- packages/eui/src/themes/index.ts
+++ packages/eui/src/themes/index.ts
@@ -5,7 +5,7 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-
+export { colorVis } from './amsterdam/global_styling/variables/_colors_vis';
 export type { EUI_THEME } from './themes';
 export {
   EUI_THEMES,
