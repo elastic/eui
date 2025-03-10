ec2f8c91531b241a1675cb48fb8cd26fbcb922c8
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-03-10 17:12:08 +0100

    feat: add more new tokens to Amsterdam theme

--- packages/eui/src/themes/amsterdam/global_styling/variables/_colors.ts
+++ packages/eui/src/themes/amsterdam/global_styling/variables/_colors.ts
@@ -307,6 +307,9 @@ export const border_colors: _EuiThemeBorderColors = {
     ['colors.lightestShade']
   ),
 
+  borderInteractiveFormsHoverPlain: 'transparent',
+  borderInteractiveFormsHoverDanger: 'transparent',
+
   borderStrongPrimary: computed(
     ([borderBasePrimary]) => borderBasePrimary,
     ['colors.borderBasePrimary']
@@ -530,6 +533,8 @@ export const dark_border_colors: _EuiThemeBorderColors = {
     ([lightestShade]) => tint(lightestShade, 0.31),
     ['colors.lightestShade']
   ),
+  borderInteractiveFormsHoverPlain: 'transparent',
+  borderInteractiveFormsHoverDanger: 'transparent',
 
   borderStrongPrimary: computed(
     ([borderBasePrimary]) => borderBasePrimary,
