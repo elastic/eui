0e8128f7a839104bb848d04cf7c351f05a8d8221
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-03-10 16:57:42 +0100

    feat: update combobox styles

--- packages/eui/src/components/combo_box/combo_box_input/combo_box_input.styles.ts
+++ packages/eui/src/components/combo_box/combo_box_input/combo_box_input.styles.ts
@@ -18,6 +18,7 @@ import {
 
 export const euiComboBoxInputStyles = (euiThemeContext: UseEuiTheme) => {
   const { euiTheme } = euiThemeContext;
+  const isExperimental = euiTheme.flags?.formVariant === 'experimental';
   const formStyles = euiFormControlStyles(euiThemeContext);
 
   return {
@@ -33,14 +34,20 @@ export const euiComboBoxInputStyles = (euiThemeContext: UseEuiTheme) => {
       ${formStyles.uncompressed}
       ${logicalCSS('height', 'auto')}
       ${logicalCSS('padding-vertical', euiTheme.size.s)}
-      ${logicalCSS('padding-left', euiTheme.size.s)}
+      ${logicalCSS(
+        'padding-left',
+        isExperimental ? euiTheme.size.m : euiTheme.size.s
+      )}
       column-gap: ${euiTheme.size.s};
       row-gap: ${euiTheme.size.xs};
     `,
     compressed: css`
       ${formStyles.compressed}
       ${logicalCSS('height', 'auto')}
-      ${logicalCSS('padding-vertical', euiTheme.size.xs)}
+      ${logicalCSS(
+        'padding-vertical',
+        isExperimental ? euiTheme.size.s : euiTheme.size.xs
+      )}
       ${logicalCSS('padding-left', euiTheme.size.xs)}
       column-gap: ${euiTheme.size.xs};
       row-gap: ${euiTheme.size.xxs};
@@ -75,7 +82,7 @@ export const euiComboBoxInputStyles = (euiThemeContext: UseEuiTheme) => {
       background: transparent;
 
       &:disabled {
-        color: ${euiTheme.colors.disabledText};
+        color: ${euiTheme.colors.textDisabled};
       }
 
       /* Ensure that no input states are visible on the hidden input */
