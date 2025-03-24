6a6c0999c73dd8525d36ee0d1aaa7244866a1d52
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2025-03-24 14:12:13 +0100

    fix: remove docusaurus theme global body color override

--- packages/docusaurus-theme/src/theme/Navbar/Layout/index.tsx
+++ packages/docusaurus-theme/src/theme/Navbar/Layout/index.tsx
@@ -19,7 +19,7 @@ const getStyles = ({ euiTheme }: UseEuiTheme) => {
       --ifm-navbar-item-padding-horizontal: 0;
       --ifm-navbar-item-padding-vertical: 0;
 
-      --ifm-navbar-background-color: ${euiTheme.colors.body};
+      --ifm-navbar-background-color: ${euiTheme.colors.textInverse};
 
       flex-grow: 0;
       flex-shrink: 0;
