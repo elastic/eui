151a1dc613c27da6235d2728b05beeb7ef652a4c
Tomasz Kajtoch <tomek@kajto.ch>
2025-03-25 11:28:15 +0100

    fix: remove docusaurus theme global body color override (#8495)

--- packages/docusaurus-theme/src/components/theme_context/theme_overrides.ts
+++ packages/docusaurus-theme/src/components/theme_context/theme_overrides.ts
@@ -1,11 +1,6 @@
 import { EuiThemeModifications } from '@elastic/eui';
 
 export const EuiThemeOverrides: EuiThemeModifications = {
-  colors: {
-    LIGHT: {
-      body: '#fff',
-    },
-  },
   font: {
     lineHeightMultiplier: 1.75,
   },
