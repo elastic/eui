6f5bd76c6c3bf01da42c6ab3dc4164341a26b9dd
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-03-27 18:37:35 +0100

    feat(colorPickerSwatch): add color label tooltips
    - adds prop showToolTip for conditional toggling, by default we do want the tooltips to render

--- packages/eui/src/components/color_picker/color_picker_swatch.stories.tsx
+++ packages/eui/src/components/color_picker/color_picker_swatch.stories.tsx
@@ -8,6 +8,8 @@
 
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { LOKI_SELECTORS, lokiPlayDecorator } from '../../../.storybook/loki';
+import { sleep } from '../../test';
 import {
   EuiColorPickerSwatch,
   EuiColorPickerSwatchProps,
@@ -16,9 +18,35 @@ import {
 const meta: Meta<EuiColorPickerSwatchProps> = {
   title: 'Forms/EuiColorPicker/EuiColorPickerSwatch',
   component: EuiColorPickerSwatch,
+  args: {
+    showToolTip: true,
+    color: '#dddddd',
+  },
 };
 
 export default meta;
 type Story = StoryObj<EuiColorPickerSwatchProps>;
 
 export const Playground: Story = {};
+
+export const ColorLabelTooltip: Story = {
+  tags: ['vrt-only'],
+  parameters: {
+    controls: {
+      exclude: ['autoFocus'],
+    },
+    loki: {
+      chromeSelector: LOKI_SELECTORS.portal,
+    },
+  },
+  args: {
+    autoFocus: true,
+    toolTipProps: {
+      position: 'right',
+    },
+  },
+  play: lokiPlayDecorator(async () => {
+    // Reduce VRT flakiness/screenshots before tooltip is fully visible
+    await sleep(300);
+  }),
+};
