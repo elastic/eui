5b53a1b4433594f2bb5b682a90175aea4863ea45
Weronika Olejniczak <weronika.olejniczak@elastic.co>
2025-03-27 15:32:18 +0100

    refactor(docusaurus-theme): use prRootUrl from siteConfig for Storybook redirect

--- packages/docusaurus-theme/src/components/storybook_link/index.tsx
+++ packages/docusaurus-theme/src/components/storybook_link/index.tsx
@@ -1,5 +1,7 @@
-import { useLocation } from '@docusaurus/router';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { EuiButton } from '@elastic/eui';
+
+import { hasPrRootUrl } from './has_pr_root_url';
 
 type Props = {
   id: string;
@@ -7,25 +9,19 @@ type Props = {
 };
 
 export const StorybookLink = ({ id, viewMode = 'story' }: Props) => {
-  const location = useLocation();
+  const { siteConfig } = useDocusaurusContext();
 
-  // Determine the base URL based on the environment
-  // This is just for the sake of POC! Long-term, it'd be better to receive the base url
-  // as an environment variable
   let baseUrl: string;
 
   if (process.env.NODE_ENV === 'development') {
     // Potentially get the Storybook config for easier maintenance
     baseUrl = 'http://localhost:6006';
-  } else if (location.pathname.includes('/pr_')) {
-    const prPath = location.pathname.split('/').slice(0, 2).join('/');
-    baseUrl = `https://eui.elastic.co${prPath}/storybook`;
+  } else if (hasPrRootUrl(siteConfig)) {
+    baseUrl = `https://eui.elastic.co/${siteConfig.customFields.prRootUrl}/storybook`;
   } else {
     baseUrl = 'https://eui.elastic.co/storybook';
   }
 
-  // Construct the dynamic href based on the environment, `id`, and `viewMode`
-  // or potentially pass the whole href as is (easy to copy-paste in docs)
   const href = `${baseUrl}/iframe.html?id=${id}&viewMode=${viewMode}`;
 
   return (
