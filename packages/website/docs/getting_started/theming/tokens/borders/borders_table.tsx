71bbfc48b1df7f1fa3cb74283684440a93d01ffa
Ryan Keairns <contactryank@gmail.com>
2025-04-03 16:11:45 -0700

    resolve conflicts

---
+++ packages/website/docs/getting_started/theming/tokens/borders/borders_table.tsx
@@ -0,0 +1,37 @@
+import { css } from '@emotion/react';
+import {
+  useEuiTheme,
+  EuiColorPickerSwatch,
+  _EuiThemeBorderTypes,
+} from '@elastic/eui';
+import { ThemeValuesTable } from '../../theme_values_table';
+
+const borderTypes: Array<keyof _EuiThemeBorderTypes> = [
+  'thin',
+  'thick',
+  'editable',
+];
+
+export const BordersTable = () => {
+  const { euiTheme } = useEuiTheme();
+
+  return (
+    <ThemeValuesTable
+      items={borderTypes.map((type) => ({
+        id: type,
+        token: `border.${type}`,
+        value: euiTheme.border[type],
+      }))}
+      render={(item) => (
+        <EuiColorPickerSwatch
+          showToolTip={false}
+          color={euiTheme.colors.emptyShade}
+          css={css`
+            border: ${item.value};
+          `}
+          disabled
+        />
+      )}
+    />
+  );
+};
