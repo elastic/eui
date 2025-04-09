da7293ad25f41330ce72d7dac878b42e66f65e6f
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-04-09 13:46:48 +0200

    feat: add new button tokens, update semantic tokens

--- packages/eui-theme-borealis/src/variables/_buttons.ts
+++ packages/eui-theme-borealis/src/variables/_buttons.ts
@@ -6,7 +6,12 @@
  * Side Public License, v 1.
  */
 
-import { _EuiThemeButton } from '@elastic/eui-theme-common';
+import {
+  _EuiThemeButton,
+  EuiThemeVariantFlags,
+  ColorModeSwitch,
+  computed,
+} from '@elastic/eui-theme-common';
 import { SEMANTIC_COLORS } from './colors/_semantic_colors';
 import {
   background_colors,
@@ -19,6 +24,22 @@ import {
   dark_text_colors,
 } from './colors/_colors_dark';
 
+const getTokenByVariant = (
+  buttonVariant: EuiThemeVariantFlags['buttonVariant'],
+  tokens: {
+    classic: ColorModeSwitch;
+    refresh: ColorModeSwitch;
+  }
+) => {
+  switch (buttonVariant) {
+    case 'refresh':
+      return tokens.refresh;
+    default: {
+      return tokens.classic;
+    }
+  }
+};
+
 const _buttons = {
   backgroundPrimary: background_colors.backgroundLightPrimary,
   backgroundAccent: background_colors.backgroundLightAccent,
@@ -26,8 +47,40 @@ const _buttons = {
   backgroundSuccess: background_colors.backgroundLightSuccess,
   backgroundWarning: background_colors.backgroundLightWarning,
   backgroundDanger: background_colors.backgroundLightDanger,
-  backgroundText: background_colors.backgroundLightText,
-  backgroundDisabled: background_colors.backgroundBaseDisabled,
+  backgroundText: computed(
+    ([buttonVariant]) => {
+      return getTokenByVariant(buttonVariant, {
+        refresh: background_colors.backgroundBasePlain,
+        classic: background_colors.backgroundLightText,
+      });
+    },
+    ['flags.buttonVariant']
+  ),
+  backgroundDisabled: computed(
+    ([buttonVariant]) => {
+      return getTokenByVariant(buttonVariant, {
+        refresh: background_colors.backgroundBasePlain,
+        classic: background_colors.backgroundBaseDisabled,
+      });
+    },
+    ['flags.buttonVariant']
+  ),
+
+  backgroundPrimaryHover: SEMANTIC_COLORS.primary70Alpha12,
+  backgroundAccentHover: SEMANTIC_COLORS.accent70Alpha12,
+  backgroundAccentSecondaryHover: SEMANTIC_COLORS.accentSecondary70Alpha12,
+  backgroundSuccessHover: SEMANTIC_COLORS.success70Alpha12,
+  backgroundWarningHover: SEMANTIC_COLORS.warning60Alpha16,
+  backgroundDangerHover: SEMANTIC_COLORS.danger70Alpha12,
+  backgroundTextHover: SEMANTIC_COLORS.primary100Alpha8,
+
+  backgroundPrimaryActive: SEMANTIC_COLORS.primary70Alpha16,
+  backgroundAccentActive: SEMANTIC_COLORS.accent70Alpha16,
+  backgroundAccentSecondaryActive: SEMANTIC_COLORS.accentSecondary70Alpha16,
+  backgroundSuccessActive: SEMANTIC_COLORS.success70Alpha16,
+  backgroundWarningActive: SEMANTIC_COLORS.warning60Alpha20,
+  backgroundDangerActive: SEMANTIC_COLORS.danger70Alpha16,
+  backgroundTextActive: SEMANTIC_COLORS.primary100Alpha12,
 
   backgroundFilledPrimary: background_colors.backgroundFilledPrimary,
   backgroundFilledAccent: background_colors.backgroundFilledAccent,
@@ -39,15 +92,38 @@ const _buttons = {
   backgroundFilledText: background_colors.backgroundFilledText,
   backgroundFilledDisabled: background_colors.backgroundBaseDisabled,
 
-  // Temp. mapping to support more variants in old theme
-  backgroundEmptyPrimaryHover: background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyAccentHover: background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyAccentSecondaryHover:
-    background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptySuccessHover: background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyWarningHover: background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyDangerHover: background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyTextHover: background_colors.backgroundBaseInteractiveHover,
+  backgroundFilledPrimaryHover: SEMANTIC_COLORS.primary100,
+  backgroundFilledAccentHover: SEMANTIC_COLORS.accent100,
+  backgroundFilledAccentSecondaryHover: SEMANTIC_COLORS.accentSecondary100,
+  backgroundFilledSuccessHover: SEMANTIC_COLORS.success100,
+  backgroundFilledWarningHover: SEMANTIC_COLORS.warning50,
+  backgroundFilledDangerHover: SEMANTIC_COLORS.danger100,
+  backgroundFilledTextHover: SEMANTIC_COLORS.shade100,
+
+  backgroundFilledPrimaryActive: SEMANTIC_COLORS.primary110,
+  backgroundFilledAccentActive: SEMANTIC_COLORS.accent110,
+  backgroundFilledAccentSecondaryActive: SEMANTIC_COLORS.accentSecondary110,
+  backgroundFilledSuccessActive: SEMANTIC_COLORS.success110,
+  backgroundFilledWarningActive: SEMANTIC_COLORS.warning60,
+  backgroundFilledDangerActive: SEMANTIC_COLORS.danger110,
+  backgroundFilledTextActive: SEMANTIC_COLORS.shade110,
+
+  backgroundEmptyPrimaryHover: SEMANTIC_COLORS.primary70Alpha12,
+  backgroundEmptyAccentHover: SEMANTIC_COLORS.accent70Alpha12,
+  backgroundEmptyAccentSecondaryHover: SEMANTIC_COLORS.accentSecondary70Alpha12,
+  backgroundEmptySuccessHover: SEMANTIC_COLORS.success70Alpha12,
+  backgroundEmptyWarningHover: SEMANTIC_COLORS.warning60Alpha16,
+  backgroundEmptyDangerHover: SEMANTIC_COLORS.danger70Alpha12,
+  backgroundEmptyTextHover: SEMANTIC_COLORS.primary100Alpha8,
+
+  backgroundEmptyPrimaryActive: SEMANTIC_COLORS.primary70Alpha16,
+  backgroundEmptyAccentActive: SEMANTIC_COLORS.accent70Alpha16,
+  backgroundEmptyAccentSecondaryActive:
+    SEMANTIC_COLORS.accentSecondary70Alpha16,
+  backgroundEmptySuccessActive: SEMANTIC_COLORS.success70Alpha16,
+  backgroundEmptyWarningActive: SEMANTIC_COLORS.warning60Alpha20,
+  backgroundEmptyDangerActive: SEMANTIC_COLORS.danger70Alpha16,
+  backgroundEmptyTextActive: SEMANTIC_COLORS.primary100Alpha12,
 
   textColorPrimary: brand_text_colors.textPrimary,
   textColorAccent: brand_text_colors.textAccent,
@@ -76,8 +152,40 @@ const _dark_buttons = {
   backgroundSuccess: dark_background_colors.backgroundLightSuccess,
   backgroundWarning: dark_background_colors.backgroundLightWarning,
   backgroundDanger: dark_background_colors.backgroundLightDanger,
-  backgroundText: dark_background_colors.backgroundLightText,
-  backgroundDisabled: dark_background_colors.backgroundBaseDisabled,
+  backgroundText: computed(
+    ([buttonVariant]) => {
+      return getTokenByVariant(buttonVariant, {
+        refresh: dark_background_colors.backgroundBasePlain,
+        classic: dark_background_colors.backgroundLightText,
+      });
+    },
+    ['flags.buttonVariant']
+  ),
+  backgroundDisabled: computed(
+    ([buttonVariant]) => {
+      return getTokenByVariant(buttonVariant, {
+        refresh: dark_background_colors.backgroundBasePlain,
+        classic: dark_background_colors.backgroundBaseDisabled,
+      });
+    },
+    ['flags.buttonVariant']
+  ),
+
+  backgroundPrimaryHover: SEMANTIC_COLORS.primary70Alpha16,
+  backgroundAccentHover: SEMANTIC_COLORS.accent70Alpha16,
+  backgroundAccentSecondaryHover: SEMANTIC_COLORS.accentSecondary70Alpha16,
+  backgroundSuccessHover: SEMANTIC_COLORS.success70Alpha16,
+  backgroundWarningHover: SEMANTIC_COLORS.warning60Alpha12,
+  backgroundDangerHover: SEMANTIC_COLORS.danger70Alpha16,
+  backgroundTextHover: SEMANTIC_COLORS.plainLightAlpha12,
+
+  backgroundPrimaryActive: SEMANTIC_COLORS.primary70Alpha20,
+  backgroundAccentActive: SEMANTIC_COLORS.accent70Alpha20,
+  backgroundAccentSecondaryActive: SEMANTIC_COLORS.accentSecondary70Alpha20,
+  backgroundSuccessActive: SEMANTIC_COLORS.success70Alpha20,
+  backgroundWarningActive: SEMANTIC_COLORS.warning60Alpha16,
+  backgroundDangerActive: SEMANTIC_COLORS.danger70Alpha20,
+  backgroundTextActive: SEMANTIC_COLORS.plainLightAlpha16,
 
   backgroundFilledPrimary: dark_background_colors.backgroundFilledPrimary,
   backgroundFilledAccent: dark_background_colors.backgroundFilledAccent,
@@ -89,20 +197,38 @@ const _dark_buttons = {
   backgroundFilledText: dark_background_colors.backgroundFilledText,
   backgroundFilledDisabled: dark_background_colors.backgroundBaseDisabled,
 
-  backgroundEmptyPrimaryHover:
-    dark_background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyAccentHover:
-    dark_background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyAccentSecondaryHover:
-    dark_background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptySuccessHover:
-    dark_background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyWarningHover:
-    dark_background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyDangerHover:
-    dark_background_colors.backgroundBaseInteractiveHover,
-  backgroundEmptyTextHover:
-    dark_background_colors.backgroundBaseInteractiveHover,
+  backgroundFilledPrimaryHover: SEMANTIC_COLORS.primary70,
+  backgroundFilledAccentHover: SEMANTIC_COLORS.accent70,
+  backgroundFilledAccentSecondaryHover: SEMANTIC_COLORS.accentSecondary70,
+  backgroundFilledSuccessHover: SEMANTIC_COLORS.success70,
+  backgroundFilledWarningHover: SEMANTIC_COLORS.warning50,
+  backgroundFilledDangerHover: SEMANTIC_COLORS.danger70,
+  backgroundFilledTextHover: SEMANTIC_COLORS.shade70,
+
+  backgroundFilledPrimaryActive: SEMANTIC_COLORS.primary80,
+  backgroundFilledAccentActive: SEMANTIC_COLORS.accent80,
+  backgroundFilledAccentSecondaryActive: SEMANTIC_COLORS.accentSecondary80,
+  backgroundFilledSuccessActive: SEMANTIC_COLORS.success80,
+  backgroundFilledWarningActive: SEMANTIC_COLORS.warning60,
+  backgroundFilledDangerActive: SEMANTIC_COLORS.danger80,
+  backgroundFilledTextActive: SEMANTIC_COLORS.shade80,
+
+  backgroundEmptyPrimaryHover: SEMANTIC_COLORS.primary70Alpha16,
+  backgroundEmptyAccentHover: SEMANTIC_COLORS.accent70Alpha16,
+  backgroundEmptyAccentSecondaryHover: SEMANTIC_COLORS.accentSecondary70Alpha16,
+  backgroundEmptySuccessHover: SEMANTIC_COLORS.success70Alpha16,
+  backgroundEmptyWarningHover: SEMANTIC_COLORS.warning60Alpha12,
+  backgroundEmptyDangerHover: SEMANTIC_COLORS.danger70Alpha16,
+  backgroundEmptyTextHover: SEMANTIC_COLORS.plainLightAlpha12,
+
+  backgroundEmptyPrimaryActive: SEMANTIC_COLORS.primary70Alpha20,
+  backgroundEmptyAccentActive: SEMANTIC_COLORS.accent70Alpha20,
+  backgroundEmptyAccentSecondaryActive:
+    SEMANTIC_COLORS.accentSecondary70Alpha20,
+  backgroundEmptySuccessActive: SEMANTIC_COLORS.success70Alpha20,
+  backgroundEmptyWarningActive: SEMANTIC_COLORS.warning60Alpha16,
+  backgroundEmptyDangerActive: SEMANTIC_COLORS.danger70Alpha20,
+  backgroundEmptyTextActive: SEMANTIC_COLORS.plainLightAlpha16,
 
   textColorPrimary: dark_brand_text_colors.textPrimary,
   textColorAccent: dark_brand_text_colors.textAccent,
--- packages/eui-theme-common/src/global_styling/variables/buttons.ts
+++ packages/eui-theme-common/src/global_styling/variables/buttons.ts
@@ -21,6 +21,22 @@ export type _EuiThemeButtonColors = {
   backgroundText: ColorModeSwitch;
   backgroundDisabled: ColorModeSwitch;
 
+  backgroundPrimaryHover: ColorModeSwitch;
+  backgroundAccentHover: ColorModeSwitch;
+  backgroundAccentSecondaryHover: ColorModeSwitch;
+  backgroundSuccessHover: ColorModeSwitch;
+  backgroundWarningHover: ColorModeSwitch;
+  backgroundDangerHover: ColorModeSwitch;
+  backgroundTextHover: ColorModeSwitch;
+
+  backgroundPrimaryActive: ColorModeSwitch;
+  backgroundAccentActive: ColorModeSwitch;
+  backgroundAccentSecondaryActive: ColorModeSwitch;
+  backgroundSuccessActive: ColorModeSwitch;
+  backgroundWarningActive: ColorModeSwitch;
+  backgroundDangerActive: ColorModeSwitch;
+  backgroundTextActive: ColorModeSwitch;
+
   backgroundFilledPrimary: ColorModeSwitch;
   backgroundFilledAccent: ColorModeSwitch;
   backgroundFilledAccentSecondary: ColorModeSwitch;
@@ -30,6 +46,22 @@ export type _EuiThemeButtonColors = {
   backgroundFilledText: ColorModeSwitch;
   backgroundFilledDisabled: ColorModeSwitch;
 
+  backgroundFilledPrimaryHover: ColorModeSwitch;
+  backgroundFilledAccentHover: ColorModeSwitch;
+  backgroundFilledAccentSecondaryHover: ColorModeSwitch;
+  backgroundFilledSuccessHover: ColorModeSwitch;
+  backgroundFilledWarningHover: ColorModeSwitch;
+  backgroundFilledDangerHover: ColorModeSwitch;
+  backgroundFilledTextHover: ColorModeSwitch;
+
+  backgroundFilledPrimaryActive: ColorModeSwitch;
+  backgroundFilledAccentActive: ColorModeSwitch;
+  backgroundFilledAccentSecondaryActive: ColorModeSwitch;
+  backgroundFilledSuccessActive: ColorModeSwitch;
+  backgroundFilledWarningActive: ColorModeSwitch;
+  backgroundFilledDangerActive: ColorModeSwitch;
+  backgroundFilledTextActive: ColorModeSwitch;
+
   backgroundEmptyPrimaryHover: ColorModeSwitch;
   backgroundEmptyAccentHover: ColorModeSwitch;
   backgroundEmptyAccentSecondaryHover: ColorModeSwitch;
@@ -37,6 +69,14 @@ export type _EuiThemeButtonColors = {
   backgroundEmptyWarningHover: ColorModeSwitch;
   backgroundEmptyDangerHover: ColorModeSwitch;
   backgroundEmptyTextHover: ColorModeSwitch;
+
+  backgroundEmptyPrimaryActive: ColorModeSwitch;
+  backgroundEmptyAccentActive: ColorModeSwitch;
+  backgroundEmptyAccentSecondaryActive: ColorModeSwitch;
+  backgroundEmptySuccessActive: ColorModeSwitch;
+  backgroundEmptyWarningActive: ColorModeSwitch;
+  backgroundEmptyDangerActive: ColorModeSwitch;
+  backgroundEmptyTextActive: ColorModeSwitch;
 
   textColorPrimary: ColorModeSwitch;
   textColorAccent: ColorModeSwitch;
