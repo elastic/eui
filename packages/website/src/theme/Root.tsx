fb1867c4e645bc7fdfd9557f4cfecf7bc19f5e65
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2025-06-02 14:17:22 +0200

    fix(website): use correct color mode for elastic charts style injection

--- packages/website/src/theme/Root.tsx
+++ packages/website/src/theme/Root.tsx
@@ -1,28 +1,29 @@
-import { useContext, useEffect } from 'react';
-import { AppThemeContext } from '@elastic/eui-docusaurus-theme/components';
+import { useEffect } from 'react';
+import { useAppTheme } from '@elastic/eui-docusaurus-theme/components';
 import chartsLightThemeUrl from '!file-loader!@elastic/charts/dist/theme_only_light.css';
 import chartsDarkThemeUrl from '!file-loader!@elastic/charts/dist/theme_only_dark.css';
 import Root from '@theme-original/Root';
 
 export default (props: any) => {
-  const themeContext = useContext(AppThemeContext);
+  const { colorMode } = useAppTheme();
 
   // This isn't optimal and should be replaced with a more appropriate solution
   useEffect(() => {
-    if (themeContext.theme !== 'light' && themeContext.theme !== 'dark') {
+    if (colorMode !== 'light' && colorMode !== 'dark') {
       return;
     }
 
     const element = document.createElement('link');
     element.rel = 'stylesheet';
-    element.href = themeContext.theme === 'light' ? chartsLightThemeUrl : chartsDarkThemeUrl;
+    element.href =
+      colorMode === 'light' ? chartsLightThemeUrl : chartsDarkThemeUrl;
     element.dataset.elasticChartsTheme = 'true';
     document.head.appendChild(element);
 
     return () => {
-      document.head.removeChild(element)
+      document.head.removeChild(element);
     };
-  }, [themeContext.theme]);
+  }, [colorMode]);
 
   return <Root {...props} />;
-}
+};
