75747594f62d48b5c1000d026091b0fe498f7304
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2025-06-05 14:09:27 +0200

    docs: move emotion content to the "getting started" section

--- packages/website/docusaurus.config.ts
+++ packages/website/docusaurus.config.ts
@@ -141,12 +141,6 @@ const config: Config = {
           label: 'Data visualization',
         },
         {
-          type: 'docSidebar',
-          sidebarId: 'developerGuides',
-          position: 'left',
-          label: 'Developer guides',
-        },
-        {
           href: 'https://github.com/elastic/eui/tree/main/packages/eui/changelogs',
           label: 'EUI Changelog',
           position: 'right',
