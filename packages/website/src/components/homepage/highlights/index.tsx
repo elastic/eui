c6b9b5b295616e1772506d5a49e1deab018dd191
Jason Stoltzfus <jastoltz24@gmail.com>
2025-06-06 17:43:05 -0400

    [Docs] Fixed form link on home page (#8751)

--- packages/website/src/components/homepage/highlights/index.tsx
+++ packages/website/src/components/homepage/highlights/index.tsx
@@ -57,7 +57,7 @@ const CONTENT_DATA = [
   },
   {
     title: 'Forms',
-    href: './docs/components/forms/form-controls/guidelines/',
+    href: './docs/components/forms/controls/guidelines/',
     svg: SvgForm,
     description: 'Inputs with validation, grouped into a flexible form layout',
   },
