4bd22c04bbcd350e6d0995e20fb117623bab81b4
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-06-16 13:50:39 +0200

    refactor: enable color mode separated dataVis and severity colors

--- packages/eui-theme-common/src/global_styling/variables/colors.ts
+++ packages/eui-theme-common/src/global_styling/variables/colors.ts
@@ -371,9 +371,6 @@ export type _EuiThemeConstantColors = {
 
   plainLight: string;
   plainDark: string;
-
-  vis: _EuiThemeVisColors;
-  severity: _EuiThemeSeverityColors;
 };
 
 export type _EuiThemeColorsMode = _EuiThemeBrandColors &
@@ -385,5 +382,10 @@ export type _EuiThemeColorsMode = _EuiThemeBrandColors &
   _EuiThemeTransparentBackgroundColors &
   _EuiThemeBorderColors;
 
-export type _EuiThemeColors = StrictColorModeSwitch<_EuiThemeColorsMode> &
+export type _EuiThemeColors = StrictColorModeSwitch<
+  _EuiThemeColorsMode & {
+    vis: _EuiThemeVisColors;
+    severity: _EuiThemeSeverityColors;
+  }
+> &
   _EuiThemeConstantColors;
