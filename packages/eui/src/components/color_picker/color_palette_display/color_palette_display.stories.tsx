70b3c867ee8f08b32533419b160726631901033a
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-06-16 15:04:03 +0200

    docs(storybook): update palettes examples
    - additionally add comments to use hooks instead of functions as those ensure theme colors are already updated without manual update

--- packages/eui/src/components/color_picker/color_palette_display/color_palette_display.stories.tsx
+++ packages/eui/src/components/color_picker/color_palette_display/color_palette_display.stories.tsx
@@ -9,10 +9,12 @@
 import React, { useEffect, useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { VIS_COLOR_STORE_EVENTS } from '@elastic/eui-theme-common';
+import { css } from '@emotion/react';
 
 import {
   EUI_VIS_COLOR_STORE,
   euiPaletteColorBlind,
+  EuiThemeProvider,
   useEuiPaletteColorBlind,
   useEuiPaletteColorBlindBehindText,
   useEuiPaletteComplementary,
@@ -26,6 +28,7 @@ import {
   useEuiPaletteSkyBlue,
   useEuiPaletteWarm,
   useEuiPaletteYellow,
+  useEuiTheme,
   useUpdateEffect,
 } from '../../../services';
 import { EuiSpacer } from '../../spacer';
@@ -97,53 +100,32 @@ export const PaletteWithStops: Story = {
 export const KitchenSink: Story = {
   tags: ['vrt-only'],
   render: function Render() {
-    const euiPaletteColorBlind = useEuiPaletteColorBlind();
-    const euiPaletteColorBlindBehindText = useEuiPaletteColorBlindBehindText();
-    const euiPaletteForStatus = useEuiPaletteForStatus(6);
-    const euiPaletteForTemperature = useEuiPaletteForTemperature(4);
-    const euiPaletteComplementary = useEuiPaletteComplementary(5);
-    const euiPaletteRed = useEuiPaletteRed(5);
-    const euiPaletteGreen = useEuiPaletteGreen(5);
-    const euiPaletteSkyBlue = useEuiPaletteSkyBlue(5);
-    const euiPaletteYellow = useEuiPaletteYellow(5);
-    const euiPaletteOrange = useEuiPaletteOrange(5);
-    const euiPaletteCool = useEuiPaletteCool(5);
-    const euiPaletteWarm = useEuiPaletteWarm(5);
-    const euiPaletteGray = useEuiPaletteGray(5);
-
-    const renderPalettes = (
-      props: Pick<EuiColorPaletteDisplayProps, 'type' | 'size'> = {
-        type: 'fixed',
-        size: 's',
-      }
-    ) => (
-      <>
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteColorBlind} />
-        <EuiColorPaletteDisplay
-          {...props}
-          palette={euiPaletteColorBlindBehindText}
-        />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteForStatus} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteForTemperature} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteComplementary} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteRed} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteGreen} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteSkyBlue} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteYellow} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteOrange} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteCool} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteWarm} />
-        <EuiColorPaletteDisplay {...props} palette={euiPaletteGray} />
-        <EuiColorPaletteDisplay {...props} palette={paletteWithStops} />
-      </>
-    );
+    const { colorMode } = useEuiTheme();
 
     return (
       <>
         <EuiFlexGroup direction="column" gutterSize="m">
-          {renderPalettes()}
+          <Palettes />
           <EuiSpacer />
-          {renderPalettes({ type: 'gradient', size: 'm' })}
+
+          <Palettes size="m" />
+          <EuiSpacer />
+
+          <EuiThemeProvider colorMode={colorMode === 'DARK' ? 'LIGHT' : 'DARK'}>
+            <EuiFlexGroup
+              direction="column"
+              gutterSize="m"
+              css={({ euiTheme }) => css`
+                padding-block: ${euiTheme.size.s};
+                background-color: ${euiTheme.colors.backgroundBasePlain};
+              `}
+            >
+              <Palettes size="m" />
+            </EuiFlexGroup>
+          </EuiThemeProvider>
+          <EuiSpacer />
+
+          <Palettes type="gradient" size="m" />
         </EuiFlexGroup>
       </>
     );
@@ -151,32 +133,62 @@ export const KitchenSink: Story = {
 };
 
 export const HighContrast: Story = {
+  ...KitchenSink,
   tags: ['vrt-only'],
   globals: { highContrastMode: true },
-  render: function Render() {
-    const euiPaletteForStatus = useEuiPaletteForStatus(6);
-    const euiPaletteForTemperature = useEuiPaletteForTemperature(4);
+};
 
-    return (
-      <>
-        <EuiColorPaletteDisplay
-          type="fixed"
-          palette={euiPaletteForStatus}
-          size="xs"
-        />
-        <EuiSpacer />
-        <EuiColorPaletteDisplay
-          type="gradient"
-          palette={euiPaletteForTemperature}
-          size="s"
-        />
-        <EuiSpacer />
-        <EuiColorPaletteDisplay
-          type="gradient"
-          palette={paletteWithStops}
-          size="m"
-        />
-      </>
-    );
-  },
+const Palettes = (
+  props: Pick<EuiColorPaletteDisplayProps, 'type' | 'size'> = {
+    type: 'fixed',
+    size: 's',
+  }
+) => {
+  const { euiTheme } = useEuiTheme();
+
+  const euiPaletteColorBlind = useEuiPaletteColorBlind();
+  const euiPaletteColorBlindBehindText = useEuiPaletteColorBlindBehindText();
+  const euiPaletteForStatus = useEuiPaletteForStatus(6);
+  const euiPaletteForTemperature = useEuiPaletteForTemperature(4);
+  const euiPaletteComplementary = useEuiPaletteComplementary(5);
+  const euiPaletteRed = useEuiPaletteRed(6);
+  const euiPaletteGreen = useEuiPaletteGreen(6);
+  const euiPaletteSkyBlue = useEuiPaletteSkyBlue(6);
+  const euiPaletteYellow = useEuiPaletteYellow(6);
+  const euiPaletteOrange = useEuiPaletteOrange(6);
+  const euiPaletteCool = useEuiPaletteCool(6);
+  const euiPaletteWarm = useEuiPaletteWarm(6);
+  const euiPaletteGray = useEuiPaletteGray(6);
+
+  const severityPalette = [
+    euiTheme.colors.severity.unknown,
+    euiTheme.colors.severity.neutral,
+    euiTheme.colors.severity.success,
+    euiTheme.colors.severity.warning,
+    euiTheme.colors.severity.risk,
+    euiTheme.colors.severity.danger,
+  ];
+
+  return (
+    <>
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteColorBlind} />
+      <EuiColorPaletteDisplay
+        {...props}
+        palette={euiPaletteColorBlindBehindText}
+      />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteForStatus} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteForTemperature} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteComplementary} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteRed} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteGreen} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteSkyBlue} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteYellow} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteOrange} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteCool} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteWarm} />
+      <EuiColorPaletteDisplay {...props} palette={euiPaletteGray} />
+      <EuiColorPaletteDisplay {...props} palette={paletteWithStops} />
+      <EuiColorPaletteDisplay {...props} palette={severityPalette} />
+    </>
+  );
 };
