3a298fd9bf69cbee63c3588a69ec677d3300ff6f
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-06-16 13:40:53 +0200

    docs: move dataVis text tokens to a separate section

--- packages/website/docs/getting-started/theming/tokens/colors/data_vis_colors_table.tsx
+++ packages/website/docs/getting-started/theming/tokens/colors/data_vis_colors_table.tsx
@@ -59,46 +59,6 @@ export const DataVisColorsTable = () => {
           value: euiTheme.colors.vis.euiColorVis9,
           token: 'colors.vis.euiColorVis9',
         },
-        {
-          value: euiTheme.colors.vis.euiColorVisText0,
-          token: 'colors.vis.euiColorVisText0',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText1,
-          token: 'colors.vis.euiColorVisText1',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText2,
-          token: 'colors.vis.euiColorVisText2',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText3,
-          token: 'colors.vis.euiColorVisText3',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText4,
-          token: 'colors.vis.euiColorVisText4',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText5,
-          token: 'colors.vis.euiColorVisText5',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText6,
-          token: 'colors.vis.euiColorVisText6',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText7,
-          token: 'colors.vis.euiColorVisText7',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText8,
-          token: 'colors.vis.euiColorVisText8',
-        },
-        {
-          value: euiTheme.colors.vis.euiColorVisText9,
-          token: 'colors.vis.euiColorVisText9',
-        },
       ]}
     />
   );
@@ -165,4 +125,55 @@ export const DataVisColorsBehindTextColorsTable = () => {
         />
       </>
     );
+};
+
+export const DataVisTextColorsTable = () => {
+  const { euiTheme } = useEuiTheme();
+
+  return (
+    <ColorsTable
+      colors={[
+        {
+          value: euiTheme.colors.vis.euiColorVisText0,
+          token: 'colors.vis.euiColorVisText0',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText1,
+          token: 'colors.vis.euiColorVisText1',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText2,
+          token: 'colors.vis.euiColorVisText2',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText3,
+          token: 'colors.vis.euiColorVisText3',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText4,
+          token: 'colors.vis.euiColorVisText4',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText5,
+          token: 'colors.vis.euiColorVisText5',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText6,
+          token: 'colors.vis.euiColorVisText6',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText7,
+          token: 'colors.vis.euiColorVisText7',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText8,
+          token: 'colors.vis.euiColorVisText8',
+        },
+        {
+          value: euiTheme.colors.vis.euiColorVisText9,
+          token: 'colors.vis.euiColorVisText9',
+        },
+      ]}
+    />
+  );
 };
