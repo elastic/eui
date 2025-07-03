ef4b7a17d244364aef901dc45b457e756b0b1773
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-07-03 12:52:08 +0200

    docs(storybook): add loading example

--- packages/eui/src/components/progress/progress.stories.tsx
+++ packages/eui/src/components/progress/progress.stories.tsx
@@ -6,9 +6,12 @@
  * Side Public License, v 1.
  */
 
+import React, { useEffect, useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { EuiProgress, COLORS } from './progress';
+import { EuiButton } from '../button';
+import { EuiFlexGroup, EuiFlexItem } from '../flex';
 
 const meta: Meta<typeof EuiProgress> = {
   title: 'Display/EuiProgress',
@@ -18,7 +21,15 @@ const meta: Meta<typeof EuiProgress> = {
     // for quicker/easier QA
     label: { control: 'text' },
     value: { control: 'number' },
-    valueText: { control: 'boolean' },
+    valueText: {
+      control: 'radio',
+      options: ['custom', 'true', 'false'],
+      mapping: {
+        custom: 'steps',
+        true: true,
+        false: false,
+      },
+    },
   },
   args: {
     color: 'success',
@@ -52,5 +63,87 @@ export const HighContrast: Story = {
     ...Determinate.args,
     size: 'xs',
     color: 'primary',
+  },
+  render: (args) => <EuiProgress {...args} />,
+};
+
+export const DeterminateLoading: Story = {
+  parameters: {
+    controls: {
+      include: ['label', 'value', 'valueText', 'max'],
+    },
+    codeSnippet: {
+      resolveStoryElementOnly: true,
+    },
+    loki: {
+      skip: true,
+    },
+  },
+  args: {
+    label: 'Loading',
+    value: 70,
+    max: 100,
+  },
+  render: function Render(args) {
+    const { value, valueText } = args;
+    const hasCustomValueText = valueText === 'steps';
+
+    const [loading, setLoading] = useState<number>(
+      typeof value === 'number'
+        ? value
+        : typeof value === 'string'
+        ? parseInt(value)
+        : 0
+    );
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>(
+      undefined
+    );
+
+    useEffect(() => {
+      if (loading >= 100 && intervalId !== null) {
+        clearInterval(intervalId);
+        setIntervalId(undefined);
+      }
+    }, [intervalId, loading]);
+
+    const increment = () => {
+      setLoading((prev: number) => {
+        if (prev >= 100) return 0;
+
+        return prev + 10;
+      });
+    };
+
+    const startLoading = () => {
+      if (loading === 0 && intervalId === undefined) {
+        const _intervalId = setInterval(() => increment(), 1000);
+
+        setIntervalId(_intervalId);
+      } else {
+        setLoading(0);
+        clearInterval(intervalId);
+        setIntervalId(undefined);
+      }
+    };
+
+    return (
+      <EuiFlexGroup>
+        <EuiFlexItem grow={false}>
+          <EuiButton onClick={startLoading}>
+            {loading === 0 ? 'Start' : 'Reset'}
+          </EuiButton>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          {/* casting due to ExclusiveUnion complexity */}
+          <EuiProgress
+            {...(args as typeof EuiProgress)}
+            value={loading}
+            valueText={
+              hasCustomValueText ? `${loading} ${valueText}` : valueText
+            }
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
   },
 };
