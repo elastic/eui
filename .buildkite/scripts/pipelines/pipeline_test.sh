dff5ea20e6bf560c450d72b27098a639d0ebdf18
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2025-07-15 22:48:53 +0200

    ci: remove React 16-specific CI steps

--- .buildkite/scripts/pipelines/pipeline_test.sh
+++ .buildkite/scripts/pipelines/pipeline_test.sh
@@ -38,12 +38,6 @@ case $TEST_TYPE in
     COMMAND="/opt/yarn*/bin/yarn --cwd packages/eui && yarn --cwd packages/eui build:workspaces && yarn --cwd packages/eui test-unit --node-options=--max_old_space_size=2048 --testMatch=non-react"
     ;;
 
-  unit:tsx:16)
-    echo "[TASK]: Running Jest .tsx tests against React 16"
-    DOCKER_OPTIONS+=(--env BUILDKITE_ANALYTICS_TOKEN="$(retry 5 vault read -field=jest_token_react16 "${buildkite_analytics_vault}")")
-    COMMAND="/opt/yarn*/bin/yarn --cwd packages/eui && yarn --cwd packages/eui build:workspaces && yarn --cwd packages/eui test-unit --node-options=--max_old_space_size=2048 --react-version=16 --testMatch=react"
-    ;;
-
   unit:tsx:17)
     echo "[TASK]: Running Jest .tsx tests against React 17"
     DOCKER_OPTIONS+=(--env BUILDKITE_ANALYTICS_TOKEN="$(retry 5 vault read -field=jest_token_react17 "${buildkite_analytics_vault}")")
@@ -54,12 +48,6 @@ case $TEST_TYPE in
     echo "[TASK]: Running Jest .tsx tests against React 18"
     DOCKER_OPTIONS+=(--env BUILDKITE_ANALYTICS_TOKEN="$(retry 5 vault read -field=jest_token_react18 "${buildkite_analytics_vault}")")
     COMMAND="/opt/yarn*/bin/yarn --cwd packages/eui && yarn --cwd packages/eui build:workspaces && yarn --cwd packages/eui test-unit --node-options=--max_old_space_size=2048 --testMatch=react"
-    ;;
-
-  cypress:16)
-    echo "[TASK]: Running Cypress tests against React 16"
-    DOCKER_OPTIONS+=(--env BUILDKITE_ANALYTICS_TOKEN="$(retry 5 vault read -field=cypress_token_react16 "${buildkite_analytics_vault}")")
-    COMMAND="/opt/yarn*/bin/yarn --cwd packages/eui && yarn --cwd packages/eui build:workspaces && yarn --cwd packages/eui cypress install && yarn --cwd packages/eui test-cypress --node-options=--max_old_space_size=2048 --react-version=16"
     ;;
 
   cypress:17)
