7f3725b581beb81e8b382c63f8712819d27addba
Weronika Olejniczak <weronika.olejniczak@elastic.co>
2025-07-24 18:12:23 +0200

    feat(EuiCheckableCard): toggle on children if non-interactive

--- packages/eui/src/components/card/checkable_card/checkable_card.spec.tsx
+++ packages/eui/src/components/card/checkable_card/checkable_card.spec.tsx
@@ -10,7 +10,7 @@
 /// <reference types="cypress-real-events" />
 /// <reference types="../../../../cypress/support" />
 
-import React, { FunctionComponent, useState } from 'react';
+import { FunctionComponent, useState } from 'react';
 
 import { EuiCheckableCard, type EuiCheckableCardProps } from '../index';
 
@@ -34,12 +34,40 @@ describe('EuiCheckableCard', () => {
   };
 
   describe('Click behavior', () => {
-    it('fired onChange only once when the checkbox is clicked', () => {
+    it('fires onChange only once when the checkbox is clicked', () => {
       cy.realMount(<StatefulCheckableCard />);
 
       cy.get('[data-test-subj=checkableCard]').realClick();
 
       cy.get('[data-test-subj=checkableCard]').should('be.checked');
+    });
+
+    it('fires onChange when clicking on non-interactive children', () => {
+      cy.realMount(
+        <StatefulCheckableCard>
+          <div data-test-subj="non-interactive-content">
+            Non-interactive text content
+          </div>
+        </StatefulCheckableCard>
+      );
+
+      cy.get('[data-test-subj=non-interactive-content]').realClick();
+
+      cy.get('[data-test-subj=checkableCard]').should('be.checked');
+    });
+
+    it('does not fire onChange when clicking on interactive children', () => {
+      cy.realMount(
+        <StatefulCheckableCard>
+          <button data-test-subj="interactive-button">
+            Interactive button
+          </button>
+        </StatefulCheckableCard>
+      );
+
+      cy.get('[data-test-subj=interactive-button]').realClick();
+
+      cy.get('[data-test-subj=checkableCard]').should('not.be.checked');
     });
   });
 });
