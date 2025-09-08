16afe837154d2f9f75894469c30d17c1815a41e3
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2025-09-08 20:10:38 +0200

    WIP

--- packages/eui/src/components/flyout/flyout.styles.ts
+++ packages/eui/src/components/flyout/flyout.styles.ts
@@ -39,11 +39,11 @@ export const euiFlyoutSlideInRight = keyframes`
 `;
 
 export const euiFlyoutSlideOutRight = keyframes`
-  from {
+  0% {
     opacity: 1;
     transform: translateX(0%);
   }
-  to {
+  75% {
     opacity: 0;
     transform: translateX(100%);
   }
