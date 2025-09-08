6ffa129d4e6a7a6678962b58c0b7c7b05c0ad789
Weronika Olejniczak <weronika.olejniczak@elastic.co>
2025-09-08 17:49:51 +0200

    feat(docusaurus-theme): make Footer use config

--- packages/docusaurus-theme/src/theme/Footer/index.tsx
+++ packages/docusaurus-theme/src/theme/Footer/index.tsx
@@ -9,16 +9,11 @@
 import { css } from '@emotion/react';
 import {
   EuiText,
-  EuiLink,
   UseEuiTheme,
   EuiThemeProvider,
   useEuiMemoizedStyles,
 } from '@elastic/eui';
-
-const ELASTIC_LICENSE_URL =
-  'https://github.com/elastic/eui/blob/main/licenses/ELASTIC-LICENSE-2.0.md';
-const SSPL_LICENSE_URL =
-  'https://github.com/elastic/eui/blob/main/licenses/SSPL-LICENSE.md';
+import { useThemeConfig } from '@docusaurus/theme-common';
 
 const getFooterStyles = ({ euiTheme }: UseEuiTheme) => {
   return {
@@ -39,22 +34,12 @@ const getFooterStyles = ({ euiTheme }: UseEuiTheme) => {
 
 const _Footer = () => {
   const styles = useEuiMemoizedStyles(getFooterStyles);
+  const { footer } = useThemeConfig();
 
   return (
     <footer css={styles.root}>
       <EuiText textAlign="center" size="s" css={styles.text}>
-        EUI is dual-licensed under{' '}
-        <EuiLink href={ELASTIC_LICENSE_URL}>Elastic License 2.0</EuiLink>
-        {' and '}
-        <EuiLink href={SSPL_LICENSE_URL}>
-          Server Side Public License, v 1
-        </EuiLink>
-        {' | '}
-        Crafted with{' '}
-        <span role="img" aria-label="love" css={styles.heart}>
-          ❤
-        </span>{' '}
-        by <EuiLink href="https://elastic.co">Elastic</EuiLink>
+        {footer?.copyright}
       </EuiText>
     </footer>
   );
