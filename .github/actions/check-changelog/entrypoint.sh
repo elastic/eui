2bc5d3c697fdcf85dddfaf6df3040a71e3e2d568
Nathan L Smith <nathan.smith@elastic.co>
2025-09-25 21:22:07 -0500

    undo change to entrypoint

--- .github/actions/check-changelog/entrypoint.sh
+++ .github/actions/check-changelog/entrypoint.sh
@@ -12,7 +12,7 @@ changelog_files=$(find packages/*/changelogs/upcoming/ -type f -name "${PR_NUMBE
 # If no changelog file is found, fail the step with an error message
 if [ -z "$changelog_files" ]; then
   echo "❌ Changelog file for PR #${PR_NUMBER} is missing."
-  echo "You need to add a changelog to this PR before it can be merged. See https://github.com/elastic/eui/blob/main/wiki/contributing-to-eui/documenting/changelogs.md"
+  echo "You need to add a changelog to this PR before it can be merged. See https://github.com/elastic/eui/blob/main/wiki/contributing-to-eui/documenting/changelogs.md."
   exit 1
 else
   echo "✅ Changelog file for PR #${PR_NUMBER} found: $changelog_files"
