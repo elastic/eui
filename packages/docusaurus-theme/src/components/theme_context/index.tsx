fd6ff58b7e9bdfc238738dba19f2fb830e655793
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-10-09 10:35:41 +0200

    refactor: update direct imports from removed themes folder

--- packages/docusaurus-theme/src/components/theme_context/index.tsx
+++ packages/docusaurus-theme/src/components/theme_context/index.tsx
@@ -18,12 +18,12 @@ import useIsBrowser from '@docusaurus/useIsBrowser';
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import {
-  EUI_THEME,
   EuiProvider,
   EuiThemeAmsterdam,
   EuiThemeColorMode,
 } from '@elastic/eui';
 import { EuiThemeBorealis } from '@elastic/eui-theme-borealis';
+import { type EUI_THEME } from '@elastic/eui-theme-common';
 
 import { EuiThemeOverrides } from './theme_overrides';
 
