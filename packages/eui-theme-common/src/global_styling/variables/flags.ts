affc3ff20158d4108c3dd747e992570ea3cef94e
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-10-09 13:44:21 +0200

    refactor: remove hasGlobalFocusColor theme flag

--- packages/eui-theme-common/src/global_styling/variables/flags.ts
+++ packages/eui-theme-common/src/global_styling/variables/flags.ts
@@ -16,6 +16,5 @@ export type EuiThemeVariantFlags = {
  * Theme specific setting flags
  */
 export type _EuiThemeFlags = {
-  hasGlobalFocusColor: boolean;
   hasVisColorAdjustment: boolean;
 } & EuiThemeVariantFlags;
