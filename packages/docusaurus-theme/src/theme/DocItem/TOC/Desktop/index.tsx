617dea4d028a12a676f428cddd6d91491901732d
Weronika Olejniczak <32842468+weronikaolejniczak@users.noreply.github.com>
2025-10-15 18:59:18 +0200

    Docusaurus theme updates for release (#8814)

--- packages/docusaurus-theme/src/theme/DocItem/TOC/Desktop/index.tsx
+++ packages/docusaurus-theme/src/theme/DocItem/TOC/Desktop/index.tsx
@@ -1,7 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
 import { JSX } from 'react';
 import { ThemeClassNames } from '@docusaurus/theme-common';
 import { useDoc } from '@docusaurus/plugin-content-docs/client';
-
 import TOC from '@theme-original/TOC';
 
 export default function DocItemTOCDesktop(): JSX.Element {
