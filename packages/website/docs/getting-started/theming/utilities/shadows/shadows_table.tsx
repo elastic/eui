96e6a8cc02ab1ec822b1a20f7ab5d3f45a85fd13
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-10-15 14:31:53 +0200

    docs: update shadow utils docs
    - include mention of underlying shadow tokens
    
    - ensure style output has a more readable formatting and output

--- packages/website/docs/getting-started/theming/utilities/shadows/shadows_table.tsx
+++ packages/website/docs/getting-started/theming/utilities/shadows/shadows_table.tsx
@@ -5,9 +5,12 @@ import {
   _EuiThemeShadowSize,
   euiShadow,
   euiShadowFlat,
+  EuiCodeBlock,
 } from '@elastic/eui';
 import { type EuiShadowOptions } from '@elastic/eui-theme-common';
+
 import { ThemeValuesTable } from '../../theme_values_table';
+import { ReactNode } from 'react';
 
 const shadowTypes: _EuiThemeShadowSize[] = ['xs', 's', 'm', 'l', 'xl'];
 
@@ -22,28 +25,37 @@ export const ShadowsTable = ({
   const items = shadowTypes.map((type) => ({
     id: type,
     token: `euiShadow(euiThemeContext, '${type}', { direction: '${direction}' })`,
-    value: euiShadow(euiThemeContext, type, {
-      direction,
-    })
-      .replaceAll(';;', ';')
-      .replaceAll(' ;', ''),
+    value: getFormattedCSS(
+      euiShadow(euiThemeContext, type, {
+        direction,
+      })
+    ),
   }));
 
   // deprecated, remove once obsolete
   items.push({
     id: 'flat',
     token: `euiShadowFlat(euiThemeContext, { direction: '${direction}' }))`,
-    value: euiShadowFlat(euiThemeContext, {
-      direction,
-    })
-      .replaceAll(';;', ';')
-      .replaceAll(' ;', ''),
+    value: getFormattedCSS(
+      euiShadowFlat(euiThemeContext, {
+        direction,
+      })
+    ),
   });
 
   return (
     <ThemeValuesTable
       items={items}
       tokenColumnProps={{ name: 'Utility' }}
+      valueColumnProps={{
+        name: 'Style output',
+        align: 'left',
+        render: (value: ReactNode) => (
+          <small>
+            <EuiCodeBlock language="css">{value}</EuiCodeBlock>
+          </small>
+        ),
+      }}
       render={(item) => (
         <EuiColorPickerSwatch
           showToolTip={false}
@@ -59,5 +71,36 @@ export const ShadowsTable = ({
         />
       )}
     />
+  );
+};
+
+/* CSS formatting utility */
+const getFormattedCSS = (cssString: string) => {
+  return (
+    cssString
+      // Remove comments
+      .replace(/\/\*[\s\S]*?\*\//g, '')
+      // Clean up semicolons
+      .replaceAll(';;', ';')
+      .replaceAll(' ;', '')
+      // Normalize all whitespace to single spaces
+      .replace(/\s+/g, ' ')
+      // Add line breaks after semicolons (for top-level properties)
+      .replace(/;\s*(?![^{]*})/g, ';\n')
+      // Add line breaks before selectors like &::after
+      .replace(/\s*(&[^{]*)\s*{/g, '\n\n$1 {')
+      // Format inside braces: add line breaks and indent
+      .replace(/{([^}]*)}/g, (match, content) => {
+        const formatted = content
+          .split(';')
+          .map((prop: string) => prop.trim())
+          .filter((prop: string) => prop.length > 0)
+          .map((prop: string) => `  ${prop};`)
+          .join('\n');
+        return `{\n${formatted}\n}`;
+      })
+      // Clean up any leading newlines
+      .replace(/^\n+/, '')
+      .trim()
   );
 };
