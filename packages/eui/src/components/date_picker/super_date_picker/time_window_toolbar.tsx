c83ed61081c6085c1fb8d6267728b9ed94285e63
Arturo Castillo Delgado <arturo.castillo@elastic.co>
2025-10-27 17:30:32 +0100

    Implement time window logic
    - step forward and backward
    - zoom out

--- packages/eui/src/components/date_picker/super_date_picker/time_window_toolbar.tsx
+++ packages/eui/src/components/date_picker/super_date_picker/time_window_toolbar.tsx
@@ -7,14 +7,15 @@
  */
 
 import React from 'react';
-// import dateMath from '@elastic/datemath';
-// import moment from 'moment';
+import dateMath from '@elastic/datemath';
+import moment, { Moment } from 'moment';
 
 import { ShortDate, ApplyTime } from '../types';
-import { useEuiMemoizedStyles, useGeneratedHtmlId } from '../../../services';
+import { usePrettyInterval } from './pretty_interval';
 
 import { EuiButtonGroupButton } from '../../button/button_group/button_group_button';
 import { euiButtonGroupButtonsStyles } from '../../button/button_group/button_group.styles';
+import { useEuiMemoizedStyles, useGeneratedHtmlId } from '../../../services';
 
 interface TimeWindowToolbarProps {
   applyTime: ApplyTime;
@@ -23,6 +24,10 @@ interface TimeWindowToolbarProps {
   compressed?: boolean;
   isDisabled?: boolean;
 }
+
+// How much time is added to the interval (or window)
+// e.g. 60 minutes * 0.3 -> 18 minutes will be added (9 on each end)
+const ZOOM_FACTOR = 0.3;
 
 /**
  * Toolbar for managing the time window with controls for moving the time window
@@ -38,21 +43,26 @@ interface TimeWindowToolbarProps {
  * - [ ] check if hiding tooltips when isDisabled is the right thing to do
  */
 export const TimeWindowToolbar: React.FC<TimeWindowToolbarProps> = ({
-  // applyTime,
-  // start,
-  // end,
+  applyTime,
+  start,
+  end,
   compressed,
   isDisabled,
 }) => {
   const buttonColor = 'text';
   const buttonSize = compressed ? 'compressed' : 'm';
   const styles = useEuiMemoizedStyles(euiButtonGroupButtonsStyles);
-  const cssStyles = [styles.euiButtonGroup__buttons, styles[buttonSize]];
+
+  const { prettyInterval, stepForward, stepBackward, zoomOut } = useTimeWindow(
+    start,
+    end,
+    applyTime
+  );
 
   // Previous
   const previousId = useGeneratedHtmlId({ prefix: 'previous' });
   const previousLabel = 'Previous'; // TODO translate
-  const previousTooltipContent = 'Previous ...'; // TODO translate
+  const previousTooltipContent = `Previous ~${prettyInterval}`; // TODO translate
 
   // Zoom out
   const zoomOutId = useGeneratedHtmlId({ prefix: 'zoom_out' });
@@ -61,15 +71,17 @@ export const TimeWindowToolbar: React.FC<TimeWindowToolbarProps> = ({
   // Next
   const nextId = useGeneratedHtmlId({ prefix: 'next' });
   const nextLabel = 'Next'; // TODO translate
-  const nextTooltipContent = 'Next ...'; // TODO translate
-
-  const handleClickForNow = () => {};
+  const nextTooltipContent = `Next ~${prettyInterval}`; // TODO translate
 
   return (
-    <div role="toolbar" css={cssStyles} className="euiButtonGroup__buttons">
+    <div
+      role="toolbar"
+      className="euiButtonGroup__buttons"
+      css={[styles.euiButtonGroup__buttons, styles[buttonSize]]}
+    >
       <EuiButtonGroupButton
         color={buttonColor}
-        onClick={handleClickForNow}
+        onClick={stepBackward}
         id={previousId}
         label={previousLabel}
         toolTipContent={!isDisabled && previousTooltipContent}
@@ -81,7 +93,7 @@ export const TimeWindowToolbar: React.FC<TimeWindowToolbarProps> = ({
       />
       <EuiButtonGroupButton
         color={buttonColor}
-        onClick={handleClickForNow}
+        onClick={zoomOut}
         id={zoomOutId}
         label={zoomOutLabel}
         toolTipContent={!isDisabled && zoomOutLabel}
@@ -93,7 +105,7 @@ export const TimeWindowToolbar: React.FC<TimeWindowToolbarProps> = ({
       />
       <EuiButtonGroupButton
         color={buttonColor}
-        onClick={handleClickForNow}
+        onClick={stepForward}
         id={nextId}
         label={nextLabel}
         toolTipContent={!isDisabled && nextTooltipContent}
@@ -106,3 +118,57 @@ export const TimeWindowToolbar: React.FC<TimeWindowToolbarProps> = ({
     </div>
   );
 };
+
+/**
+ *
+ * Partly adapted from date_picker/super_date_picker/quick_select_popover/quick_select.tsx
+ *
+ * @todo check variable names, most of them are terrible
+ */
+export function useTimeWindow(
+  start: ShortDate,
+  end: ShortDate,
+  apply: ApplyTime
+) {
+  const min = dateMath.parse(start) as Moment;
+  const max = dateMath.parse(end, { roundUp: true }) as Moment;
+  const diff = max.diff(min);
+  // terrible name, I meant what's added on the edges
+  // e.g. 25% substracted to the start, 25% added to the end
+  const edgeDiff = diff * (ZOOM_FACTOR / 2);
+
+  const prettyInterval = usePrettyInterval(false, diff, {
+    shortHand: true,
+  });
+  // const prettyZoomInterval = usePrettyInterval(false, diff + edgeDiff * 2, {
+  //   shortHand: true,
+  // });
+
+  return {
+    prettyInterval,
+    stepForward,
+    stepBackward,
+    zoomOut,
+  };
+
+  function stepForward() {
+    apply({
+      start: moment(max).toISOString(),
+      end: moment(max).add(diff, 'ms').toISOString(),
+    });
+  }
+
+  function stepBackward() {
+    apply({
+      start: moment(min).subtract(diff, 'ms').toISOString(),
+      end: moment(min).toISOString(),
+    });
+  }
+
+  function zoomOut() {
+    apply({
+      start: moment(min).subtract(edgeDiff, 'ms').toISOString(),
+      end: moment(max).add(edgeDiff, 'ms').toISOString(),
+    });
+  }
+}
