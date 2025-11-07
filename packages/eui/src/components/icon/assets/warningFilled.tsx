ab91be9349385a4f752d0c78e55c075194b3d575
Michael Marcialis <michael.l.marcialis@gmail.com>
2025-11-07 16:49:16 -0500

    updated ellipsis and added filled status variants

--- packages/eui/src/components/icon/assets/warningFilled.tsx
+++ packages/eui/src/components/icon/assets/warningFilled.tsx
@@ -28,10 +28,7 @@ const EuiIconWarningFilled = ({
     {...props}
   >
     {title ? <title id={titleId}>{title}</title> : null}
-    <path
-      fillRule="evenodd"
-      d="M8 1a1 1 0 0 1 .864.496l7 12A1 1 0 0 1 15 15H1a1 1 0 0 1-.864-1.504l7-12A1 1 0 0 1 8 1Zm.9 4.995-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995a.905.905 0 1 1 1.8 0ZM8 13a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z"
-    />
+    <path d="M8 1a1 1 0 0 1 .863.496l7 12A1 1 0 0 1 15 15H1a1 1 0 0 1-.864-1.504l7-12A1 1 0 0 1 8 1Zm0 10a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm-.5-6v5h1V5h-1Z" />
   </svg>
 );
 export const icon = EuiIconWarningFilled;
