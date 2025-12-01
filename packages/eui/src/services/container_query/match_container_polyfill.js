4e5e43bb7a461a64850b808520808aab052ebda0
Arturo Castillo Delgado <arturo.castillo@elastic.co>
2025-12-01 18:20:01 +0100

    Lint (missing license header)

--- packages/eui/src/services/container_query/match_container_polyfill.js
+++ packages/eui/src/services/container_query/match_container_polyfill.js
@@ -1,18 +1,26 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
 /**
  * @license MIT License
- * 
+ *
  * Copyright (c) 2025 Martin Winkler
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +28,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- * 
+ *
  * {@see https://github.com/teetotum/match-container/blob/main/matchContainer.js}
  */
 
@@ -36,7 +44,7 @@
   // Even if two IDs are generated within the same milisecond the increased counter ensures
   // that they cannot clash.
 
-  const polyfill_key = "bae45330cd3d4e0e96b60d26b57009b5";
+  const polyfill_key = 'bae45330cd3d4e0e96b60d26b57009b5';
   const polyfill = Symbol.for(polyfill_key);
   window[polyfill] =
     window[polyfill] ??
@@ -63,9 +71,9 @@
     constructor(element, containerQueryString) {
       super();
       this.container = containerQueryString;
-      const unique_name = "container-query-observer-" + createID();
+      const unique_name = `container-query-observer-${createID()}`;
       const markerAttribute = `data-${unique_name}`;
-      element.setAttribute(markerAttribute, "");
+      element.setAttribute(markerAttribute, '');
       const sentinelProperty = `--${unique_name}`;
       const containerQuerySheet = new CSSStyleSheet();
       const css = `
@@ -82,7 +90,7 @@
         containerQuerySheet,
       ];
       const style = getComputedStyle(element);
-      this.matches = style.getPropertyValue(sentinelProperty) === "--true";
+      this.matches = style.getPropertyValue(sentinelProperty) === '--true';
 
       this._startObserving(sentinelProperty, containerQueryString, element);
     }
@@ -90,10 +98,10 @@
     _startObserving(sentinelProperty, containerQueryString, observedElement) {
       const _callback = (values) => {
         if (sentinelProperty in values) {
-          const matches = values[sentinelProperty] === "--true";
+          const matches = values[sentinelProperty] === '--true';
           this.matches = matches;
           // raise 'change' event
-          const event = new ContainerQueryListEvent("change");
+          const event = new ContainerQueryListEvent('change');
           event.matches = matches;
           event.container = containerQueryString;
           this.dispatchEvent(event);
@@ -103,12 +111,12 @@
       const _previousValues = {};
 
       observedElement.style.setProperty(
-        "transition",
+        'transition',
         `${sentinelProperty} 0.001ms step-start`
       );
       observedElement.style.setProperty(
-        "transition-behavior",
-        "allow-discrete"
+        'transition-behavior',
+        'allow-discrete'
       );
       const onTransitionRun = (e) => {
         const targetElement = e.target;
@@ -127,7 +135,7 @@
           }
         }
       };
-      observedElement.addEventListener("transitionrun", onTransitionRun);
+      observedElement.addEventListener('transitionrun', onTransitionRun);
 
       // init _previousValues
       const computedStyle = getComputedStyle(observedElement);
