f0e6ce03434b7f10f06de5a1f75af41cd70791e6
Arturo Castillo Delgado <arturo.castillo@elastic.co>
2026-02-03 14:58:05 +0100

    Test util

--- packages/eui/src/components/date_range_picker/utils.test.ts
+++ packages/eui/src/components/date_range_picker/utils.test.ts
@@ -6,6 +6,61 @@
  * Side Public License, v 1.
  */
 
+import {
+  DATE_TYPE_ABSOLUTE,
+  DATE_TYPE_NOW,
+  DATE_TYPE_RELATIVE,
+  isValidTimeRange,
+  type TimeRange,
+} from './utils';
+
 describe('isValidTimeRange', () => {
-  it.todo('should return true if the time range is valid');
+  const baseRange = (): TimeRange => ({
+    value: 'test',
+    start: 'now-1d',
+    end: 'now',
+    startDate: new Date(0),
+    endDate: new Date(1000),
+    type: [DATE_TYPE_RELATIVE, DATE_TYPE_NOW],
+    isNaturalLanguage: false,
+    isValid: false,
+  });
+
+  it('returns false when a date is missing', () => {
+    expect(
+      isValidTimeRange({ ...baseRange(), startDate: null })
+    ).toBeFalsy();
+    expect(isValidTimeRange({ ...baseRange(), endDate: null })).toBeFalsy();
+  });
+
+  it('returns false when both types are NOW', () => {
+    const range = {
+      ...baseRange(),
+      type: [DATE_TYPE_NOW, DATE_TYPE_NOW] as TimeRange['type'],
+    };
+
+    expect(isValidTimeRange(range)).toBeFalsy();
+  });
+
+  it('returns false when start is after end', () => {
+    const range = {
+      ...baseRange(),
+      startDate: new Date(2000),
+      endDate: new Date(1000),
+    };
+
+    expect(isValidTimeRange(range)).toBeFalsy();
+  });
+
+  it('returns true when start is before or equal to end', () => {
+    expect(isValidTimeRange(baseRange())).toBeTruthy();
+    expect(
+      isValidTimeRange({
+        ...baseRange(),
+        startDate: new Date(500),
+        endDate: new Date(500),
+        type: [DATE_TYPE_ABSOLUTE, DATE_TYPE_ABSOLUTE],
+      })
+    ).toBeTruthy();
+  });
 });
