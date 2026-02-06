339b6fcd4da11fff9c92ca50074cbafdf0d252c7
Ragini Pandey <pandeyragini55@gmail.com>
2026-02-07 00:09:46 +0530

    [VersionSwitcher] Add support for fetching versions from a URL to ensure all deployed versions are visible

--- packages/website/src/theme/Navbar/index.tsx
+++ packages/website/src/theme/Navbar/index.tsx
@@ -24,6 +24,7 @@ export default function Navbar(): ReactNode {
     }),
     previousVersionUrl: 'https://eui.elastic.co',
     versions,
+    versionsUrl: 'https://eui.elastic.co/versions.json',
   };
 
   return (
