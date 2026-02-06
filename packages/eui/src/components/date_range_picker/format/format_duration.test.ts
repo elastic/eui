a6686f2f59e75037aa3b064659bfb7a3d3364447
Arturo Castillo Delgado <arturo.castillo@elastic.co>
2026-02-06 07:50:11 +0100

    Refactor durationToDisplayShortText
    with better rounding and more exact approximation logic, probably still WIP

--- packages/eui/src/components/date_range_picker/format/format_duration.test.ts
+++ packages/eui/src/components/date_range_picker/format/format_duration.test.ts
@@ -8,50 +8,46 @@
 
 import { durationToDisplayShortText, MS_PER } from './format_duration';
 
+const seconds = (num: number) => new Date(num * MS_PER.second);
+const minutes = (num: number) => new Date(num * MS_PER.minute);
+const hours = (num: number) => new Date(num * MS_PER.hour);
+const days = (num: number) => new Date(num * MS_PER.day);
+const weeks = (num: number) => new Date(num * MS_PER.week);
+const months = (num: number) => new Date(num * MS_PER.month);
+const years = (num: number) => new Date(num * MS_PER.year);
+
 describe('durationToDisplayShortText', () => {
   it('formats basic durations', () => {
     const start = new Date(0);
 
-    expect(durationToDisplayShortText(start, new Date(100))).toBe('100ms');
-    expect(durationToDisplayShortText(start, new Date(5 * MS_PER.second))).toBe(
-      '5s'
-    );
-    expect(
-      durationToDisplayShortText(start, new Date(15 * MS_PER.minute))
-    ).toBe('15min');
-    expect(durationToDisplayShortText(start, new Date(12 * MS_PER.hour))).toBe(
-      '12h'
-    );
-    expect(durationToDisplayShortText(start, new Date(48 * MS_PER.hour))).toBe(
-      '2d'
-    );
-    expect(durationToDisplayShortText(start, new Date(3 * MS_PER.day))).toBe(
-      '3d'
-    );
-    expect(durationToDisplayShortText(start, new Date(2 * MS_PER.week))).toBe(
-      '2w'
-    );
-    expect(durationToDisplayShortText(start, new Date(4 * MS_PER.month))).toBe(
-      '4mos'
-    );
-    expect(durationToDisplayShortText(start, new Date(1 * MS_PER.year))).toBe(
-      '1y'
-    );
+    expect(durationToDisplayShortText(start, new Date(500))).toBe('500ms');
+    expect(durationToDisplayShortText(start, seconds(5))).toBe('5s');
+    expect(durationToDisplayShortText(start, minutes(15))).toBe('15min');
+    expect(durationToDisplayShortText(start, hours(12))).toBe('12h');
+    expect(durationToDisplayShortText(start, hours(48))).toBe('2d');
+    expect(durationToDisplayShortText(start, days(3))).toBe('3d');
+    expect(durationToDisplayShortText(start, weeks(2))).toBe('2w');
+    expect(durationToDisplayShortText(start, months(4))).toBe('4mos');
+    expect(durationToDisplayShortText(start, years(1))).toBe('1y');
+    expect(durationToDisplayShortText(start, years(1.5))).toBe('1.5y');
   });
 
-  it('adds approximation tilde (when -> TBD)', () => {
+  it('adds approximation tilde according to deviation thresholds', () => {
     const start = new Date(0);
 
-    expect(
-      durationToDisplayShortText(start, new Date(60 * MS_PER.second))
-    ).toBe('1min');
-    expect(
-      durationToDisplayShortText(
-        start,
-        new Date(60 * MS_PER.second + 1 * MS_PER.second)
-      )
-    ).toBe('~1min');
-
-    // TODO once approximation logic is final
+    expect(durationToDisplayShortText(start, seconds(59))).toBe('59s');
+    expect(durationToDisplayShortText(start, new Date(59900))).toBe('59s');
+    expect(durationToDisplayShortText(start, minutes(1))).toBe('1min');
+    expect(durationToDisplayShortText(start, minutes(59))).toBe('59min');
+    expect(durationToDisplayShortText(start, minutes(61))).toBe('~1h');
+    expect(durationToDisplayShortText(start, minutes(119))).toBe('~2h');
+    expect(durationToDisplayShortText(start, minutes(150))).toBe('~3h');
+    expect(durationToDisplayShortText(start, hours(23))).toBe('23h');
+    expect(durationToDisplayShortText(start, hours(25))).toBe('~1d');
+    expect(durationToDisplayShortText(start, days(6))).toBe('6d');
+    expect(durationToDisplayShortText(start, days(7))).toBe('1w');
+    expect(durationToDisplayShortText(start, days(25))).toBe('~4w');
+    expect(durationToDisplayShortText(start, days(105))).toBe('~3mos');
+    expect(durationToDisplayShortText(start, days(360))).toBe('~1y');
   });
 });
