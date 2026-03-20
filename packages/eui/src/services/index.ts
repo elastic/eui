c3fe82afb24315d4f8249de4f3c6edbecd96fba5
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2026-03-20 11:35:08 +0100

    [EuiDataGrid] Fix scrolling on focus return (#9453)
    Co-authored-by: Weronika Olejniczak <weronika.olejniczak@elastic.co>

--- packages/eui/src/services/index.ts
+++ packages/eui/src/services/index.ts
@@ -85,7 +85,19 @@ export {
   formatNumber,
   formatText,
 } from './format';
-export * from './hooks';
+export {
+  useDependentState,
+  useCombinedRefs,
+  setMultipleRefs,
+  useForceRender,
+  useLatest,
+  useDeepEqual,
+  isMouseEvent,
+  useMouseMove,
+  useUpdateEffect,
+  useEuiDisabledElement,
+  type EuiDisabledProps,
+} from './hooks';
 export { isEvenlyDivisibleBy, isWithinRange } from './number';
 export { Pager } from './paging';
 export { calculatePopoverPosition, findPopoverPosition } from './popover';
