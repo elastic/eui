5ba550b22eddf05dd120e7aab565044db567020b
Weronika Olejniczak <weronika.olejniczak@elastic.co>
2026-04-27 17:16:41 +0200

    chore: revert temp stories

--- packages/eui/src/components/tool_tip/tool_tip.stories.tsx
+++ packages/eui/src/components/tool_tip/tool_tip.stories.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import React, { useState } from 'react';
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { enableFunctionToggleControls } from '../../../.storybook/utils';
@@ -14,16 +14,6 @@ import { LOKI_SELECTORS, lokiPlayDecorator } from '../../../.storybook/loki';
 import { sleep } from '../../test';
 import { EuiFlexGroup } from '../flex';
 import { EuiButton } from '../button';
-import { EuiFlyout, EuiFlyoutHeader, EuiFlyoutBody } from '../flyout';
-import {
-  EuiModal,
-  EuiModalHeader,
-  EuiModalHeaderTitle,
-  EuiModalBody,
-} from '../modal';
-import { EuiPopover } from '../popover';
-import { EuiText } from '../text';
-import { EuiTitle } from '../title';
 import {
   EuiToolTip,
   EuiToolTipProps,
@@ -80,117 +70,6 @@ export const Playground: Story = {
     // Reduce VRT flakiness/screenshots before tooltip is fully visible
     await sleep(300);
   }),
-};
-
-/**
- * TODO: REMOVE BEFORE MERGING
- *
- * `Escape` key propagation stories
- *
- * QA: hover or focus the tooltip trigger, press `Escape` and the tooltip should close
- * but the overlay should stay open. Pressing `Escape` again (no tooltip) closes the overlay.
- */
-
-const InsideFlyoutStory = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  return (
-    <>
-      <EuiButton onClick={() => setIsOpen(true)}>Open flyout</EuiButton>
-      {isOpen && (
-        <EuiFlyout onClose={() => setIsOpen(false)} size="s">
-          <EuiFlyoutHeader hasBorder>
-            <EuiTitle size="m">
-              <h2>Flyout</h2>
-            </EuiTitle>
-          </EuiFlyoutHeader>
-          <EuiFlyoutBody>
-            <EuiText size="s">
-              <p>
-                Hover or focus the button to show the tooltip, then press{' '}
-                <kbd>Escape</kbd>. The tooltip should close but the flyout
-                should stay open.
-              </p>
-            </EuiText>
-            <br />
-            <EuiToolTip content="Tooltip content">
-              <EuiButton>Tooltip trigger</EuiButton>
-            </EuiToolTip>
-          </EuiFlyoutBody>
-        </EuiFlyout>
-      )}
-    </>
-  );
-};
-
-export const InsideFlyout: Story = {
-  render: () => <InsideFlyoutStory />,
-};
-
-const InsideModalStory = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  return (
-    <>
-      <EuiButton onClick={() => setIsOpen(true)}>Open modal</EuiButton>
-      {isOpen && (
-        <EuiModal onClose={() => setIsOpen(false)}>
-          <EuiModalHeader>
-            <EuiModalHeaderTitle>Modal</EuiModalHeaderTitle>
-          </EuiModalHeader>
-          <EuiModalBody>
-            <EuiText size="s">
-              <p>
-                Hover or focus the button to show the tooltip, then press{' '}
-                <kbd>Escape</kbd>. The tooltip should close but the modal should
-                stay open.
-              </p>
-            </EuiText>
-            <br />
-            <EuiToolTip content="Tooltip content">
-              <EuiButton>Tooltip trigger</EuiButton>
-            </EuiToolTip>
-          </EuiModalBody>
-        </EuiModal>
-      )}
-    </>
-  );
-};
-
-export const InsideModal: Story = {
-  render: () => <InsideModalStory />,
-};
-
-const InsidePopoverStory = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  return (
-    <EuiPopover
-      isOpen={isOpen}
-      closePopover={() => setIsOpen(false)}
-      button={
-        <EuiButton onClick={() => setIsOpen((open) => !open)}>
-          Toggle popover
-        </EuiButton>
-      }
-    >
-      <EuiText size="s">
-        <p>
-          Hover or focus the button to show the tooltip, then press{' '}
-          <kbd>Escape</kbd>. The tooltip should close but the popover should
-          stay open.
-        </p>
-      </EuiText>
-      <br />
-      <EuiToolTip content="Tooltip content">
-        <EuiButton>Tooltip trigger</EuiButton>
-      </EuiToolTip>
-    </EuiPopover>
-  );
-};
-
-export const InsidePopover: Story = {
-  render: () => <InsidePopoverStory />,
 };
 
 /**
