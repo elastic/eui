1b70699c3df033d1082f5705bbac3769946d575a
dmc0125 <dominikmichal0124@gmail.com>
2026-05-19 12:59:21 +0200

    [EuiOutsideClickDetector] Migrate from class to function component

--- packages/eui/src/components/outside_click_detector/outside_click_detector.tsx
+++ packages/eui/src/components/outside_click_detector/outside_click_detector.tsx
@@ -9,12 +9,12 @@
 import {
   Children,
   cloneElement,
-  Component,
-  EventHandler,
   MouseEvent as ReactMouseEvent,
   ReactElement,
+  useRef,
+  useEffect,
 } from 'react';
-import { htmlIdGenerator } from '../../services/accessibility';
+import { useGeneratedHtmlId } from '../../services/accessibility';
 
 export interface EuiEvent extends Event {
   euiGeneratedBy: string[];
@@ -33,120 +33,98 @@ export interface EuiOutsideClickDetectorProps {
   onTouchEnd?: (event: ReactMouseEvent) => void;
 }
 
-export class EuiOutsideClickDetector extends Component<EuiOutsideClickDetectorProps> {
-  // We are working with the assumption that a click event is
-  // equivalent to a sequential, compound press and release of
-  // the pointing device (mouse, finger, stylus, etc.).
-  // A click event's target can be imprecise, as the value will be
-  // the closest common ancestor of the press (mousedown, touchstart)
-  // and release (mouseup, touchend) events (often <body />) if
-  // the the target of each event differs.
-  // We need the actual event targets to make the correct decisions
-  // about user intention. So, consider the down/start and up/end
-  // items below as the deconstruction of a click event.
+export const EuiOutsideClickDetector = (
+  props: EuiOutsideClickDetectorProps
+) => {
+  // the id is used to identify which EuiOutsideClickDetector
+  // is the source of a click event; as the click event bubbles
+  // up and reaches the click detector's child component the
+  // id value is stamped on the event. This id is inspected
+  // in the document's click handler, and if the id doesn't
+  // exist or doesn't match this detector's id, then trigger
+  // the outsideClick callback.
+  //
+  // Taking this approach instead of checking if the event's
+  // target element exists in this component's DOM sub-tree is
+  // necessary for handling clicks originating from children
+  // rendered through React's portals (EuiPortal). The id tracking
+  // works because React guarantees the event bubbles through the
+  // virtual DOM and executes EuiClickDetector's onClick handler,
+  // stamping the id even though the event originates outside
+  // this component's reified DOM tree.
+  const id = useGeneratedHtmlId();
+  const capturedDownIds = useRef<string[]>([]);
 
-  private id: string;
+  useEffect(() => {
+    function onClickOutside(e: Event) {
+      const { isDisabled, onOutsideClick } = props;
 
-  private capturedDownIds: string[];
+      if (isDisabled) {
+        capturedDownIds.current = [];
+        return;
+      }
 
-  constructor(props: EuiOutsideClickDetectorProps) {
-    super(props);
+      const event = e as unknown as EuiEvent;
 
-    // the id is used to identify which EuiOutsideClickDetector
-    // is the source of a click event; as the click event bubbles
-    // up and reaches the click detector's child component the
-    // id value is stamped on the event. This id is inspected
-    // in the document's click handler, and if the id doesn't
-    // exist or doesn't match this detector's id, then trigger
-    // the outsideClick callback.
-    //
-    // Taking this approach instead of checking if the event's
-    // target element exists in this component's DOM sub-tree is
-    // necessary for handling clicks originating from children
-    // rendered through React's portals (EuiPortal). The id tracking
-    // works because React guarantees the event bubbles through the
-    // virtual DOM and executes EuiClickDetector's onClick handler,
-    // stamping the id even though the event originates outside
-    // this component's reified DOM tree.
-    this.id = htmlIdGenerator()();
+      if (
+        (event.euiGeneratedBy && event.euiGeneratedBy.includes(id)) ||
+        capturedDownIds.current.includes(id)
+      ) {
+        capturedDownIds.current = [];
+        return;
+      }
 
-    this.capturedDownIds = [];
-  }
-
-  onClickOutside: EventHandler<any> = (e: Event) => {
-    const { isDisabled, onOutsideClick } = this.props;
-
-    if (isDisabled) {
-      this.capturedDownIds = [];
-      return;
+      capturedDownIds.current = [];
+      return onOutsideClick(event);
     }
 
-    const event = e as unknown as EuiEvent;
+    document.addEventListener('mouseup', onClickOutside);
+    document.addEventListener('touchend', onClickOutside);
+    return () => {
+      document.removeEventListener('mouseup', onClickOutside);
+      document.removeEventListener('touchend', onClickOutside);
+    };
+  }, [props.isDisabled, props.onOutsideClick]);
 
-    if (
-      (event.euiGeneratedBy && event.euiGeneratedBy.includes(this.id)) ||
-      this.capturedDownIds.includes(this.id)
-    ) {
-      this.capturedDownIds = [];
-      return;
-    }
-
-    this.capturedDownIds = [];
-    return onOutsideClick(event);
-  };
-
-  componentDidMount() {
-    document.addEventListener('mouseup', this.onClickOutside);
-    document.addEventListener('touchend', this.onClickOutside);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mouseup', this.onClickOutside);
-    document.removeEventListener('touchend', this.onClickOutside);
-  }
-
-  onChildClick = (
+  function onChildClick(
     event: ReactMouseEvent,
     cb: (event: ReactMouseEvent) => void
-  ) => {
+  ) {
     // to support nested click detectors, build an array
     // of detector ids that have been encountered;
     if (event.nativeEvent.hasOwnProperty('euiGeneratedBy')) {
-      (event.nativeEvent as unknown as EuiEvent).euiGeneratedBy.push(this.id);
+      (event.nativeEvent as unknown as EuiEvent).euiGeneratedBy.push(id);
     } else {
-      (event.nativeEvent as unknown as EuiEvent).euiGeneratedBy = [this.id];
+      (event.nativeEvent as unknown as EuiEvent).euiGeneratedBy = [id];
     }
     if (cb) cb(event);
-  };
-
-  onChildMouseDown = (event: ReactMouseEvent) => {
-    this.onChildClick(event, (e) => {
-      const nativeEvent = e.nativeEvent as unknown as EuiEvent;
-      this.capturedDownIds = nativeEvent.euiGeneratedBy;
-      if (this.props.onMouseDown) this.props.onMouseDown(e);
-      if (this.props.onTouchStart) this.props.onTouchStart(e);
-    });
-  };
-
-  onChildMouseUp = (event: ReactMouseEvent) => {
-    this.onChildClick(event, (e) => {
-      if (this.props.onMouseUp) this.props.onMouseUp(e);
-      if (this.props.onTouchEnd) this.props.onTouchEnd(e);
-    });
-  };
-
-  render() {
-    const props = {
-      ...this.props.children.props,
-      ...{
-        onMouseDown: this.onChildMouseDown,
-        onTouchStart: this.onChildMouseDown,
-        onMouseUp: this.onChildMouseUp,
-        onTouchEnd: this.onChildMouseUp,
-      },
-    };
-
-    const child = Children.only(this.props.children);
-    return cloneElement(child, props);
   }
-}
+
+  function onChildMouseDown(event: ReactMouseEvent) {
+    onChildClick(event, (e) => {
+      const nativeEvent = e.nativeEvent as unknown as EuiEvent;
+      capturedDownIds.current = nativeEvent.euiGeneratedBy;
+      if (props.onMouseDown) props.onMouseDown(e);
+      if (props.onTouchStart) props.onTouchStart(e);
+    });
+  }
+
+  function onChildMouseUp(event: ReactMouseEvent) {
+    onChildClick(event, (e) => {
+      if (props.onMouseUp) props.onMouseUp(e);
+      if (props.onTouchEnd) props.onTouchEnd(e);
+    });
+  }
+
+  const child = Children.only(props.children);
+  const childProps = {
+    ...props.children.props,
+    ...{
+      onMouseDown: onChildMouseDown,
+      onTouchStart: onChildMouseDown,
+      onMouseUp: onChildMouseUp,
+      onTouchEnd: onChildMouseUp,
+    },
+  };
+  return cloneElement(child, childProps);
+};
