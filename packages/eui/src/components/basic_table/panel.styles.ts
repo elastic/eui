dab725a97f897f555a3e2c33fb9f925fc5ae9caf
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2026-06-05 15:08:04 +0200

    chore: simplify panel styles

--- packages/eui/src/components/basic_table/panel.styles.ts
+++ packages/eui/src/components/basic_table/panel.styles.ts
@@ -8,19 +8,16 @@
 
 import { css } from '@emotion/react';
 import type { UseEuiTheme } from '../../services';
+import { logicalShorthandCSS } from '../../global_styling';
 
 export const euiBasicTablePanelStyles = ({ euiTheme }: UseEuiTheme) => css`
   background-color: ${euiTheme.colors.backgroundBaseSubdued};
   border: ${euiTheme.border.thin};
-  border-radius: ${euiTheme.border.radius.medium};
-
-  /* Reset bottom border styles if the next sibling is EuiBasicTable or another panel */
-  &:has(+ .euiBasicTable),
-  & {
-    border-end-start-radius: 0;
-    border-end-end-radius: 0;
-    border-block-end-width: 0;
-  }
+  border-block-end-width: 0;
+  ${logicalShorthandCSS(
+    'border-radius',
+    `${euiTheme.border.radius.medium} ${euiTheme.border.radius.medium} 0 0`
+  )}
 
   /* Reset top border styles if the previous sibling is another panel */
   & + & {
