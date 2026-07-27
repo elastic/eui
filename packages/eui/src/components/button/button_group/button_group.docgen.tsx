212261eb16a3f4643897255daafd2a095f032d72
Lene Gadewoll <lene.gadewoll@elastic.co>
2026-07-27 11:47:12 +0200

    docs: add Children API to docs

---
+++ packages/eui/src/components/button/button_group/button_group.docgen.tsx
@@ -0,0 +1,27 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+// This file only exists for react-docgen-typescript (used by both the website
+// and Storybook), which cannot extract prop descriptions from an overloaded
+// function or from type-alias intersections with anonymous object literals.
+// NOTE: This file should *NOT* be exported publicly.
+// @see packages/eui-docgen
+
+import React, { FunctionComponent } from 'react';
+import {
+  EuiButtonGroupProps as EuiButtonGroupOptionsPropsType,
+  EuiButtonGroupChildrenProps as EuiButtonGroupChildrenPropsType,
+} from './button_group';
+
+export const EuiButtonGroupOptionsProps: FunctionComponent<
+  EuiButtonGroupOptionsPropsType
+> = () => <></>;
+
+export const EuiButtonGroupChildrenProps: FunctionComponent<
+  EuiButtonGroupChildrenPropsType
+> = () => <></>;
