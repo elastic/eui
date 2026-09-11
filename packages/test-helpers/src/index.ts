b24f63a40e874b57091cbd213b95482f6cee636a
Stelios Mavro <stelios.mavro@elastic.co>
2026-09-11 09:06:50 +0300

    [test-helpers] Add EuiTreeViewObject

--- packages/test-helpers/src/index.ts
+++ packages/test-helpers/src/index.ts
@@ -22,3 +22,4 @@ export { EuiContextMenuObject } from './playwright/components/context_menu/objec
 export { EuiModalObject } from './playwright/components/modal/object';
 export { EuiBasicTableObject } from './playwright/components/basic_table/object';
 export { EuiColorPickerObject } from './playwright/components/color_picker/object';
+export { EuiTreeViewObject } from './playwright/components/tree_view/object';
