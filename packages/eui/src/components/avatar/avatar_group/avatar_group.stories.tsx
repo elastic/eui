0bec462d7aaa4143e2f3a1835e25eeb1dc4652d7
boriskirov <boris.kirov@elastic.co>
2026-09-14 15:01:38 +0200

    add: making the storybook example richer

--- packages/eui/src/components/avatar/avatar_group/avatar_group.stories.tsx
+++ packages/eui/src/components/avatar/avatar_group/avatar_group.stories.tsx
@@ -9,8 +9,12 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
+import { EuiFlexGroup, EuiFlexItem } from '../../flex';
+import { EuiPanel } from '../../panel';
 import { EuiSpacer } from '../../spacer';
+import { EuiTitle } from '../../title';
 import { EuiAvatar } from '../avatar';
+import { EuiText } from '../../text';
 import { EuiAvatarGroup, EuiAvatarGroupProps } from './avatar_group';
 
 const meta: Meta<EuiAvatarGroupProps> = {
@@ -46,6 +50,39 @@ export const Playground: Story = {
       <EuiAvatarGroup {...args} />
       <EuiSpacer />
       <EuiAvatarGroup {...args} size="s" legend={`${args.legend} (s)`} />
+      <EuiSpacer size="xl" />
+      <EuiTitle size="xs">
+        <h3>Bordered panel</h3>        
+      </EuiTitle>
+      <EuiText>        
+        <p>A bordered panel with an avatar group where the `disableExpand` prop is set to `true`. That way we avoid the avatar group from expanding to the width of the panel, and cause some layout issues and bad experience.</p>        
+      </EuiText>
+      <EuiSpacer size="s" />
+      <EuiFlexGroup responsive={false} gutterSize="none">
+        <EuiFlexItem grow={false}>
+          <EuiPanel hasBorder paddingSize="s" hasShadow={false} grow={false}>
+            <EuiAvatarGroup {...args} disableExpand legend="Assignees" />
+          </EuiPanel>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer size="xl" />
+      <EuiTitle size="xs">
+        <h3>Page header</h3>
+      </EuiTitle>
+      <EuiText>        
+        <p>A page header where the avatar group is used to display assignees, and the `disableExpand` prop is set to `false` due to floating space in the header.</p>
+      </EuiText>
+      <EuiSpacer size="s" />
+      <EuiFlexGroup alignItems="center" gutterSize="m" responsive={false}>
+        <EuiFlexItem grow={false}>
+          <EuiTitle size="s">
+            <h2>Investigation</h2>
+          </EuiTitle>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiAvatarGroup {...args} disableExpand={false} legend="Assignees" />
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </>
   ),
 };
