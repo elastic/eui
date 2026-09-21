c0e7fbd572baf41402ce06d189428d96b99b951c
JoseLuisGJ <joseluisgj@gmail.com>
2026-09-21 13:27:40 +0200

    Fixing import issue

--- packages/eui/src/components/form/form_label_append/form_label_append.stories.tsx
+++ packages/eui/src/components/form/form_label_append/form_label_append.stories.tsx
@@ -6,6 +6,7 @@
  * Side Public License, v 1.
  */
 
+import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { hideStorybookControls } from '../../../../.storybook/utils';
